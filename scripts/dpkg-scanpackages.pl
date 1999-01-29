e04a641142dfa21cb03c19071c9306c0ad83afe1
Guy Maor <maor@debian.org>
1999-01-29 08:59:33 +0000

    allow hierarchial sections

--- scripts/dpkg-scanpackages.pl
+++ scripts/dpkg-scanpackages.pl
@@ -145,6 +145,9 @@ while (<O>) {
     }
     $pv{$p,'Priority'}= $priority;
     $pv{$p,'Section'}= $section;
+    ($sectioncut = $section) =~ s:^[^/]*/::;
+    if (length($psubdir{$p}) && $section ne $psubdir{$p} &&
+	$sectioncut ne $psubdir{$p}) {
     if (length($psubdir{$p}) && $section ne $psubdir{$p}) {
         print(STDERR " !! Package $p has \`Section: $section',".
                      " but file is in \`$psubdir{$p}' !!\n") || die $!;
