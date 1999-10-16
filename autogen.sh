55cf13ba8083e7e2ec222b52d44cf088e65ab411
Ben Collins <bcollins@debian.org>
1999-10-16 02:26:05 +0000

    added option for adding --copy if autogen is run with it

--- autogen.sh
+++ autogen.sh
@@ -1,18 +1,20 @@
 #! /bin/sh
 
+copy=$1
+
 # Start by setting up everything for main tree
 aclocal -I ./automake
 autoheader
-gettextize
-libtoolize --force
-automake --add-missing --foreign
+gettextize $copy
+libtoolize --force $copy
+automake --add-missing --foreign $copy
 autoconf
 
 # Utils has it's own configure, so we need to repeat this there
 cd utils
 aclocal -I ../automake
 autoheader
-automake --foreign
+automake --foreign $copy
 autoconf
 
 # Return to the previous directory
