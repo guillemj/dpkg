ca7089d5ca7c7210afef700f1b3d4ba23e627253
Ben Collins <bcollins@debian.org>
1999-10-29 11:16:25 +0000

    Added cs.gmo to the gen list (really need an automated way to detect all .po files)

--- po/update.sh
+++ po/update.sh
@@ -5,7 +5,7 @@ xgettext --default-domain=dpkg --directory=.. \
 	 --files-from=POTFILES.in && test ! -f dpkg.po \
 	 || ( rm -f dpkg.pot && mv dpkg.po dpkg.pot )
 
-catalogs='en.gmo fr.gmo es.gmo ja_JP.ujis.gmo'
+catalogs='en.gmo fr.gmo es.gmo ja_JP.ujis.gmo cs.gmo'
 for cat in $catalogs; do
   lang=`echo $cat | sed 's/\.gmo$//'`
   mv $lang.po $lang.old.po
