6e1eb71ebffbbaca2e4bcb6427f16beedecf222f
Wichert Akkerman <wakkerma@debian.org>
2001-09-12 15:00:27 +0000

    change copyright to just GPL 2

--- lib/showpkg.c
+++ lib/showpkg.c
@@ -5,9 +5,8 @@
  * Copyright (C) 2001 Wichert Akkerman <wakkerma@debian.org>
  *
  * This is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2,
- * or (at your option) any later version.
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
  *
  * This is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
