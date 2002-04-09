2ac5bf9540a394e45b8a6cce4ef203a8fc19c718
Wichert Akkerman <wakkerma@debian.org>
2002-04-09 22:41:55 +0000

    Add LFS support

--- utils/md5sum.c
+++ utils/md5sum.c
@@ -13,13 +13,14 @@
  *   to support GNU gettext.
  * This file is in the public domain.
  */
+
+#include "config.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <getopt.h>
 #include <unistd.h>
-
-#include "config.h"
 
 /* Take care of NLS matters.  */
 
