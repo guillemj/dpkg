1816757e5cc57bcf6b62df86a8960db95787412c
Adam Heath <doogie@debian.org>
2003-09-15 23:49:28 +0000

    In dpkg-buildpackage, call debian/rules -qn build-arch, and if it's available, modify -B handling appropriately.  If build-arch is not available, then when -B was called, do *not* pass -B on to dpkg-checkbuilddeps.

--- scripts/dpkg-buildpackage.sh
+++ scripts/dpkg-buildpackage.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-version="1.3.0"; # This line modified by Makefile
+version="1.10.10"; # This line modified by Makefile
 
 progname="`basename \"$0\"`"
 usageversion () {
@@ -60,6 +60,7 @@ signchanges='withecho signfile'
 cleansource=false
 checkbuilddep=true
 checkbuilddep_args=''
+buildtarget=build
 binarytarget=binary
 sourcestyle=''
 version=''
@@ -70,6 +71,7 @@ noclean=false
 usepause=false
 warnable_error=0
 passopts=''
+debian/rules -qn build-arch 2>/dev/null && buildarchavailable=true || buildarchavailable=false
 
 while [ $# != 0 ]
 do
@@ -97,7 +99,7 @@ do
 	-nc)	noclean=true; if [ -z "$binaryonly" ]; then binaryonly=-b; fi ;;
 	-b)	binaryonly=-b; [ "$sourceonly" ] && \
 			{ echo >&2 "$progname: cannot combine $1 and -S" ; exit 2 ; } ;;
-	-B)	binaryonly=-B; checkbuilddep_args=-B; binarytarget=binary-arch; [ "$sourceonly" ] && \
+	-B)	binaryonly=-B; binarytarget=binary-arch; if $buildarchavailable; then checkbuilddep_args=-B; fi; [ "$sourceonly" ] && \
 			{ echo >&2 "$progname: cannot combine $1 and -S" ; exit 2 ; } ;;
 	-S)	sourceonly=-S; checkbuilddep=false; [ "$binaryonly" ] && \
 			{ echo >&2 "$progname: cannot combine $binaryonly and $1" ; exit 2 ; } ;;
@@ -196,7 +198,7 @@ if [ x$binaryonly = x ]; then
 	cd ..; withecho dpkg-source $passopts $diffignore $tarignore -b "$dirn"; cd "$dirn"
 fi
 if [ x$sourceonly = x ]; then
-	withecho debian/rules build 
+	withecho debian/rules $buildtarget 
 	withecho $rootcommand debian/rules $binarytarget
 fi
 if [ "$usepause" = "true" ] && \
