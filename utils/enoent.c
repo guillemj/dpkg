81242df279d31cd7025a4487b9370b95a070cccd
Frank Lichtenheld <djpig@debian.org>
2006-05-19 20:20:30 +0000

    utils/enoent.c: Add a comment at the top of the file explaining why we use this odd program at all.

--- utils/enoent.c
+++ utils/enoent.c
@@ -1,3 +1,7 @@
+/* This program ensures that dpkg-divert and update-alternatives don't depend on POSIX.pm
+ * being available and usable. This is probably a good thing since the perl packages have
+ * known deficiencies to ensure that during upgrades. */
+
 #include <stdio.h>
 #include <errno.h>
 
