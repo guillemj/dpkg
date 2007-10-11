76209ff4c9d406a4d167d09745ebf8e0b48c3394
Frank Lichtenheld <djpig@debian.org>
2007-10-12 01:09:16 +0200

    Dpkg::BuildOptions: Add tests and fix errors found
    This whole module was one giant brown paper bag bug...

--- scripts/Dpkg/BuildOptions.pm
+++ scripts/Dpkg/BuildOptions.pm
@@ -11,9 +11,10 @@ sub parse {
     unless ($env) { return {}; }
 
     my %opts;
-    if ($env =~ s/(noopt|nostrip|nocheck),?//ig) {
+    while ($env =~ s/(noopt|nostrip|nocheck),?//i) {
 	$opts{lc $1} = '';
-    } elsif ($env =~ s/(parallel)=(-?\d+),?//ig) {
+    }
+    while ($env =~ s/(parallel)=(-?\d+),?//i) {
 	$opts{lc $1} = $2;
     }
 
@@ -35,6 +36,7 @@ sub set {
     }
 
     $ENV{DEB_BUILD_OPTIONS} = $env if $env;
+    return $env;
 }
 
 1;
