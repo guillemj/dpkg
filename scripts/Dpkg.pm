8b282a3e34710f5182b35d5ae5a591ca83d2df52
Frank Lichtenheld <djpig@debian.org>
2007-12-04 22:49:42 +0100

    Dpkg: Include information about supported compression formats
    Currently this is only used by dpkg-source, but other scripts
    like dpkg-genchanges will need to use this information, too.

--- scripts/Dpkg.pm
+++ scripts/Dpkg.pm
@@ -5,6 +5,9 @@ use warnings;
 
 use base qw(Exporter);
 our @EXPORT = qw($version $progname $admindir $dpkglibdir $pkgdatadir);
+our %EXPORT_TAGS = ( 'compression' =>
+		     [ qw(@comp_supported %comp_supported %comp_ext $comp_regex) ] );
+our @EXPORT_OK = @{$EXPORT_TAGS{compression}};
 
 our ($progname) = $0 =~ m#(?:.*/)?([^/]*)#;
 
@@ -13,5 +16,11 @@ our $version = "1.14";
 our $admindir = "/var/lib/dpkg";
 our $dpkglibdir = ".";
 our $pkgdatadir = "..";
+
+# Compression
+our @comp_supported = qw(gzip bzip2 lzma);
+our %comp_supported = map { $_ => 1 } @comp_supported;
+our %comp_ext = ( gzip => 'gz', bzip2 => 'bz2', lzma => 'lzma' );
+our $comp_regex = '(?:gz|bz2|lzma)';
 
 1;
