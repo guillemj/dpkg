22ddeee0236bdff13745c1c4c4f81c1f30951138
Raphael Hertzog <hertzog@debian.org>
2007-12-29 15:07:53 +0100

    Add a warning to avoid adding unnecessary stuff in Dpkg.pm

--- scripts/Dpkg.pm
+++ scripts/Dpkg.pm
@@ -3,6 +3,12 @@ package Dpkg;
 use strict;
 use warnings;
 
+# This module is the only one provided by dpkg and not dpkg-dev
+#
+# Don't add things here if you don't need them in dpkg itself.
+# If you do, and also use the new stuff in dpkg-dev, you'll have to bump
+# the dependency of dpkg-dev on dpkg.
+
 use base qw(Exporter);
 our @EXPORT = qw($version $progname $admindir $dpkglibdir $pkgdatadir);
 
