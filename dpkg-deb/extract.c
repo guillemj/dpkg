guillemj/dpkg

commit ea93ed48c17445d01b67f3fa8a20a5a644a89e5b

Fixed up several warning types: const, casting, incorrect sizes, signed/unsigned comparisons.

dpkg-deb/extract.c
@@ -85,7 +85,7 @@ void extracthalf(const char *debar, const char *directory,
   char versionbuf[40];
   float versionnum;
   char ctrllenbuf[40], *infobuf;
-  long ctrllennum, memberlen= 0;
+  size_t ctrllennum, memberlen= 0;
   int dummy, l= 0;
   pid_t c1=0,c2,c3;
   unsigned char *ctrlarea= 0;

lib/varbuf.c
@@ -44,7 +44,7 @@ void varbufdupc(struct varbuf *v, int c, ssize_t n) {
 }
 
 int varbufprintf(struct varbuf *v, const char *fmt, ...) {
-  int ou, r;
+  unsigned int ou, r;
   va_list al;
 
   ou= v->used;
@@ -62,7 +62,7 @@ int varbufprintf(struct varbuf *v, const char *fmt, ...) {
 }
 
 int varbufvprintf(struct varbuf *v, const char *fmt, va_list va) {
-  int ou, r;
+  unsigned int ou, r;
   va_list al;
 
   ou= v->used;

utils/start-stop-daemon.c
@@ -134,7 +134,7 @@ static void *xmalloc(int size);
 static void push(struct pid_list **list, int pid);
 static void do_help(void);
 static void parse_options(int argc, char * const *argv);
-static int pid_is_user(int pid, int uid);
+static int pid_is_user(int pid, uid_t uid);
 static int pid_is_cmd(int pid, const char *name);
 static void check(int pid);
 static void do_pidfile(const char *name);
@@ -338,7 +338,7 @@ static int parse_integer (const char *string, int *value_r) {
 
 static int parse_signal (const char *signal_str, int *signal_nr)
 {
-	int i;
+	unsigned int i;
 
 	if (parse_integer(signal_str, signal_nr) == 0)
 		return 0;
@@ -403,7 +403,7 @@ parse_schedule(const char *schedule_str) {
 		while (schedule_str != NULL) {
 			slash = strchr(schedule_str,'/');
 			str_len = slash ? slash - schedule_str : strlen(schedule_str);
-			if (str_len >= sizeof(item_buf))
+			if (str_len >= (ptrdiff_t)sizeof(item_buf))
 				badusage("invalid schedule item: far too long"
 					 " (you must delimit items with slashes)");
 			memcpy(item_buf, schedule_str, str_len);
@@ -577,7 +577,7 @@ pid_is_exec(int pid, const struct stat *esb)
 
 
 static int
-pid_is_user(int pid, int uid)
+pid_is_user(int pid, uid_t uid)
 {
 	struct stat sb;
 	char buf[32];

dpkg-deb/main.c
@@ -136,7 +136,7 @@ static void setaction(const struct cmdinfo *cip, const char *value) {
   if (cipaction)
     badusage(_("conflicting actions --%s and --%s"),cip->olong,cipaction->olong);
   cipaction= cip;
-  assert(cip-cmdinfos < sizeof(dofunctions)*sizeof(dofunction*));
+  assert((int)(cip-cmdinfos) < (int)(sizeof(dofunctions)*sizeof(dofunction*)));
   action= dofunctions[cip-cmdinfos];
 }
 

main/archives.c
@@ -279,7 +279,8 @@ int tarobject(struct TarInfo *ti) {
   const char *usename;
     
   struct tarcontext *tc= (struct tarcontext*)ti->UserData;
-  int statr, fd, r, i, existingdirectory;
+  int statr, fd, i, existingdirectory;
+  size_t r;
   struct stat stab, stabd;
   char databuf[TARBLKSZ];
   struct fileinlist *nifd;
@@ -718,7 +719,7 @@ void check_conflict(struct dependency *dep, struct pkginfo *pkg,
 	/* if this gets triggered, it means a package has > 10 conflicts/replaces
 	 * pairs, which is the package's fault
 	 */
-	assert(cflict_index < sizeof(conflictor));
+	assert(cflict_index < (int)sizeof(conflictor));
         /* This conflict is OK - we'll remove the conflictor. */
 	conflictor[cflict_index++]= fixbyrm;
         varbuffree(&conflictwhy); varbuffree(&removalwhy);
@@ -780,30 +781,31 @@ void archivefiles(const char *const *argv) {
     m_pipe(pi);
     if (!(fc= m_fork())) {
       const char *const *ap;
+      char **narglist;
       int i;
       m_dup2(pi[1],1); close(pi[0]); close(pi[1]);
       for (i=0, ap=argv; *ap; ap++, i++);
-      arglist= m_malloc(sizeof(char*)*(i+15));
-      arglist[0]= FIND;
+      narglist= m_malloc(sizeof(char*)*(i+15));
+      narglist[0]= strdup(FIND);
       for (i=1, ap=argv; *ap; ap++, i++) {
         if (strchr(FIND_EXPRSTARTCHARS,(*ap)[0])) {
           char *a;
           a= m_malloc(strlen(*ap)+10);
           strcpy(a,"./");
           strcat(a,*ap);
-          arglist[i]= a;
+          narglist[i]= a;
         } else {
-          arglist[i]= *ap;
+          narglist[i]= strdup(*ap);
         }
       }
-      arglist[i++]= "-follow"; /*  When editing these, make sure that     */
-      arglist[i++]= "-name";   /*  arglist is mallocd big enough, above.  */
-      arglist[i++]= ARCHIVE_FILENAME_PATTERN;
-      arglist[i++]= "-type";
-      arglist[i++]= "f";
-      arglist[i++]= "-print0";
-      arglist[i++]= 0;
-      execvp(FIND, (char* const*)arglist);
+      narglist[i++]= strdup("-follow"); /*  When editing these, make sure that     */
+      narglist[i++]= strdup("-name");   /*  arglist is mallocd big enough, above.  */
+      narglist[i++]= strdup(ARCHIVE_FILENAME_PATTERN);
+      narglist[i++]= strdup("-type");
+      narglist[i++]= strdup("f");
+      narglist[i++]= strdup("-print0");
+      narglist[i++]= 0;
+      execvp(FIND, narglist);
       ohshite(_("failed to exec find for --recursive"));
     }
     close(pi[1]);

main/main.c
@@ -284,7 +284,7 @@ static void setstatuspipe(const struct cmdinfo *cip, const char *value) {
 
 void setforce(const struct cmdinfo *cip, const char *value) {
   const char *comma;
-  int l;
+  size_t l;
   const struct forceinfo *fip;
 
   if (!strcmp(value,"help")) {
@@ -333,7 +333,7 @@ Forcing options marked [*] are enabled by default.\n"),
 	for (fip=forceinfos; fip->name; fip++)
 	  *fip->opt= cip->arg;
       else
-	badusage(_("unknown force/refuse option `%.*s'"), l<250 ? l : 250, value);
+	badusage(_("unknown force/refuse option `%.*s'"), l<250 ? (int)l : 250, value);
     else
       *fip->opt= cip->arg;
     if (!comma) break;
@@ -422,7 +422,13 @@ static const struct cmdinfo cmdinfos[]= {
 
 static void execbackend(int argc, const char *const *argv) NONRETURNING;
 static void execbackend(int argc, const char *const *argv) {
-  execvp(BACKEND, (char* const*) argv);
+  char **nargv= malloc(sizeof(char *) * argc + 1);
+  int i= 0;
+  if (!nargv) ohshite(_("couldn't malloc in execbackend"));
+  while (i < argc)
+    nargv[i++]= strdup(argv[i]);
+  nargv[i]= 0;
+  execvp(BACKEND, nargv);
   ohshite(_("failed to exec dpkg-deb"));
 }
 void commandfd(const char *const *argv) {
@@ -468,7 +474,7 @@ void commandfd(const char *const *argv) {
     if (c == EOF) ohshit(_("unexpected eof before end of line %d"),lno);
     if (!argc) continue;
     varbufaddc(&linevb,0);
-printf("line=`%*s'\n",linevb.used,linevb.buf);
+printf("line=`%*s'\n",(int)linevb.used,linevb.buf);
     oldargs= newargs= realloc(oldargs,sizeof(const char *) * (argc + 1));
     argc= 1;
     ptr= linevb.buf;

split/info.c
@@ -74,9 +74,10 @@ static char *nextline(char **ripp, const char *fn, const char *what) {
 struct partinfo *read_info(FILE *partfile, const char *fn, struct partinfo *ir) {
   /* returns info (nfmalloc'd) if was an archive part and we read it, 0 if it wasn't */
   static char *readinfobuf= 0;
-  static int readinfobuflen= 0;
+  static size_t readinfobuflen= 0;
 
-  unsigned long thisilen, templong;
+  size_t thisilen;
+  unsigned int templong;
   char magicbuf[sizeof(PARTMAGIC)-1], *rip, *partnums, *slash;
   struct ar_hdr arh;
   int c;

split/main.c
@@ -143,7 +143,7 @@ static void setaction(const struct cmdinfo *cip, const char *value) {
   if (cipaction)
     badusage(_("conflicting actions --%s and --%s"),cip->olong,cipaction->olong);
   cipaction= cip;
-  assert(cip-cmdinfos < sizeof(dofunctions)*sizeof(dofunction*));
+  assert((int)(cip-cmdinfos) < (int)(sizeof(dofunctions)*sizeof(dofunction*)));
   action= dofunctions[cip-cmdinfos];
 }
 

commit daf4cadfd9258c2eaede0255400e60f5622e627b

Fix signed vs unsigned value comparisons
Those were making some code to never be executed. This fixes most of the
bugs introduced with commit ea93ed48c17445d01b67f3fa8a20a5a644a89e5b.

dpkg-deb/extract.c
@@ -63,10 +63,12 @@ static void readfail(FILE *a, const char *filename, const char *what) {
   }
 }
 
-static unsigned long parseheaderlength(const char *inh, size_t len,
-                                       const char *fn, const char *what) {
+static size_t
+parseheaderlength(const char *inh, size_t len,
+                  const char *fn, const char *what)
+{
   char lintbuf[15];
-  unsigned long r;
+  ssize_t r;
   char *endp;
 
   if (memchr(inh,0,len))
@@ -75,10 +77,12 @@ static unsigned long parseheaderlength(const char *inh, size_t len,
   memcpy(lintbuf,inh,len);
   lintbuf[len]= ' ';
   *strchr(lintbuf,' ')= 0;
-  r= strtoul(lintbuf,&endp,10);
+  r = strtol(lintbuf, &endp, 10);
+  if (r < 0)
+    ohshit(_("file `%.250s' is corrupt - negative member length %zi"), fn, r);
   if (*endp)
     ohshit(_("file `%.250s' is corrupt - bad digit (code %d) in %s"),fn,*endp,what);
-  return r;
+  return (size_t)r;
 }
 
 void extracthalf(const char *debar, const char *directory,
@@ -118,8 +122,6 @@ void extracthalf(const char *debar, const char *directory,
         ohshit(_("file `%.250s' is corrupt - bad magic at end of first header"),debar);
       memberlen= parseheaderlength(arh.ar_size,sizeof(arh.ar_size),
                                    debar,"member length");
-      if (memberlen<0)
-        ohshit(_("file `%.250s' is corrupt - negative member length %zi"),debar,memberlen);
       if (!header_done) {
         if (memcmp(arh.ar_name,"debian-binary   ",sizeof(arh.ar_name)) &&
 	    memcmp(arh.ar_name,"debian-binary/   ",sizeof(arh.ar_name)))

dpkg-deb/info.c
@@ -96,7 +96,7 @@ static void info_spew(const char *debar, const char *directory,
     pathlen = strlen(directory) + strlen(component) + 2;
     controlfile = (void *) realloc((void *) controlfile, pathlen);
     if (!controlfile)
-      ohshite(_("realloc failed (%lu bytes)"), pathlen);
+      ohshite(_("realloc failed (%zu bytes)"), pathlen);
     memset(controlfile, 0, sizeof(controlfile));
 
     strcat(controlfile, directory);

dpkg-split/queue.c
@@ -97,8 +97,8 @@ void do_auto(const char *const *argv) {
   const char *partfile;
   struct partinfo *pi, *refi, *npi, **partlist, *otherthispart;
   struct partqueue *pq;
-  unsigned int i, j;
-  int ap;
+  unsigned int i;
+  int j, ap;
   long nr;
   FILE *part;
   void *buffer;
@@ -163,7 +163,7 @@ void do_auto(const char *const *argv) {
     /* There are still some parts missing. */
     for (i=0, ap=0; i<refi->maxpartn; i++)
       if (!partlist[i])
-        printf("%s%d", !ap++ ? "" : i==j ? _(" and ") : ", ", i+1);
+        printf("%s%d", !ap++ ? "" : i == (unsigned int)j ? _(" and ") : ", ", i + 1);
     printf(").\n");
 
   } else {

lib/varbuf.c
@@ -44,7 +44,8 @@ void varbufdupc(struct varbuf *v, int c, ssize_t n) {
 }
 
 int varbufprintf(struct varbuf *v, const char *fmt, ...) {
-  unsigned int ou, r;
+  size_t ou;
+  int r;
   va_list al;
 
   ou= v->used;
@@ -57,12 +58,13 @@ int varbufprintf(struct varbuf *v, const char *fmt, ...) {
     va_end(al);
     if (r < 0) r= (v->size-ou+1) * 2;
     v->used= ou+r;
-  } while (r >= v->size-ou-1);
+  } while (r >= (int)(v->size - ou - 1));
   return r;
 }
 
 int varbufvprintf(struct varbuf *v, const char *fmt, va_list va) {
-  unsigned int ou, r;
+  size_t ou;
+  int r;
   va_list al;
 
   ou= v->used;
@@ -74,7 +76,7 @@ int varbufvprintf(struct varbuf *v, const char *fmt, va_list va) {
     r= vsnprintf(v->buf+ou,v->size-ou,fmt,al);
     if (r < 0) r= (v->size-ou+1) * 2;
     v->used= ou+r;
-  } while (r >= v->size-ou-1);
+  } while (r >= (int)(v->size - ou - 1));
   return r;
 }
 

src/archives.c
@@ -374,7 +374,7 @@ int tarobject(struct TarInfo *ti) {
   struct conffile *conff;
   struct tarcontext *tc= (struct tarcontext*)ti->UserData;
   int statr, fd, i, existingdirectory, keepexisting;
-  size_t r;
+  ssize_t r;
   struct stat stab, stabtmp;
   char databuf[TARBLKSZ];
   struct fileinlist *nifd, **oldnifd;
@@ -765,7 +765,7 @@ int tarobject(struct TarInfo *ti) {
         varbufextend(&symlinkfn);
         r= readlink(fnamevb.buf,symlinkfn.buf,symlinkfn.size);
         if (r<0) ohshite(_("unable to read link `%.255s'"),ti->Name);
-      } while (r == symlinkfn.size);
+      } while ((size_t)r == symlinkfn.size);
       symlinkfn.used= r; varbufaddc(&symlinkfn,0);
       if (symlink(symlinkfn.buf,fnametmpvb.buf))
         ohshite(_("unable to make backup symlink for `%.255s'"),ti->Name);

src/main.c
@@ -352,7 +352,7 @@ static void setforce(const struct cmdinfo *cip, const char *value) {
 
   for (;;) {
     comma= strchr(value,',');
-    l= comma ? (int)(comma-value) : strlen(value);
+    l = comma ? (size_t)(comma - value) : strlen(value);
     for (fip=forceinfos; fip->name; fip++)
       if (!strncmp(fip->name,value,l) && strlen(fip->name)==l) break;
     if (!fip->name) {

utils/start-stop-daemon.c
@@ -455,7 +455,7 @@ parse_schedule(const char *schedule_str) {
 	char item_buf[20];
 	const char *slash;
 	int count, repeatat;
-	ptrdiff_t str_len;
+	size_t str_len;
 
 	count = 0;
 	for (slash = schedule_str; *slash; slash++)
@@ -481,8 +481,8 @@ parse_schedule(const char *schedule_str) {
 		repeatat = -1;
 		while (schedule_str != NULL) {
 			slash = strchr(schedule_str,'/');
-			str_len = slash ? slash - schedule_str : strlen(schedule_str);
-			if (str_len >= (ptrdiff_t)sizeof(item_buf))
+			str_len = slash ? (size_t)(slash - schedule_str) : strlen(schedule_str);
+			if (str_len >= sizeof(item_buf))
 				badusage("invalid schedule item: far too long"
 					 " (you must delimit items with slashes)");
 			memcpy(item_buf, schedule_str, str_len);