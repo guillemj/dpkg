guillemj/dpkg

commit eb483c17ebb9a47c8b99b4319e682ab685213090

Use new VARBUF_INIT macro instead of varbufinit to initialize variables

src/query.c
@@ -246,12 +246,10 @@ void searchfiles(const char *const *argv) {
   const char *thisarg;
   int found;
   struct varbuf path;
-  static struct varbuf vb;
+  static struct varbuf vb = VARBUF_INIT;
   
   if (!*argv)
     badusage(_("--search needs at least one file name pattern argument"));
-
-  varbufinit(&path);
 
   modstatdb_init(admindir,msdbrw_readonly|msdbrw_noavail);
   ensure_allinstfiles_available_quiet();

lib/compression.c
@@ -36,9 +36,7 @@ fd_fd_filter(int fd_in, int fd_out,
 
 void decompress_cat(enum compress_type type, int fd_in, int fd_out, char *desc, ...) {
   va_list al;
-  struct varbuf v;
-
-  varbufinit(&v);
+  struct varbuf v = VARBUF_INIT;
 
   va_start(al,desc);
   varbufvprintf(&v, desc, al);
@@ -103,10 +101,8 @@ void decompress_cat(enum compress_type type, int fd_in, int fd_out, char *desc, 
 
 void compress_cat(enum compress_type type, int fd_in, int fd_out, const char *compression, char *desc, ...) {
   va_list al;
-  struct varbuf v;
+  struct varbuf v = VARBUF_INIT;
   char combuf[6];
-
-  varbufinit(&v);
 
   va_start(al,desc);
   varbufvprintf(&v, desc, al);

lib/dpkg-db.h
@@ -366,6 +366,8 @@ const char *versiondescribe(const struct versionrevision*,
 
 struct varbuf;
 
+#define VARBUF_INIT { 0, 0, NULL }
+
 extern void varbufaddc(struct varbuf *v, int c);
 extern void varbufdupc(struct varbuf *v, int c, ssize_t s);
 int varbufprintf(struct varbuf *v, const char *fmt, ...) PRINTFFORMAT(2,3);
@@ -378,7 +380,8 @@ void varbuffree(struct varbuf *v);
 extern void varbufaddbuf(struct varbuf *v, const void *s, const int l);
 
 /* varbufinit must be called exactly once before the use of each varbuf
- * (including before any call to varbuffree).
+ * (including before any call to varbuffree), or the variable must be
+ * initialized with VARBUF_INIT.
  *
  * However, varbufs allocated `static' are properly initialised anyway and
  * do not need varbufinit; multiple consecutive calls to varbufinit before

lib/dump.c
@@ -337,9 +337,8 @@ void varbufrecord(struct varbuf *vb,
 
 void writerecord(FILE *file, const char *filename,
                  const struct pkginfo *pigp, const struct pkginfoperfile *pifp) {
-  struct varbuf vb;
+  struct varbuf vb = VARBUF_INIT;
 
-  varbufinit(&vb);
   varbufrecord(&vb,pigp,pifp);
   varbufaddc(&vb,'\0');
   if (fputs(vb.buf,file) < 0)
@@ -357,7 +356,7 @@ void writedb(const char *filename, int available, int mustsync) {
   char *oldfn, *newfn;
   const char *which;
   FILE *file;
-  struct varbuf vb;
+  struct varbuf vb = VARBUF_INIT;
   int old_umask;
 
   which= available ? "available" : "status";
@@ -365,7 +364,6 @@ void writedb(const char *filename, int available, int mustsync) {
   strcpy(oldfn,filename); strcat(oldfn,OLDDBEXT);
   newfn= m_malloc(strlen(filename)+sizeof(NEWDBEXT));
   strcpy(newfn,filename); strcat(newfn,NEWDBEXT);
-  varbufinit(&vb);
 
   old_umask = umask(022);
   file= fopen(newfn,"w");

lib/mlib.c
@@ -265,10 +265,9 @@ off_t buffer_copy_setup_##name(type1 n1, int typeIn, void *procIn,\
 {\
   va_list al;\
   buffer_arg a1, a2;\
-  struct varbuf v;\
+  struct varbuf v = VARBUF_INIT;\
   off_t ret;\
   a1.name1 = n1; a2.name2 = n2;\
-  varbufinit(&v);\
   va_start(al,desc);\
   varbufvprintf(&v, desc, al);\
   va_end(al);\

lib/showpkg.c
@@ -199,15 +199,11 @@ struct lstitem* parseformat(const char* fmt) {
 
 
 void show1package(const struct lstitem* head, struct pkginfo *pkg) {
-	struct varbuf vb, fb, wb;
+	struct varbuf vb = VARBUF_INIT, fb = VARBUF_INIT, wb = VARBUF_INIT;
 
 	/* Make sure we have package info available, even if it's all empty. */
 	if (!pkg->installed.valid)
 		blankpackageperfile(&pkg->installed);
-
-	varbufinit(&vb);
-	varbufinit(&fb);
-	varbufinit(&wb);
 
 	while (head) {
 		int ok;

src/archives.c
@@ -898,10 +898,8 @@ static int try_remove_can(struct deppossi *pdep,
 void check_breaks(struct dependency *dep, struct pkginfo *pkg,
                   const char *pfilename) {
   struct pkginfo *fixbydeconf;
-  struct varbuf why;
+  struct varbuf why = VARBUF_INIT;
   int ok;
-
-  varbufinit(&why);
 
   fixbydeconf = NULL;
   if (depisok(dep, &why, &fixbydeconf, 0)) {
@@ -956,11 +954,8 @@ void check_conflict(struct dependency *dep, struct pkginfo *pkg,
                     const char *pfilename) {
   struct pkginfo *fixbyrm;
   struct deppossi *pdep, flagdeppossi;
-  struct varbuf conflictwhy, removalwhy;
+  struct varbuf conflictwhy = VARBUF_INIT, removalwhy = VARBUF_INIT;
   struct dependency *providecheck;
-  
-  varbufinit(&conflictwhy);
-  varbufinit(&removalwhy);
 
   fixbyrm = NULL;
   if (depisok(dep, &conflictwhy, &fixbyrm, 0)) {

src/configure.c
@@ -77,7 +77,8 @@ void deferred_configure(struct pkginfo *pkg) {
 	 * Try 4 (only if --force-depends).
 	 *  Do anyway.
 	 */
-	struct varbuf aemsgs, cdr, cdr2;
+	struct varbuf aemsgs = VARBUF_INIT;
+	struct varbuf cdr = VARBUF_INIT, cdr2 = VARBUF_INIT;
 	char *cdr2rest;
 	int ok, r, useredited, distedited;
 	struct conffile *conff;
@@ -99,7 +100,6 @@ void deferred_configure(struct pkginfo *pkg) {
 		if (findbreakcycle(pkg))
 			sincenothing= 0; 
 
-	varbufinit(&aemsgs);
 	ok = dependencies_ok(pkg, NULL, &aemsgs);
 	if (ok == 1) {
 		varbuffree(&aemsgs);
@@ -170,8 +170,6 @@ void deferred_configure(struct pkginfo *pkg) {
 		 * If `*.dpkg-new' no longer exists we assume that we've already
 		 * processed this one.
 		 */
-		varbufinit(&cdr);
-		varbufinit(&cdr2);
 		for (conff= pkg->installed.conffiles; conff; conff= conff->next) {
 			r= conffderef(pkg, &cdr, conff->name);
 			if (r == -1) {

commit d0d55d09d465844ed3d9cb9ada2507e07365f9a5

dpkg-query: Use VARBUF_INIT to initialize path instead of vb
Thinko introduced in eb483c17ebb9a47c8b99b4319e682ab685213090.

src/query.c
@@ -245,8 +245,8 @@ void searchfiles(const char *const *argv) {
   struct fileiterator *it;
   const char *thisarg;
   int found;
-  struct varbuf path;
-  static struct varbuf vb = VARBUF_INIT;
+  struct varbuf path = VARBUF_INIT;
+  static struct varbuf vb;
   
   if (!*argv)
     badusage(_("--search needs at least one file name pattern argument"));