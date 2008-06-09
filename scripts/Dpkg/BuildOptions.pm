9866c4bafdf147ad764f31e2747f5c1012802b1d
Raphael Hertzog <hertzog@debian.org>
2008-06-09 19:42:28 +0200

    dpkg-buildpackage: use space as separator in DEB_BUILD_OPTIONS
    * scripts/Dpkg/BuildOptions.pm (parse, set): Use space as the
    official separator in DEB_BUILD_OPTIONS. Check for validity of
    flags and print a warning if a bad option is detected. Rewrote
    the logic of set() to avoid adding options twice in non-overwrite
    mode.

--- scripts/Dpkg/BuildOptions.pm
+++ scripts/Dpkg/BuildOptions.pm
@@ -12,16 +12,16 @@ sub parse {
 
     my %opts;
 
-    foreach (split(/[\s,]+/, $env)) {
-	# FIXME: This is pending resolution of Debian bug #430649
-	/^([a-z][a-z0-9_-]*)(=(\w*))?$/;
+    foreach (split(/\s+/, $env)) {
+	unless (/^([a-z][a-z0-9_-]*)(=(\S*))?$/) {
+            warning(_g("invalid flag in DEB_BUILD_OPTIONS: %s"), $_);
+            next;
+        }
 
 	my ($k, $v) = ($1, $3 || '');
 
 	# Sanity checks
-	if (!$k) {
-	    next;
-	} elsif ($k =~ /^(noopt|nostrip|nocheck)$/ && length($v)) {
+	if ($k =~ /^(noopt|nostrip|nocheck)$/ && length($v)) {
 	    $v = '';
 	} elsif ($k eq 'parallel' && $v !~ /^-?\d+$/) {
 	    next;
@@ -37,18 +37,15 @@ sub set {
     my ($opts, $overwrite) = @_;
     $overwrite = 1 if not defined($overwrite);
 
-    my $env = $overwrite ? '' : $ENV{DEB_BUILD_OPTIONS}||'';
-    if ($env) { $env .= ',' }
-
+    my $new = {};
+    $new = parse() unless $overwrite;
     while (my ($k, $v) = each %$opts) {
-	if ($v) {
-	    $env .= "$k=$v,";
-	} else {
-	    $env .= "$k,";
-	}
+        $new->{$k} = $v;
     }
 
-    $ENV{DEB_BUILD_OPTIONS} = $env if $env;
+    my $env = join(" ", map { $new->{$_} ? $_ . "=" . $new->{$_} : $_ } keys %$new);
+
+    $ENV{DEB_BUILD_OPTIONS} = $env;
     return $env;
 }
 
