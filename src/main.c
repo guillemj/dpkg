27855f4f837e3d2c16a10e3851369875fe0ff093
Guillem Jover <guillem@debian.org>
2008-09-08 02:45:27 +0300

    dpkg: Remove left over debug printf in --command-fd code

--- src/main.c
+++ src/main.c
@@ -572,7 +572,6 @@ void commandfd(const char *const *argv) {
     if (c == EOF) ohshit(_("unexpected eof before end of line %d"),lno);
     if (!argc) continue;
     varbufaddc(&linevb,0);
-printf("line=`%*s'\n",(int)linevb.used,linevb.buf);
     oldargs = newargs = m_realloc(oldargs, sizeof(const char *) * (argc + 1));
     argc= 1;
     ptr= linevb.buf;
