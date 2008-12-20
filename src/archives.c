guillemj/dpkg

commit 854ad168370000ca46f4ba091c2d5bb05274b6ef

Improve processing of disappearing conffiles (Ian Jackson). This is part of the fix for #108587.
* lib/dpkg-db.h (conffile): Add `obsolete' field.
* lib/dump.c (w_conffiles): Write "obsolete" at the
end of conffile entry if obsolete is set.
* lib/fields.c (f_conffiles): Parse entries for
obsolete conffiles correctly.
* src/filesdb.h (filenamenode.flags): Add new
flag for obsolete conffiles.
* src/remove.c (removal_bulk_remove_configfiles):
Handle obsolete conffiles.
* src/archives.c (newconff_append): New function
to append a filenamenode to a fileinlist.
(addfiletolist): New function to add a filenamenode
to a tarcontext.
(tarobject): Use new addfiletolist function.
Handle case where a new package takes over
an obsolete conffile from another package.
* src/archives.h: Add declaration of the
addfiletolist function.
* src/processarc.c (process_archive): Use new
newconff_append function from archives.c.
Detect obsoleted conffiles and mark them as such.
* src/help.c (chmodsafe_unlink): Make it possible
to differentiate between failed chmod and failed
unlink by adding a new `failed' argument which
will be set to the name of the failed command.
(chmodsafe_unlink_statted): New function that
can be called if we already have a stat result for
the file/directory to be removed.
(ensure_pathname_nonexisting): Give better error
messages by utilizing the changes to
chmodsafe_unlink.
* src/main.h: Reflect changes in archives.c
and help.c (add declarations for newconff_append
and chmodsafe_unlink_statted and change the
one of chmodsafe_unlink).
(conffopt): Add new isold flag.

src/archives.c
@@ -315,14 +315,25 @@ int unlinkorrmdir(const char *filename) {
   errno= e; return r;
 }
 
+struct fileinlist *addfiletolist(struct tarcontext *tc,
+				 struct filenamenode *namenode) {
+  struct fileinlist *nifd;
+  
+  nifd= obstack_alloc(&tar_obs, sizeof(struct fileinlist));
+  nifd->namenode= namenode;
+  nifd->next= 0; *tc->newfilesp= nifd; tc->newfilesp= &nifd->next;
+  return nifd;
+}
+
 int tarobject(struct TarInfo *ti) {
   static struct varbuf conffderefn, hardlinkfn, symlinkfn;
   const char *usename;
-    
+
+  struct conffile *conff;
   struct tarcontext *tc= (struct tarcontext*)ti->UserData;
   int statr, fd, i, existingdirectory, keepexisting;
   size_t r;
-  struct stat stab, stabd;
+  struct stat stab, stabtmp;
   char databuf[TARBLKSZ];
   struct fileinlist *nifd, **oldnifd;
   struct pkginfo *divpkg, *otherpkg;
@@ -336,9 +347,7 @@ int tarobject(struct TarInfo *ti) {
    * been stripped by TarExtractor (lib/tarfn.c).
    */
   oldnifd= tc->newfilesp;
-  nifd= obstack_alloc(&tar_obs, sizeof(struct fileinlist));
-  nifd->namenode= findnamenode(ti->Name, 0);
-  nifd->next= 0; *tc->newfilesp= nifd; tc->newfilesp= &nifd->next;
+  nifd= addfiletolist(tc, findnamenode(ti->Name, 0));
   nifd->namenode->flags |= fnnf_new_inarchive;
 
   debug(dbg_eachfile,
@@ -417,7 +426,7 @@ int tarobject(struct TarInfo *ti) {
     break;
   case Directory:
     /* If it's already an existing directory, do nothing. */
-    if (!stat(fnamevb.buf,&stabd) && S_ISDIR(stabd.st_mode)) {
+    if (!stat(fnamevb.buf,&stabtmp) && S_ISDIR(stabtmp.st_mode)) {
       debug(dbg_eachfiledetail,"tarobject Directory exists");
       existingdirectory= 1;
     }
@@ -463,6 +472,29 @@ int tarobject(struct TarInfo *ti) {
         if (otherpkg->status == stat_configfiles) continue;
         /* Perhaps we're removing a conflicting package ? */
         if (otherpkg->clientdata->istobe == itb_remove) continue;
+
+	/* Is the file an obsolete conffile in the other package
+	 * and a conffile in the new package ? */
+	if ((nifd->namenode->flags & fnnf_new_conff) &&
+	    !statr && S_ISREG(stab.st_mode)) {
+	  for (conff= otherpkg->installed.conffiles;
+	       conff;
+	       conff= conff->next) {
+	    if (!conff->obsolete)
+	      continue;
+	    if (stat(conff->name, &stabtmp))
+	      if (errno == ENOENT || errno == ENOTDIR || errno == ELOOP)
+		continue;
+	    if (stabtmp.st_dev == stab.st_dev &&
+		stabtmp.st_ino == stab.st_ino)
+	      break;
+	  }
+	  if (conff)
+	    debug(dbg_eachfiledetail,"tarobject other's obsolete conffile");
+	    /* processarc.c will have copied its hash already. */
+	    continue;
+	}
+
         if (does_replace(tc->pkg,&tc->pkg->available,otherpkg)) {
           printf(_("Replacing files in old package %s ...\n"),otherpkg->name);
           otherpkg->clientdata->replacingfilesandsaid= 1;
@@ -1101,6 +1133,18 @@ int wanttoinstall(struct pkginfo *pkg, const struct versionrevision *ver, int sa
       return 0;
     }
   }
+}
+
+struct fileinlist *newconff_append(struct fileinlist ***newconffileslastp_io,
+				   struct filenamenode *namenode) {
+  struct fileinlist *newconff;
+
+  newconff= m_malloc(sizeof(struct fileinlist));
+  newconff->next= 0;
+  newconff->namenode= namenode;
+  **newconffileslastp_io= newconff;
+  *newconffileslastp_io= &newconff->next;
+  return newconff;
 }
 
 /* vi: ts=8 sw=2

lib/dpkg-db.h
@@ -84,6 +84,7 @@ struct conffile {
   struct conffile *next;
   const char *name;
   const char *hash;
+  int obsolete;
 };
 
 struct filedetails {

lib/dump.c
@@ -233,6 +233,7 @@ void w_conffiles(struct varbuf *vb,
     if (i!=pifp->conffiles) varbufaddc(vb,'\n');
     varbufaddc(vb,' '); varbufaddstr(vb,i->name); varbufaddc(vb,' ');
     varbufaddstr(vb,i->hash);
+    if (i->obsolete) varbufaddstr(vb," obsolete");
   }
   if (flags&fw_printheader)
     varbufaddc(vb,'\n');

lib/fields.c
@@ -224,13 +224,38 @@ void f_configversion(struct pkginfo *pigp, struct pkginfoperfile *pifp,
                      "in Config-Version string `%.250s': %.250s"),value,emsg);
 }
 
+void conffvalue_lastword(const char *value, const char *from,
+			 const char *endent,
+			 const char **word_start_r, int *word_len_r,
+			 const char **new_from_r,
+			 const char *filename, int lno,
+			 FILE *warnto, int *warncount, struct pkginfo *pigp) {
+  /* the code in f_conffiles ensures that value[-1]==' ', which is helpful */
+  const char *lastspc;
+  
+  if (from <= value+1) goto malformed;
+  for (lastspc= from-1; *lastspc != ' '; lastspc--);
+  if (lastspc <= value+1 || lastspc >= endent-1) goto malformed;
+
+  *new_from_r= lastspc;
+  *word_start_r= lastspc + 1;
+  *word_len_r= (int)(from - *word_start_r);
+  return;
+
+malformed:
+  parseerr(NULL,filename,lno, warnto,warncount,pigp,0,
+	   _("value for `conffiles' has malformatted line `%.*s'"),
+	   (int)(endent-value > 250 ? 250 : endent-value), value);
+}
+
 void f_conffiles(struct pkginfo *pigp, struct pkginfoperfile *pifp,
                  enum parsedbflags flags,
                  const char *filename, int lno, FILE *warnto, int *warncount,
                  const char *value, const struct fieldinfo *fip) {
+  static const char obsolete_str[]= "obsolete";
   struct conffile **lastp, *newlink;
-  const char *endent, *endfn;
-  int c, namelen, hashlen;
+  const char *endent, *endfn, *hashstart;
+  int c, namelen, hashlen, obsolete;
   char *newptr;
   
   lastp= &pifp->conffiles;
@@ -240,11 +265,15 @@ void f_conffiles(struct pkginfo *pigp, struct pkginfoperfile *pifp,
     if (c != ' ') parseerr(NULL,filename,lno, warnto,warncount,pigp,0, _("value for"
                            " `conffiles' has line starting with non-space `%c'"), c);
     for (endent= value; (c= *endent)!=0 && c != '\n'; endent++);
-    for (endfn= endent; *endfn != ' '; endfn--);
-    if (endfn <= value+1 || endfn >= endent-1)
-      parseerr(NULL,filename,lno, warnto,warncount,pigp,0,
-               _("value for `conffiles' has malformatted line `%.*s'"),
-               (int)(endent-value > 250 ? 250 : endent-value), value);
+    conffvalue_lastword(value, endent, endent,
+			&hashstart, &hashlen, &endfn,
+			filename,lno,warnto,warncount,pigp);
+    obsolete= (hashlen == sizeof(obsolete_str)-1 &&
+	       !memcmp(hashstart, obsolete_str, hashlen));
+    if (obsolete)
+      conffvalue_lastword(value, endfn, endent,
+			  &hashstart, &hashlen, &endfn,
+			  filename,lno,warnto,warncount,pigp);
     newlink= nfmalloc(sizeof(struct conffile));
     value= skip_slash_dotslash(value);
     namelen= (int)(endfn-value);
@@ -255,9 +284,10 @@ void f_conffiles(struct pkginfo *pigp, struct pkginfoperfile *pifp,
     memcpy(newptr+1,value,namelen);
     newptr[namelen+1]= 0;
     newlink->name= newptr;
-    hashlen= (int)(endent-endfn)-1; newptr= nfmalloc(hashlen+1);
-    memcpy(newptr,endfn+1,hashlen); newptr[hashlen]= 0;
+    newptr= nfmalloc(hashlen+1);
+    memcpy(newptr,hashstart,hashlen); newptr[hashlen]= 0;
     newlink->hash= newptr;
+    newlink->obsolete= obsolete;
     newlink->next =NULL;
     *lastp= newlink;
     lastp= &newlink->next;

src/archives.h
@@ -66,6 +66,9 @@ int filesavespackage(struct fileinlist*, struct pkginfo*,
 void check_conflict(struct dependency *dep, struct pkginfo *pkg,
                     const char *pfilename);
 
+struct fileinlist *addfiletolist(struct tarcontext *tc,
+				 struct filenamenode *namenode);
+
 extern int cleanup_pkg_failed, cleanup_conflictor_failed;
 
 #endif /* ARCHIVES_H */

src/filesdb.h
@@ -65,6 +65,7 @@ struct filenamenode {
     fnnf_new_conff=           000001, /* in the newconffiles list */
     fnnf_new_inarchive=       000002, /* in the new filesystem archive */
     fnnf_old_conff=           000004, /* in the old package's conffiles list */
+    fnnf_obs_conff=           000100, /* obsolete conffile */
     fnnf_elide_other_lists=   000010, /* must remove from other packages' lists */
     fnnf_no_atomic_overwrite= 000020, /* >=1 instance is a dir, cannot rename over */
     fnnf_placed_on_disk=      000040, /* new file has been placed on the disk */

src/help.c
@@ -430,23 +430,35 @@ void oldconffsetflags(const struct conffile *searchconff) {
   while (searchconff) {
     namenode= findnamenode(searchconff->name, 0); /* XXX */
     namenode->flags |= fnnf_old_conff;
+    if (!namenode->oldhash)
+      namenode->oldhash= searchconff->hash;
     debug(dbg_conffdetail, "oldconffsetflags `%s' namenode %p flags %o",
           searchconff->name, namenode, namenode->flags);
     searchconff= searchconff->next;
   }
 }
 
-int chmodsafe_unlink(const char *pathname) {
+int chmodsafe_unlink(const char *pathname, const char **failed) {
+  /* Sets *failed to `chmod' or `unlink' if those calls fail (which is
+   * always unexpected).  If stat fails it leaves *failed alone. */
   struct stat stab;
 
   if (lstat(pathname,&stab)) return -1;
-  if (S_ISREG(stab.st_mode) ? (stab.st_mode & 07000) :
-      !(S_ISLNK(stab.st_mode) || S_ISDIR(stab.st_mode) ||
-   S_ISFIFO(stab.st_mode) || S_ISSOCK(stab.st_mode))) {
+  *failed= N_("unlink");
+  return chmodsafe_unlink_statted(pathname, &stab, failed);
+}
+  
+int chmodsafe_unlink_statted(const char *pathname, const struct stat *stab,
+			     const char **failed) {
+  /* Sets *failed to `chmod'' if that call fails (which is always
+   * unexpected).  If unlink fails it leaves *failed alone. */
+  if (S_ISREG(stab->st_mode) ? (stab->st_mode & 07000) :
+      !(S_ISLNK(stab->st_mode) || S_ISDIR(stab->st_mode) ||
+	S_ISFIFO(stab->st_mode) || S_ISSOCK(stab->st_mode))) {
     /* We chmod it if it is 1. a sticky or set-id file, or 2. an unrecognised
-     * object (ie, not a file, link, directory, fifo or socket
+     * object (ie, not a file, link, directory, fifo or socket)
      */
-    if (chmod(pathname,0600)) return -1;
+    if (chmod(pathname,0600)) { *failed= N_("chmod"); return -1; }
   }
   if (unlink(pathname)) return -1;
   return 0;
@@ -454,7 +466,7 @@ int chmodsafe_unlink(const char *pathname) {
 
 void ensure_pathname_nonexisting(const char *pathname) {
   int c1;
-  const char *u;
+  const char *u, *failed;
 
   u= skip_slash_dotslash(pathname);
   assert(*u);
@@ -462,15 +474,19 @@ void ensure_pathname_nonexisting(const char *pathname) {
   debug(dbg_eachfile,"ensure_pathname_nonexisting `%s'",pathname);
   if (!rmdir(pathname)) return; /* Deleted it OK, it was a directory. */
   if (errno == ENOENT || errno == ELOOP) return;
+  failed= N_("delete");
   if (errno == ENOTDIR) {
     /* Either it's a file, or one of the path components is.  If one
      * of the path components is this will fail again ...
      */
-    if (!chmodsafe_unlink(pathname)) return; /* OK, it was */
+    if (!chmodsafe_unlink(pathname, &failed)) return; /* OK, it was */
     if (errno == ENOTDIR) return;
   }
-  if (errno != ENOTEMPTY) /* Huh ? */
-    ohshite(_("failed to rmdir/unlink `%.255s'"),pathname);
+  if (errno != ENOTEMPTY) { /* Huh ? */
+    char mbuf[250];
+    snprintf(mbuf, sizeof(mbuf), N_("failed to %s `%%.255s'"), failed);
+    ohshite(_(mbuf),pathname);
+  }
   c1= m_fork();
   if (!c1) {
     execlp(RM,"rm","-rf","--",pathname,(char*)0);

src/main.h
@@ -61,9 +61,10 @@ enum conffopt {
   cfof_prompt        =     001,
   cfof_keep          =     002,
   cfof_install       =     004,
-  cfof_backup        =    0100,
-  cfof_newconff      =    0200,
-  cfof_isnew         =    0400,
+  cfof_backup        =   00100,
+  cfof_newconff      =   00200,
+  cfof_isnew         =   00400,
+  cfof_isold         =   01000,
   cfom_main          =     007,
   cfo_keep           =   cfof_keep,
   cfo_prompt_keep    =   cfof_keep | cfof_prompt,
@@ -103,6 +104,8 @@ void filesdbinit(void);
 void archivefiles(const char *const *argv);
 void process_archive(const char *filename);
 int wanttoinstall(struct pkginfo *pkg, const struct versionrevision *ver, int saywhy);
+struct fileinlist *newconff_append(struct fileinlist ***newconffileslastp_io,
+				   struct filenamenode *namenode);
 
 /* from update.c */
 
@@ -179,7 +182,9 @@ void ensure_package_clientdata(struct pkginfo *pkg);
 const char *pkgadminfile(struct pkginfo *pkg, const char *whichfile);
 void oldconffsetflags(const struct conffile *searchconff);
 void ensure_pathname_nonexisting(const char *pathname);
-int chmodsafe_unlink(const char *pathname); /* chmod 600, then unlink */
+int chmodsafe_unlink(const char *pathname, const char **failed);
+int chmodsafe_unlink_statted(const char *pathname, const struct stat *stab,
+			     const char **failed);
 void checkpath(void);
 struct filenamenode *namenodetouse(struct filenamenode*, struct pkginfo*);
 

commit 4021e3db0f30bf4a19abb2a54fe5758654baa4e3

Error out when trying to replace a conffile w/o a Replaces field
Do not allow installing packages with non-obsolete conffiles owned by
other packages without a proper Replaces field. Closes: #508392

Regression introduced in commit 854ad168370000ca46f4ba091c2d5bb05274b6ef,
present since dpkg 1.13.14.

src/archives.c
@@ -542,10 +542,11 @@ int tarobject(struct TarInfo *ti) {
 		stabtmp.st_ino == stab.st_ino)
 	      break;
 	  }
-	  if (conff)
+	  if (conff) {
 	    debug(dbg_eachfiledetail,"tarobject other's obsolete conffile");
 	    /* processarc.c will have copied its hash already. */
 	    continue;
+	  }
 	}
 
         if (does_replace(tc->pkg,&tc->pkg->available,otherpkg)) {