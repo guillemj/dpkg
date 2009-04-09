349aeaa4f2673678cf09d5a31d4b80db1f534639
Raphael Hertzog <hertzog@debian.org>
2009-04-09 18:07:09 +0200

    dpkg-vendor: new script to query various vendor information
    This tool is meant to be used in debian/rules files to have common source
    packages across multiple distributions and yet still have slightly
    different binary packages.
    
    To automatically conserve customizations across derivatives of a given
    distribution, one can use “dpkg-vendor --derives-from vendor” so that all
    derivatives keep the same customizations when they rebuild the source
    package even if the current vendor is no more the same.

---
+++ scripts/dpkg-vendor.pl
@@ -0,0 +1,113 @@
+#!/usr/bin/perl
+#
+# dpkg-vendor
+#
+# Copyright © 2009 Raphaël Hertzog <hertzog@debian.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+
+use strict;
+use warnings;
+
+use Dpkg;
+use Dpkg::Gettext;
+use Dpkg::ErrorHandling;
+use Dpkg::Vendor qw(get_vendor_info get_current_vendor);
+
+textdomain("dpkg-dev");
+
+sub version {
+    printf _g("Debian %s version %s.\n"), $progname, $version;
+
+    printf _g("
+Copyright (C) 2009 Raphael Hertzog <hertzog\@debian.org>.");
+
+    printf _g("
+This is free software; see the GNU General Public Licence version 2 or
+later for copying conditions. There is NO warranty.
+");
+}
+
+sub usage {
+    printf _g(
+"Usage: %s [<option> ...] [<action>]
+
+Options:
+  --vendor <vendor>       assume <vendor> is the current vendor
+
+Actions:
+  --is <vendor>           returns true if current vendor is <vendor>.
+  --derives-from <vendor> returns true if current vendor derives from <vendor>.
+  --query <field>         print the content of the vendor-specific field.
+  --help                  show this help message.
+  --version               show the version.
+"), $progname;
+}
+
+my ($vendor, $param, $action);
+
+while (@ARGV) {
+    $_ = shift(@ARGV);
+    if (m/^--vendor$/) {
+        $vendor = shift(@ARGV);
+    } elsif (m/^--(is|derives-from|query)$/) {
+        badusage(_g("two commands specified: --%s and --%s"), $1, $action)
+            if defined($action);
+        $action = $1;
+        $param = shift(@ARGV);
+    } elsif (m/^-(h|-help)$/) {
+        usage();
+        exit 0;
+    } elsif (m/^--version$/) {
+        version();
+        exit 0;
+    } else {
+	usageerr(_g("unknown option \`%s'"), $_);
+    }
+}
+
+# Uses $ENV{DEB_VENDOR} if set
+$vendor = get_current_vendor() unless defined($vendor);
+
+my $info = get_vendor_info($vendor);
+unless (defined($info)) {
+    error(_g("vendor %s doesn't exist in /etc/dpkg/origins/"),
+          $vendor || "default");
+}
+
+if ($action eq 'is') {
+
+    exit(0) if lc($param) eq lc($info->{'Vendor'});
+    exit(1);
+
+} elsif ($action eq 'derives-from') {
+
+    exit(0) if lc($param) eq lc($info->{'Vendor'});
+    while (defined($info) && exists $info->{'Parent'}) {
+        $info = get_vendor_info($info->{'Parent'});
+        exit(0) if lc($param) eq lc($info->{'Vendor'});
+    }
+    exit(1);
+
+} elsif ($action eq 'query') {
+
+    if (exists $info->{$param}) {
+        print $info->{$param} . "\n";
+        exit(0);
+    } else {
+        exit(1);
+    }
+
+}
