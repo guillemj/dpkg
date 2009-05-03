da0538290b7dbbfb3c537441470767fd0879c985
Guillem Jover <guillem@debian.org>
2009-05-03 14:07:00 +0200

    Add '.hgtags' to the default dpkg-source -i regex and -I pattern
    Closes: #525854

--- scripts/Dpkg/Source/Package.pm
+++ scripts/Dpkg/Source/Package.pm
@@ -48,7 +48,7 @@ our $diff_ignore_default_regexp = '
 # File-names that should be ignored (never directories)
 (?:^|/)(?:DEADJOE|\.arch-inventory|\.(?:bzr|cvs|hg|git)ignore)$|
 # File or directory names that should be ignored
-(?:^|/)(?:CVS|RCS|\.deps|\{arch\}|\.arch-ids|\.svn|\.hg|_darcs|\.git|
+(?:^|/)(?:CVS|RCS|\.deps|\{arch\}|\.arch-ids|\.svn|\.hg(?:tags)?|_darcs|\.git|
 \.shelf|_MTN|\.be|\.bzr(?:\.backup|tags)?)(?:$|/.*$)
 ';
 # Take out comments and newlines
@@ -77,6 +77,7 @@ our @tar_ignore_default_pattern = qw(
 .gitignore
 .hg
 .hgignore
+.hgtags
 .shelf
 .svn
 CVS
