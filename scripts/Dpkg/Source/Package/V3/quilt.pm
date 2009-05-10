57c5a9dd52955d5b2fd55f224d70cb4f3f40f437
Raphael Hertzog <hertzog@debian.org>
2009-05-10 19:15:29 +0200

    dpkg-source: minor update to "3.0 (quilt)" format
    Do not update/create debian/patches/.dpkg-source-applied during build,
    it's only meant to document what patches have been applied at extraction
    time.
    
    Also try to cleanup in case the patch serie is empty after removal of the
    automatic patch. First remove debian/patches/series if it's empty and then
    debian/patches/ itself in case it's empty as well.

--- scripts/Dpkg/Source/Package/V3/quilt.pm
+++ scripts/Dpkg/Source/Package/V3/quilt.pm
@@ -262,9 +262,6 @@ sub register_autopatch {
                 print SERIES "$auto_patch\n";
                 close(SERIES);
             }
-            open(APPLIED, ">>", $applied) || syserr(_g("cannot write %s"), $applied);
-            print APPLIED "$auto_patch\n";
-            close(APPLIED);
         } else {
             # If quilt was used, ensure its meta-information are
             # synchronized with the updated patch
@@ -293,6 +290,8 @@ sub register_autopatch {
                                  wait_child => 1, to_file => '/dev/null');
             }
         }
+        # Clean up empty series
+        unlink($series) if not -s $series;
     }
 }
 
