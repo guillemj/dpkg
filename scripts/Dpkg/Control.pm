22699815920b1cb8550e06dcf2ffe1442b0ee890
Raphaël Hertzog <hertzog@debian.org>
2009-09-13 21:32:30 +0200

    Drop unused code that has been merged in Dpkg::Control

--- scripts/Dpkg/Control.pm
+++ scripts/Dpkg/Control.pm
@@ -26,7 +26,7 @@ use Dpkg::Control::Types;
 use Dpkg::Control::Hash;
 
 use base qw(Dpkg::Control::Hash Exporter);
-our @EXPORT = qw(parsecdata CTRL_UNKNOWN CTRL_INFO_SRC CTRL_INFO_PKG CTRL_APT_SRC
+our @EXPORT = qw(CTRL_UNKNOWN CTRL_INFO_SRC CTRL_INFO_PKG CTRL_APT_SRC
                  CTRL_APT_PKG CTRL_PKG_SRC CTRL_PKG_DEB CTRL_FILE_CHANGES
                  CTRL_FILE_VENDOR CTRL_FILE_STATUS CTRL_CHANGELOG);
 
@@ -104,85 +104,6 @@ All the methods of Dpkg::Control::Hash are available. Those listed below
 are either new or overriden with a different behaviour.
 
 =over 4
-
-=item $obj = Dpkg::Control::parsecdata($input, $file, %options)
-
-$input is a filehandle, $file is the name of the file corresponding to
-$input. %options can contain two parameters: allow_pgp=>1 allows the parser
-to extrac the block of a data in a PGP-signed message (defaults to 0),
-and allow_duplicate=>1 ask the parser to not fail when it detects
-duplicate fields.
-
-The return value is a reference to a tied hash (Dpkg::Fields::Object) that
-can be used to access the various fields.
-
-=cut
-
-sub parsecdata {
-    my ($input, $file, %options) = @_;
-
-    $options{allow_pgp} = 0 unless exists $options{allow_pgp};
-    $options{allow_duplicate} = 0 unless exists $options{allow_duplicate};
-
-    my $paraborder = 1;
-    my $fields = undef;
-    my $cf = ''; # Current field
-    my $expect_pgp_sig = 0;
-    while (<$input>) {
-	s/\s*\n$//;
-	next if (m/^$/ and $paraborder);
-	next if (m/^#/);
-	$paraborder = 0;
-	if (m/^(\S+?)\s*:\s*(.*)$/) {
-	    unless (defined $fields) {
-		my %f;
-		tie %f, "Dpkg::Fields::Object";
-		$fields = \%f;
-	    }
-	    if (exists $fields->{$1}) {
-		unless ($options{allow_duplicate}) {
-		    syntaxerr($file, sprintf(_g("duplicate field %s found"), capit($1)));
-		}
-	    }
-	    $fields->{$1} = $2;
-	    $cf = $1;
-	} elsif (m/^\s+\S/) {
-	    length($cf) || syntaxerr($file, _g("continued value line not in field"));
-	    $fields->{$cf} .= "\n$_";
-	} elsif (m/^-----BEGIN PGP SIGNED MESSAGE/) {
-	    $expect_pgp_sig = 1;
-	    if ($options{allow_pgp}) {
-		# Skip PGP headers
-		while (<$input>) {
-		    last if m/^$/;
-		}
-	    } else {
-		syntaxerr($file, _g("PGP signature not allowed here"));
-	    }
-	} elsif (m/^$/) {
-	    if ($expect_pgp_sig) {
-		# Skip empty lines
-		$_ = <$input> while defined($_) && $_ =~ /^\s*$/;
-		length($_) ||
-                    syntaxerr($file, _g("expected PGP signature, found EOF after blank line"));
-		s/\n$//;
-		m/^-----BEGIN PGP SIGNATURE/ ||
-		    syntaxerr($file,
-			sprintf(_g("expected PGP signature, found something else \`%s'"), $_));
-		# Skip PGP signature
-		while (<$input>) {
-		    last if m/^-----END PGP SIGNATURE/;
-		}
-		length($_) ||
-                    syntaxerr($file, _g("unfinished PGP signature"));
-	    }
-	    last; # Finished parsing one block
-	} else {
-	    syntaxerr($file, _g("line with unknown format (not field-colon-value)"));
-	}
-    }
-    return $fields;
-}
 
 =item my $c = Dpkg::Control->new(%opts)
 
