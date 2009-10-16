f8270519a63210a5b6eb02cfff1bdc24cbfe4ef8
Raphaël Hertzog <hertzog@debian.org>
2009-10-16 12:44:11 +0200

    Dpkg::Compression: add support for xz compression method
    Update the dpkg-source manual page accordingly.

--- scripts/Dpkg/Compression.pm
+++ scripts/Dpkg/Compression.pm
@@ -8,12 +8,14 @@ our @EXPORT = qw(@comp_supported %comp_supported %comp_ext $comp_regex
 		 %comp_prog %comp_decomp_prog
 		 get_compression_from_filename);
 
-our @comp_supported = qw(gzip bzip2 lzma);
+our @comp_supported = qw(gzip bzip2 lzma xz);
 our %comp_supported = map { $_ => 1 } @comp_supported;
-our %comp_ext = (gzip => 'gz', bzip2 => 'bz2', lzma => 'lzma');
-our $comp_regex = '(?:gz|bz2|lzma)';
-our %comp_prog = (gzip => 'gzip', bzip2 => 'bzip2', lzma => 'lzma');
-our %comp_decomp_prog = (gzip => 'gunzip', bzip2 => 'bunzip2', lzma => 'unlzma');
+our %comp_ext = (gzip => 'gz', bzip2 => 'bz2', lzma => 'lzma', xz => 'xz');
+our $comp_regex = '(?:gz|bz2|lzma|xz)';
+our %comp_prog = (gzip => 'gzip', bzip2 => 'bzip2', lzma => 'lzma',
+		  xz => 'xz');
+our %comp_decomp_prog = (gzip => 'gunzip', bzip2 => 'bunzip2', lzma => 'unlzma',
+			 xz => 'unxz');
 
 sub get_compression_from_filename {
     my $filename = shift;
