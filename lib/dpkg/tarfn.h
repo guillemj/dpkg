368c47b7376334efacdf0347f01860c42dc9a002
Guillem Jover <guillem@debian.org>
2009-10-25 17:50:18 +0100

    Expand text in license header for unversioned GPL

--- lib/dpkg/tarfn.h
+++ lib/dpkg/tarfn.h
@@ -1,8 +1,21 @@
 /*
- * Functions for extracting tar archives.
- * Bruce Perens, April-May 1995
+ * libdpkg - Debian packaging suite library routines
+ * tarfn.h - tar archive extraction functions
+ *
  * Copyright © 1995 Bruce Perens
- * This is free software under the GNU General Public License.
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef LIBDPKG_TARFN_H
--- scripts/dpkg-checkbuilddeps.pl
+++ scripts/dpkg-checkbuilddeps.pl
@@ -1,5 +1,21 @@
 #!/usr/bin/perl
-# GPL copyright 2001 by Joey Hess <joeyh@debian.org>
+#
+# dpkg-checkbuilddeps
+#
+# Copyright © 2001 Joey Hess <joeyh@debian.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 use strict;
 use warnings;
