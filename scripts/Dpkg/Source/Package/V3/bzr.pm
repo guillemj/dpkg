9447c1795135f9658c088da90fee12e42c469020
Guillem Jover <guillem@debian.org>
2009-11-07 22:05:53 +0100

    Unify format in license headers
    Indent license text to one space after the '#'. Add '#' to blank lines
    between paragraphs and remove trailing '#' after the license text.

--- scripts/Dpkg/Source/Package/V3/bzr.pm
+++ scripts/Dpkg/Source/Package/V3/bzr.pm
@@ -19,6 +19,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package Dpkg::Source::Package::V3::bzr;
 
 use strict;
