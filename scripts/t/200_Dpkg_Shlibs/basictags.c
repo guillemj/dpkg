bf4c40bc0cebcf937854aa259c6729da9c975768
Guillem Jover <guillem@debian.org>
2010-02-08 14:56:39 +0100

    scripts/t: Add missing function prototypes

--- scripts/t/200_Dpkg_Shlibs/basictags.c
+++ scripts/t/200_Dpkg_Shlibs/basictags.c
@@ -7,21 +7,27 @@
  * $ objdump -wfpTR libbasictags.so.1 > objdump.basictags-i386
  */
 
+void symbol11_optional(void);
 void symbol11_optional(void) {}
 
 #ifdef AMD64
+void symbol21_amd64(void);
 void symbol21_amd64(void) {}
 #endif
 
 #ifndef AMD64
+void symbol22_i386(void);
 void symbol22_i386(void) {}
 #endif
 
+void symbol31_randomtag(void);
 void symbol31_randomtag(void) {}
 
 /* (arch=i386|optional)symbol41_i386_and_optional@Base */
 #ifndef AMD64
+void symbol41_i386_and_optional(void);
 void symbol41_i386_and_optional(void) {}
 #endif
 
+void symbol51_untagged(void);
 void symbol51_untagged(void) {}
