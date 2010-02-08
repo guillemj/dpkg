2b6d9bdd3fe969401470bc26f6b30982ac798ec3
Guillem Jover <guillem@debian.org>
2010-02-08 14:54:45 +0100

    scripts/t: Use void in function arguments to denote no arguments
    Fixes warnings due to old style function definition.

--- scripts/t/200_Dpkg_Shlibs/basictags.c
+++ scripts/t/200_Dpkg_Shlibs/basictags.c
@@ -7,21 +7,21 @@
  * $ objdump -wfpTR libbasictags.so.1 > objdump.basictags-i386
  */
 
-void symbol11_optional() {}
+void symbol11_optional(void) {}
 
 #ifdef AMD64
-void symbol21_amd64() {}
+void symbol21_amd64(void) {}
 #endif
 
 #ifndef AMD64
-void symbol22_i386() {}
+void symbol22_i386(void) {}
 #endif
 
-void symbol31_randomtag() {}
+void symbol31_randomtag(void) {}
 
 /* (arch=i386|optional)symbol41_i386_and_optional@Base */
 #ifndef AMD64
-void symbol41_i386_and_optional() {}
+void symbol41_i386_and_optional(void) {}
 #endif
 
-void symbol51_untagged() {}
+void symbol51_untagged(void) {}
