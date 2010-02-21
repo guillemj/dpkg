2e9ea71aeff38e53b56348b6b89b00a1fe0cb9e9
Guillem Jover <guillem@debian.org>
2010-02-21 05:52:10 +0100

    libdpkg: Add a new pkg-queue module

---
+++ lib/dpkg/pkg-queue.c
@@ -0,0 +1,90 @@
+/*
+ * dpkg - main program for package management
+ * pkg-queue.c - primitives for pkg queue handling
+ *
+ * Copyright © 2010 Guillem Jover <guillem@debian.org>
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <config.h>
+#include <compat.h>
+
+#include <stdlib.h>
+
+#include <dpkg/dpkg-db.h>
+#include <dpkg/pkg-queue.h>
+
+void
+pkg_queue_init(struct pkg_queue *queue)
+{
+	queue->head = NULL;
+	queue->tail = NULL;
+	queue->length = 0;
+}
+
+void
+pkg_queue_destroy(struct pkg_queue *queue)
+{
+	pkg_list_free(queue->head);
+	pkg_queue_init(queue);
+}
+
+int
+pkg_queue_is_empty(struct pkg_queue *queue)
+{
+	return (queue->head == NULL);
+}
+
+struct pkg_list *
+pkg_queue_push(struct pkg_queue *queue, struct pkginfo *pkg)
+{
+	struct pkg_list *node;
+
+	node = pkg_list_new(pkg, NULL);
+
+	if (queue->tail == NULL)
+		queue->head = node;
+	else
+		queue->tail->next = node;
+
+	queue->tail = node;
+
+	queue->length++;
+
+	return node;
+}
+
+struct pkginfo *
+pkg_queue_pop(struct pkg_queue *queue)
+{
+	struct pkg_list *node;
+	struct pkginfo *pkg;
+
+	if (pkg_queue_is_empty(queue))
+		return NULL;
+
+	node = queue->head;
+	pkg = node->pkg;
+
+	queue->head = node->next;
+	if (queue->head == NULL)
+		queue->tail = NULL;
+
+	free(node);
+	queue->length--;
+
+	return pkg;
+}
+
---
+++ lib/dpkg/pkg-queue.h
@@ -0,0 +1,47 @@
+/*
+ * dpkg - main program for package management
+ * pkg-queue.h - primitives for pkg queue handling
+ *
+ * Copyright © 2010 Guillem Jover <guillem@debian.org>
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DPKG_PKG_QUEUE_H
+#define DPKG_PKG_QUEUE_H
+
+#include <dpkg/macros.h>
+#include <dpkg/pkg-list.h>
+
+DPKG_BEGIN_DECLS
+
+struct pkg_queue {
+	struct pkg_list *head, *tail;
+	int length;
+};
+
+#define PKG_QUEUE_INIT \
+	(struct pkg_queue){ .head = NULL, .tail = NULL, .length = 0 }
+
+void pkg_queue_init(struct pkg_queue *queue);
+void pkg_queue_destroy(struct pkg_queue *queue);
+
+int pkg_queue_is_empty(struct pkg_queue *queue);
+
+struct pkg_list *pkg_queue_push(struct pkg_queue *queue, struct pkginfo *pkg);
+struct pkginfo *pkg_queue_pop(struct pkg_queue *queue);
+
+DPKG_END_DECLS
+
+#endif /* DPKG_PKG_QUEUE_H */
