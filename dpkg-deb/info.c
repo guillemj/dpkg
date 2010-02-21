guillemj/dpkg

commit c264dd83d33250927fe165de5336145afdf99610

Fix small memory leaks related to scandir()
The array of pointers returned by scandir and the pointers in it were
not being freed.

dpkg-deb/info.c
@@ -169,7 +169,10 @@ static void info_list(const char *debar, const char *directory) {
     } else {
       printf(_("     not a plain file          %.255s\n"), cdep->d_name);
     }
+    free(cdep);
   }
+  free(cdlist);
+
   if (!(cc= fopen("control","r"))) {
     if (errno != ENOENT) ohshite(_("failed to read `control' (in `%.255s')"),directory);
     fputs(_("(no `control' file in control archive!)\n"), stdout);

lib/dpkg/myopt.c
@@ -176,6 +176,7 @@ load_config_dir(const char *prog, const struct cmdinfo* cmdinfos)
 
     myfileopt(filename, cmdinfos);
 
+    free(dlist[i]);
     free(filename);
   }
 

commit b066610b394bfe06b8af5efb0a34c2b56c865705

dpkg-deb: Fix double free in --info
Change the code to not increase cdlist, and instead index it, so that
we can free it at the end.

Regression introduced in commit c264dd83d33250927fe165de5336145afdf99610.

dpkg-deb/info.c
@@ -131,7 +131,7 @@ static void info_list(const char *debar, const char *directory) {
   char interpreter[INTERPRETER_MAX+1], *p;
   int il, lines;
   struct dirent **cdlist, *cdep;
-  int cdn;
+  int cdn, n;
   FILE *cc;
   struct stat stab;
   int c;
@@ -139,8 +139,8 @@ static void info_list(const char *debar, const char *directory) {
   cdn= scandir(".", &cdlist, &ilist_select, alphasort);
   if (cdn == -1) ohshite(_("cannot scan directory `%.255s'"),directory);
 
-  while (cdn-- >0) {
-    cdep= *cdlist++;
+  for (n = 0; n < cdn; n++) {
+    cdep = cdlist[n];
     if (stat(cdep->d_name,&stab))
       ohshite(_("cannot stat `%.255s' (in `%.255s')"),cdep->d_name,directory);
     if (S_ISREG(stab.st_mode)) {