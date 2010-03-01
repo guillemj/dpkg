4e80526259046215c52f4024c7e4bc4fbdc6cd71
Guillem Jover <guillem@debian.org>
2010-03-02 00:24:44 +0100

    t/200_Dpkg_Shlibs: Reformat for coding style conformance

--- scripts/t/Dpkg_Shlibs/patterns.cpp
+++ scripts/t/Dpkg_Shlibs/patterns.cpp
@@ -1,52 +1,47 @@
 #define EXPORT(x) x {}
 
-namespace NSA
-{
-    class ClassA
-    {
+namespace NSA {
+	class ClassA {
 	private:
-	    class Private
-	    {
+		class Private {
 		public:
-		    Private();
-		    virtual ~Private();
-		    void privmethod1(int);
-		    void privmethod2(int);
-	    };
-	    Private *p;
-	    class Internal
-	    {
+			Private();
+			virtual ~Private();
+			void privmethod1(int);
+			void privmethod2(int);
+		};
+		Private *p;
+		class Internal {
 		public:
-		    Internal();
-		    virtual ~Internal();
-		    void internal_method1(char);
-		    void internal_method2(char);
-	    };
-	    Internal *i;
+			Internal();
+			virtual ~Internal();
+			void internal_method1(char);
+			void internal_method2(char);
+		};
+		Internal *i;
 	public:
-	    ClassA();
-	    virtual ~ClassA();
-	    virtual void generate_vt(const char *) const;
-    };
+		ClassA();
+		virtual ~ClassA();
+		virtual void generate_vt(const char *) const;
+	};
 
-    EXPORT(ClassA::Private::Private());
-    EXPORT(ClassA::Private::~Private());
-    EXPORT(void ClassA::Private::privmethod1(int));
-    EXPORT(void ClassA::Private::privmethod2(int));
+	EXPORT(ClassA::Private::Private());
+	EXPORT(ClassA::Private::~Private());
+	EXPORT(void ClassA::Private::privmethod1(int));
+	EXPORT(void ClassA::Private::privmethod2(int));
 
-    EXPORT(ClassA::Internal::Internal());
-    EXPORT(ClassA::Internal::~Internal());
-    EXPORT(void ClassA::Internal::internal_method1(char));
-    EXPORT(void ClassA::Internal::internal_method2(char));
+	EXPORT(ClassA::Internal::Internal());
+	EXPORT(ClassA::Internal::~Internal());
+	EXPORT(void ClassA::Internal::internal_method1(char));
+	EXPORT(void ClassA::Internal::internal_method2(char));
 
-    EXPORT(ClassA::ClassA());
-    EXPORT(ClassA::~ClassA());
-    EXPORT(void ClassA::generate_vt(const char *) const);
+	EXPORT(ClassA::ClassA());
+	EXPORT(ClassA::~ClassA());
+	EXPORT(void ClassA::generate_vt(const char *) const);
 };
 
-class ClassB
-{
-    public:
+class ClassB {
+public:
 	ClassB();
 	virtual ~ClassB();
 	virtual void generate_vt(const char *) const;
@@ -56,9 +51,8 @@ EXPORT(ClassB::ClassB());
 EXPORT(ClassB::~ClassB());
 EXPORT(void ClassB::generate_vt(const char *) const);
 
-class ClassC
-{
-    public:
+class ClassC {
+public:
 	ClassC();
 	virtual ~ClassC();
 	virtual void generate_vt(const char *) const;
@@ -68,40 +62,38 @@ EXPORT(ClassC::ClassC());
 EXPORT(ClassC::~ClassC());
 EXPORT(void ClassC::generate_vt(const char *) const);
 
-namespace NSB
-{
-    class ClassD : public NSA::ClassA, public ClassB, public ClassC
-    {
+namespace NSB {
+	class ClassD : public NSA::ClassA, public ClassB, public ClassC {
 	public:
-	    ClassD();
-	    virtual ~ClassD();
-	    virtual void generate_vt(const char *) const override;
-    };
+		ClassD();
+		virtual ~ClassD();
+		virtual void generate_vt(const char *) const override;
+	};
 
-    EXPORT(ClassD::ClassD());
-    EXPORT(ClassD::~ClassD());
-    EXPORT(void ClassD::generate_vt(const char *) const);
+	EXPORT(ClassD::ClassD());
+	EXPORT(ClassD::~ClassD());
+	EXPORT(void ClassD::generate_vt(const char *) const);
 
-    class Symver {
+	class Symver {
 	public:
-	    Symver();
-	    ~Symver();
+		Symver();
+		~Symver();
 
-	    void symver_method1();
-	    void symver_method2();
-    };
+		void symver_method1();
+		void symver_method2();
+	};
 
-    EXPORT(Symver::Symver());
-    EXPORT(Symver::~Symver());
-    EXPORT(void Symver::symver_method1());
-    EXPORT(void Symver::symver_method2());
+	EXPORT(Symver::Symver());
+	EXPORT(Symver::~Symver());
+	EXPORT(void Symver::symver_method1());
+	EXPORT(void Symver::symver_method2());
 
-    class SymverOptional {
+	class SymverOptional {
 	public:
-	    SymverOptional();
-	    ~SymverOptional();
-    };
+		SymverOptional();
+		~SymverOptional();
+	};
 
-    EXPORT(SymverOptional::SymverOptional());
-    EXPORT(SymverOptional::~SymverOptional());
+	EXPORT(SymverOptional::SymverOptional());
+	EXPORT(SymverOptional::~SymverOptional());
 };
