guillemj/dpkg

commit f426b031ac858fa30ace69959a43b61fb40f4be9

dselect: Use max/min instead of lesserint/greaterint

dselect/dselect.h
@@ -134,9 +134,6 @@ public:
   virtual ~baselist();
 };
 
-static inline int lesserint(int a, int b) { return a<b ? a : b; }
-static inline int greaterint(int a, int b) { return a>b ? a : b; }
-
 void displayhelp(const struct helpmenuentry *menu, int key);
 
 void mywerase(WINDOW *win);

TODO
@@ -45,7 +45,6 @@ TODO
    - Use internerr instead of assert, and print more meaninful messages.
    - Make actionfunction return int, and avoid global exitcode variables.
    - Move ACTION and OBSOLETE, etc to ‘myopt.h’.
-   - Use max/min in dselect instead of lesserint/greaterint.
    - Refactor packageslump into generic code.
    - Refactor subprocess execution (argument building etc).
    - Replace raw write and read calls with safe variants (signals etc).

dselect/basecmds.cc
@@ -72,7 +72,7 @@ void baselist::kd_top() {
 void baselist::kd_bottom() {
   topofscreen= nitems - list_height;
   if (topofscreen < 0) topofscreen= 0;
-  setcursor(lesserint(topofscreen + list_height - 1, nitems-1));
+  setcursor(min(topofscreen + list_height - 1, nitems - 1));
 }
 
 void baselist::kd_redraw() {

dselect/baselist.cc
@@ -281,7 +281,7 @@ void baselist::dosearch() {
   int offset, index;
   if (debug) fprintf(debug,"baselist[%p]::dosearch(); searchstring=`%s'\n",
                      this,searchstring);
-  for (offset=1, index=greaterint(topofscreen,cursorline+1);
+  for (offset = 1, index = max(topofscreen, cursorline + 1);
        offset<nitems;
        offset++, index++) {
     if (index >= nitems) index -= nitems;
@@ -298,8 +298,8 @@ void baselist::dosearch() {
 
 void baselist::refreshinfo() {
   pnoutrefresh(infopad, infotopofscreen,leftofscreen, info_row,0,
-               lesserint(info_row + info_height - 1, info_row + MAX_DISPLAY_INFO - 1),
-               lesserint(total_width - leftofscreen - 1, xmax - 1));
+               min(info_row + info_height - 1, info_row + MAX_DISPLAY_INFO - 1),
+               min(total_width - leftofscreen - 1, xmax - 1));
   
   if (whatinfo_height) {
     mywerase(whatinfowin);

dselect/basetop.cc
@@ -26,12 +26,10 @@
 #include "dselect.h"
 
 void baselist::refreshlist() {
-  redrawitemsrange(topofscreen,lesserint(nitems,topofscreen+list_height));
+  redrawitemsrange(topofscreen, min(nitems, topofscreen + list_height));
   int y, x, maxy, maxx;
-  y= lesserint(list_row + list_height - 1,
-               list_row + nitems - topofscreen - 1);
-  x= lesserint(total_width - leftofscreen - 1,
-               xmax - 1);
+  y = min(list_row + list_height - 1, list_row + nitems - topofscreen - 1);
+  x = min(total_width - leftofscreen - 1, xmax - 1);
   pnoutrefresh(listpad, 0, leftofscreen, list_row, 0, y, x);
   getmaxyx(listpad,maxy,maxx);
   y++;
@@ -50,5 +48,5 @@ void baselist::redrawitemsrange(int start, int end) {
 
 void baselist::refreshcolheads() {
   pnoutrefresh(colheadspad, 0,leftofscreen, colheads_row,0,
-               colheads_row, lesserint(total_width - leftofscreen - 1, xmax - 1));
+               colheads_row, min(total_width - leftofscreen - 1, xmax - 1));
 }

dselect/methlist.cc
@@ -86,7 +86,7 @@ void methodlist::redrawthisstate() {
           _("Access method `%s'."),
           table[cursorline]->name);
   pnoutrefresh(thisstatepad, 0,0, thisstate_row,0,
-               thisstate_row, lesserint(total_width - 1, xmax - 1));
+               thisstate_row, min(total_width - 1, xmax - 1));
 }
 
 void methodlist::redraw1itemsel(int index, int selected) {

dselect/pkgcmds.cc
@@ -215,7 +215,7 @@ void packagelist::kd_versiondisplay() {
   ldrawnstart= ldrawnend= -1;
   redrawtitle();
   redrawcolheads();
-  redrawitemsrange(topofscreen,lesserint(topofscreen+list_height,nitems));
+  redrawitemsrange(topofscreen, min(topofscreen + list_height, nitems));
   refreshlist();
 }
 
@@ -226,7 +226,7 @@ void packagelist::kd_verbose() {
   ldrawnstart= ldrawnend= -1;
   redrawtitle();
   redrawcolheads();
-  redrawitemsrange(topofscreen,lesserint(topofscreen+list_height,nitems));
+  redrawitemsrange(topofscreen, min(topofscreen + list_height, nitems));
   refreshlist();
 }
 

dselect/pkgdepcon.cc
@@ -61,13 +61,13 @@ int packagelist::checkdependers(pkginfo *pkg, int changemade) {
   if (pkg->available.valid) {
     for (possi= pkg->available.depended; possi; possi= possi->nextrev) {
       if (!useavailable(possi->up->up)) continue;
-      changemade= greaterint(changemade,resolvedepcon(possi->up));
+      changemade = max(changemade, resolvedepcon(possi->up));
     }
   }
   if (pkg->installed.valid) {
     for (possi= pkg->installed.depended; possi; possi= possi->nextrev) {
       if (useavailable(possi->up->up)) continue;
-      changemade= greaterint(changemade,resolvedepcon(possi->up));
+      changemade = max(changemade, resolvedepcon(possi->up));
     }
   }
   return changemade;
@@ -94,7 +94,7 @@ int packagelist::resolvesuggest() {
       for (depends= findinfo(table[index]->pkg)->depends;
            depends;
            depends= depends->next) {
-        changemade= greaterint(changemade,resolvedepcon(depends));
+        changemade = max(changemade, resolvedepcon(depends));
       }
       changemade= checkdependers(table[index]->pkg,changemade);
       for (depends= findinfo(table[index]->pkg)->depends;
@@ -108,7 +108,7 @@ int packagelist::resolvesuggest() {
                 this, index, table[index]->pkg->name, changemade);
     }
     if (!changemade) break;
-    maxchangemade= greaterint(maxchangemade, changemade);
+    maxchangemade = max(maxchangemade, changemade);
   }
   if (debug)
     fprintf(debug,"packagelist[%p]::resolvesuggest() done; maxchangemade=%d\n",

commit 7e98241b11ce27e8d7fb87f1e64ecd03ec0ea0bf

dselect: Show dependency/conflicts resolution screen again
Fix by switching the C++ code to use STL's min() and max() instead of
preprocessor macros, to avoid multiple evaluation of arguments.

Regression introduced in commit f426b031ac858fa30ace69959a43b61fb40f4be9.

Closes: #574816

Based-on-patch-by: Robert Luberda <robert@debian.org>

dselect/dselect.h
@@ -24,6 +24,11 @@
 
 #include <signal.h>
 
+#include <algorithm>
+
+using std::min;
+using std::max;
+
 #include "dselect-curses.h"
 
 #define DSELECT		"dselect"

lib/dpkg/macros.h
@@ -59,12 +59,15 @@
 #define array_count(a) (sizeof(a) / sizeof((a)[0]))
 #endif
 
+/* For C++ use native implementations from STL or similar. */
+#ifndef __cplusplus
 #ifndef min
 #define min(a, b) ((a) < (b) ? (a) : (b))
 #endif
 
 #ifndef max
 #define max(a, b) ((a) > (b) ? (a) : (b))
+#endif
 #endif
 
 #endif /* LIBDPKG_MACROS_H */