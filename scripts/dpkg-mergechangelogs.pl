9eb5c777c584ad91b1dae636282f7572d12c2b36
Raphaël Hertzog <hertzog@debian.org>
2010-04-22 07:56:54 +0200

    dpkg-mergechangelogs: avoid using UTF-8 on translatable strings

--- scripts/dpkg-mergechangelogs.pl
+++ scripts/dpkg-mergechangelogs.pl
@@ -50,7 +50,7 @@ BEGIN {
 sub version {
     printf _g("Debian %s version %s.\n"), $progname, $version;
 
-    printf "\n" . _g("Copyright (C) 2009-2010 Raphaël Hertzog.");
+    printf "\n" . _g("Copyright (C) 2009-2010 Raphael Hertzog.");
 
     printf "\n" . _g(
 "This is free software; see the GNU General Public License version 2 or
