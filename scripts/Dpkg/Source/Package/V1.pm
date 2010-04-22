28d116f4eadc4cf3a49b974d64df19458f6d34ac
Raphaël Hertzog <hertzog@debian.org>
2010-04-22 07:54:30 +0200

    dpkg-source: fix ignore regexp for source format 1.0 when -i is not used
    The variable is defined by default but empty and the check was wrong
    leading all files to be ignored.

--- scripts/Dpkg/Source/Package/V1.pm
+++ scripts/Dpkg/Source/Package/V1.pm
@@ -43,7 +43,7 @@ sub init_options {
     my ($self) = @_;
     # Don't call $self->SUPER::init_options() on purpose, V1.0 has no
     # ignore by default
-    if (defined $self->{'options'}{'diff_ignore_regexp'}) {
+    if ($self->{'options'}{'diff_ignore_regexp'}) {
 	$self->{'options'}{'diff_ignore_regexp'} .= '|(?:^|/)debian/source/local-options$';
     } else {
 	$self->{'options'}{'diff_ignore_regexp'} = '(?:^|/)debian/source/local-options$';
