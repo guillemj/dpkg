2a2c2ef9188d718c914000fc6ab48f33acc54ddb
Guillem Jover <guillem@debian.org>
2010-07-02 11:56:47 +0200

    Move Dpkg.pm and Dpkg/Gettext.pm from dpkg to libdpkg-perl

--- scripts/Dpkg.pm
+++ scripts/Dpkg.pm
@@ -18,12 +18,6 @@ use warnings;
 
 our $VERSION = "1.00";
 
-# This module is the only one provided by dpkg and not dpkg-dev
-#
-# Don't add things here if you don't need them in dpkg itself.
-# If you do, and also use the new stuff in dpkg-dev, you'll have to bump
-# the dependency of dpkg-dev on dpkg.
-
 use base qw(Exporter);
 our @EXPORT = qw($version $progname $admindir $dpkglibdir $pkgdatadir);
 
