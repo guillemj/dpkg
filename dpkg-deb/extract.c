guillemj/dpkg

commit f4f4a1aa8dec678b6f34abcbcb8efd5b75966028

use do_fd_copy when extracting ar files

dpkg-deb/extract.c
@@ -245,13 +245,8 @@ void extracthalf(const char *debar, const char *directory,
     m_pipe(p1);
     if (!(c1= m_fork())) {
       close(p1[0]);
-      if (!(pi= fdopen(p1[1],"w"))) ohshite(_("failed to fdopen p1 in copy"));
-      while (memberlen > 0) {
-        if ((c= getc(ar)) == EOF) readfail(ar,debar,_("member data"));
-        if (putc(c,pi) == EOF) ohshite(_("failed to write to pipe in copy"));
-        memberlen--;
-      }
-      if (fclose(pi) == EOF) ohshite(_("failed to close pipe in copy"));
+      do_fd_copy(fileno(ar), p1[1], memberlen, _("failed to write to pipe in copy"));
+      if (close(p1[1]) == EOF) ohshite(_("failed to close pipe in copy"));
       exit(0);
     }
     close(p1[1]);

commit df863c9b8ddf377b6cb89ac9079770356bd69c65

dpkg-deb: Do not use EOF on file descriptor calls
The code was working because EOF is usually -1, but that's not
guaranteed by any standard. It's also conceptually wrong to mix this
definition from stream based I/O on file dscriptor based I/O.

For the close() call, just check that the return value is not 0.

Regression introduced in f4f4a1aa8dec678b6f34abcbcb8efd5b75966028.

dpkg-deb/extract.c
@@ -257,7 +257,7 @@ void extracthalf(const char *debar, const char *directory,
   if (!c1) {
     close(p1[0]);
     stream_fd_copy(ar, p1[1], memberlen, _("failed to write to pipe in copy"));
-    if (close(p1[1]) == EOF)
+    if (close(p1[1]))
       ohshite(_("failed to close pipe in copy"));
     exit(0);
   }