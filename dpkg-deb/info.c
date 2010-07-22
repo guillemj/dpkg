c24925105318dfa5a8ce19b54be80be69876094d
Guillem Jover <guillem@debian.org>
2010-07-22 18:27:16 +0200

    dpkg-deb: Parse control file into available structure
    This unconfuses the code to make it clear we are not dealing with
    installed package information. It's also needed as part of the
    infrastructure changes for multiarch support.

--- dpkg-deb/info.c
+++ dpkg-deb/info.c
@@ -258,8 +258,9 @@ void do_showinfo(const char* const* argv) {
 
   info_prepare(&argv,&debar,&directory,1);
 
-  parsedb(CONTROLFILE, pdb_ignorefiles, &pkg, NULL, NULL);
-  pkg_format_show(fmt, pkg, &pkg->installed);
+  parsedb(CONTROLFILE, pdb_recordavailable | pdb_rejectstatus | pdb_ignorefiles,
+          &pkg, NULL, NULL);
+  pkg_format_show(fmt, pkg, &pkg->available);
 }
 
 
