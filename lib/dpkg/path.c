guillemj/dpkg

commit f35d66dbc228bc8ad2c5255dee1bf4ecf9ee6e06

libdpkg: Change path_quote_filename size argument type to size_t

lib/dpkg/path.c
@@ -77,7 +77,7 @@ path_skip_slash_dotslash(const char *path)
  * - ukai@debian.or.jp
  */
 char *
-path_quote_filename(char *buf, int size, const char *s)
+path_quote_filename(char *buf, size_t size, const char *s)
 {
 	char *r = buf;
 

lib/dpkg/path.h
@@ -32,7 +32,7 @@ DPKG_BEGIN_DECLS
 
 size_t path_rtrim_slash_slashdot(char *path);
 const char *path_skip_slash_dotslash(const char *path);
-char *path_quote_filename(char *buf, int size, const char *s);
+char *path_quote_filename(char *buf, size_t size, const char *s);
 
 DPKG_END_DECLS
 

commit e759410b2e9eb1c48bb344d2ad17bfc420ddf57a

libdpkg: Fix buffer overflow in path_quote_filename
When the string was longer than the size limit, the loop would continue
as the unsigned size would wrap around 0 to SIZE_MAX, and subsequently
segfault on the out-of-bounds access. Use ssize_t for the size variable.

Regression introduced in f35d66dbc228bc8ad2c5255dee1bf4ecf9ee6e06.

lib/dpkg/path.c
@@ -105,9 +105,10 @@ path_make_temp_template(const char *suffix)
  * but here we escape all 8 bit chars, in order make it simple.
  */
 char *
-path_quote_filename(char *dst, const char *src, size_t size)
+path_quote_filename(char *dst, const char *src, size_t n)
 {
 	char *r = dst;
+	ssize_t size = (ssize_t)n;
 
 	while (size > 0) {
 		switch (*src) {