guillemj/dpkg

commit 58e7276b69be105735eccb3c18c2b28d3a2ec2e5

libdpkg: Ensure parsed packages have a usable arch member
In case the package didn't have an Architecture field, warn and
initialize it to an empty string, so that it can be safely used for
string comparisons, printing, etc.

This fixes a potential segfault when setting the environment variable
DPKG_MAINTSCRIPT_ARCH from a package w/o an Architecture field, which
are a thing from the distant past, but still should be supported.

src/processarc.c
@@ -282,8 +282,7 @@ void process_archive(const char *filename) {
     return;
   }
 
-  if (pkg->available.arch && *pkg->available.arch &&
-      strcmp(pkg->available.arch, "all") &&
+  if (strcmp(pkg->available.arch, "all") &&
       strcmp(pkg->available.arch, native_arch))
     forcibleerr(fc_architecture,
                 _("package architecture (%s) does not match system (%s)"),

dpkg-deb/build.c
@@ -368,12 +368,12 @@ static char *
 pkg_get_pathname(const char *dir, struct pkginfo *pkg)
 {
   char *path;
-  const char *versionstring, *arch;
+  const char *versionstring, *arch_sep;
 
   versionstring = versiondescribe(&pkg->available.version, vdew_never);
-  arch = pkg->available.arch;
+  arch_sep = pkg->available.arch[0] == '\0' ? "" : "_";
   m_asprintf(&path, "%s/%s_%s%s%s%s", dir, pkg->name, versionstring,
-             arch ? "_" : "", arch ? arch : "", DEBEXT);
+             arch_sep, pkg->available.arch, DEBEXT);
 
   return path;
 }

lib/dpkg/parse.c
@@ -168,8 +168,10 @@ pkg_parse_verify(struct parsedb_state *ps,
     parse_ensure_have_field(ps, pkg, &pkgbin->maintainer, "maintainer");
     parse_must_have_field(ps, pkg, pkgbin->version.version, "version");
   }
-  if (ps->flags & pdb_recordavailable)
-    parse_ensure_have_field(ps, pkg, &pkgbin->arch, "architecture");
+
+  /* We always want usable architecture information, so that it can be used
+   * safely on string comparisons and the like. */
+  parse_ensure_have_field(ps, pkg, &pkgbin->arch, "architecture");
 
   /* Check the Config-Version information:
    * If there is a Config-Version it is definitely to be used, but

commit 13637ddfaf473fa5fd67264da9f38841eeed7401

Fix installation of packages without architecture field
Refactoring done in commit 58e7276b69be105735eccb3c18c2b28d3a2ec2e5
introduced a regression: a package without an architecture field
can no longer be installed without --force-architecture.

src/processarc.c
@@ -282,7 +282,8 @@ void process_archive(const char *filename) {
     return;
   }
 
-  if (strcmp(pkg->available.arch, "all") &&
+  if (strcmp(pkg->available.arch, "") &&
+      strcmp(pkg->available.arch, "all") &&
       strcmp(pkg->available.arch, native_arch))
     forcibleerr(fc_architecture,
                 _("package architecture (%s) does not match system (%s)"),