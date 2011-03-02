guillemj/dpkg

commit dc98261980767f258046f8a4c2e020fcdc92c8b6

dpkg-divert: Use DPKG_MAINTSCRIPT_PACKAGE environment variable
Use it as package name on when no --package or --local options have
been specified.

src/divertcmd.c
@@ -705,6 +705,7 @@ int
 main(int argc, const char * const *argv)
 {
 	jmp_buf ejbuf;
+	const char *env_pkgname;
 	int (*actionfunction)(const char *const *argv);
 	int ret;
 
@@ -714,6 +715,10 @@ main(int argc, const char * const *argv)
 
 	standard_startup(&ejbuf);
 	myopt(&argv, cmdinfos);
+
+	env_pkgname = getenv(MAINTSCRIPTPKGENVVAR);
+	if (!opt_pkgname && env_pkgname)
+		setpackage(NULL, env_pkgname);
 
 	if (!cipaction)
 		setaction(&cmdinfo_add, NULL);

man/dpkg-divert.8
@@ -1,4 +1,4 @@
-.TH dpkg\-divert 8 "2008-08-18" "Debian Project" "dpkg utilities"
+.TH dpkg\-divert 8 "2010-10-12" "Debian Project" "dpkg utilities"
 .SH NAME
 dpkg\-divert \- override a package's version of a file
 .
@@ -108,6 +108,12 @@ dpkg-divert \-\-package wibble \-\-divert /usr/bin/example.foo \-\-rename /usr/b
 To remove that diversion:
 .HP
 dpkg-divert \-\-package wibble \-\-rename \-\-remove /usr/bin/example
+.
+.SH ENVIRONMENT
+.TP
+.B DPKG_MAINTSCRIPT_PACKAGE
+If set and the \fI\-\-local\fP and \fI\-\-package\fP options have not been
+specified, \fBdpkg\-divert\fP will use it as the package name.
 .
 .SH FILES
 .TP

commit 8fc97bc94c08f4da2f18c3f4aa43db148bd8f6e2

dpkg-divert: Honour --local when DPKG_MAINTSCRIPT_PACKAGE is present
Regression introduced in commit dc98261980767f258046f8a4c2e020fcdc92c8b6-

Reported-by: Martin Pitt <martin.pitt@ubuntu.com>
Suggested-by: Colin Watson <cjwatson@ubuntu.com>

src/divertcmd.c
@@ -716,7 +716,7 @@ main(int argc, const char * const *argv)
 	myopt(&argv, cmdinfos);
 
 	env_pkgname = getenv("DPKG_MAINTSCRIPT_PACKAGE");
-	if (!opt_pkgname && env_pkgname)
+	if (opt_pkgname_match_any && env_pkgname)
 		setpackage(NULL, env_pkgname);
 
 	if (!cipaction)