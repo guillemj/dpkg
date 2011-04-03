guillemj/dpkg

commit b3eb59cf43c286bb6c906c00ef1cdc0ec21f474d

dpkg: Set the modification time for unpacked symlinks if possible
Use lutimes() conditionally as it might not be widely available.

src/archives.c
@@ -235,16 +235,20 @@ tarobject_set_mtime(struct tar_entry *te, const char *path)
 {
   struct timeval tv[2];
 
-  if (te->type == tar_filetype_symlink)
-    return;
-
   tv[0].tv_sec = currenttime;
   tv[0].tv_usec = 0;
   tv[1].tv_sec = te->mtime;
   tv[1].tv_usec = 0;
 
-  if (utimes(path, tv))
-    ohshite(_("error setting timestamps of `%.255s'"), path);
+  if (te->type == tar_filetype_symlink) {
+#ifdef HAVE_LUTIMES
+    if (lutimes(path, tv))
+      ohshite(_("error setting timestamps of `%.255s'"), path);
+#endif
+  } else {
+    if (utimes(path, tv))
+      ohshite(_("error setting timestamps of `%.255s'"), path);
+  }
 }
 
 static void

configure.ac
@@ -84,7 +84,7 @@ DPKG_CHECK_COMPAT_FUNCS([getopt getopt_long obstack_free \
                          strnlen strerror strsignal asprintf \
                          scandir alphasort unsetenv])
 AC_CHECK_FUNCS([strtoul strtoimax isascii bcopy memcpy setsid getdtablesize \
-                posix_fadvise])
+                lutimes posix_fadvise])
 
 DPKG_MMAP
 

commit 888a5bbbd916d4a2f3a519c1c45519e8614d1782

dpkg: don't ohshite if lutimes() fails with ENOSYS
Glibc implements lutimes() on top of utimensat() which is only provided
by Linux >= 2.6.22. With older kernels it returns ENOSYS.

This regression was introduced by commit
b3eb59cf43c286bb6c906c00ef1cdc0ec21f474d which implemented support for
setting timestamps of symlinks.

Reported-by: Michael Prokop <mika@debian.org>

src/archives.c
@@ -242,7 +242,7 @@ tarobject_set_mtime(struct tar_entry *te, const char *path)
 
   if (te->type == tar_filetype_symlink) {
 #ifdef HAVE_LUTIMES
-    if (lutimes(path, tv))
+    if (lutimes(path, tv) && errno != ENOSYS)
       ohshite(_("error setting timestamps of `%.255s'"), path);
 #endif
   } else {