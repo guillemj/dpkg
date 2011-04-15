guillemj/dpkg

commit 506dd27b56a570e019e90974ea285fe4fd2f8bf8

dpkg-split: Do not slurp the entire part into memory when splitting
The current code is slurping the entire part into memory, which works
fine when the partsize is small but might be unbearable when the
partsize is too big. Use fd_fd_copy() via dpkg_ar_member_put_file()
instead, which will guarantee small constant memory usage.

dpkg-split/split.c
@@ -119,11 +119,11 @@ mksplit(const char *file_src, const char *prefix, size_t maxpartsize,
 	char *package, *version;
 	int nparts, curpart;
 	off_t partsize;
+	off_t last_partsize;
 	char *prefixdir = NULL, *msdos_prefix = NULL;
 	struct varbuf file_dst = VARBUF_INIT;
 	struct varbuf partmagic = VARBUF_INIT;
 	struct varbuf partname = VARBUF_INIT;
-	char *partdata;
 
 	fd_src = open(file_src, O_RDONLY);
 	if (fd_src < 0)
@@ -141,6 +141,9 @@ mksplit(const char *file_src, const char *prefix, size_t maxpartsize,
 	version = deb_field(file_src, "Version");
 
 	partsize = maxpartsize - HEADERALLOWANCE;
+	last_partsize = st.st_size % partsize;
+	if (last_partsize == 0)
+		last_partsize = partsize;
 	nparts = (st.st_size + partsize - 1) / partsize;
 
 	setvbuf(stdout, NULL, _IONBF, 0);
@@ -163,11 +166,8 @@ mksplit(const char *file_src, const char *prefix, size_t maxpartsize,
 		prefix = clean_msdos_filename(msdos_prefix);
 	}
 
-	partdata = m_malloc(partsize);
-
 	for (curpart = 1; curpart <= nparts; curpart++) {
 		int fd_dst;
-		ssize_t partrealsize;
 
 		varbufreset(&file_dst);
 		/* Generate output filename. */
@@ -186,12 +186,10 @@ mksplit(const char *file_src, const char *prefix, size_t maxpartsize,
 			             prefix, curpart, nparts);
 		}
 
-		/* Read data from the original package. */
-		partrealsize = read(fd_src, partdata, partsize);
-		if (partrealsize < 0)
-			ohshite("mksplit: read");
+		if (curpart == nparts)
+			partsize = last_partsize;
 
-		if ((size_t)partrealsize > maxpartsize) {
+		if ((size_t)partsize > maxpartsize) {
 			ohshit(_("Header is too long, making part too long. "
 			       "Your package name or version\n"
 			       "numbers must be extraordinarily long, "
@@ -216,8 +214,8 @@ mksplit(const char *file_src, const char *prefix, size_t maxpartsize,
 
 		/* Write the data part. */
 		varbufprintf(&partname, "data.%d", curpart);
-		dpkg_ar_member_put_mem(file_dst.buf, fd_dst, partname.buf,
-		                       partdata, (size_t)partrealsize);
+		dpkg_ar_member_put_file(file_dst.buf, fd_dst, partname.buf,
+		                        fd_src, partsize);
 		varbufreset(&partname);
 
 		close(fd_dst);
@@ -228,7 +226,6 @@ mksplit(const char *file_src, const char *prefix, size_t maxpartsize,
 	varbuf_destroy(&file_dst);
 	varbuf_destroy(&partname);
 	varbuf_destroy(&partmagic);
-	free(partdata);
 
 	free(prefixdir);
 	free(msdos_prefix);

commit 438e5813caa759a1c6dbfa282e68ab451b425e75

dpkg-split: Correctly set the last part split size
The partsize for each part should be the same, the code was wrongly
setting the partsize to the real size of the data being written.

Regression introduced in commit 506dd27b56a570e019e90974ea285fe4fd2f8bf8.

dpkg-split/split.c
@@ -119,7 +119,7 @@ mksplit(const char *file_src, const char *prefix, off_t maxpartsize,
 	char *package, *version;
 	int nparts, curpart;
 	off_t partsize;
-	off_t last_partsize;
+	off_t cur_partsize, last_partsize;
 	char *prefixdir = NULL, *msdos_prefix = NULL;
 	struct varbuf file_dst = VARBUF_INIT;
 	struct varbuf partmagic = VARBUF_INIT;
@@ -186,9 +186,11 @@ mksplit(const char *file_src, const char *prefix, off_t maxpartsize,
 		}
 
 		if (curpart == nparts)
-			partsize = last_partsize;
+			cur_partsize = last_partsize;
+		else
+			cur_partsize = partsize;
 
-		if (partsize > maxpartsize) {
+		if (cur_partsize > maxpartsize) {
 			ohshit(_("Header is too long, making part too long. "
 			       "Your package name or version\n"
 			       "numbers must be extraordinarily long, "
@@ -215,7 +217,7 @@ mksplit(const char *file_src, const char *prefix, off_t maxpartsize,
 		/* Write the data part. */
 		varbuf_printf(&partname, "data.%d", curpart);
 		dpkg_ar_member_put_file(file_dst.buf, fd_dst, partname.buf,
-		                        fd_src, partsize);
+		                        fd_src, cur_partsize);
 		varbuf_reset(&partname);
 
 		close(fd_dst);