guillemj/dpkg

commit b3f669039f128d715ac7ac71abaeac86f0954112

libdpkg: Switch pri_other to be a fallback namevalue

lib/dpkg/fields.c
@@ -36,7 +36,7 @@
 static int
 convert_string(struct parsedb_state *ps, const struct pkginfo *pigp,
                const char *what, const struct namevalue *ivip,
-               const char *startp, const char **endpp, int otherwise)
+               const char *startp, const char **endpp)
 {
   const char *ep;
   const struct namevalue *nvip;
@@ -45,10 +45,8 @@ convert_string(struct parsedb_state *ps, const struct pkginfo *pigp,
     parse_error(ps, pigp, _("%s is missing"), what);
 
   nvip = namevalue_find_by_name(ivip, startp);
-  if (nvip == NULL) {
-    if (otherwise != -1) return otherwise;
+  if (nvip == NULL)
     parse_error(ps, pigp, _("'%.50s' is not allowed for %s"), startp, what);
-  }
 
   ep = startp + nvip->length;
   while (isspace(*ep))
@@ -136,7 +134,7 @@ f_boolean(struct pkginfo *pigp, struct pkgbin *pifp,
     return;
 
   boolean = convert_string(ps, pigp, _("yes/no in boolean field"),
-                           booleaninfos, value, NULL, -1);
+                           booleaninfos, value, NULL);
   PKGPFIELD(pifp, fip->integer, bool) = boolean;
 }
 
@@ -156,7 +154,7 @@ f_priority(struct pkginfo *pigp, struct pkgbin *pifp,
 {
   if (!*value) return;
   pigp->priority = convert_string(ps, pigp, _("word in `priority' field"),
-                                  priorityinfos, value, NULL, pri_other);
+                                  priorityinfos, value, NULL);
   if (pigp->priority == pri_other) pigp->otherpriority= nfstrsave(value);
 }
 
@@ -175,13 +173,13 @@ f_status(struct pkginfo *pigp, struct pkgbin *pifp,
 
   pigp->want = convert_string(ps, pigp,
                               _("first (want) word in `status' field"),
-                              wantinfos, value, &ep, -1);
+                              wantinfos, value, &ep);
   pigp->eflag = convert_string(ps, pigp,
                                _("second (error) word in `status' field"),
-                               eflaginfos, ep, &ep, -1);
+                               eflaginfos, ep, &ep);
   pigp->status = convert_string(ps, pigp,
                                 _("third (status) word in `status' field"),
-                                statusinfos, ep, NULL, -1);
+                                statusinfos, ep, NULL);
 }
 
 void

lib/dpkg/parsehelp.c
@@ -84,7 +84,7 @@ const struct namevalue priorityinfos[] = {
   NAMEVALUE_DEF("standard",                      pri_standard),
   NAMEVALUE_DEF("optional",                      pri_optional),
   NAMEVALUE_DEF("extra",                         pri_extra),
-  NAMEVALUE_DEF("this is a bug - please report", pri_other),
+  NAMEVALUE_FALLBACK_DEF("this is a bug - please report", pri_other),
   NAMEVALUE_DEF("unknown",                       pri_unknown),
   { .name = NULL }
 };

commit d84b36a81088721b340e52664aa2c3379fea2a9a

libdpkg: Allow again unknown Priority field values
The code was not properly advancing the end of the value string when
reaching the fallback case (on values not known for the Priority field),
and the validation for junk after the first word was failing on the
actual first word.

This change reverts to the previous behaviour of blindly accepting the
whole value string for a field, regardless of it being multiword, when
it's the fallback case.

Regression introduced in commit b3f669039f128d715ac7ac71abaeac86f0954112.

Analysis-by: Raphaël Hertzog <hertzog@debian.org>
Analysis-by: Guillem Jover <guillem@debian.org>

lib/dpkg/fields.c
@@ -48,9 +48,14 @@ parse_nv_next(struct parsedb_state *ps, const struct pkginfo *pigp,
   if (nv == NULL)
     parse_error(ps, pigp, _("'%.50s' is not allowed for %s"), str_start, what);
 
-  str_end = str_start + nv->length;
-  while (isspace(str_end[0]))
-    str_end++;
+  /* We got the fallback value, skip further string validation. */
+  if (nv->length == 0) {
+    str_end = NULL;
+  } else {
+    str_end = str_start + nv->length;
+    while (isspace(str_end[0]))
+      str_end++;
+  }
   *strp = str_end;
 
   return nv->value;
@@ -64,7 +69,7 @@ parse_nv_last(struct parsedb_state *ps, const struct pkginfo *pkg,
   int value;
 
   value = parse_nv_next(ps, pkg, what, nv_head, &str);
-  if (str[0] != '\0')
+  if (str != NULL && str[0] != '\0')
     parse_error(ps, pkg, _("junk after %s"), what);
 
   return value;