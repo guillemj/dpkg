guillemj/dpkg

commit 83b591340ec92cf0e9814e8403da7711cbbc6b46

dpkg: properly remove triggers information during package removal
Before this change, a package removal would not remove the triggers
information from the internal files. When you reinstalled the same
package without any trigger, dpkg would improperly believe that
the package implemented some triggers.

However the triggers were correctly unregistered during a package
upgrade that dropped the triggers.

With this commit, we also remove triggers interest file that are empty and
thus no longer needed.

lib/dpkg/triglib.c
@@ -409,6 +409,7 @@ trk_explicit_interest_change(const char *trig,  struct pkginfo *pkg, int signum)
 	static struct varbuf newfn;
 	char buf[1024];
 	FILE *nf;
+	bool empty = true;
 
 	trk_explicit_start(trig);
 	varbuf_reset(&newfn);
@@ -424,9 +425,23 @@ trk_explicit_interest_change(const char *trig,  struct pkginfo *pkg, int signum)
 		if (!strcmp(buf, pkg->name))
 			continue;
 		fprintf(nf, "%s\n", buf);
+		empty = false;
 	}
-	if (signum > 0)
+	if (signum > 0) {
 		fprintf(nf, "%s\n", pkg->name);
+		empty = false;
+	}
+
+	if (empty) {
+		/* The triggers interest file is no longer needed, drop it */
+		fclose(nf); /* We don't care if it fails */
+		if (unlink(newfn.buf))
+			ohshite(_("cannot remove `%.250s'"), newfn.buf);
+		if (unlink(trk_explicit_fn.buf))
+			ohshite(_("cannot remove `%.250s'"), trk_explicit_fn.buf);
+		dir_sync_path(triggersdir);
+		return;
+	}
 
 	if (ferror(nf))
 		ohshite(_("unable to write new trigger interest file `%.250s'"),
@@ -528,6 +543,14 @@ trig_file_interests_save(void)
 
 	if (filetriggers_edited <= 0)
 		return;
+
+	if (!filetriggers.head) {
+		/* No file trigger left, drop the file */
+		if (unlink(triggersfilefile) && errno != ENOENT)
+			ohshite(_("cannot remove `%.250s'"), triggersfilefile);
+		dir_sync_path(triggersdir);
+		return;
+	}
 
 	nf = fopen(triggersnewfilefile, "w");
 	if (!nf)

src/remove.c
@@ -292,6 +292,10 @@ removal_bulk_remove_files(struct pkginfo *pkg)
     maintainer_script_installed(pkg, POSTRMFILE, "post-removal",
                                 "remove", NULL);
 
+    trig_parse_ci(pkgadminfile(pkg, TRIGGERSCIFILE),
+                  trig_cicb_interest_delete, NULL, pkg);
+    trig_file_interests_save();
+
     debug(dbg_general, "removal_bulk cleaning info directory");
     pkg_infodb_foreach(pkg, removal_bulk_remove_file);
     dir_sync_path(pkgadmindir());

commit 5c32f0c0e4c537fc00bf3ccace8799fe27a522aa

libdpkg: Properly pop the cleanup handler in trk_explicit_interest_change
In case of error all cleanup handlers will get called but the nf FILE
variable might have been already closed, which would cause the
subsequent fclose() to fail, this will not be fatal as the return
value for fclose() in the cleanup handler is not checked, but it's
still not correct.

Refactor trk_explicit_interest_change() into
trk_explicit_interest_remove(), trk_explicit_interest_flush() and
trk_explicit_interest_commit() to reflow the function and make it
clearer by sharing the main code path.

Regression introduced in commit 83b591340ec92cf0e9814e8403da7711cbbc6b46.

lib/dpkg/triglib.c
@@ -404,6 +404,37 @@ trk_explicit_activate_awaiter(struct pkginfo *aw)
 }
 
 static void
+trk_explicit_interest_flush(const char *newfilename, FILE *nf)
+{
+	if (ferror(nf))
+		ohshite(_("unable to write new trigger interest file `%.250s'"),
+		        newfilename);
+	if (fflush(nf))
+		ohshite(_("unable to flush new trigger interest file '%.250s'"),
+		        newfilename);
+	if (fsync(fileno(nf)))
+		ohshite(_("unable to sync new trigger interest file '%.250s'"),
+		        newfilename);
+}
+
+static void
+trk_explicit_interest_commit(const char *newfilename)
+{
+	if (rename(newfilename, trk_explicit_fn.buf))
+		ohshite(_("unable to install new trigger interest file `%.250s'"),
+		        trk_explicit_fn.buf);
+}
+
+static void
+trk_explicit_interest_remove(const char *newfilename)
+{
+	if (unlink(newfilename))
+		ohshite(_("cannot remove `%.250s'"), newfilename);
+	if (unlink(trk_explicit_fn.buf))
+		ohshite(_("cannot remove `%.250s'"), trk_explicit_fn.buf);
+}
+
+static void
 trk_explicit_interest_change(const char *trig,  struct pkginfo *pkg, int signum)
 {
 	static struct varbuf newfn;
@@ -432,34 +463,18 @@ trk_explicit_interest_change(const char *trig,  struct pkginfo *pkg, int signum)
 		empty = false;
 	}
 
-	if (empty) {
-		/* The triggers interest file is no longer needed, drop it */
-		fclose(nf); /* We don't care if it fails */
-		if (unlink(newfn.buf))
-			ohshite(_("cannot remove `%.250s'"), newfn.buf);
-		if (unlink(trk_explicit_fn.buf))
-			ohshite(_("cannot remove `%.250s'"), trk_explicit_fn.buf);
-		dir_sync_path(triggersdir);
-		return;
-	}
+	if (!empty)
+		trk_explicit_interest_flush(newfn.buf, nf);
 
-	if (ferror(nf))
-		ohshite(_("unable to write new trigger interest file `%.250s'"),
-		        newfn.buf);
-	if (fflush(nf))
-		ohshite(_("unable to flush new trigger interest file '%.250s'"),
-		        newfn.buf);
-	if (fsync(fileno(nf)))
-		ohshite(_("unable to sync new trigger interest file '%.250s'"),
-		        newfn.buf);
 	pop_cleanup(ehflag_normaltidy);
 	if (fclose(nf))
 		ohshite(_("unable to close new trigger interest file `%.250s'"),
 		        newfn.buf);
 
-	if (rename(newfn.buf, trk_explicit_fn.buf))
-		ohshite(_("unable to install new trigger interest file `%.250s'"),
-		        trk_explicit_fn.buf);
+	if (empty)
+		trk_explicit_interest_remove(newfn.buf);
+	else
+		trk_explicit_interest_commit(newfn.buf);
 
 	dir_sync_path(triggersdir);
 }