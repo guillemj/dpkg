guillemj/dpkg

commit 83b591340ec92cf0e9814e8403da7711cbbc6b46

dpkg: properly remove triggers information during package removal
Before this change, a package removal would not remove the triggers
information from the internal files. When you reinstalled the same
package without any trigger, dpkg would improperly believe that
the package implemented some triggers.

However the triggers were correctly unregistered during a package
upgrade that dropped the triggers.

With this commit, we also remove triggers interest file that are empty and
thus no longer needed.

lib/dpkg/triglib.c
@@ -409,6 +409,7 @@ trk_explicit_interest_change(const char *trig,  struct pkginfo *pkg, int signum)
 	static struct varbuf newfn;
 	char buf[1024];
 	FILE *nf;
+	bool empty = true;
 
 	trk_explicit_start(trig);
 	varbuf_reset(&newfn);
@@ -424,9 +425,23 @@ trk_explicit_interest_change(const char *trig,  struct pkginfo *pkg, int signum)
 		if (!strcmp(buf, pkg->name))
 			continue;
 		fprintf(nf, "%s\n", buf);
+		empty = false;
 	}
-	if (signum > 0)
+	if (signum > 0) {
 		fprintf(nf, "%s\n", pkg->name);
+		empty = false;
+	}
+
+	if (empty) {
+		/* The triggers interest file is no longer needed, drop it */
+		fclose(nf); /* We don't care if it fails */
+		if (unlink(newfn.buf))
+			ohshite(_("cannot remove `%.250s'"), newfn.buf);
+		if (unlink(trk_explicit_fn.buf))
+			ohshite(_("cannot remove `%.250s'"), trk_explicit_fn.buf);
+		dir_sync_path(triggersdir);
+		return;
+	}
 
 	if (ferror(nf))
 		ohshite(_("unable to write new trigger interest file `%.250s'"),
@@ -528,6 +543,14 @@ trig_file_interests_save(void)
 
 	if (filetriggers_edited <= 0)
 		return;
+
+	if (!filetriggers.head) {
+		/* No file trigger left, drop the file */
+		if (unlink(triggersfilefile) && errno != ENOENT)
+			ohshite(_("cannot remove `%.250s'"), triggersfilefile);
+		dir_sync_path(triggersdir);
+		return;
+	}
 
 	nf = fopen(triggersnewfilefile, "w");
 	if (!nf)

src/remove.c
@@ -292,6 +292,10 @@ removal_bulk_remove_files(struct pkginfo *pkg)
     maintainer_script_installed(pkg, POSTRMFILE, "post-removal",
                                 "remove", NULL);
 
+    trig_parse_ci(pkgadminfile(pkg, TRIGGERSCIFILE),
+                  trig_cicb_interest_delete, NULL, pkg);
+    trig_file_interests_save();
+
     debug(dbg_general, "removal_bulk cleaning info directory");
     pkg_infodb_foreach(pkg, removal_bulk_remove_file);
     dir_sync_path(pkgadmindir());

commit b8038ef4721d53bb037583b9355a1f00dbcdc740

libdpkg: Properly reset filetriggers_edited in trig_file_interests_save
The function was not resetting filetriggers_edited when no triggers were
left, and duplicated part of the existing functionality.

Refactor into trig_file_interests_remove() and
trig_file_interests_update() to make them use the same main code path.

Regression introduced in commit 83b591340ec92cf0e9814e8403da7711cbbc6b46.

lib/dpkg/triglib.c
@@ -550,22 +550,18 @@ edited:
 	filetriggers_edited = 1;
 }
 
-void
-trig_file_interests_save(void)
+static void
+trig_file_interests_remove(void)
+{
+	if (unlink(triggersfilefile) && errno != ENOENT)
+		ohshite(_("cannot remove `%.250s'"), triggersfilefile);
+}
+
+static void
+trig_file_interests_update(void)
 {
 	struct trigfileint *tfi;
 	FILE *nf;
-
-	if (filetriggers_edited <= 0)
-		return;
-
-	if (!filetriggers.head) {
-		/* No file trigger left, drop the file */
-		if (unlink(triggersfilefile) && errno != ENOENT)
-			ohshite(_("cannot remove `%.250s'"), triggersfilefile);
-		dir_sync_path(triggersdir);
-		return;
-	}
 
 	nf = fopen(triggersnewfilefile, "w");
 	if (!nf)
@@ -594,6 +590,18 @@ trig_file_interests_save(void)
 	if (rename(triggersnewfilefile, triggersfilefile))
 		ohshite(_("unable to install new file triggers file as `%.250s'"),
 		        triggersfilefile);
+}
+
+void
+trig_file_interests_save(void)
+{
+	if (filetriggers_edited <= 0)
+		return;
+
+	if (!filetriggers.head)
+		trig_file_interests_remove();
+	else
+		trig_file_interests_update();
 
 	dir_sync_path(triggersdir);
 