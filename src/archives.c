guillemj/dpkg

commit 00e5640a99be03aba40c9e08a663b90d8f8aa797

dpkg (1.13.2) experimental; urgency=low
  * md5sum has been removed, the coreutils or textutils version will be
    diverted to /usr/bin/md5sum.  Closes: #6633, #136110.
    The following bugs are filed against the dpkg md5sum, so no longer apply.
    Closes: #95755, #193877, #223381, #264195, #270241, #286632,
  * Take Replaces into account when installing packages; don't issue a
    "trying to overwrite" error if the file that already exists is in
    a package that Replaces the one being installed.
    Closes: #164595, #184635, #277890.
  * Allow actions and status changes to be logged to a file.  Disabled by
    default, uncomment line in /etc/dpkg/dpkg.cfg to enable.
    Closes: #957, #53376, #77109, #143882, #284499.
  * Don't truncate output of 'dpkg -l' when stdout is not a tty.
    Closes: #92263, #253860, #258608, #261822, #282790.
  * Fix further compilation problems with gcc 4.0.  Closes: #299699
  * Handle tar files without trailing slash in directory names.
    Closes: #287152.
  * Output arguments to maintainer scripts with -D2.  Closes: #237684,
    #296030.

  * Architecture Support:
    - Added ppc64.  Closes: #263743.
    - Split archtable into cputable and ostable, archtable is retained
      for compatibility with other packages that might use it only.
    - dpkg-architecture no longer canonises -t argument.  Closes: #173205.
    - dpkg-architecture output includes new DEB_*_ARCH_OS and
      DEB_*_ARCH_CPU variables that contain the Debian system and CPU
      names respectively.
    - dpkg-architecture outputs (mostly) correct GNU system names now,
      in particular this means that it will output "linux-gnu" instead
      of "linux".  You should use the new _ARCH_OS variables instead.
  * Documentation:
    - Add examples to dpkg-divert(8).  Closes: #291816.
    - Correct typo in dpkg-architecture(1).  Closes: #299090.

 -- Scott James Remnant <scott@netsplit.com>  Fri, 18 Mar 2005 16:21:32 +0000

src/archives.c
@@ -314,11 +314,11 @@ int tarobject(struct TarInfo *ti) {
   const char *usename;
     
   struct tarcontext *tc= (struct tarcontext*)ti->UserData;
-  int statr, fd, i, existingdirectory;
+  int statr, fd, i, existingdirectory, keepexisting;
   size_t r;
   struct stat stab, stabd;
   char databuf[TARBLKSZ];
-  struct fileinlist *nifd;
+  struct fileinlist *nifd, **oldnifd;
   struct pkginfo *divpkg, *otherpkg;
   struct filepackages *packageslump;
   mode_t am;
@@ -329,6 +329,7 @@ int tarobject(struct TarInfo *ti) {
    * The trailing / put on the end of names in tarfiles has already
    * been stripped by TarExtractor (lib/tarfn.c).
    */
+  oldnifd= tc->newfilesp;
   nifd= obstack_alloc(&tar_obs, sizeof(struct fileinlist));
   nifd->namenode= findnamenode(ti->Name, 0);
   nifd->next= 0; *tc->newfilesp= nifd; tc->newfilesp= &nifd->next;
@@ -423,6 +424,7 @@ int tarobject(struct TarInfo *ti) {
     ohshit(_("archive contained object `%.255s' of unknown type 0x%x"),ti->Name,ti->Type);
   }
 
+  keepexisting= 0;
   if (!existingdirectory) {
     for (packageslump= nifd->namenode->packages;
          packageslump;
@@ -442,7 +444,12 @@ int tarobject(struct TarInfo *ti) {
           if (otherpkg == divpkg || tc->pkg == divpkg) continue;
         }
         /* Nope ?  Hmm, file conflict, perhaps.  Check Replaces. */
-        if (otherpkg->clientdata->replacingfilesandsaid) continue;
+	switch (otherpkg->clientdata->replacingfilesandsaid) {
+	case 2:
+	  keepexisting= 1;
+	case 1:
+	  continue;
+	}
         /* Is the package with the conflicting file in the `config files
          * only' state ?  If so it must be a config file and we can
          * silenty take it over.
@@ -453,6 +460,11 @@ int tarobject(struct TarInfo *ti) {
         if (does_replace(tc->pkg,&tc->pkg->available,otherpkg)) {
           printf(_("Replacing files in old package %s ...\n"),otherpkg->name);
           otherpkg->clientdata->replacingfilesandsaid= 1;
+	} else if (does_replace(otherpkg,&otherpkg->installed,tc->pkg)) {
+	  printf(_("Replaced by files in installed package %s ...\n"),
+		 otherpkg->name);
+          otherpkg->clientdata->replacingfilesandsaid= 2;
+	  keepexisting = 1;
         } else {
           if (!statr && S_ISDIR(stab.st_mode)) {
             forcibleerr(fc_overwritedir, _("trying to overwrite directory `%.250s' "
@@ -480,6 +492,23 @@ int tarobject(struct TarInfo *ti) {
   ensure_pathname_nonexisting(fnametmpvb.buf);
 
   if (existingdirectory) return 0;
+  if (keepexisting) {
+    obstack_free(&tar_obs, nifd);
+    tc->newfilesp= oldnifd;
+    *oldnifd = 0;
+
+    /* We need to advance the tar file to the next object, so read the
+     * file data and set it to oblivion.
+     */
+    if ((ti->Type == NormalFile0) || (ti->Type == NormalFile1)) {
+      char fnamebuf[256];
+      fd_null_copy(tc->backendpipe, ti->Size, _("gobble replaced file `%.255s'"),quote_filename(fnamebuf,256,ti->Name));
+      r= ti->Size % TARBLKSZ;
+      if (r > 0) r= safe_read(tc->backendpipe,databuf,TARBLKSZ - r);
+    }
+
+    return 0;
+  }
 
   /* Now we start to do things that we need to be able to undo
    * if something goes wrong.

commit b6ca9bcdb5887e30fe65ecd2105da9dcd55a351c

dpkg: Unpack missing shared directories when the package is replaced
When unpacking a directory missing in the file system with that path
shared by at least one other package, from a package that is Replaced
by other packages, assume the missing path is a shared directory and
avoid setting ‘keepexisting’ and as such returning prematurely from
tarobject(), because in that case we need to recreate the lost
directory. As a side effect this also prevents printing the message
“Replaced by files in installed package” which is bogus in this case,
as directories are always shared.

Regression introduced in commit 00e5640a99be03aba40c9e08a663b90d8f8aa797.

Closes: #631808

src/archives.c
@@ -583,6 +583,15 @@ tarobject(void *ctx, struct tar_entry *ti)
           continue;
       }
 
+      /* If the new object is a directory and the previous object does
+       * not exist assume it's also a directory and skip further checks.
+       * XXX: Ideally with more information about the installed files we
+       * could perform more clever checks. */
+      if (statr != 0 && ti->type == tar_filetype_dir) {
+        debug(dbg_eachfile, "tarobject ... assuming shared directory");
+        continue;
+      }
+
       /* Nope? Hmm, file conflict, perhaps. Check Replaces. */
       switch (otherpkg->clientdata->replacingfilesandsaid) {
       case 2:
@@ -636,6 +645,7 @@ tarobject(void *ctx, struct tar_entry *ti)
         nifd->namenode->flags &= ~fnnf_new_inarchive;
         keepexisting = true;
       } else {
+        /* At this point we are replacing something without a Replaces. */
         if (!statr && S_ISDIR(stab.st_mode)) {
           forcibleerr(fc_overwritedir,
                       _("trying to overwrite directory '%.250s' "
@@ -644,16 +654,12 @@ tarobject(void *ctx, struct tar_entry *ti)
                       versiondescribe(&otherpkg->installed.version,
                                       vdew_nonambig));
         } else {
-          /* At this point we are replacing something without a Replaces.
-           * If the new object is a directory and the previous object does
-           * not exist assume it's also a directory and don't complain. */
-          if (!(statr && ti->type == tar_filetype_dir))
-            forcibleerr(fc_overwrite,
-                        _("trying to overwrite '%.250s', "
-                          "which is also in package %.250s %.250s"),
-                        nifd->namenode->name, otherpkg->name,
-                        versiondescribe(&otherpkg->installed.version,
-                                        vdew_nonambig));
+          forcibleerr(fc_overwrite,
+                      _("trying to overwrite '%.250s', "
+                        "which is also in package %.250s %.250s"),
+                      nifd->namenode->name, otherpkg->name,
+                      versiondescribe(&otherpkg->installed.version,
+                                      vdew_nonambig));
         }
       }
     }