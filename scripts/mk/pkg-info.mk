c10517e66a9d62b4a2be9b08f7d3f42378c46ff9
Raphaël Hertzog <hertzog@debian.org>
2011-09-21 08:48:37 +0200

    scripts/mk/pkg-info.mk: adjust variable names
    Rename DEB_VERSION_NOREV and DEB_VERSION_NOEPOCH to
    DEB_VERSION_EPOCH_UPSTREAM and DEB_VERSION_UPSTREAM_REVISION to define
    them by inclusion rather than by exclusion.
    
    Rename DEB_SOURCE_PACKAGE into the less verbose DEB_SOURCE.
    
    Suggested-by: Guillem Jover <guillem@debian.org>

--- scripts/mk/pkg-info.mk
+++ scripts/mk/pkg-info.mk
@@ -1,17 +1,17 @@
 # Makefile snippet defining the following variables:
 #
-# DEB_SOURCE_PACKAGE: the source package name
-# DEB_VERSION: the full version of the package
-# DEB_VERSION_NOREV: the package's version without the Debian revision
-# DEB_VERSION_NOEPOCH: the package's version without the Debian epoch
+# DEB_SOURCE: the source package name
+# DEB_VERSION: the full version of the package (epoch + upstream vers. + revision)
+# DEB_VERSION_EPOCH_UPSTREAM: the package's version without the Debian revision
+# DEB_VERSION_UPSTREAM_REVISION: the package's version without the Debian epoch
 # DEB_VERSION_UPSTREAM: the package's upstream version
 # DEB_DISTRIBUTION: the first distribution of the current entry in debian/changelog
 
 dpkg_late_eval ?= $(or $(value DPKG_CACHE_$(1)),$(eval DPKG_CACHE_$(1) := $(shell $(2)))$(value DPKG_CACHE_$(1)))
 
-DEB_SOURCE_PACKAGE = $(call dpkg_late_eval,DEB_SOURCE_PACKAGE,awk '/^Source: / { print $$2 }' debian/control)
+DEB_SOURCE = $(call dpkg_late_eval,DEB_SOURCE,awk '/^Source: / { print $$2 }' debian/control)
 DEB_VERSION = $(call dpkg_late_eval,DEB_VERSION,dpkg-parsechangelog | awk '/^Version: / { print $$2 }')
-DEB_VERSION_NOREV = $(call dpkg_late_eval,DEB_VERSION_NOREV,echo '$(DEB_VERSION)' | sed -e 's/-[^-]*$$//')
-DEB_VERSION_NOEPOCH = $(call dpkg_late_eval,DEB_VERSION_NOEPOCH,echo '$(DEB_VERSION)' | sed -e 's/^[0-9]*://')
-DEB_VERSION_UPSTREAM = $(call dpkg_late_eval,DEB_VERSION_UPSTREAM,echo '$(DEB_VERSION_NOREV)' | sed -e 's/^[0-9]*://')
+DEB_VERSION_EPOCH_UPSTREAM = $(call dpkg_late_eval,DEB_VERSION_EPOCH_UPSTREAM,echo '$(DEB_VERSION)' | sed -e 's/-[^-]*$$//')
+DEB_VERSION_UPSTREAM_REVISION = $(call dpkg_late_eval,DEB_VERSION_UPSTREAM_REVISION,echo '$(DEB_VERSION)' | sed -e 's/^[0-9]*://')
+DEB_VERSION_UPSTREAM = $(call dpkg_late_eval,DEB_VERSION_UPSTREAM,echo '$(DEB_VERSION_EPOCH_UPSTREAM)' | sed -e 's/^[0-9]*://')
 DEB_DISTRIBUTION = $(call dpkg_late_eval,DEB_DISTRIBUTION,dpkg-parsechangelog | awk '/^Distribution: / { print $$2 }')
