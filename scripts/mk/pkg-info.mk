cfba9b80fea680d561671713b78044c365a14924
Raphaël Hertzog <hertzog@debian.org>
2011-09-21 22:44:00 +0200

    scripts/mk/pkg-info.mk: DEB_DISTRIBUTION returns all listed distributions
    Update DEB_DISTRIBUTION to return all distributions listed in the current
    changelog entry instead of only the first one.

--- scripts/mk/pkg-info.mk
+++ scripts/mk/pkg-info.mk
@@ -5,7 +5,7 @@
 # DEB_VERSION_EPOCH_UPSTREAM: the package's version without the Debian revision
 # DEB_VERSION_UPSTREAM_REVISION: the package's version without the Debian epoch
 # DEB_VERSION_UPSTREAM: the package's upstream version
-# DEB_DISTRIBUTION: the first distribution of the current entry in debian/changelog
+# DEB_DISTRIBUTION: the distribution(s) listed in the current entry of debian/changelog
 
 dpkg_late_eval ?= $(or $(value DPKG_CACHE_$(1)),$(eval DPKG_CACHE_$(1) := $(shell $(2)))$(value DPKG_CACHE_$(1)))
 
@@ -14,4 +14,4 @@ DEB_VERSION = $(call dpkg_late_eval,DEB_VERSION,dpkg-parsechangelog | awk '/^Ver
 DEB_VERSION_EPOCH_UPSTREAM = $(call dpkg_late_eval,DEB_VERSION_EPOCH_UPSTREAM,echo '$(DEB_VERSION)' | sed -e 's/-[^-]*$$//')
 DEB_VERSION_UPSTREAM_REVISION = $(call dpkg_late_eval,DEB_VERSION_UPSTREAM_REVISION,echo '$(DEB_VERSION)' | sed -e 's/^[0-9]*://')
 DEB_VERSION_UPSTREAM = $(call dpkg_late_eval,DEB_VERSION_UPSTREAM,echo '$(DEB_VERSION_EPOCH_UPSTREAM)' | sed -e 's/^[0-9]*://')
-DEB_DISTRIBUTION = $(call dpkg_late_eval,DEB_DISTRIBUTION,dpkg-parsechangelog | awk '/^Distribution: / { print $$2 }')
+DEB_DISTRIBUTION = $(call dpkg_late_eval,DEB_DISTRIBUTION,dpkg-parsechangelog | sed -n -e '/^Distribution: / { s/^Distribution: //; p }')
