guillemj/dpkg

commit 383247f0d2dfbd3d0d6388f87869ac1abaa8b7cd

Do not use absolute paths for programs in perl and shell code
The location of programs on the filesystem is not standardized, and as
such using absolute paths is not a portable assumption to make. This
causes test suite errors on at least Mac OS X.

Use just the program name on perl system() calls, when needing the full
path use Dpkg::Path::find_command() in perl code and “which” in shell
code.

Reported-by: Stuart Ambler <stuart@zulazon.com>

scripts/Dpkg/Source/Package.pm
@@ -44,7 +44,7 @@ use Dpkg::Checksums;
 use Dpkg::Version;
 use Dpkg::Compression;
 use Dpkg::Exit;
-use Dpkg::Path qw(check_files_are_the_same);
+use Dpkg::Path qw(check_files_are_the_same find_command);
 use Dpkg::IPC;
 use Dpkg::Vendor qw(run_vendor_hook);
 
@@ -342,9 +342,9 @@ sub check_signature {
     my ($self) = @_;
     my $dsc = $self->get_filename();
     my @exec;
-    if (-x '/usr/bin/gpgv') {
+    if (find_command('gpgv')) {
         push @exec, "gpgv";
-    } elsif (-x '/usr/bin/gpg') {
+    } elsif (find_command('gpg') {
         push @exec, "gpg", "--no-default-keyring", "-q", "--verify";
     }
     if (scalar(@exec)) {

dselect/methods/Debian/Dselect/Ftp.pm
@@ -58,7 +58,7 @@ sub store_config {
 
 sub view_mirrors {
   if (-f '/usr/lib/dpkg/methods/ftp/README.mirrors.txt') {
-    system('/usr/bin/pager', '/usr/lib/dpkg/methods/ftp/README.mirrors.txt');
+    system('pager', '/usr/lib/dpkg/methods/ftp/README.mirrors.txt');
   } elsif (-f '/usr/lib/dpkg/methods/ftp/README.mirrors.txt.gz') {
     system('gzip -dc /usr/lib/dpkg/methods/ftp/README.mirrors.txt.gz | pager');
   } else {

dselect/methods/disk/setup
@@ -241,7 +241,7 @@ then
 			response="$defaultnfsserver"
 		fi
 		if [ -z "$response" ]; then continue; fi
-		if [ -x /usr/bin/rpcinfo ]
+		if [ -x "`which rpcinfo`" ]
 		then
 			if rpcinfo -u "$response" mountd | grep -q 'ready'
 			then
@@ -249,7 +249,7 @@ then
 			else
 				echo "$response appears not to be an NFS server."
 			fi
-		elif [ -x /bin/ping ]
+		elif [ -x "`which ping`" ]
 		then
 			if ping -q -c 1 "$response" | grep -q ', 1 packets received'
 			then
@@ -259,7 +259,7 @@ then
 			fi
 		else
 			echo \
-"(I can't check that now because there is no /usr/bin/rpcinfo or /bin/ping.)"
+"(I can't check that now because there is no rpcinfo or ping.)"
 			nfsserver="$response"
 		fi
 	done

dselect/methods/ftp/install
@@ -131,7 +131,7 @@ procstatus();
 sub dcmpvers {
     my($a, $p, $b) = @_;
     my ($r);
-    $r = system("/usr/bin/dpkg", "--compare-versions", "$a", "$p", "$b");
+    $r = system("dpkg", "--compare-versions", "$a", "$p", "$b");
     $r = $r/256;
     if ($r == 0) {
 	return 1;

dselect/methods/multicd/install
@@ -53,7 +53,7 @@ xit=1
 do_umount() {
 	if [ "$method" = "multi_mount" ]
 	then
-		echo /bin/true
+		echo true
 		return
 	fi
 	if [ -n "$umount" ]; then
@@ -66,7 +66,7 @@ do_mount() {
 
 	if [ "$method" = "multi_mount" ]
 	then
-		echo /bin/true
+		echo true
 		return
 	fi
 

dselect/methods/multicd/setup
@@ -282,7 +282,7 @@ then
 			response="$defaultnfsserver"
 		fi
 		if [ -z "$response" ]; then continue; fi
-		if [ -x /usr/bin/rpcinfo ]
+		if [ -x "`which rpcinfo`" ]
 		then
 			if rpcinfo -u "$response" mountd >/dev/null
 			then
@@ -290,7 +290,7 @@ then
 			else
 				echo "$response appears not to be an NFS server."
 			fi
-		elif [ -x /bin/ping ]
+		elif [ -x "`which ping`" ]
 		then
 			if ping -q -c 1 "$response" | grep -q ', 1 packets received'
 			then
@@ -300,7 +300,7 @@ then
 			fi
 		else
 			echo \
-"(I can't check that now because there is no /usr/bin/rpcinfo or /bin/ping.)"
+"(I can't check that now because there is no rpcinfo or ping.)"
 			nfsserver="$response"
 		fi
 	done

scripts/Dpkg/Vendor/Ubuntu.pm
@@ -26,6 +26,7 @@ our $VERSION = "0.01";
 
 use Dpkg::ErrorHandling;
 use Dpkg::Gettext;
+use Dpkg::Path qw(find_command);
 use Dpkg::Control::Types;
 use Dpkg::BuildOptions;
 use Dpkg::Arch qw(debarch_eq get_host_arch);
@@ -119,7 +120,7 @@ sub run_hook {
 	if (defined $hardening) {
 	    my $flag = 'DEB_BUILD_HARDENING';
 	    if ($hardening ne "0") {
-		if (! -x '/usr/bin/hardened-cc') {
+		if (!find_command('hardened-cc')) {
 		    syserr(_g("'hardening' flag found but 'hardening-wrapper' not installed"));
 		}
 		if ($hardening ne "1") {

scripts/dpkg-gensymbols.pl
@@ -27,7 +27,7 @@ use Dpkg::Gettext;
 use Dpkg::ErrorHandling;
 use Dpkg::Control::Info;
 use Dpkg::Changelog::Parse;
-use Dpkg::Path qw(check_files_are_the_same);
+use Dpkg::Path qw(check_files_are_the_same find_command);
 
 textdomain("dpkg-dev");
 
@@ -306,7 +306,7 @@ unless ($quiet) {
 	my $diff_label = sprintf("%s (%s_%s_%s)",
 	($ref_symfile->{file}) ? $ref_symfile->{file} : "new_symbol_file",
 	$oppackage, $sourceversion, $host_arch);
-	system("diff", "-u", "-L", $diff_label, $a, $b) if -x "/usr/bin/diff";
+	system("diff", "-u", "-L", $diff_label, $a, $b) if find_command("diff");
     }
 }
 exit($exitcode);

commit ffd6804def0f898cb61bd421330a2920dd960a65

Dpkg::Source::Package: Add missing parenthesis in find_command() call
Regression introduced in commit 383247f0d2dfbd3d0d6388f87869ac1abaa8b7cd.

Closes: #652113

Reported-by: Vincent Danjean <vdanjean@debian.org>

scripts/Dpkg/Source/Package.pm
@@ -344,7 +344,7 @@ sub check_signature {
     my @exec;
     if (find_command('gpgv')) {
         push @exec, "gpgv";
-    } elsif (find_command('gpg') {
+    } elsif (find_command('gpg')) {
         push @exec, "gpg", "--no-default-keyring", "-q", "--verify";
     }
     if (scalar(@exec)) {