guillemj/dpkg

commit 78ffee002f47daed249e62778587383f2cd2a21a

Dpkg::BuildFlags: update embedded API changelog

scripts/Dpkg/BuildFlags.pm
@@ -390,6 +390,10 @@ the prepend operation everywhere.
 New method: $bf->load_maintainer_config() that update the build flags
 based on the package maintainer directives.
 
+=head Version 1.02
+
+New methods: $bf->get_features(), $bf->has_features(), $bf->set_feature().
+
 =head1 AUTHOR
 
 Raphaël Hertzog <hertzog@debian.org>

commit b1a377f0de1c884b939e10d0f05c3527c89d5ec6

Dpkg::BuildFlags: Use =head2 instead of =head
This fixes a build failure with Test::Pod installed.

Regression introduced in commit 78ffee002f47daed249e62778587383f2cd2a21a.

scripts/Dpkg/BuildFlags.pm
@@ -390,7 +390,7 @@ the prepend operation everywhere.
 New method: $bf->load_maintainer_config() that update the build flags
 based on the package maintainer directives.
 
-=head Version 1.02
+=head2 Version 1.02
 
 New methods: $bf->get_features(), $bf->has_features(), $bf->set_feature().
 