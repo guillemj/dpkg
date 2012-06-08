guillemj/dpkg

commit 4ce011f68f8e499882db638c065008ce4015cd38

libdpkg: Use trig_file_activate_byname() instead of ad-hoc code
The trk_file functions were open coding this function, use it instead.

lib/dpkg/triglib.c
@@ -533,8 +533,6 @@ ok:
 	filetriggers_edited = 0;
 }
 
-static struct filenamenode *filetriggers_activating;
-
 void
 trig_file_activate_byname(const char *trig, struct pkginfo *aw)
 {
@@ -558,15 +556,12 @@ trig_file_activate(struct filenamenode *trig, struct pkginfo *aw)
 static void
 trk_file_activate_start(void)
 {
-	filetriggers_activating = trigh.namenode_find(trig_activating_name, 1);
 }
 
 static void
 trk_file_activate_awaiter(struct pkginfo *aw)
 {
-	if (!filetriggers_activating)
-		return;
-	trig_file_activate(filetriggers_activating, aw);
+	trig_file_activate_byname(trig_activating_name, aw);
 }
 
 static void

commit c39373bdc04c83e33f92bf374236bf04a894ae93

libdpkg: Fix explicit file trigger activation
The lex parser passes a pointer to a buffer with the text, and as such
if we just keep a copy of the pointer the contents will change over the
parsing process. Instead copy the contents on trk_file_activate_start(),
and use that copy later on.

Regression introduced in commit 4ce011f68f8e499882db638c065008ce4015cd38.

Closes: #676684

lib/dpkg/triglib.c
@@ -587,15 +587,18 @@ trig_path_activate_byname(const char *trig, struct pkginfo *aw)
 	trig_file_activate_parents(trig, aw);
 }
 
+static const char *trk_file_trig;
+
 static void
 trk_file_activate_start(void)
 {
+	trk_file_trig = nfstrsave(trig_activating_name);
 }
 
 static void
 trk_file_activate_awaiter(struct pkginfo *aw)
 {
-	trig_path_activate_byname(trig_activating_name, aw);
+	trig_path_activate_byname(trk_file_trig, aw);
 }
 
 static void