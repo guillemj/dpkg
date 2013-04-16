4d276f405de3cef010ad6429ec0c224d50606ca6
Guillem Jover <guillem@debian.org>
2013-04-16 22:51:39 +0200

    Debian::Dselect::Ftp: Add a package declaration

--- dselect/methods/Debian/Dselect/Ftp.pm
+++ dselect/methods/Debian/Dselect/Ftp.pm
@@ -11,6 +11,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+package Debian::Dselect::Ftp;
+
 use Net::FTP;
 use Exporter;
 use Data::Dumper;
