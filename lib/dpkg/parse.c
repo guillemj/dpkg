guillemj/dpkg

commit 86823d5792af8328107c3d86e486d2cf373da128

dpkg: change behaviour of --merge-avail
With this change, dpkg --merge-avail does not update a package's
information if the version provided is older than the one already listed
in the available file.

lib/dpkg/parse.c
@@ -328,6 +328,11 @@ int parsedb(const char *filename, enum parsedbflags flags,
 
     pigp= findpackage(newpig.name);
     pifp= (flags & pdb_recordavailable) ? &pigp->available : &pigp->installed;
+
+    if ((flags & pdb_ignoreolder) &&
+	versioncompare(&newpifp->version, &pifp->version) < 0)
+      continue;
+
     if (!pifp->valid) blankpackageperfile(pifp);
 
     /* Copy the priority and section across, but don't overwrite existing

lib/dpkg/dpkg-db.h
@@ -338,7 +338,8 @@ enum parsedbflags {
   pdb_recordavailable   =001, /* Store in `available' in-core structures, not `status' */
   pdb_rejectstatus      =002, /* Throw up an error if `Status' encountered             */
   pdb_weakclassification=004, /* Ignore priority/section info if we already have any   */
-  pdb_ignorefiles       =010  /* Ignore files info if we already have them             */
+  pdb_ignorefiles       =010, /* Ignore files info if we already have them             */
+  pdb_ignoreolder       =020  /* Ignore packages with older versions already read      */
 };
 
 const char *illegal_packagename(const char *p, const char **ep);

src/update.c
@@ -84,7 +84,8 @@ void updateavailable(const char *const *argv) {
     parsedb(vb.buf, pdb_recordavailable | pdb_rejectstatus, NULL, NULL, NULL);
 
   if (cipaction->arg != act_avclear)
-    count += parsedb(sourcefile, pdb_recordavailable | pdb_rejectstatus,
+    count += parsedb(sourcefile,
+		     pdb_recordavailable | pdb_rejectstatus | pdb_ignoreolder,
                      NULL, NULL, NULL);
 
   if (!f_noact) {

commit 71d6e1b6c16df782dbe2736a285e7bd9040becd4

libdpkg: Only ignore older packages if the existing version is informative
When ignoring older packages (either by explicit request, or when
parsing available files), we should not ignore them when the existing
version is empty, because then we would consider that the empty version
(equivalent to "0-0") is newer than "0~0-0" for example.

This affects any program using libdpkg to parse the available file.

Regression introduced in commit 86823d5792af8328107c3d86e486d2cf373da128.

Closes: #676664

lib/dpkg/parse.c
@@ -742,6 +742,7 @@ int parsedb(const char *filename, enum parsedbflags flags,
       db_pkgbin = &db_pkg->installed;
 
     if (((flags & pdb_ignoreolder) || ps.type == pdb_file_available) &&
+        dpkg_version_is_informative(&db_pkgbin->version) &&
         dpkg_version_compare(&new_pkgbin->version, &db_pkgbin->version) < 0)
       continue;
 