guillemj/dpkg

commit 65ade6390b47fe3ec6a0e2ba341f3d553bf4bbc6

dpkg: Activate file triggers on removal more accurately
Activate them only when we know we are inevitably removing things.

src/remove.c
@@ -283,8 +283,6 @@ removal_bulk_remove_files(struct pkginfo *pkg)
         continue;
       }
 
-      trig_file_activate(usenode, pkg);
-
       if (is_dir) {
         debug(dbg_eachfiledetail, "removal_bulk is a directory");
         /* Only delete a directory or a link to one if we're the only
@@ -301,6 +299,8 @@ removal_bulk_remove_files(struct pkginfo *pkg)
         if (dir_is_used_by_others(namenode, pkg))
           continue;
       }
+
+      trig_file_activate(usenode, pkg);
 
       varbuf_trunc(&fnvb, before);
       varbuf_add_str(&fnvb, DPKGTEMPEXT);
@@ -389,7 +389,6 @@ static void removal_bulk_remove_leftover_dirs(struct pkginfo *pkg) {
     }
 
     usenode = namenodetouse(namenode, pkg, &pkg->installed);
-    trig_file_activate(usenode, pkg);
 
     varbuf_reset(&fnvb);
     varbuf_add_str(&fnvb, instdir);
@@ -408,6 +407,8 @@ static void removal_bulk_remove_leftover_dirs(struct pkginfo *pkg) {
       if (dir_is_used_by_others(namenode, pkg))
         continue;
     }
+
+    trig_file_activate(usenode, pkg);
 
     debug(dbg_eachfiledetail, "removal_bulk removing '%s'", fnvb.buf);
     if (!rmdir(fnvb.buf) || errno == ENOENT || errno == ELOOP) continue;

commit e00a290311af19dbf8d83a61c1ccca040a941207

dpkg: Activate all path components for file triggers on removal
Because directory processing might get skipped on removal (contrary to
unpack), as they might be shared with other packages, some file triggers
will not get activated as the code was changed to do more optimal
activation. The problem is that the removed paths need to activate all
parent paths to be effective.

Regression introduced in commit 65ade6390b47fe3ec6a0e2ba341f3d553bf4bbc6.

Closes: #725437

src/remove.c
@@ -304,7 +304,7 @@ removal_bulk_remove_files(struct pkginfo *pkg)
           continue;
       }
 
-      trig_file_activate(usenode, pkg);
+      trig_path_activate(usenode, pkg);
 
       varbuf_trunc(&fnvb, before);
       varbuf_add_str(&fnvb, DPKGTEMPEXT);
@@ -412,7 +412,7 @@ static void removal_bulk_remove_leftover_dirs(struct pkginfo *pkg) {
         continue;
     }
 
-    trig_file_activate(usenode, pkg);
+    trig_path_activate(usenode, pkg);
 
     debug(dbg_eachfiledetail, "removal_bulk removing '%s'", fnvb.buf);
     if (!rmdir(fnvb.buf) || errno == ENOENT || errno == ELOOP) continue;