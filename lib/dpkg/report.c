guillemj/dpkg

commit 50c1cb4d6e8b4c3ee739646f9df05992b806ea5e

libdpkg: Use fully buffered output on non-tty
If stdout is not a tty, we don't need immediate feedback, and not
buffering it can cause a significant slow down in case the amount
printed each time is very small.

Reported-by: Shawn Landden <shawnlandden@gmail.com>

lib/dpkg/report.c
@@ -24,6 +24,7 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #include <dpkg/macros.h>
 #include <dpkg/i18n.h>
@@ -33,7 +34,10 @@
 void
 dpkg_set_report_buffer(FILE *fp)
 {
-	setvbuf(fp, NULL, _IONBF, 0);
+	if (isatty(STDOUT_FILENO))
+		setvbuf(fp, NULL, _IONBF, 0);
+	else
+		setvbuf(fp, NULL, _IOFBF, 0);
 }
 
 static int warn_count = 0;

commit d7b89a456a071356a352b4bc7d66740f9050dcc2

libdpkg: Use fp instead of stdout on isatty() in dpkg_set_report_buffer()
Do not hardcode the file descriptor when calling isatty(), use the
one provided as an argument.

Regression introduced in commit 50c1cb4d6e8b4c3ee739646f9df05992b806ea5e.

lib/dpkg/report.c
@@ -34,7 +34,7 @@
 void
 dpkg_set_report_buffer(FILE *fp)
 {
-	if (isatty(STDOUT_FILENO))
+	if (isatty(fileno(fp)))
 		setvbuf(fp, NULL, _IONBF, 0);
 	else
 		setvbuf(fp, NULL, _IOFBF, 0);