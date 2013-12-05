guillemj/dpkg

commit 5ae4ce9c2dfddbb36e2e0fc24dde3a27196f00f1

Consistently use regex instead of regexp when possible
All changed instances are documentation or private code interfaces.
The only remaining variable instance with a regexp name is a publicly
exposed variable, which will eventually disappear once it has gone
through the deprecation process.

scripts/Dpkg/Source/Package.pm
@@ -51,8 +51,7 @@ use Dpkg::Vendor qw(run_vendor_hook);
 use POSIX qw(:errno_h :sys_wait_h);
 use File::Basename;
 
-# Public variables
-our $diff_ignore_default_regexp = '
+my $diff_ignore_default_regex = '
 # Ignore general backup files
 (?:^|/).*~$|
 # Ignore emacs recovery files
@@ -69,8 +68,12 @@ our $diff_ignore_default_regexp = '
 \.shelf|_MTN|\.be|\.bzr(?:\.backup|tags)?)(?:$|/.*$)
 ';
 # Take out comments and newlines
-$diff_ignore_default_regexp =~ s/^#.*$//mg;
-$diff_ignore_default_regexp =~ s/\n//sg;
+$diff_ignore_default_regex =~ s/^#.*$//mg;
+$diff_ignore_default_regex =~ s/\n//sg;
+
+# Public variables
+our $diff_ignore_default_regexp = $diff_ignore_default_regex;
+
 no warnings 'qw'; ## no critic (TestingAndDebugging::ProhibitNoWarnings)
 our @tar_ignore_default_pattern = qw(
 *.a
@@ -167,8 +170,8 @@ sub init_options {
     my ($self) = @_;
     # Use full ignore list by default
     # note: this function is not called by V1 packages
-    $self->{options}{diff_ignore_regexp} ||= $diff_ignore_default_regexp;
-    $self->{options}{diff_ignore_regexp} .= '|(?:^|/)debian/source/local-.*$';
+    $self->{options}{diff_ignore_regex} ||= $diff_ignore_default_regex;
+    $self->{options}{diff_ignore_regex} .= '|(?:^|/)debian/source/local-.*$';
     if (defined $self->{options}{tar_ignore}) {
         $self->{options}{tar_ignore} = [ @tar_ignore_default_pattern ]
             unless @{$self->{options}{tar_ignore}};

dselect/methods/Dselect/Ftp.pm
@@ -375,7 +375,7 @@ sub do_mdtm {
 	    # build a system time
 	    $time = Time::Local::timegm(0, $minutes, $hours, $day, $month, $year);
 	} else {
-	    die 'regexp match failed on LIST output';
+	    die 'regex match failed on LIST output';
 	}
     }
 

man/dpkg-buildpackage.1
@@ -215,7 +215,7 @@ Do not sign the \fB.changes\fP file.
 Force the signing of the resulting files (since dpkg 1.17.0),
 regardless of \fB\-us\fP or \fB\-uc\fP or other internal heuristics.
 .TP
-.BR \-i [\fIregexp\fP]
+.BR \-i [\fIregex\fP]
 .TQ
 .BR \-I [\fIpattern\fP]
 .TQ

man/dpkg-source.1
@@ -176,32 +176,32 @@ files. Supported values are:
 .IR 1 " to " 9 ", " best ", and " fast .
 The default is \fI9\fP for gzip and bzip2, \fI6\fP for xz and lzma.
 .TP
-.BR \-i "[\fIregexp\fP], " \-\-diff\-ignore [=\fIregexp\fP]
+.BR \-i "[\fIregex\fP], " \-\-diff\-ignore [=\fIregex\fP]
 You may specify a perl regular expression to match files you want
 filtered out of the list of files for the diff. (This list is
 generated by a find command.) (If the source package is being built as a
 version 3 source package using a VCS, this can be used to ignore
 uncommited changes on specific files. Using \-i.* will ignore all of them.)
-\fB\-i\fP by itself enables the option, with a default regexp that will
+\fB\-i\fP by itself enables the option, with a default regex that will
 filter out control files and directories of the most common revision
 control systems, backup and swap files and Libtool build output
-directories. There can only be one active regexp, of multiple
+directories. There can only be one active regex, of multiple
 \fB\-i\fP options only the last one will take effect.
 
 This is very helpful in cutting out extraneous files that get included
 in the diff, e.g. if you maintain your source in a revision control
 system and want to use a checkout to build a source package without
 including the additional files and directories that it will usually
-contain (e.g. CVS/, .cvsignore, .svn/). The default regexp is already
+contain (e.g. CVS/, .cvsignore, .svn/). The default regex is already
 very exhaustive, but if you need to replace it, please note that by
 default it can match any part of a path, so if you want to match the
 begin of a filename or only full filenames, you will need to provide
 the necessary anchors (e.g. '(^|/)', '($|/)') yourself.
 .TP
-.BR \-\-extend\-diff\-ignore =\fIregexp\fP
+.BR \-\-extend\-diff\-ignore =\fIregex\fP
 The perl regular expression specified will extend the default value
 of \fB\-\-diff\-ignore\fP and its current value (if set). It does this
-by concatenating "\fB|\fP\fIregexp\fP" to the existing value.
+by concatenating "\fB|\fP\fIregex\fP" to the existing value.
 This option is convenient to use in \fBdebian/source/options\fP to exclude
 some auto-generated files from the automatic patch generation.
 .TP
@@ -231,7 +231,7 @@ to each part of the path individually. The exact semantic of tar's
 http://www.gnu.org/software/tar/manual/tar.html#wildcards for a full
 documentation.
 
-The default regexp and patterns for both options can be seen
+The default regex and patterns for both options can be seen
 in the output of the \fB\-\-help\fP command.
 .SS Generic extract options
 .TP

scripts/Dpkg/Index.pm
@@ -208,7 +208,7 @@ sub get_by_key {
 =item my @keys = $index->get_keys(%criteria)
 
 Returns the keys of items that matches all the criteria. The key of the
-%criteria hash is a field name and the value is either a regexp that needs
+%criteria hash is a field name and the value is either a regex that needs
 to match the field value, or a reference to a function that must return
 true and that receives the field value as single parameter, or a scalar
 that must be equal to the field value.

scripts/Dpkg/Source/Package/V1.pm
@@ -44,10 +44,10 @@ sub init_options {
     my ($self) = @_;
     # Don't call $self->SUPER::init_options() on purpose, V1.0 has no
     # ignore by default
-    if ($self->{options}{diff_ignore_regexp}) {
-	$self->{options}{diff_ignore_regexp} .= '|(?:^|/)debian/source/local-.*$';
+    if ($self->{options}{diff_ignore_regex}) {
+	$self->{options}{diff_ignore_regex} .= '|(?:^|/)debian/source/local-.*$';
     } else {
-	$self->{options}{diff_ignore_regexp} = '(?:^|/)debian/source/local-.*$';
+	$self->{options}{diff_ignore_regex} = '(?:^|/)debian/source/local-.*$';
     }
     push @{$self->{options}{tar_ignore}}, 'debian/source/local-options',
          'debian/source/local-patch-header';
@@ -176,7 +176,7 @@ sub do_build {
     my $sourcestyle = $self->{options}{sourcestyle};
     my @argv = @{$self->{options}{ARGV}};
     my @tar_ignore = map { "--exclude=$_" } @{$self->{options}{tar_ignore}};
-    my $diff_ignore_regexp = $self->{options}{diff_ignore_regexp};
+    my $diff_ignore_regex = $self->{options}{diff_ignore_regex};
 
     if (scalar(@argv) > 1) {
         usageerr(_g('-b takes at most a directory and an orig source ' .
@@ -356,7 +356,7 @@ sub do_build {
         $diff->create();
         $diff->add_diff_directory($origdir, $dir,
                 basedirname => $basedirname,
-                diff_ignore_regexp => $diff_ignore_regexp,
+                diff_ignore_regex => $diff_ignore_regex,
                 options => []); # Force empty set of options to drop the
                                 # default -p option
         $diff->finish() || $ur++;

scripts/Dpkg/Source/Package/V2.pm
@@ -295,8 +295,8 @@ sub after_build {
 sub prepare_build {
     my ($self, $dir) = @_;
     $self->{diff_options} = {
-        diff_ignore_regexp => $self->{options}{diff_ignore_regexp} .
-                              '|(^|/)debian/patches/.dpkg-source-applied$',
+        diff_ignore_regex => $self->{options}{diff_ignore_regex} .
+                             '|(^|/)debian/patches/.dpkg-source-applied$',
         include_removal => $self->{options}{include_removal},
         include_timestamp => $self->{options}{include_timestamp},
         use_dev_null => 1,

scripts/Dpkg/Source/Package/V3/Bzr.pm
@@ -91,7 +91,7 @@ sub do_build {
     my @argv = @{$self->{options}{ARGV}};
     # TODO: warn here?
     #my @tar_ignore = map { "--exclude=$_" } @{$self->{options}{tar_ignore}};
-    my $diff_ignore_regexp = $self->{options}{diff_ignore_regexp};
+    my $diff_ignore_regex = $self->{options}{diff_ignore_regex};
 
     $dir =~ s{/+$}{}; # Strip trailing /
     my ($dirname, $updir) = fileparse($dir);
@@ -122,8 +122,8 @@ sub do_build {
     while (<$bzr_status_fh>) {
         chomp;
         next unless s/^ +//;
-        if (! length $diff_ignore_regexp ||
-            ! m/$diff_ignore_regexp/o) {
+        if (! length $diff_ignore_regex ||
+            ! m/$diff_ignore_regex/o) {
             push @files, $_;
         }
     }

commit 453d2813b9a74a37f0bf21810e269e853706ea3e

Dpkg::Source::Package: Make $diff_ignore_default_regexp a true alias
If the users are modifying this variable, just copying the real one to
the public one will not do it, because changes will not be seen by the
module code. Instead create a real alias.

Regression introduced in commit 5ae4ce9c2dfddbb36e2e0fc24dde3a27196f00f1.

scripts/Dpkg/Source/Package.pm
@@ -79,7 +79,8 @@ $diff_ignore_default_regex =~ s/\n//sg;
 # Public variables
 # XXX: Backwards compatibility, stop exporting on VERSION 2.00.
 ## no critic (Variables::ProhibitPackageVars)
-our $diff_ignore_default_regexp = $diff_ignore_default_regex;
+our $diff_ignore_default_regexp;
+*diff_ignore_default_regexp = \$diff_ignore_default_regex;
 
 no warnings 'qw'; ## no critic (TestingAndDebugging::ProhibitNoWarnings)
 our @tar_ignore_default_pattern = qw(