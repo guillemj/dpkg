guillemj/dpkg

commit 1cccaf0d0c509f9ac77f5448a0573e2d1c9c1ae4

dpkg-source: Do not modify $diff_ignore_default_regexp package variable
This is the default, only change scoped variables, this will help in
further refactoring, and so that we get a correct default on --help.

scripts/dpkg-source.pl
@@ -76,6 +76,7 @@ my %override;
 
 my $substvars = Dpkg::Substvars->new();
 my $tar_ignore_default_pattern_done;
+my $diff_ignore_regex = $Dpkg::Source::Package::diff_ignore_default_regexp;
 
 my @options;
 my @cmdline_options;
@@ -167,9 +168,9 @@ while (@options) {
     } elsif (m/^-U([^\=:]+)$/) {
         $remove{$1} = 1;
     } elsif (m/^-(?:i|-diff-ignore(?:$|=))(.*)$/) {
-        $options{diff_ignore_regex} = $1 ? $1 : $Dpkg::Source::Package::diff_ignore_default_regexp;
+        $options{diff_ignore_regex} = $1 ? $1 : $diff_ignore_regex;
     } elsif (m/^--extend-diff-ignore=(.+)$/) {
-	$Dpkg::Source::Package::diff_ignore_default_regexp .= "|$1";
+	$diff_ignore_regex .= "|$1";
 	if ($options{diff_ignore_regex}) {
 	    $options{diff_ignore_regex} .= "|$1";
 	}

commit 5d8db1edd45be3969fe94bb63237b8159664eeb6

dpkg-source: Update default diff ignore regex on --extend-diff-ignore
The Dpkg::Source::Package formats >= V2, expect an updated default
diff ignore regex when --extend-diff-ignore is used, for example in
the debian/source/options file, but no --diff-ignore argument is
passed.

This partially reverts commit 1cccaf0d0c509f9ac77f5448a0573e2d1c9c1ae4,
which introduced this regression.

scripts/dpkg-source.pl
@@ -43,6 +43,7 @@ use Dpkg::Version;
 use Dpkg::Vars;
 use Dpkg::Changelog::Parse;
 use Dpkg::Source::Package qw(get_default_diff_ignore_regex
+                             set_default_diff_ignore_regex
                              get_default_tar_ignore_pattern);
 use Dpkg::Vendor qw(run_vendor_hook);
 
@@ -175,6 +176,7 @@ while (@options) {
 	if ($options{diff_ignore_regex}) {
 	    $options{diff_ignore_regex} .= "|$1";
 	}
+	set_default_diff_ignore_regex($diff_ignore_regex);
     } elsif (m/^-(?:I|-tar-ignore=)(.+)$/) {
         push @{$options{tar_ignore}}, $1;
     } elsif (m/^-(?:I|-tar-ignore)$/) {