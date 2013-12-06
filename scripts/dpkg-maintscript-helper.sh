ae25c683c0141ac5ec6554f56d6c8af6f0764b03
Guillem Jover <guillem@debian.org>
2013-12-06 16:04:27 +0100

    Use https:// URLs instead of http:// when possible

--- scripts/dpkg-maintscript-helper.sh
+++ scripts/dpkg-maintscript-helper.sh
@@ -16,10 +16,10 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 # The conffile related functions are inspired by
-# http://wiki.debian.org/DpkgConffileHandling
+# https://wiki.debian.org/DpkgConffileHandling
 
 # This script is documented in dpkg-maintscript-helper(1)
 
