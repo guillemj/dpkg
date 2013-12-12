guillemj/dpkg

commit 50c1cb4d6e8b4c3ee739646f9df05992b806ea5e

libdpkg: Use fully buffered output on non-tty
If stdout is not a tty, we don't need immediate feedback, and not
buffering it can cause a significant slow down in case the amount
printed each time is very small.

Reported-by: Shawn Landden <shawnlandden@gmail.com>

lib/dpkg/report.c
@@ -24,6 +24,7 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #include <dpkg/macros.h>
 #include <dpkg/i18n.h>
@@ -33,7 +34,10 @@
 void
 dpkg_set_report_buffer(FILE *fp)
 {
-	setvbuf(fp, NULL, _IONBF, 0);
+	if (isatty(STDOUT_FILENO))
+		setvbuf(fp, NULL, _IONBF, 0);
+	else
+		setvbuf(fp, NULL, _IOFBF, 0);
 }
 
 static int warn_count = 0;

commit 903d7cce6e29dde4d745bec69dd39b7b1ade1bf1

libdpkg: Change non-tty output to be line buffered by default
Switch it to be fully buffered only for programs that have precious and
abundant output, not just progress reporting output (i.e. dpkg-query).

This was causing out-of-order error and debug messages in relation to
normal progress reporting, which could be very confusing.

Regression introduced in commit 50c1cb4d6e8b4c3ee739646f9df05992b806ea5e.

lib/dpkg/libdpkg.map
@@ -31,6 +31,7 @@ LIBDPKG_PRIVATE {
 	ohshite;
 	ohshit;
 	do_internerr;
+	dpkg_set_report_piped_mode;
 	dpkg_set_report_buffer;
 	warning_get_count;
 	warningv;

lib/dpkg/report.c
@@ -31,13 +31,21 @@
 #include <dpkg/progname.h>
 #include <dpkg/report.h>
 
+static int piped_mode = _IOLBF;
+
+void
+dpkg_set_report_piped_mode(int mode)
+{
+	piped_mode = mode;
+}
+
 void
 dpkg_set_report_buffer(FILE *fp)
 {
 	if (isatty(fileno(fp)))
 		setvbuf(fp, NULL, _IONBF, 0);
 	else
-		setvbuf(fp, NULL, _IOFBF, 0);
+		setvbuf(fp, NULL, piped_mode, 0);
 }
 
 static int warn_count = 0;

lib/dpkg/report.h
@@ -35,6 +35,7 @@ DPKG_BEGIN_DECLS
  * @{
  */
 
+void dpkg_set_report_piped_mode(int mode);
 void dpkg_set_report_buffer(FILE *fp);
 
 int warning_get_count(void);

src/querycmd.c
@@ -861,6 +861,7 @@ static const struct cmdinfo cmdinfos[]= {
 int main(int argc, const char *const *argv) {
   int ret;
 
+  dpkg_set_report_piped_mode(_IOFBF);
   dpkg_locales_init(PACKAGE);
   dpkg_program_init("dpkg-query");
   dpkg_options_parse(&argv, cmdinfos, printforhelp);