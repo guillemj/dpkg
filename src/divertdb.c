guillemj/dpkg

commit ab56d08abe274182c0cab03425610ae61a074eff

dpkg: Keep the diversions and statoverride database files open
The database files must be kept open during an entire dpkg run, because
we need to check if the files have changed, and rely on the inode number,
which could get eagerly reused by the filesystem if these files get
replaced multiple times in maintainer scripts.

Add a code comment explaining the rationale, so that this does not happen
again in the future.

This partially reverts commit 579b90b61b24a538cf1709d7ceda0b38fec631b3.

Closes: #731524

src/divertdb.c
@@ -49,6 +49,7 @@ ensure_diversions(void)
 	static struct stat sb_prev;
 	struct stat sb_next;
 	char linebuf[MAXDIVERTFILENAME];
+	static FILE *file_prev;
 	FILE *file;
 	struct diversion *ov, *oicontest, *oialtname;
 
@@ -62,17 +63,30 @@ ensure_diversions(void)
 		if (errno != ENOENT)
 			ohshite(_("failed to open diversions file"));
 	} else {
+		setcloexec(fileno(file), diversionsname);
+
 		if (fstat(fileno(file), &sb_next))
 			ohshite(_("failed to fstat diversions file"));
 
-		if (sb_prev.st_dev == sb_next.st_dev &&
+		/*
+		 * We need to keep the database file open so that the
+		 * filesystem cannot reuse the inode number (f.ex. during
+		 * multiple dpkg-divert invocations in a maintainer script),
+		 * otherwise the following check might turn true, and we
+		 * would skip reloading a modified database.
+		 */
+		if (file_prev &&
+		    sb_prev.st_dev == sb_next.st_dev &&
 		    sb_prev.st_ino == sb_next.st_ino) {
-			fclose(file);
 			onerr_abort--;
 			return;
 		}
 		sb_prev = sb_next;
+
+		if (file_prev)
+			fclose(file_prev);
 	}
+	file_prev = file;
 
 	for (ov = diversions; ov; ov = ov->next) {
 		ov->useinstead->divert->camefrom->divert = NULL;
@@ -112,6 +126,5 @@ ensure_diversions(void)
 		diversions = oicontest;
 	}
 
-	fclose(file);
 	onerr_abort--;
 }

src/statdb.c
@@ -112,6 +112,7 @@ ensure_statoverrides(void)
 {
 	static struct stat sb_prev;
 	struct stat sb_next;
+	static FILE *file_prev;
 	FILE *file;
 	char *loaded_list, *loaded_list_end, *thisline, *nextline, *ptr;
 	struct file_stat *fso;
@@ -127,17 +128,30 @@ ensure_statoverrides(void)
 		if (errno != ENOENT)
 			ohshite(_("failed to open statoverride file"));
 	} else {
+		setcloexec(fileno(file), statoverridename);
+
 		if (fstat(fileno(file), &sb_next))
 			ohshite(_("failed to fstat statoverride file"));
 
-		if (sb_prev.st_dev == sb_next.st_dev &&
+		/*
+		 * We need to keep the database file open so that the
+		 * filesystem cannot reuse the inode number (f.ex. during
+		 * multiple dpkg-statoverride invocations in a maintainer
+		 * script), otherwise the following check might turn true,
+		 * and we would skip reloading a modified database.
+		 */
+		if (file_prev &&
+		    sb_prev.st_dev == sb_next.st_dev &&
 		    sb_prev.st_ino == sb_next.st_ino) {
-			fclose(file);
 			onerr_abort--;
 			return;
 		}
 		sb_prev = sb_next;
+
+		if (file_prev)
+			fclose(file_prev);
 	}
+	file_prev = file;
 
 	if (!file) {
 		onerr_abort--;
@@ -147,7 +161,6 @@ ensure_statoverrides(void)
 	/* If the statoverride list is empty we don't need to bother
 	 * reading it. */
 	if (!sb_next.st_size) {
-		fclose(file);
 		onerr_abort--;
 		return;
 	}
@@ -220,6 +233,5 @@ ensure_statoverrides(void)
 		thisline = nextline;
 	}
 
-	fclose(file);
 	onerr_abort--;
 }

commit 0b583bfec3e4aadf4cf6a5aec226f31dfe04e5c4

dpkg: Fix file descriptor leaks in diversions and statoverride databases
Regression introduced in commit ab56d08abe274182c0cab03425610ae61a074eff,
while too eagerly removing fclose() calls.

Closes: #734783

src/divertdb.c
@@ -78,15 +78,15 @@ ensure_diversions(void)
 		if (file_prev &&
 		    sb_prev.st_dev == sb_next.st_dev &&
 		    sb_prev.st_ino == sb_next.st_ino) {
+			fclose(file);
 			onerr_abort--;
 			debug(dbg_general, "%s: same, skipping", __func__);
 			return;
 		}
 		sb_prev = sb_next;
-
-		if (file_prev)
-			fclose(file_prev);
 	}
+	if (file_prev)
+		fclose(file_prev);
 	file_prev = file;
 
 	for (ov = diversions; ov; ov = ov->next) {

src/statdb.c
@@ -144,15 +144,15 @@ ensure_statoverrides(void)
 		if (file_prev &&
 		    sb_prev.st_dev == sb_next.st_dev &&
 		    sb_prev.st_ino == sb_next.st_ino) {
+			fclose(file);
 			onerr_abort--;
 			debug(dbg_general, "%s: same, skipping", __func__);
 			return;
 		}
 		sb_prev = sb_next;
-
-		if (file_prev)
-			fclose(file_prev);
 	}
+	if (file_prev)
+		fclose(file_prev);
 	file_prev = file;
 
 	/* Reset statoverride information. */