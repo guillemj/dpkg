guillemj/dpkg

commit c2ee90f92ab6c915db774403414b2ea70c967c3e

Dpkg::Control::HashCore: Do not emit trailing space on field empty first line
This spurious space is unnecessary and takes a non-insignificant amount
of disk space on archive index files for example.

Update dpkg-dev Breaks on devscripts to version << 2.14.4, as previous
versions of debchange expect a trailing space from dpkg-parsechangelog
output.

Closes: #749044

Based-on-patch-by: Johannes Schauer <j.schauer@email.de>

scripts/Dpkg/Control/HashCore.pm
@@ -349,22 +349,25 @@ sub output {
             # Skip whitespace-only fields
             next if $$self->{drop_empty} and $value !~ m/\S/;
 	    # Escape data to follow control file syntax
-	    my @lines = split(/\n/, $value);
-	    $value = (scalar @lines) ? shift @lines : '';
+	    my ($first_line, @lines) = split /\n/, $value;
+
+	    my $kv = "$key:";
+	    $kv .= ' ' . $first_line if $first_line;
+	    $kv .= "\n";
 	    foreach (@lines) {
 		s/\s+$//;
 		if (/^$/ or /^\.+$/) {
-		    $value .= "\n .$_";
+		    $kv .= " .$_\n";
 		} else {
-		    $value .= "\n $_";
+		    $kv .= " $_\n";
 		}
 	    }
 	    # Print it out
             if ($fh) {
-	        print { $fh } "$key: $value\n"
+	        print { $fh } $kv
 	            or syserr(_g('write error on control data'));
             }
-	    $str .= "$key: $value\n" if defined wantarray;
+	    $str .= $kv if defined wantarray;
 	}
     }
     return $str;

scripts/t/Dpkg_Control.t
@@ -53,7 +53,7 @@ Long-Field: line1
    line 3 line 3 line 3
  ..
  line 4
-Empty-Field: 
+Empty-Field:
 
 Package: mypackage1
 Architecture: any

debian/control
@@ -74,7 +74,7 @@ Depends: libdpkg-perl (= ${source:Version}), bzip2, xz-utils,
 Recommends: gcc | c-compiler, build-essential, fakeroot,
  gnupg | gnupg2, gpgv | gpgv2, libalgorithm-merge-perl
 Suggests: debian-keyring
-Breaks: dpkg-cross (<< 2.0.0), devscripts (<< 2.10.26)
+Breaks: dpkg-cross (<< 2.0.0), devscripts (<< 2.14.4)
 Replaces: manpages-it (<< 2.80-4)
 Description: Debian package development tools
  This package provides the development tools (including dpkg-source)

commit 397448b7486b14ce3d844743c71e277a0b086141

Dpkg::Control::HashCore: Only skip actual empty first line field values
Regression introduced in commit c2ee90f92ab6c915db774403414b2ea70c967c3e.

We need to check if the value is empty, not if it's false. Add a
regression test.

scripts/Dpkg/Control/HashCore.pm
@@ -352,7 +352,7 @@ sub output {
 	    my ($first_line, @lines) = split /\n/, $value;
 
 	    my $kv = "$key:";
-	    $kv .= ' ' . $first_line if $first_line;
+	    $kv .= ' ' . $first_line if length $first_line;
 	    $kv .= "\n";
 	    foreach (@lines) {
 		s/\s+$//;

scripts/t/Dpkg_Control.t
@@ -16,7 +16,7 @@
 use strict;
 use warnings;
 
-use Test::More tests => 22;
+use Test::More tests => 23;
 use IO::String;
 
 BEGIN {
@@ -45,6 +45,7 @@ my $io = IO::String->new();
 $c->output($io);
 my $value = ${$io->string_ref()};
 my $expected = 'Source: mysource
+Numeric-Field: 0
 My-Field-One: myvalue1
 My-Field-Two: myvalue2
 Long-Field: line1
@@ -74,6 +75,7 @@ is($value, $expected, "Dump of $datadir/control-1");
 
 my $src = $c->get_source();
 is($src, $c->[0], 'array representation of Dpkg::Control::Info 1/2');
+is($src->{'numeric-field'}, '0', 'Numeric 0 value parsed correctly');
 is($src->{'my-field-one'}, 'myvalue1', 'Access field through badly capitalized field name');
 is($src->{'long-field'},
 'line1

scripts/t/Dpkg_Control/control-1
@@ -1,5 +1,6 @@
 Source: mysource
 # This is a comment
+numeric-field: 0
 my-field-one: myvalue1
 my-field-two: myvalue2
 long-field: line1