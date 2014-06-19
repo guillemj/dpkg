6f84a47a24bbc79332a0db9819b9df72c4a06970
Guillem Jover <guillem@debian.org>
2014-06-20 00:28:17 +0200

    build: Export PATH

--- Test.mk
+++ Test.mk
@@ -12,6 +12,7 @@ endif
 
 ifdef DPKG_BUILDTREE
 PATH := $(DPKG_BUILDTREE)/dpkg-deb:$(DPKG_BUILDTREE)/dpkg-split:$(DPKG_BUILDTREE)/src:$(DPKG_BUILDTREE)/utils:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export PATH
 BEROOT := sudo env PATH=$(PATH) $(DPKG_MAINTSCRIPT_DEBUG)
 else
 BEROOT := sudo env $(DPKG_MAINTSCRIPT_DEBUG)
