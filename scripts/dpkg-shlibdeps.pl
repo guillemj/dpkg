guillemj/dpkg

commit 7104dc16110603687b85c6e2b9a0170b17caab8c

dpkg-shlibdeps: Add a hint to the error on no dependency information found
Suggest in the output to check if the library is actually packaged.

[guillem@debian.org:
 - Line-wrap to 80 columns.
 - Slight rewording. ]

Closes: #756230

Signed-off-by: Guillem Jover <guillem@debian.org>

scripts/dpkg-shlibdeps.pl
@@ -291,7 +291,9 @@ foreach my $file (keys %exec) {
 		    # 3/ when we have been asked to do so
 		    $ignore++ if $ignore_missing_info;
 		    error(_g('no dependency information found for %s ' .
-		             '(used by %s)'), $lib, $file)
+		             '(used by %s)\n' .
+		             'Hint: check if the library actually comes ' .
+		             'from a package.'), $lib, $file)
 		        unless $ignore;
 		}
 	    }

commit 2379a80fbdc49084894b448a49d6859486b92d02

dpkg-shlibdeps: fix error string embedding literal '\n' instead of the expected newline
Update the translation strings accordingly.

Error introduced in 7104dc16110603687b85c6e2b9a0170b17caab8c.

scripts/dpkg-shlibdeps.pl
@@ -290,10 +290,10 @@ foreach my $file (keys %exec) {
 		    $ignore++ unless scalar(split_soname($soname));
 		    # 3/ when we have been asked to do so
 		    $ignore++ if $ignore_missing_info;
-		    error(_g('no dependency information found for %s ' .
-		             '(used by %s)\n' .
-		             'Hint: check if the library actually comes ' .
-		             'from a package.'), $lib, $file)
+		    error(_g("no dependency information found for %s " .
+		             "(used by %s)\n" .
+		             "Hint: check if the library actually comes " .
+		             "from a package."), $lib, $file)
 		        unless $ignore;
 		}
 	    }

scripts/po/ca.po
@@ -8,7 +8,7 @@ msgstr ""
 "Project-Id-Version: dpkg-dev 1.15.8.6\n"
 "Report-Msgid-Bugs-To: debian-dpkg@lists.debian.org\n"
 "POT-Creation-Date: 2014-08-15 03:51+0200\n"
-"PO-Revision-Date: 2010-10-24 18:10+0200\n"
+"PO-Revision-Date: 2014-08-15 14:10+0200\n"
 "Last-Translator: Jordi Mallach <jordi@debian.org>\n"
 "Language-Team: Catalan <debian-l10n-catalan@lists.debian.org>\n"
 "Language: ca\n"
@@ -1285,8 +1285,8 @@ msgstr ""
 #: scripts/dpkg-shlibdeps.pl:293
 #, perl-format
 msgid ""
-"no dependency information found for %s (used by %s)\\nHint: check if the "
-"library actually comes from a package."
+"no dependency information found for %s (used by %s)\n"
+"Hint: check if the library actually comes from a package."
 msgstr ""
 
 #: scripts/dpkg-shlibdeps.pl:326

scripts/po/de.po
@@ -7,7 +7,7 @@ msgstr ""
 "Project-Id-Version: dpkg-dev 1.17.6\n"
 "Report-Msgid-Bugs-To: debian-dpkg@lists.debian.org\n"
 "POT-Creation-Date: 2014-08-15 03:51+0200\n"
-"PO-Revision-Date: 2014-08-10 17:17+0200\n"
+"PO-Revision-Date: 2014-08-15 14:10+0200\n"
 "Last-Translator: Helge Kreutzmann <debian@helgefjell.de>\n"
 "Language-Team: German <debian-l10n-german@lists.debian.org>\n"
 "Language: de\n"
@@ -1525,11 +1525,11 @@ msgstr "%s hat einen unerwarteten SONAME (%s)"
 #: scripts/dpkg-shlibdeps.pl:293
 #, perl-format
 msgid ""
-"no dependency information found for %s (used by %s)\\nHint: check if the "
-"library actually comes from a package."
+"no dependency information found for %s (used by %s)\n"
+"Hint: check if the library actually comes from a package."
 msgstr ""
-"keine Abhängigkeitsinformationen für %s (verwendet von %s) gefunden\\nTipp: "
-"Prüfen Sie, ob die Bibliothek tatsächlich aus einem Paket stammt."
+"keine Abhängigkeitsinformationen für %s (verwendet von %s) gefunden\n"
+"Tipp: Prüfen Sie, ob die Bibliothek tatsächlich aus einem Paket stammt."
 
 #: scripts/dpkg-shlibdeps.pl:326
 msgid ""

scripts/po/dpkg-dev.pot
@@ -1201,8 +1201,8 @@ msgstr ""
 #: scripts/dpkg-shlibdeps.pl:293
 #, perl-format
 msgid ""
-"no dependency information found for %s (used by %s)\\nHint: check if the "
-"library actually comes from a package."
+"no dependency information found for %s (used by %s)\n"
+"Hint: check if the library actually comes from a package."
 msgstr ""
 
 #: scripts/dpkg-shlibdeps.pl:326

scripts/po/es.po
@@ -32,7 +32,7 @@ msgstr ""
 "Project-Id-Version: dpkg-dev 1.16.8\n"
 "Report-Msgid-Bugs-To: debian-dpkg@lists.debian.org\n"
 "POT-Creation-Date: 2014-08-15 03:51+0200\n"
-"PO-Revision-Date: 2012-08-07 11:40+0200\n"
+"PO-Revision-Date: 2014-08-15 14:11+0200\n"
 "Last-Translator: Omar Campagne <ocampagne@gmail.com>\n"
 "Language-Team: Spanish <debian-l10n-spanish@lists.debian.org>\n"
 "Language: es\n"
@@ -1819,8 +1819,8 @@ msgstr "%s tiene un SONAME inesperado (%s)"
 #, fuzzy, perl-format
 #| msgid "no dependency information found for %s (used by %s)"
 msgid ""
-"no dependency information found for %s (used by %s)\\nHint: check if the "
-"library actually comes from a package."
+"no dependency information found for %s (used by %s)\n"
+"Hint: check if the library actually comes from a package."
 msgstr ""
 "No se ha encontrado información de dependencia para %s (utilizado por %s)"
 

scripts/po/fr.po
@@ -11,7 +11,7 @@ msgstr ""
 "Project-Id-Version: dpkg-dev 1.17.7\n"
 "Report-Msgid-Bugs-To: debian-dpkg@lists.debian.org\n"
 "POT-Creation-Date: 2014-08-15 03:51+0200\n"
-"PO-Revision-Date: 2014-08-15 14:20+0200\n"
+"PO-Revision-Date: 2014-08-15 14:22+0200\n"
 "Last-Translator: Steve Petruzzello <dlist@bluewin.ch>\n"
 "Language-Team: French <debian-l10n-french@lists.debian.org>\n"
 "Language: fr\n"
@@ -1596,8 +1596,8 @@ msgstr "%s utilise un SONAME inattendu (%s)"
 #: scripts/dpkg-shlibdeps.pl:293
 #, perl-format
 msgid ""
-"no dependency information found for %s (used by %s)\\nHint: check if the "
-"library actually comes from a package."
+"no dependency information found for %s (used by %s)\n"
+"Hint: check if the library actually comes from a package."
 msgstr "pas d'information de dépendance trouvée pour %s (utilisé par %s)\n"
 "Indication: vérifiez que la bibliothèque provienne bien d'un paquet."
 

scripts/po/pl.po
@@ -10,7 +10,7 @@ msgstr ""
 "Project-Id-Version: dpkg-dev 1.15.4\n"
 "Report-Msgid-Bugs-To: debian-dpkg@lists.debian.org\n"
 "POT-Creation-Date: 2014-08-15 03:51+0200\n"
-"PO-Revision-Date: 2012-07-28 20:29+0200\n"
+"PO-Revision-Date: 2014-08-15 14:11+0200\n"
 "Last-Translator: Michał Kułach <michal.kulach@gmail.com>\n"
 "Language-Team: Polish <debian-l10n-polish@lists.debian.org>\n"
 "Language: pl\n"
@@ -1737,8 +1737,8 @@ msgstr "%s ma nieoczekiwane SONAME (%s)"
 #, fuzzy, perl-format
 #| msgid "no dependency information found for %s (used by %s)"
 msgid ""
-"no dependency information found for %s (used by %s)\\nHint: check if the "
-"library actually comes from a package."
+"no dependency information found for %s (used by %s)\n"
+"Hint: check if the library actually comes from a package."
 msgstr "nie znaleziono informacji o zależnościach dla %s (używanego przez %s)"
 
 #: scripts/dpkg-shlibdeps.pl:326

scripts/po/ru.po
@@ -8,7 +8,7 @@ msgstr ""
 "Project-Id-Version: dpkg-dev 1.16.9\n"
 "Report-Msgid-Bugs-To: debian-dpkg@lists.debian.org\n"
 "POT-Creation-Date: 2014-08-15 03:51+0200\n"
-"PO-Revision-Date: 2013-01-25 18:57+0400\n"
+"PO-Revision-Date: 2014-08-15 14:11+0200\n"
 "Last-Translator: Yuri Kozlov <yuray@komyakino.ru>\n"
 "Language-Team: Russian <debian-l10n-russian@lists.debian.org>\n"
 "Language: ru\n"
@@ -1742,8 +1742,8 @@ msgstr "%s имеет неожидаемый SONAME (%s)"
 #, fuzzy, perl-format
 #| msgid "no dependency information found for %s (used by %s)"
 msgid ""
-"no dependency information found for %s (used by %s)\\nHint: check if the "
-"library actually comes from a package."
+"no dependency information found for %s (used by %s)\n"
+"Hint: check if the library actually comes from a package."
 msgstr "информация о зависимостях не найдена для %s (используется %s)"
 
 #: scripts/dpkg-shlibdeps.pl:326

scripts/po/sv.po
@@ -8,7 +8,7 @@ msgstr ""
 "Project-Id-Version: dpkg-dev 1.17.11\n"
 "Report-Msgid-Bugs-To: debian-dpkg@lists.debian.org\n"
 "POT-Creation-Date: 2014-08-15 03:51+0200\n"
-"PO-Revision-Date: 2014-06-14 20:28+0100\n"
+"PO-Revision-Date: 2014-08-15 14:12+0200\n"
 "Last-Translator: Peter Krefting <peterk@debian.org>\n"
 "Language-Team: Svenska <debian-l10n-swedish@lists.debian.org>\n"
 "Language: sv\n"
@@ -1589,8 +1589,8 @@ msgstr "%s har ett oväntat SONAMN (%s)"
 #, fuzzy, perl-format
 #| msgid "no dependency information found for %s (used by %s)"
 msgid ""
-"no dependency information found for %s (used by %s)\\nHint: check if the "
-"library actually comes from a package."
+"no dependency information found for %s (used by %s)\n"
+"Hint: check if the library actually comes from a package."
 msgstr "hittade ingen beroendeinformation för %s (använd av %s)"
 
 #: scripts/dpkg-shlibdeps.pl:326