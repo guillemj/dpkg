guillemj/dpkg

commit ded3dfed77b7fd268914e19e8081b43c9233cc05

libdpkg: Fix memory leaks in buffer_copy() on error conditions
Check for errors from subfunctions and break out of the loop to be able
to release resources. Check also for unexpected EOF only after freeing
resources, so that we don't end up leaking them.

Warned-by: coverity

lib/dpkg/buffer.c
@@ -196,7 +196,7 @@ buffer_copy(struct buffer_data *read_data,
 	while (bufsize > 0) {
 		bytesread = buffer_read(read_data, buf, bufsize, err);
 		if (bytesread < 0)
-			return -1;
+			break;
 		if (bytesread == 0)
 			break;
 
@@ -212,19 +212,21 @@ buffer_copy(struct buffer_data *read_data,
 
 		byteswritten = buffer_write(write_data, buf, bytesread, err);
 		if (byteswritten < 0)
-			return -1;
+			break;
 		if (byteswritten == 0)
 			break;
 
 		totalwritten += byteswritten;
 	}
 
-	if (limit > 0)
-		return dpkg_put_error(err, _("unexpected end of file or stream"));
-
 	buffer_filter_done(filter);
 
 	free(buf);
+
+	if (limit > 0)
+		return dpkg_put_error(err, _("unexpected end of file or stream"));
+	if (bytesread < 0 || byteswritten < 0)
+		return -1;
 
 	return totalread;
 }

commit b851df1817ebaf60a95ae6478824c0e732890fc0

libdpkg: Fix error message from buffer_copy() read and write function
The buffer_read() and buffer_write() function error codes should have
higher precedence than a possible error from a truncated stream, otherwise
we will get confusing or just wrong error messages.

Regression introduced in commit ded3dfed77b7fd268914e19e8081b43c9233cc05.

Closes: #759498

lib/dpkg/buffer.c
@@ -223,10 +223,10 @@ buffer_copy(struct buffer_data *read_data,
 
 	free(buf);
 
-	if (limit > 0)
-		return dpkg_put_error(err, _("unexpected end of file or stream"));
 	if (bytesread < 0 || byteswritten < 0)
 		return -1;
+	if (limit > 0)
+		return dpkg_put_error(err, _("unexpected end of file or stream"));
 
 	return totalread;
 }