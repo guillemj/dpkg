e68f081acd9f1c210d3464b0a5e2c732e1b5c025
Guillem Jover <guillem@debian.org>
2014-10-06 19:28:23 +0200

    scripts/mk: Set DEB_TARGET_ variables from architecture.mk
    Missed in commit f29ed62d0c340869752c61d55a2df74159c31625.

--- scripts/mk/architecture.mk
+++ scripts/mk/architecture.mk
@@ -6,6 +6,6 @@ dpkg_late_eval ?= $(or $(value DPKG_CACHE_$(1)),$(eval DPKG_CACHE_$(1) := $(shel
 
 dpkg_architecture_setvar = $(1) ?= $(call dpkg_late_eval,$(1),dpkg-architecture -q$(1))
 
-$(foreach machine,BUILD HOST,\
+$(foreach machine,BUILD HOST TARGET,\
   $(foreach var,ARCH ARCH_OS ARCH_CPU ARCH_BITS ARCH_ENDIAN GNU_CPU GNU_SYSTEM GNU_TYPE MULTIARCH,\
     $(eval $(call dpkg_architecture_setvar,DEB_$(machine)_$(var)))))
