guillemj/dpkg

commit 51c55641fb6c66e149ea93c344cb788c68978c75

dpkg-shlibdeps: Initialize with correct minimum version from a symbols file
When initializing the minimum version for a dependency, we have to use
the greater one from any SONAME present in that symbols file, because
the shared libraries might not have been available before that version
for example.

This also guarantees a reproducible output, as the minimum version
selected is not dependant on the hash order.

Closes: #764721

[guillem@debian.org:
 - Reword debugging message. ]

Signed-off-by: Guillem Jover <guillem@debian.org>

scripts/dpkg-shlibdeps.pl
@@ -255,13 +255,9 @@ foreach my $file (keys %exec) {
                 # package and we really need it)
 		my $dep = $symfile->get_dependency($soname);
 		my $minver = $symfile->get_smallest_version($soname) || '';
-		foreach my $subdep (split /\s*,\s*/, $dep) {
-		    if (not exists $dependencies{$cur_field}{$subdep}) {
-			$dependencies{$cur_field}{$subdep} = Dpkg::Version->new($minver);
-                        print " Initialize dependency ($subdep) with minimal " .
-                              "version ($minver)\n" if $debug > 1;
-		    }
-		}
+		update_dependency_version($dep, $minver);
+		print " Minimal version of ($dep) initialized with ($minver)\n"
+		    if $debug > 1;
 	    } else {
 		# No symbol file found, fall back to standard shlibs
                 print "Using shlibs+objdump for $soname (file $lib)\n" if $debug;

commit 8f0122e0c439de1ee4521a9009427aa3704de521

dpkg-shlibdeps: Handle empty minimum versions when initializing them
If the minimum version is empty (which means it was 0), we cannot use it
to compare against the existing minimum version if that exists, and we
should not be overwritting that, either.

Regression introduced in commit 51c55641fb6c66e149ea93c344cb788c68978c75.

Closes: #764929

scripts/dpkg-shlibdeps.pl
@@ -617,7 +617,7 @@ sub update_dependency_version {
 	if (exists $dependencies{$cur_field}{$subdep} and
 	    defined($dependencies{$cur_field}{$subdep}))
 	{
-	    if ($dependencies{$cur_field}{$subdep} eq '' or
+	    if ($dependencies{$cur_field}{$subdep} eq '' or $minver ne '' and
 		version_compare_relation($minver, REL_GT,
 				         $dependencies{$cur_field}{$subdep}))
 	    {