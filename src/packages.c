guillemj/dpkg

commit 35c1c59cfad75d75b3d98974ad201f95d932adb6

dpkg: Defer trigger processing if the packages do not fulfill dependencies
The spec specified this behavior, but the code never abided by it.
Change it now that most of the packages with trigger cycles have been
switched to noawaiting triggers. The rest will make dpkg bail out on
upgrade.

We might need to add appropriate versioned Breaks before the Debian
release to get a smoother distribution upgrade.

Closes: #671711

src/trigproc.c
@@ -316,6 +316,7 @@ trigproc(struct pkginfo *pkg)
 {
 	static struct varbuf namesarg;
 
+	struct varbuf depwhynot = VARBUF_INIT;
 	struct trigpend *tp;
 	struct pkginfo *gaveup;
 
@@ -326,12 +327,34 @@ trigproc(struct pkginfo *pkg)
 	pkg->clientdata->trigprocdeferred = NULL;
 
 	if (pkg->trigpend_head) {
+		enum dep_check ok;
+
 		assert(pkg->status == PKG_STAT_TRIGGERSPENDING ||
 		       pkg->status == PKG_STAT_TRIGGERSAWAITED);
 
 		gaveup = check_trigger_cycle(pkg);
 		if (gaveup == pkg)
 			return;
+
+		ok = dependencies_ok(pkg, NULL, &depwhynot);
+		if (ok == DEP_CHECK_OK) {
+			debug(dbg_triggers, "trigproc %s dependencies satisfied, "
+			      "processing", pkg_name(pkg, pnaw_always));
+		} else if (ok == DEP_CHECK_DEFER) {
+			debug(dbg_triggers, "trigproc %s dependencies unsatisfied, "
+			      "defer processing", pkg_name(pkg, pnaw_always));
+			varbuf_destroy(&depwhynot);
+
+			enqueue_package(pkg);
+			return;
+		} else if (ok == DEP_CHECK_HALT) {
+			varbuf_end_str(&depwhynot);
+			debug(dbg_triggers, "trigproc %s dependencies unsatisfied, "
+			      "skipping due to:\n %s",
+			      pkg_name(pkg, pnaw_always), depwhynot.buf);
+			varbuf_destroy(&depwhynot);
+			return;
+		}
 
 		printf(_("Processing triggers for %s (%s) ...\n"),
 		       pkg_name(pkg, pnaw_nonambig),

commit e121d882c92099d1f1aa9eedd0c0e90806a44efa

dpkg: Fix trigger dependency checks and cycle detection
Break dependency cycles on dependtry > 1 in trigproc(), before calling
dependencies_ok(). But if we have a dependency cycle where a package A
awaits triggers and package P has them pending, and both depend on each
other, the dependency cycle breaking code is not smart enough to break
it at the correct place, as the relationship is directional. So we handle
it specially on deppossi_ok_found(), in case we are in the cycle breaking
dependtry. Otherwise we just defer it, but do not record that it can be
fixed by trigger processing, because we would get into an inifite loop.

Move trigger cycle detection after dependency checks, so that it does
not detect bogus trigger cycles that would have been avoided simply by
the dependency checks and cycle breaking code.

Regression introduced in commit 35c1c59cfad75d75b3d98974ad201f95d932adb6.

Closes: #765434, #765668, #765734, #765781, #765789, #765952

src/packages.c
@@ -414,6 +414,24 @@ deppossi_ok_found(struct pkginfo *possdependee, struct pkginfo *requiredby,
         debug(dbg_depcondetail, "      triggers-awaited, no fixbytrig");
         goto unsuitable;
       }
+      /* If we have a dependency cycle where a package A awaits trigger
+       * processing and package P has them pending, and both depend on each
+       * other, the dependency cycle breaking code is not smart enough to
+       * break it at the correct place, as the relationship is directional.
+       * So we handle it specially here.
+       *
+       * Otherwise we just defer it, but do not record that it can be fixed
+       * by trigger processing, because we would get into an inifite loop. */
+      if (requiredby == possdependee->trigaw.head->pend) {
+        if (dependtry > 1) {
+          debug(dbg_depcondetail, "      triggers-awaited, fixed by us, "
+                                  "break cycle so ok and found");
+          return FOUND_OK;
+        } else {
+          debug(dbg_depcondetail, "      triggers-awaited, fixed by us, defer");
+          return FOUND_DEFER;
+        }
+      }
       /* We don't check the status of trigaw.head->pend here, just in case
        * we get into the pathological situation where Triggers-Awaited but
        * the named package doesn't actually have any pending triggers. In

src/trigproc.c
@@ -332,9 +332,9 @@ trigproc(struct pkginfo *pkg)
 		assert(pkg->status == PKG_STAT_TRIGGERSPENDING ||
 		       pkg->status == PKG_STAT_TRIGGERSAWAITED);
 
-		gaveup = check_trigger_cycle(pkg);
-		if (gaveup == pkg)
-			return;
+		if (dependtry > 1)
+			if (findbreakcycle(pkg))
+				sincenothing = 0;
 
 		ok = dependencies_ok(pkg, NULL, &depwhynot);
 		if (ok == DEP_CHECK_OK) {
@@ -355,6 +355,10 @@ trigproc(struct pkginfo *pkg)
 			varbuf_destroy(&depwhynot);
 			return;
 		}
+
+		gaveup = check_trigger_cycle(pkg);
+		if (gaveup == pkg)
+			return;
 
 		printf(_("Processing triggers for %s (%s) ...\n"),
 		       pkg_name(pkg, pnaw_nonambig),