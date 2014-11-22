guillemj/dpkg

commit ce27f5dc0c3dc6ed7656d09784ea461407765d7d

dpkg: Add a requeueing insertion protection for process_queue()
We should not requeue packages already in the queue. Just protect it
in a similar way in how the deferred triggers queue is protected. In
the future these can be unified.

src/filesdb.c
@@ -105,6 +105,7 @@ ensure_package_clientdata(struct pkginfo *pkg)
   pkg->clientdata = nfmalloc(sizeof(struct perpackagestate));
   pkg->clientdata->istobe = PKG_ISTOBE_NORMAL;
   pkg->clientdata->color = PKG_CYCLE_WHITE;
+  pkg->clientdata->enqueued = false;
   pkg->clientdata->fileslistvalid = false;
   pkg->clientdata->files = NULL;
   pkg->clientdata->replacingfilesandsaid = 0;

src/main.h
@@ -50,6 +50,8 @@ struct perpackagestate {
   /** Used during cycle detection. */
   enum pkg_cycle_color color;
 
+  bool enqueued;
+
   /**
    * filelistvalid  files  Meaning
    * -------------  -----  -------

src/packages.c
@@ -56,6 +56,10 @@ int sincenothing = 0, dependtry = 0;
 void
 enqueue_package(struct pkginfo *pkg)
 {
+  ensure_package_clientdata(pkg);
+  if (pkg->clientdata->enqueued)
+    return;
+  pkg->clientdata->enqueued = true;
   pkg_queue_push(&queue, pkg);
 }
 
@@ -207,6 +211,9 @@ void process_queue(void) {
     pkg = pkg_queue_pop(&queue);
     if (!pkg)
       continue; /* Duplicate, which we removed earlier. */
+
+    ensure_package_clientdata(pkg);
+    pkg->clientdata->enqueued = false;
 
     action_todo = cipaction->arg_int;
 

commit c4e109999d90e99199754e3e419ef90477b2a046

dpkg: Restore multiple processing checks for packages and archives
The notices about duplicate entries not being processed got suppressed
when the requeueing insertion protection got enabled. Add a new variable
to track when packages or archives have been processed more than once.

Regression introduced in commit ce27f5dc0c3dc6ed7656d09784ea461407765d7d.

src/filesdb.c
@@ -109,6 +109,7 @@ ensure_package_clientdata(struct pkginfo *pkg)
   pkg->clientdata->fileslistvalid = false;
   pkg->clientdata->files = NULL;
   pkg->clientdata->replacingfilesandsaid = 0;
+  pkg->clientdata->cmdline_seen = 0;
   pkg->clientdata->listfile_phys_offs = 0;
   pkg->clientdata->trigprocdeferred = NULL;
 }

src/main.h
@@ -64,6 +64,7 @@ struct perpackagestate {
   bool fileslistvalid;
   struct fileinlist *files;
   int replacingfilesandsaid;
+  int cmdline_seen;
 
   off_t listfile_phys_offs;
 
@@ -193,6 +194,7 @@ int clearselections(const char *const *argv);
 
 void md5hash(struct pkginfo *pkg, char *hashbuf, const char *fn);
 void enqueue_package(struct pkginfo *pkg);
+void enqueue_package_mark_seen(struct pkginfo *pkg);
 void process_queue(void);
 int packages(const char *const *argv);
 void removal_bulk(struct pkginfo *pkg);

src/packages.c
@@ -63,6 +63,13 @@ enqueue_package(struct pkginfo *pkg)
   pkg_queue_push(&queue, pkg);
 }
 
+void
+enqueue_package_mark_seen(struct pkginfo *pkg)
+{
+  enqueue_package(pkg);
+  pkg->clientdata->cmdline_seen++;
+}
+
 static void
 enqueue_pending(void)
 {
@@ -119,7 +126,7 @@ enqueue_specified(const char *const *argv)
       badusage(_("you must specify packages by their own names, "
                  "not by quoting the names of the files they come in"));
     }
-    enqueue_package(pkg);
+    enqueue_package_mark_seen(pkg);
   }
 }
 
@@ -185,8 +192,10 @@ void process_queue(void) {
   }
   for (rundown = queue.head; rundown; rundown = rundown->next) {
     ensure_package_clientdata(rundown->pkg);
-    if (rundown->pkg->clientdata->istobe == istobe) {
-      /* Erase the queue entry - this is a second copy! */
+
+    /* We have processed this package more than once. There are no duplicates
+     * as we make sure of that when enqueuing them. */
+    if (rundown->pkg->clientdata->cmdline_seen > 1) {
       switch (cipaction->arg_int) {
       case act_triggers:
       case act_configure: case act_remove: case act_purge:
@@ -201,10 +210,8 @@ void process_queue(void) {
       default:
         internerr("unknown action '%d'", cipaction->arg_int);
       }
-      rundown->pkg = NULL;
-   } else {
-      rundown->pkg->clientdata->istobe= istobe;
     }
+    rundown->pkg->clientdata->istobe = istobe;
   }
 
   while (!pkg_queue_is_empty(&queue)) {

src/unpack.c
@@ -1480,5 +1480,5 @@ void process_archive(const char *filename) {
   }
 
   if (cipaction->arg_int == act_install)
-    enqueue_package(pkg);
+    enqueue_package_mark_seen(pkg);
 }