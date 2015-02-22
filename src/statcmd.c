guillemj/dpkg

commit e4d6db177fad401ddc8432cf0e2c64e4fcf7bc0d

dpkg-statoverride: Do not abort when user and group names are unknown
When parsing the statoverride database from dpkg-statoverride do
not consider it an error and refuse to operate at all if the user
or group names are not known to the system, just preserve them.

Closes: #563307

src/statcmd.c
@@ -122,7 +122,15 @@ statdb_node_new(const char *user, const char *group, const char *mode)
 	filestat = nfmalloc(sizeof(*filestat));
 
 	filestat->uid = statdb_parse_uid(user);
+	if (filestat->uid == (uid_t)-1)
+		filestat->uname = user;
+	else
+		filestat->uname = NULL;
 	filestat->gid = statdb_parse_gid(group);
+	if (filestat->gid == (gid_t)-1)
+		filestat->gname = group;
+	else
+		filestat->gname = NULL;
 	filestat->mode = statdb_parse_mode(mode);
 
 	return filestat;
@@ -174,12 +182,16 @@ statdb_node_print(FILE *out, struct filenamenode *file)
 	pw = getpwuid(filestat->uid);
 	if (pw)
 		fprintf(out, "%s ", pw->pw_name);
+	else if (filestat->uname)
+		fprintf(out, "%s ", filestat->uname);
 	else
 		fprintf(out, "#%d ", filestat->uid);
 
 	gr = getgrgid(filestat->gid);
 	if (gr)
 		fprintf(out, "%s ", gr->gr_name);
+	else if (filestat->gname)
+		fprintf(out, "%s ", filestat->gname);
 	else
 		fprintf(out, "#%d ", filestat->gid);
 
@@ -356,7 +368,7 @@ main(int argc, const char *const *argv)
 		badusage(_("need an action option"));
 
 	filesdbinit();
-	ensure_statoverrides();
+	ensure_statoverrides(STATDB_PARSE_LAX);
 
 	ret = cipaction->action(argv);
 

lib/dpkg/file.h
@@ -39,6 +39,11 @@ struct file_stat {
 	uid_t uid;
 	gid_t gid;
 	mode_t mode;
+
+	/* Used by dpkg-statoverride when parsing the database to preserve the
+	 * user and group names in case the system does not know about them. */
+	const char *uname;
+	const char *gname;
 };
 
 void file_copy_perms(const char *src, const char *dst);

src/archives.c
@@ -1643,7 +1643,7 @@ archivefiles(const char *const *argv)
   fnameidlu= fnamevb.used;
 
   ensure_diversions();
-  ensure_statoverrides();
+  ensure_statoverrides(STATDB_PARSE_NORMAL);
 
   while ((thisarg = *argp++) != NULL) {
     if (setjmp(ejbuf)) {

src/filesdb.h
@@ -155,10 +155,15 @@ void ensure_package_clientdata(struct pkginfo *pkg);
 
 void ensure_diversions(void);
 
+enum statdb_parse_flags {
+	STATDB_PARSE_NORMAL = 0,
+	STATDB_PARSE_LAX = 1,
+};
+
 uid_t statdb_parse_uid(const char *str);
 gid_t statdb_parse_gid(const char *str);
 mode_t statdb_parse_mode(const char *str);
-void ensure_statoverrides(void);
+void ensure_statoverrides(enum statdb_parse_flags flags);
 
 #define LISTFILE           "list"
 #define HASHFILE           "md5sums"

src/statdb.c
@@ -60,10 +60,11 @@ statdb_parse_uid(const char *str)
 		uid = (uid_t)value;
 	} else {
 		struct passwd *pw = getpwnam(str);
+
 		if (pw == NULL)
-			ohshit(_("syntax error: unknown user '%s' in statoverride file"),
-			       str);
-		uid = pw->pw_uid;
+			uid = (uid_t)-1;
+		else
+			uid = pw->pw_uid;
 	}
 
 	return uid;
@@ -85,10 +86,11 @@ statdb_parse_gid(const char *str)
 		gid = (gid_t)value;
 	} else {
 		struct group *gr = getgrnam(str);
+
 		if (gr == NULL)
-			ohshit(_("syntax error: unknown group '%s' in statoverride file"),
-			       str);
-		gid = gr->gr_gid;
+			gid = (gid_t)-1;
+		else
+			gid = gr->gr_gid;
 	}
 
 	return gid;
@@ -108,7 +110,7 @@ statdb_parse_mode(const char *str)
 }
 
 void
-ensure_statoverrides(void)
+ensure_statoverrides(enum statdb_parse_flags flags)
 {
 	static struct stat sb_prev;
 	struct stat sb_next;
@@ -201,6 +203,14 @@ ensure_statoverrides(void)
 		*ptr = '\0';
 
 		fso->uid = statdb_parse_uid(thisline);
+		if (fso->uid == (uid_t)-1)
+			fso->uname = thisline;
+		else
+			fso->uname = NULL;
+
+		if (fso->uid == (uid_t)-1 && !(flags & STATDB_PARSE_LAX))
+			ohshit(_("unknown user '%s' in statoverride file"),
+			       thisline);
 
 		/* Move to the next bit */
 		thisline = ptr + 1;
@@ -214,6 +224,14 @@ ensure_statoverrides(void)
 		*ptr = '\0';
 
 		fso->gid = statdb_parse_gid(thisline);
+		if (fso->gid == (gid_t)-1)
+			fso->gname = thisline;
+		else
+			fso->gname = NULL;
+
+		if (fso->gid == (gid_t)-1 && !(flags & STATDB_PARSE_LAX))
+			ohshit(_("unknown group '%s' in statoverride file"),
+			       thisline);
 
 		/* Move to the next bit */
 		thisline = ptr + 1;

commit 5c58085b746d234066ea94d386182cfc72c612ba

dpkg-statoverride: Do not accept unknown user or group names on --add
We should not allow adding user or group names to the database that do
not exist in the system passwd database, as the main dpkg program uses
strict parsing and will fail with an unrecoverable fatal error if it
cannot find a matching name for a uid/gid.

Regression introduced in commit e4d6db177fad401ddc8432cf0e2c64e4fcf7bc0d,
where we went from too strict to too lax parsing.

Closes: #775124

src/statcmd.c
@@ -123,14 +123,12 @@ statdb_node_new(const char *user, const char *group, const char *mode)
 
 	filestat->uid = statdb_parse_uid(user);
 	if (filestat->uid == (uid_t)-1)
-		filestat->uname = nfstrsave(user);
-	else
-		filestat->uname = NULL;
+		ohshit(_("user '%s' does not exist"), user);
+	filestat->uname = NULL;
 	filestat->gid = statdb_parse_gid(group);
 	if (filestat->gid == (gid_t)-1)
-		filestat->gname = nfstrsave(group);
-	else
-		filestat->gname = NULL;
+		ohshit(_("group '%s' does not exist"), group);
+	filestat->gname = NULL;
 	filestat->mode = statdb_parse_mode(mode);
 
 	return filestat;