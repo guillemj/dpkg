guillemj/dpkg

commit 4266c68bc4516d706544555026b00c5be759b24c

dpkg-maintscript-helper: Handle symlinks and pathnames ending in slash
For symlinks, error out as the argument is obviously bogus. For
pathnames just strip the trailing slash, as this is a common way to
name directories.

Closes: #771752

scripts/dpkg-maintscript-helper.sh
@@ -254,6 +254,8 @@ symlink_to_dir() {
 	[ -n "$SYMLINK" ] || error "symlink parameter is missing"
 	[ "${SYMLINK#/}" = "$SYMLINK" ] && \
 		error "symlink pathname is not an absolute path"
+	[ "${SYMLINK%/}" = "$SYMLINK" ] && \
+		error "symlink pathname ends with a slash"
 	[ -n "$SYMLINK_TARGET" ] || error "original symlink target is missing"
 	[ -n "$1" ] || error "maintainer script parameters are missing"
 
@@ -305,7 +307,7 @@ symlink_to_dir() {
 ## Functions to replace a directory with a symlink
 ##
 dir_to_symlink() {
-	local PATHNAME="$1"
+	local PATHNAME="${1%/}"
 	local SYMLINK_TARGET="$2"
 	local LASTVERSION="$3"
 	local PACKAGE="$4"

commit f027f743929abca7c1351c5f8150e10272072415

dpkg-maintscript-helper: Fix logic in symlink argument check
The check was inverted.

Regression introduced in commit 4266c68bc4516d706544555026b00c5be759b24c.

scripts/dpkg-maintscript-helper.sh
@@ -256,7 +256,7 @@ symlink_to_dir() {
 	[ -n "$SYMLINK" ] || error "symlink parameter is missing"
 	[ "${SYMLINK#/}" = "$SYMLINK" ] && \
 		error "symlink pathname is not an absolute path"
-	[ "${SYMLINK%/}" = "$SYMLINK" ] && \
+	[ "${SYMLINK%/}" = "$SYMLINK" ] || \
 		error "symlink pathname ends with a slash"
 	[ -n "$SYMLINK_TARGET" ] || error "original symlink target is missing"
 	[ -n "$1" ] || error "maintainer script parameters are missing"