guillemj/dpkg

commit ab15fd24dd1a8207ab1463410338f24283989f7c

Dpkg::Dist::Files: Parse filename on add_file() to initialize files values

scripts/Dpkg/Dist/Files.pm
@@ -110,15 +110,12 @@ sub get_file {
 sub add_file {
     my ($self, $filename, $section, $priority) = @_;
 
-    # XXX: Ideally we'd need to parse the filename, to match the behaviour
-    # on parse(), and initialize the other attributes, although no code is
-    # in need of this for now, at least in dpkg-dev.
+    my $file = $self->parse_filename($filename);
+    error(g_('invalid filename %s'), $filename) unless defined $file;
+    $file->{section} = $section;
+    $file->{priority} = $priority;
 
-    $self->{files}->{$filename} = {
-        filename => $filename,
-        section => $section,
-        priority => $priority,
-    };
+    $self->{files}->{$filename} = $file;
 }
 
 sub del_file {

scripts/t/Dpkg_Dist_Files.t
@@ -16,7 +16,7 @@
 use strict;
 use warnings;
 
-use Test::More tests => 15;
+use Test::More tests => 17;
 
 use_ok('Dpkg::Dist::Files');
 
@@ -62,6 +62,11 @@ my %expected = (
         section => 'webdocs',
         priority => 'optional',
     },
+    'added-on-the-fly' => {
+        filename => 'added-on-the-fly',
+        section => 'void',
+        priority => 'wish',
+    },
 );
 
 my $dist = Dpkg::Dist::Files->new();
@@ -96,7 +101,16 @@ pkg-templ_1.2.3_arch.type section priority
 FILES
 
 $dist->add_file('added-on-the-fly', 'void', 'wish');
+is_deeply($dist->get_file('added-on-the-fly'), $expected{'added-on-the-fly'},
+    'Get added file added-on-the-fly');
+
 $dist->add_file('pkg-arch_2.0.0_amd64.deb', 'void', 'imperative');
+my %expected_pkg_arch = %{$expected{'pkg-arch_2.0.0_amd64.deb'}};
+$expected_pkg_arch{section} = 'void';
+$expected_pkg_arch{priority} = 'imperative';
+is_deeply($dist->get_file('pkg-arch_2.0.0_amd64.deb'), \%expected_pkg_arch,
+    'Get modified file pkg-arch_2.0.0_amd64.deb');
+
 $dist->del_file('pkg-indep_0.0.1-2_all.deb');
 is($dist->get_file('unknown'), undef, 'Get unknown file');
 is($dist->get_file('pkg-indep_0.0.1-2_all.deb'), undef, 'Get deleted file');

commit 448faad0f04d7f56e5b708ff76213fe0f92c2ae2

Dpkg::Dist::Files: Allow ~ in added filenames
When we are constructing a .changes file we add the source artifacts
to the distribution manually, before loading the distribution list
from debian/files.

We should allow ~ in the filename as that's a valid character in a
version number.

Regression introduced in commit ab15fd24dd1a8207ab1463410338f24283989f7c.

Reported-by: Niels Thykier <niels@thykier.net>

scripts/Dpkg/Dist/Files.pm
@@ -58,7 +58,7 @@ sub parse_filename {
         $file->{version} = $3;
         $file->{arch} = $4;
         $file->{package_type} = $5;
-    } elsif ($fn =~ m/^([-+.,_0-9a-zA-Z]+)$/) {
+    } elsif ($fn =~ m/^([-+.,_0-9a-zA-Z~]+)$/) {
         $file->{filename} = $1;
     } else {
         $file = undef;

scripts/t/Dpkg_Dist_Files.t
@@ -16,7 +16,7 @@
 use strict;
 use warnings;
 
-use Test::More tests => 20;
+use Test::More tests => 23;
 
 use_ok('Dpkg::Dist::Files');
 
@@ -25,6 +25,16 @@ my $datadir = $srcdir . '/t/Dpkg_Dist_Files';
 
 my $expected;
 my %expected = (
+    'pkg-src_2.0+1A~rc1-1.dsc' => {
+        filename => 'pkg-src_2.0+1A~rc1-1.dsc',
+        section => 'source',
+        priority => 'extra',
+    },
+    'pkg-src_2.0+1A~rc1-1.tar.xz' => {
+        filename => 'pkg-src_2.0+1A~rc1-1.tar.xz',
+        section => 'source',
+        priority => 'extra',
+    },
     'pkg-templ_1.2.3_arch.type' => {
         filename => 'pkg-templ_1.2.3_arch.type',
         package => 'pkg-templ',
@@ -115,6 +125,23 @@ $dist->del_file('pkg-indep_0.0.1-2_all.deb');
 is($dist->get_file('unknown'), undef, 'Get unknown file');
 is($dist->get_file('pkg-indep_0.0.1-2_all.deb'), undef, 'Get deleted file');
 is($dist->output(), $expected, 'Modified dist object');
+
+$expected = <<'FILES';
+pkg-src_2.0+1A~rc1-1.dsc source extra
+pkg-src_2.0+1A~rc1-1.tar.xz source extra
+FILES
+
+$dist->reset();
+$dist->add_file('pkg-src_2.0+1A~rc1-1.dsc', 'source', 'extra');
+$dist->add_file('pkg-src_2.0+1A~rc1-1.tar.xz', 'source', 'extra');
+
+is_deeply($dist->get_file('pkg-src_2.0+1A~rc1-1.dsc'),
+          $expected{'pkg-src_2.0+1A~rc1-1.dsc'},
+          'Get added file pkg-src_2.0+1A~rc1-1.dsc');
+is_deeply($dist->get_file('pkg-src_2.0+1A~rc1-1.tar.xz'),
+          $expected{'pkg-src_2.0+1A~rc1-1.tar.xz'},
+          'Get added file pkg-src_2.0+1A~rc1-1.tar.xz');
+is($dist->output, $expected, 'Added source files');
 
 $expected = <<'FILES';
 pkg-arch_2.0.0_amd64.deb admin required