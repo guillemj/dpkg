guillemj/dpkg

commit ab15fd24dd1a8207ab1463410338f24283989f7c

Dpkg::Dist::Files: Parse filename on add_file() to initialize files values

scripts/Dpkg/Dist/Files.pm
@@ -110,15 +110,12 @@ sub get_file {
 sub add_file {
     my ($self, $filename, $section, $priority) = @_;
 
-    # XXX: Ideally we'd need to parse the filename, to match the behaviour
-    # on parse(), and initialize the other attributes, although no code is
-    # in need of this for now, at least in dpkg-dev.
+    my $file = $self->parse_filename($filename);
+    error(g_('invalid filename %s'), $filename) unless defined $file;
+    $file->{section} = $section;
+    $file->{priority} = $priority;
 
-    $self->{files}->{$filename} = {
-        filename => $filename,
-        section => $section,
-        priority => $priority,
-    };
+    $self->{files}->{$filename} = $file;
 }
 
 sub del_file {

scripts/t/Dpkg_Dist_Files.t
@@ -16,7 +16,7 @@
 use strict;
 use warnings;
 
-use Test::More tests => 15;
+use Test::More tests => 17;
 
 use_ok('Dpkg::Dist::Files');
 
@@ -62,6 +62,11 @@ my %expected = (
         section => 'webdocs',
         priority => 'optional',
     },
+    'added-on-the-fly' => {
+        filename => 'added-on-the-fly',
+        section => 'void',
+        priority => 'wish',
+    },
 );
 
 my $dist = Dpkg::Dist::Files->new();
@@ -96,7 +101,16 @@ pkg-templ_1.2.3_arch.type section priority
 FILES
 
 $dist->add_file('added-on-the-fly', 'void', 'wish');
+is_deeply($dist->get_file('added-on-the-fly'), $expected{'added-on-the-fly'},
+    'Get added file added-on-the-fly');
+
 $dist->add_file('pkg-arch_2.0.0_amd64.deb', 'void', 'imperative');
+my %expected_pkg_arch = %{$expected{'pkg-arch_2.0.0_amd64.deb'}};
+$expected_pkg_arch{section} = 'void';
+$expected_pkg_arch{priority} = 'imperative';
+is_deeply($dist->get_file('pkg-arch_2.0.0_amd64.deb'), \%expected_pkg_arch,
+    'Get modified file pkg-arch_2.0.0_amd64.deb');
+
 $dist->del_file('pkg-indep_0.0.1-2_all.deb');
 is($dist->get_file('unknown'), undef, 'Get unknown file');
 is($dist->get_file('pkg-indep_0.0.1-2_all.deb'), undef, 'Get deleted file');

commit 354bdeeee01d3faf8c19e13f7fcbd6ee5cc7b83a

Dpkg::Dist::Files: Allow colons (:) in added filenames
Regression introduced in commit ab15fd24dd1a8207ab1463410338f24283989f7c.

Packages can contain colons in their filenames if the upstream version
contains colons.

Reported-by: Jakub Wilk <jwilk@debian.org>

scripts/Dpkg/Dist/Files.pm
@@ -52,13 +52,13 @@ sub parse_filename {
 
     my $file;
 
-    if ($fn =~ m/^(([-+.0-9a-z]+)_([^_]+)_([-\w]+)\.([a-z0-9.]+))$/) {
+    if ($fn =~ m/^(([-+:.0-9a-z]+)_([^_]+)_([-\w]+)\.([a-z0-9.]+))$/) {
         $file->{filename} = $1;
         $file->{package} = $2;
         $file->{version} = $3;
         $file->{arch} = $4;
         $file->{package_type} = $5;
-    } elsif ($fn =~ m/^([-+.,_0-9a-zA-Z~]+)$/) {
+    } elsif ($fn =~ m/^([-+:.,_0-9a-zA-Z~]+)$/) {
         $file->{filename} = $1;
     } else {
         $file = undef;

scripts/t/Dpkg_Dist_Files.t
@@ -16,7 +16,7 @@
 use strict;
 use warnings;
 
-use Test::More tests => 23;
+use Test::More tests => 24;
 
 use_ok('Dpkg::Dist::Files');
 
@@ -25,13 +25,13 @@ my $datadir = $srcdir . '/t/Dpkg_Dist_Files';
 
 my $expected;
 my %expected = (
-    'pkg-src_2.0+1A~rc1-1.dsc' => {
-        filename => 'pkg-src_2.0+1A~rc1-1.dsc',
+    'pkg-src_4:2.0+1A~rc1-1.dsc' => {
+        filename => 'pkg-src_4:2.0+1A~rc1-1.dsc',
         section => 'source',
         priority => 'extra',
     },
-    'pkg-src_2.0+1A~rc1-1.tar.xz' => {
-        filename => 'pkg-src_2.0+1A~rc1-1.tar.xz',
+    'pkg-src_4:2.0+1A~rc1-1.tar.xz' => {
+        filename => 'pkg-src_4:2.0+1A~rc1-1.tar.xz',
         section => 'source',
         priority => 'extra',
     },
@@ -71,6 +71,11 @@ my %expected = (
         filename => 'BY-HAND-file',
         section => 'webdocs',
         priority => 'optional',
+    },
+    'another:filename' => {
+        filename => 'another:filename',
+        section => 'by-hand',
+        priority => 'extra',
     },
     'added-on-the-fly' => {
         filename => 'added-on-the-fly',
@@ -127,20 +132,25 @@ is($dist->get_file('pkg-indep_0.0.1-2_all.deb'), undef, 'Get deleted file');
 is($dist->output(), $expected, 'Modified dist object');
 
 $expected = <<'FILES';
-pkg-src_2.0+1A~rc1-1.dsc source extra
-pkg-src_2.0+1A~rc1-1.tar.xz source extra
+another:filename by-hand extra
+pkg-src_4:2.0+1A~rc1-1.dsc source extra
+pkg-src_4:2.0+1A~rc1-1.tar.xz source extra
 FILES
 
 $dist->reset();
-$dist->add_file('pkg-src_2.0+1A~rc1-1.dsc', 'source', 'extra');
-$dist->add_file('pkg-src_2.0+1A~rc1-1.tar.xz', 'source', 'extra');
+$dist->add_file('pkg-src_4:2.0+1A~rc1-1.dsc', 'source', 'extra');
+$dist->add_file('pkg-src_4:2.0+1A~rc1-1.tar.xz', 'source', 'extra');
+$dist->add_file('another:filename', 'by-hand', 'extra');
 
-is_deeply($dist->get_file('pkg-src_2.0+1A~rc1-1.dsc'),
-          $expected{'pkg-src_2.0+1A~rc1-1.dsc'},
-          'Get added file pkg-src_2.0+1A~rc1-1.dsc');
-is_deeply($dist->get_file('pkg-src_2.0+1A~rc1-1.tar.xz'),
-          $expected{'pkg-src_2.0+1A~rc1-1.tar.xz'},
-          'Get added file pkg-src_2.0+1A~rc1-1.tar.xz');
+is_deeply($dist->get_file('pkg-src_4:2.0+1A~rc1-1.dsc'),
+          $expected{'pkg-src_4:2.0+1A~rc1-1.dsc'},
+          'Get added file pkg-src_4:2.0+1A~rc1-1.dsc');
+is_deeply($dist->get_file('pkg-src_4:2.0+1A~rc1-1.tar.xz'),
+          $expected{'pkg-src_4:2.0+1A~rc1-1.tar.xz'},
+          'Get added file pkg-src_4:2.0+1A~rc1-1.tar.xz');
+is_deeply($dist->get_file('another:filename'),
+          $expected{'another:filename'},
+          'Get added file another:filename');
 is($dist->output, $expected, 'Added source files');
 
 $expected = <<'FILES';