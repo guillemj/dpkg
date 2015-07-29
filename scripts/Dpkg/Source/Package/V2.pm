guillemj/dpkg

commit 5e59d94a17051b1185c591a1fc3eed00e3b01070

perl: Switch from legacy File::Path functions to new ones
Use make_path() instead of mkpath(), and remove_tree() instead of
rmtree().

scripts/Dpkg/Source/Package/V2.pm
@@ -42,7 +42,7 @@ use POSIX qw(:errno_h);
 use Cwd;
 use File::Basename;
 use File::Temp qw(tempfile tempdir);
-use File::Path;
+use File::Path qw(make_path);
 use File::Spec;
 use File::Find;
 use File::Copy;
@@ -537,7 +537,7 @@ sub do_build {
 
     # Install the diff as the new autopatch
     if ($self->{options}{auto_commit}) {
-        mkpath(File::Spec->catdir($dir, 'debian', 'patches'));
+        make_path(File::Spec->catdir($dir, 'debian', 'patches'));
         $autopatch = $self->register_patch($dir, $tmpdiff,
                                            $self->get_autopatch_name());
         info(g_('local changes have been recorded in a new patch: %s'),
@@ -679,7 +679,7 @@ sub do_commit {
         $patch_name =~ s/\s+/-/g;
         $patch_name =~ s/\///g;
     }
-    mkpath(File::Spec->catdir($dir, 'debian', 'patches'));
+    make_path(File::Spec->catdir($dir, 'debian', 'patches'));
     my $patch = $self->register_patch($dir, $tmpdiff, $patch_name);
     my @editors = ('sensible-editor', $ENV{VISUAL}, $ENV{EDITOR}, 'vi');
     my $editor = first { find_command($_) } @editors;
@@ -750,7 +750,7 @@ sub update_debian_source_include_binaries {
     return unless scalar(@unknown_binaries);
 
     my $incbin_file = $self->{include_binaries_path};
-    mkpath(File::Spec->catdir($self->{dir}, 'debian', 'source'));
+    make_path(File::Spec->catdir($self->{dir}, 'debian', 'source'));
     open(my $incbin_fh, '>>', $incbin_file)
         or syserr(g_('cannot write %s'), $incbin_file);
     foreach my $binary (@unknown_binaries) {

dselect/methods/ftp/install.pl
@@ -21,7 +21,7 @@ use warnings;
 
 eval q{
     use Net::FTP;
-    use File::Path;
+    use File::Path qw(make_path remove_tree);
     use File::Basename;
     use File::Find;
     use Data::Dumper;
@@ -56,7 +56,7 @@ my $methdir = "$vardir/methods/ftp";
 read_config("$methdir/vars");
 
 chdir "$methdir";
-mkpath(["$methdir/$CONFIG{dldir}"], 0, 0755);
+make_path("$methdir/$CONFIG{dldir}", { mode => 0755 });
 
 
 #Read md5sums already calculated
@@ -230,7 +230,7 @@ foreach my $pkg (keys(%pkgs)) {
 	}
 	$dir = dirname($fn);
 	if(! -d "$dldir/$dir") {
-	    mkpath(["$dldir/$dir"], 0, 0755);
+	    make_path("$dldir/$dir", { mode => 0755 });
 	}
 	@info = @{$pkgfiles{$fn}};
 	$csize = int($info[1]/1024)+1;
@@ -615,7 +615,7 @@ if (yesno('y', "\nDo you wish to delete the installed package (.deb) files?")) {
 
 # remove whole ./debian directory if user wants to
 if (yesno('n', "\nDo you want to remove $dldir directory?")) {
-    rmtree("$dldir");
+    remove_tree($dldir);
 }
 
 #Store useful md5sums

scripts/Dpkg/Source/Patch.pm
@@ -30,7 +30,7 @@ use POSIX qw(:errno_h :sys_wait_h);
 use File::Find;
 use File::Basename;
 use File::Spec;
-use File::Path;
+use File::Path qw(make_path);
 use File::Compare;
 use Fcntl ':mode';
 #XXX: Needed for sub-second timestamps, require recent perl
@@ -549,7 +549,7 @@ sub prepare_apply {
     my ($self, $analysis, %opts) = @_;
     if ($opts{create_dirs}) {
 	foreach my $dir (keys %{$analysis->{dirtocreate}}) {
-	    eval { mkpath($dir, 0, 0777); };
+	    eval { make_path($dir, { mode => 0777 }); };
 	    syserr(g_('cannot create directory %s'), $dir) if $@;
 	}
     }

scripts/dpkg-name.pl
@@ -22,7 +22,7 @@ use warnings;
 use strict;
 
 use File::Basename;
-use File::Path;
+use File::Path qw(make_path);
 
 use Dpkg ();
 use Dpkg::Gettext;
@@ -183,7 +183,7 @@ sub move($)
         my $dir = getdir($filename, $fields, $arch);
         if (! -d $dir) {
             if ($options{createdir}) {
-                if (mkpath($dir)) {
+                if (make_path($dir)) {
                     info(g_("created directory '%s'"), $dir);
                 } else {
                     error(g_("cannot create directory '%s'"), $dir);

commit 81b7b5c3435a571b82938d1179c77803ac88be76

Dpkg::Source::Package::V2: Import make_path from File::Path
Regression introduced in commit 5e59d94a17051b1185c591a1fc3eed00e3b01070.

Closes: #789957

scripts/Dpkg/Source/Package/V2.pm
@@ -694,7 +694,7 @@ package Dpkg::Source::Package::V2::BinaryFiles;
 use Dpkg::ErrorHandling;
 use Dpkg::Gettext;
 
-use File::Path;
+use File::Path qw(make_path);
 
 sub new {
     my ($this, $dir) = @_;