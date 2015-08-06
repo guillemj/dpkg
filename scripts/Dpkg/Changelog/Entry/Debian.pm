guillemj/dpkg

commit 7a71b4b78e8a81158c45073dee05b0d1cc46b71c

Dpkg::Changelog::Entry::Debian: Use Time::Piece instead of Date::Parse
The new module is in the perl core distribution, which reduces the build
and run-time dependencies, helping with architecture bootstrapping.

Prompted-by: Helmut Grohne <helmut@subdivi.de>

scripts/Dpkg/Changelog/Entry/Debian.pm
@@ -29,7 +29,7 @@ our @EXPORT_OK = qw(
 );
 
 use Exporter qw(import);
-use Date::Parse;
+use Time::Piece;
 
 use Dpkg::Gettext;
 use Dpkg::Control::Fields;
@@ -164,7 +164,13 @@ sub check_trailer {
 	if ($3 ne '  ') {
 	    push @errors, g_('badly formatted trailer line');
 	}
-	unless (defined str2time($4)) {
+
+	my $fmt = '';
+	$fmt .= '%a, ' if defined $5;
+	$fmt .= '%d %b %Y %T %z';
+
+	local $ENV{LC_ALL} = 'C';
+	unless (defined Time::Piece->strptime($4, $fmt)) {
 	    push @errors, sprintf(g_("couldn't parse date %s"), $4);
 	}
     } else {

README
@@ -76,7 +76,6 @@ To enable optional functionality or programs, this software might be needed:
 
 To run the test suite («make check»):
 
-  TimeDate perl module
   IO-String perl module
   Test::Pod perl module (optional)
   Test::Strict perl module (optional)

debian/control
@@ -15,7 +15,7 @@ Build-Depends: debhelper (>= 9), pkg-config, flex,
  libselinux1-dev (>= 1.28-4) [linux-any],
  libkvm-dev [kfreebsd-any],
  libncursesw5-dev,
- libtimedate-perl, libio-string-perl
+ libio-string-perl
 
 Package: libdpkg-dev
 Section: libdevel
@@ -68,7 +68,7 @@ Section: perl
 Priority: optional
 Architecture: all
 Multi-Arch: foreign
-Depends: dpkg (>= 1.16.3), perl, libtimedate-perl, ${misc:Depends}
+Depends: dpkg (>= 1.16.3), perl, ${misc:Depends}
 Recommends: libfile-fcntllock-perl, bzip2, xz-utils
 Suggests: debian-keyring, gnupg | gnupg2, gpgv | gpgv2,
  gcc | c-compiler, binutils, patch

commit afef8fbf76e4d87ea8ab824370507f5017a4818e

Dpkg::Changelog::Entry::Debian: Only warn on invalid week days
Regression introduced in commit 7a71b4b78e8a81158c45073dee05b0d1cc46b71c.

The previous implementation using Date::Parse ignored invalid week
days, and the new one using Time::Piece is strict, so we get fatal
errors. Validate the week day ourselves, emit a warning in case of
an invalid value, and ignore it when passing the value to strptime
from Time::Piece.

Reported-by: Jakub Wilk <jwilk@debian.org>

scripts/Dpkg/Changelog/Entry/Debian.pm
@@ -64,7 +64,9 @@ our $regex_header = qr/^(\w$name_chars*) \(([^\(\) \t]+)\)((?:\s+$name_chars+)+)
 
 # The matched content is the maintainer name ($1), its email ($2),
 # some blanks ($3) and the timestamp ($4).
-our $regex_trailer = qr/^ \-\- (.*) <(.*)>(  ?)((\w+\,\s*)?\d{1,2}\s+\w+\s+\d{4}\s+\d{1,2}:\d\d:\d\d\s+[-+]\d{4})\s*$/o;
+our $regex_trailer = qr/^ \-\- (.*) <(.*)>(  ?)(((\w+)\,\s*)?(\d{1,2}\s+\w+\s+\d{4}\s+\d{1,2}:\d\d:\d\d\s+[-+]\d{4}))\s*$/o;
+
+my %week_day = map { $_ => 1 } qw(Mon Tue Wed Thu Fri Sat Sun);
 
 ## use critic
 
@@ -165,12 +167,15 @@ sub check_trailer {
 	    push @errors, g_('badly formatted trailer line');
 	}
 
-	my $fmt = '';
-	$fmt .= '%a, ' if defined $5;
-	$fmt .= '%d %b %Y %T %z';
+	# Validate the week day. Date::Parse used to ignore it, but Time::Piece
+	# is much more strict and it does not gracefully handle bogus values.
+	if (defined $5 and not exists $week_day{$6}) {
+	    push @errors, sprintf(g_('ignoring invalid week day \'%s\''), $6);
+	}
 
+	# Ignore the week day ('%a, '), as we have validated it above.
 	local $ENV{LC_ALL} = 'C';
-	unless (defined Time::Piece->strptime($4, $fmt)) {
+	unless (defined Time::Piece->strptime($7, '%d %b %Y %T %z')) {
 	    push @errors, sprintf(g_("couldn't parse date %s"), $4);
 	}
     } else {