f467eb0f53d3b78e8565790e8caf008b60abac66
Guillem Jover <guillem@debian.org>
2015-09-15 15:28:39 +0200

    libdpkg: Fix inadvertent license change back from GPL2 to GPL2+
    In commit b95907e6e0f3f25136fb2ebcc8d3489efb208dea, several functions
    were refactored out from src/filesdb.c (with a GPL2+ license), into
    lib/utils.c (with a GPL2 license), inadvertently making them change
    license.
    
    Ian clarified that this was a mistake in:
    
      <https://lists.debian.org/debian-dpkg/2015/08/msg00029.html>
    
    Signed-off-by: Ian Jackson <ijackson@chiark.greenend.org.uk>
    Signed-off-by: Guillem Jover <guillem@debian.org>

--- lib/dpkg/utils.c
+++ lib/dpkg/utils.c
@@ -2,9 +2,12 @@
  * libdpkg - Debian packaging suite library routines
  * utils.c - helper functions for dpkg
  *
+ * Copyright © 1995, 2008 Ian Jackson <ian@chiark.greenend.org.uk>
+ *
  * This is free software; you can redistribute it and/or modify
- * it under the terms of version 2 of the GNU General Public
- * License version 2 as published by the Free Software Foundation.
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
  * This is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
