guillemj/dpkg

commit d287060bb1a45f5de33eb27034a7d8b27b039dbb

scripts: Use error() and errormsg() instead of printing on STDERR directly
This way any transformation done for error messages gets applied
consistently to all error output.

scripts/dpkg-checkbuilddeps.pl
@@ -138,12 +138,12 @@ if ($bc_value) {
 }
 
 if (@unmet) {
-	printf { *STDERR } g_('%s: Unmet build dependencies: '), $Dpkg::PROGNAME;
-	print { *STDERR } join(' ', map { $_->output() } @unmet), "\n";
+	error(g_('Unmet build dependencies: %s'),
+	      join(' ', map { $_->output() } @unmet));
 }
 if (@conflicts) {
-	printf { *STDERR } g_('%s: Build conflicts: '), $Dpkg::PROGNAME;
-	print { *STDERR } join(' ', map { $_->output() } @conflicts), "\n";
+	error(g_('Build conflicts: %s'),
+	      join(' ', map { $_->output() } @conflicts));
 }
 exit 1 if @unmet || @conflicts;
 

scripts/Dpkg/Source/Package/V1.pm
@@ -403,8 +403,7 @@ sub do_build {
     }
 
     if ($ur) {
-        printf { *STDERR } g_('%s: unrepresentable changes to source') . "\n",
-               $Dpkg::PROGNAME;
+        errormsg(g_('unrepresentable changes to source'));
         exit(1);
     }
 }

commit a1d208fa56300c8545207ddf30d2762e7bf0f5c4

dpkg-checkbuilddeps: Exit with 1 on unsatisfied dependencies
Regression introduced in commit d287060bb1a45f5de33eb27034a7d8b27b039dbb.

We cannot rely on die's error code as it might be anything.

Prompted-by: David Kalnischkies <david@kalnischkies.de>

scripts/dpkg-checkbuilddeps.pl
@@ -138,12 +138,12 @@ if ($bc_value) {
 }
 
 if (@unmet) {
-	error(g_('Unmet build dependencies: %s'),
-	      join(' ', map { $_->output() } @unmet));
+	errormsg(g_('Unmet build dependencies: %s'),
+	         join(' ', map { $_->output() } @unmet));
 }
 if (@conflicts) {
-	error(g_('Build conflicts: %s'),
-	      join(' ', map { $_->output() } @conflicts));
+	errormsg(g_('Build conflicts: %s'),
+	         join(' ', map { $_->output() } @conflicts));
 }
 exit 1 if @unmet || @conflicts;
 