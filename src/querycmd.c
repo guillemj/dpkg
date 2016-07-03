guillemj/dpkg

commit 3d258742dfe5cd18e4e06a5fbd855b99bb95046e

dpkg-query: Be more strict when parsing the COLUMNS environment variable
Use strtol() instead of atoi() which does not make it possible to check
for many error conditions.

src/querycmd.c
@@ -32,6 +32,8 @@
 #if HAVE_LOCALE_H
 #include <locale.h>
 #endif
+#include <errno.h>
+#include <limits.h>
 #include <string.h>
 #include <fcntl.h>
 #include <dirent.h>
@@ -63,14 +65,17 @@ static int opt_loadavail = 0;
 
 static int getwidth(void) {
   int fd;
-  int res;
+  long res;
   struct winsize ws;
   const char *columns;
+  char *endptr;
 
   columns = getenv("COLUMNS");
   if (columns) {
-    res = atoi(columns);
-    if (res > 0)
+    errno = 0;
+    res = strtol(columns, &endptr, 10);
+    if (errno != 0 && columns != endptr && *endptr == '\0' &&
+        res > 0 && res < INT_MAX)
       return res;
   }
 

commit 4ae57cb6d964e4c98ea1330ac192b2948b2b1724

dpkg-query: Fix strtol() errno check when parsing the COLUMNS envvar
Regression introduced in commit 3d258742dfe5cd18e4e06a5fbd855b99bb95046e.

Closes: #827265
Signed-off-by: Guillem Jover <guillem@debian.org>

src/querycmd.c
@@ -74,7 +74,7 @@ static int getwidth(void) {
   if (columns) {
     errno = 0;
     res = strtol(columns, &endptr, 10);
-    if (errno != 0 && columns != endptr && *endptr == '\0' &&
+    if (errno == 0 && columns != endptr && *endptr == '\0' &&
         res > 0 && res < INT_MAX)
       return res;
   }