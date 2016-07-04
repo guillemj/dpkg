guillemj/dpkg

commit ad94a98cf614e1c4129f8611080232d69d210a0a

dpkg-buipdpackage: Do not use build target fallback on arch:any + arch:all
As part of the transition to make build-indep and build-arch mandatory
and to be able to remove the fallback code without needing a flag day,
we split the transition in chunks. This is the first one which is made
mandatory. More will come.

scripts/dpkg-buildpackage.pl
@@ -722,6 +722,21 @@ sub build_target_fallback {
     return if $buildtarget eq 'build';
     return if scalar @debian_rules != 1;
 
+    # Check if we are building both arch:all and arch:any packages, in which
+    # case we now require working build-indep and build-arch targets.
+    my $pkg_arch = 0;
+    my $ctrl = Dpkg::Control::Info->new();
+
+    foreach my $bin ($ctrl->get_packages()) {
+        if ($bin->{Architecture} eq 'all') {
+            $pkg_arch |= BUILD_ARCH_INDEP;
+        } else {
+            $pkg_arch |= BUILD_ARCH_DEP;
+        }
+    }
+
+    return if $pkg_arch == BUILD_BINARY;
+
     # Check if the build-{arch,indep} targets are supported. If not, fallback
     # to build.
     my $pid = spawn(exec => [ 'make', '-f', @debian_rules, '-qn', $buildtarget ],

man/dpkg-buildpackage.1
@@ -496,9 +496,10 @@ by \fBdpkg\-buildflags\fP. This is no longer the case.
 \fBdpkg\-buildpackage\fP is using the \fBbuild\-arch\fP and
 \fBbuild\-indep\fP targets since dpkg 1.16.2. Those targets are thus
 mandatory. But to avoid breakages of existing packages, and ease
-the transition, it will fallback to using the \fBbuild\fP target
-if \fBmake \-f debian/rules \-qn\fP \fIbuild-target\fP returns 2 as
-exit code.
+the transition, if the source package does not build both architecture
+independent and dependent binary packages (since dpkg 1.18.8) it will
+fallback to use the \fBbuild\fP target if \fBmake \-f debian/rules \-qn\fP
+\fIbuild-target\fP returns 2 as exit code.
 .SH BUGS
 It should be possible to specify spaces and shell metacharacters
 and initial arguments for

commit 383260e568cef224269ab19d4250f2a87177b778

scripts: Require Dpkg::Control::Info in dpkg-buildpackage
Regression introduced in commit ad94a98cf614e1c4129f8611080232d69d210a0a.

Closes: #829542
Reported-by: Helmut Grohne <helmut@subdivi.de>

scripts/dpkg-buildpackage.pl
@@ -40,6 +40,7 @@ use Dpkg::Compression;
 use Dpkg::Checksums;
 use Dpkg::Version;
 use Dpkg::Control;
+use Dpkg::Control::Info;
 use Dpkg::Changelog::Parse;
 use Dpkg::Path qw(find_command);
 use Dpkg::IPC;