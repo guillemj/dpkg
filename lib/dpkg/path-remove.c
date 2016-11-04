4daaec6bb83c65bc7b83022dd241b5d6bbf83b86
Guillem Jover <guillem@debian.org>
2016-11-04 05:28:26 +0100

    dpkg: Do not fail when removing non-existent files on read-only filesystems
    Trying to rmdir(2) or unlink(2) a non-existent pathname on at least
    Linux returns with EROFS. Handle this case specifically to check if
    the pathname exists with access(2).
    
    Closes: #838877

--- lib/dpkg/path-remove.c
+++ lib/dpkg/path-remove.c
@@ -137,6 +137,13 @@ path_remove_tree(const char *pathname)
 		if (errno == ENOTDIR)
 			return;
 	}
+	/* Trying to remove a directory or a file on a read-only filesystem,
+	 * even if non-existent, always returns EROFS. */
+	if (errno == EROFS) {
+		if (access(pathname, F_OK) < 0 && errno == ENOENT)
+			return;
+		errno = EROFS;
+	}
 	if (errno != ENOTEMPTY && errno != EEXIST) /* Huh? */
 		ohshite(_("unable to securely remove '%.255s'"), pathname);
 
