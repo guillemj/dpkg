guillemj/dpkg

commit ea22158eb8b0dcaf42b0cdacd5b4560764f353c8

Dpkg::Changelog: Add new Timestamp field to output
This field contains the date of the entry in seconds since the epoch. To
make it easier to retrieve the date in a format that is easier to handle.

scripts/Dpkg/Changelog.pm
@@ -495,6 +495,7 @@ sub _format_dpkg {
     $f->{Distribution} = join(' ', $src->get_distributions());
     $f->{Maintainer} = $src->get_maintainer() // '';
     $f->{Date} = $src->get_timestamp() // '';
+    $f->{Timestamp} = $src->get_timepiece->epoch // '';
     $f->{Changes} = $src->get_dpkg_changes();
 
     # handle optional fields
@@ -550,6 +551,7 @@ sub _format_rfc822 {
 	$f->{Distribution} = join(' ', $entry->get_distributions());
 	$f->{Maintainer} = $entry->get_maintainer() // '';
 	$f->{Date} = $entry->get_timestamp() // '';
+	$f->{Timestamp} = $entry->get_timepiece->epoch // '';
 	$f->{Changes} = $entry->get_dpkg_changes();
 
 	# handle optional fields
@@ -612,6 +614,10 @@ person that created the (selected) entry
 =item Date
 
 date of the (selected) entry
+
+=item Timestamp
+
+date of the (selected) entry as a timestamp in seconds since the epoch
 
 =item Closes
 
@@ -765,6 +771,8 @@ with only one of the options specified.
 New method: $c->format_range().
 
 Deprecated methods: $c->dpkg(), $c->rfc822().
+
+New field Timestamp in output formats.
 
 =head2 Version 1.00 (dpkg 1.15.6)
 

scripts/Dpkg/Control/FieldsCore.pm
@@ -332,6 +332,9 @@ our %FIELDS = (
         allowed => ALL_SRC,
         separator => FIELD_SEP_COMMA,
     },
+    'Timestamp' => {
+        allowed => CTRL_CHANGELOG,
+    },
     'Triggers-Awaited' => {
         allowed => CTRL_FILE_STATUS,
         separator => FIELD_SEP_SPACE,
@@ -425,7 +428,7 @@ our %FIELD_ORDER = (
     ],
     CTRL_CHANGELOG() => [
         qw(Source Binary-Only Version Distribution Urgency Maintainer
-        Date Closes Changes)
+        Timestamp Date Closes Changes)
     ],
     CTRL_FILE_STATUS() => [ # Same as fieldinfos in lib/dpkg/parse.c
         qw(Package Essential Status Priority Section Installed-Size Origin

commit b8227f0b53e67d0b43f295f01dcd8e08a5cce5aa

Dpkg::Changelog: Cope gracefully with a missing trailer timestamp
The get_timepiece methods might return an undef value if there was no
timestamp in the trailer. Do not call the Time::Piece epoch method in
that case, as that causes a perl error.

Regression introduced in commit ea22158eb8b0dcaf42b0cdacd5b4560764f353c8.

Closes: #843248
Based-on-patch-by: Ian Jackson <ijackson@chiark.greenend.org.uk>

scripts/Dpkg/Changelog.pm
@@ -495,7 +495,7 @@ sub _format_dpkg {
     $f->{Distribution} = join(' ', $src->get_distributions());
     $f->{Maintainer} = $src->get_maintainer() // '';
     $f->{Date} = $src->get_timestamp() // '';
-    $f->{Timestamp} = $src->get_timepiece->epoch // '';
+    $f->{Timestamp} = $src->get_timepiece && $src->get_timepiece->epoch // '';
     $f->{Changes} = $src->get_dpkg_changes();
 
     # handle optional fields
@@ -551,7 +551,7 @@ sub _format_rfc822 {
 	$f->{Distribution} = join(' ', $entry->get_distributions());
 	$f->{Maintainer} = $entry->get_maintainer() // '';
 	$f->{Date} = $entry->get_timestamp() // '';
-	$f->{Timestamp} = $entry->get_timepiece->epoch // '';
+	$f->{Timestamp} = $entry->get_timepiece && $entry->get_timepiece->epoch // '';
 	$f->{Changes} = $entry->get_dpkg_changes();
 
 	# handle optional fields

scripts/Dpkg/Changelog/Entry.pm
@@ -285,6 +285,8 @@ sub get_timestamp {
 
 Return the timestamp of the changelog entry as a Time::Piece object.
 
+This function might return undef if there was no timestamp.
+
 =cut
 
 sub get_timepiece {

scripts/Dpkg/Changelog/Entry/Debian.pm
@@ -393,6 +393,8 @@ sub get_timestamp {
 
 Return the timestamp of the changelog entry as a Time::Piece object.
 
+This function might return undef if there was no timestamp.
+
 =cut
 
 sub get_timepiece {