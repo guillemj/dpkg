guillemj/dpkg

commit b51bc4ff0cd540b82e131470053eeed1c3d2f026

dpkg-maintscript-helper: Use the new --validate-version command
Do not abuse the --compare-versions command to perform version
validation, use the new --validate-version command expressly added
now to perform such task. Because the latter returns appropriate exit
codes, does not print messages in debug mode and is the correct
interface to use.

Closes: #844701

scripts/dpkg-maintscript-helper.sh
@@ -48,9 +48,8 @@ rm_conffile() {
 		error "environment variable DPKG_MAINTSCRIPT_NAME is required"
 	[ "${CONFFILE}" != "${CONFFILE#/}" ] || \
 		error "conffile '$CONFFILE' is not an absolute path"
-	# Use --compare-versions to validate the version number.
-	[ -z "$(dpkg --compare-versions -- "$LASTVERSION" eq '0' 2>&1)" ] || \
-		error "version '$LASTVERSION' is not valid"
+	VERSIONCHECK=$(dpkg --validate-version -- "$LASTVERSION" 2>&1) || \
+		error "$VERSIONCHECK"
 
 	debug "Executing $0 rm_conffile in $DPKG_MAINTSCRIPT_NAME" \
 	      "of $DPKG_MAINTSCRIPT_PACKAGE"
@@ -162,9 +161,8 @@ mv_conffile() {
 		error "old-conffile '$OLDCONFFILE' is not an absolute path"
 	[ "${NEWCONFFILE}" != "${NEWCONFFILE#/}" ] || \
 		error "new-conffile '$NEWCONFFILE' is not an absolute path"
-	# Use --compare-versions to validate the version number.
-	[ -z "$(dpkg --compare-versions -- "$LASTVERSION" eq '0' 2>&1)" ] || \
-		error "version '$LASTVERSION' is not valid"
+	VERSIONCHECK=$(dpkg --validate-version -- "$LASTVERSION" 2>&1) || \
+		error "$VERSIONCHECK"
 
 	debug "Executing $0 mv_conffile in $DPKG_MAINTSCRIPT_NAME" \
 	      "of $DPKG_MAINTSCRIPT_PACKAGE"

commit 0b8cb1a8ed053e2e6a28c2100397002222dd7eaf

dpkg-maintscript-helper: Handle empty versions as they are optional arguments
Regression introduced in commit b51bc4ff0cd540b82e131470053eeed1c3d2f026.

Closes: #848422

scripts/dpkg-maintscript-helper.sh
@@ -48,8 +48,7 @@ rm_conffile() {
 		error "environment variable DPKG_MAINTSCRIPT_NAME is required"
 	[ "${CONFFILE}" != "${CONFFILE#/}" ] || \
 		error "conffile '$CONFFILE' is not an absolute path"
-	VERSIONCHECK=$(dpkg --validate-version -- "$LASTVERSION" 2>&1) || \
-		error "$VERSIONCHECK"
+	validate_optional_version "$LASTVERSION"
 
 	debug "Executing $0 rm_conffile in $DPKG_MAINTSCRIPT_NAME" \
 	      "of $DPKG_MAINTSCRIPT_PACKAGE"
@@ -161,8 +160,7 @@ mv_conffile() {
 		error "old-conffile '$OLDCONFFILE' is not an absolute path"
 	[ "${NEWCONFFILE}" != "${NEWCONFFILE#/}" ] || \
 		error "new-conffile '$NEWCONFFILE' is not an absolute path"
-	VERSIONCHECK=$(dpkg --validate-version -- "$LASTVERSION" 2>&1) || \
-		error "$VERSIONCHECK"
+	validate_optional_version "$LASTVERSION"
 
 	debug "Executing $0 mv_conffile in $DPKG_MAINTSCRIPT_NAME" \
 	      "of $DPKG_MAINTSCRIPT_PACKAGE"
@@ -493,6 +491,18 @@ abort_dir_to_symlink()
 }
 
 # Common functions
+validate_optional_version() {
+	local VERSION="$1"
+
+	if [ -z "$VERSION" ]; then
+		return
+	fi
+
+	if ! VERSIONCHECK=$(dpkg --validate-version -- "$VERSION" 2>&1); then
+		error "$VERSIONCHECK"
+	fi
+}
+
 ensure_package_owns_file() {
 	local PACKAGE="$1"
 	local FILE="$2"