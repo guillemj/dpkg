guillemj/dpkg

commit e4cb12a710457b103a7544c4de5e9fc1b2bd24d8

libdpkg: Do not allow blank lines in field values
Blank lines in this case mean, lines containing only spaces and tabs.
This ensure the Perl and C parsers match, and it follows RFC-2822 style
format, and Debian policy.

Closes: #308082

lib/dpkg/parse.c
@@ -380,6 +380,8 @@ int parsedb(const char *filename, enum parsedbflags flags,
 
     /* Loop per field. */
     for (;;) {
+      bool blank_line;
+
       fs.fieldstart = dataptr - 1;
       while (!EOF_mmap(dataptr, endptr) && !isspace(c) && c!=':' && c!=MSDOS_EOF_CHAR)
         c= getc_mmap(dataptr);
@@ -412,9 +414,16 @@ int parsedb(const char *filename, enum parsedbflags flags,
         parse_error(&ps, new_pkg,
                     _("MSDOS EOF char in value of field `%.*s' (missing newline?)"),
                     fs.fieldlen, fs.fieldstart);
+
+      blank_line = false;
+
       fs.valuestart = dataptr - 1;
       for (;;) {
         if (c == '\n' || c == MSDOS_EOF_CHAR) {
+          if (blank_line)
+            parse_error(&ps, new_pkg,
+                        _("blank line in value of field '%.*s'"),
+                        fs.fieldlen, fs.fieldstart);
           ps.lno++;
 	  if (EOF_mmap(dataptr, endptr)) break;
           c= getc_mmap(dataptr);
@@ -422,11 +431,16 @@ int parsedb(const char *filename, enum parsedbflags flags,
           if (EOF_mmap(dataptr, endptr) || c == '\n' || !isspace(c)) break;
           ungetc_mmap(c,dataptr, data);
           c= '\n';
-        } else if (EOF_mmap(dataptr, endptr)) {
+          blank_line = true;
+        } else if (blank_line && !isspace(c)) {
+          blank_line = false;
+        }
+
+        if (EOF_mmap(dataptr, endptr))
           parse_error(&ps, new_pkg,
                       _("EOF during value of field `%.*s' (missing final newline)"),
                       fs.fieldlen, fs.fieldstart);
-        }
+
         c= getc_mmap(dataptr);
       }
       fs.valuelen = dataptr - fs.valuestart - 1;

commit eeeae2acc87ab380b732491d22af7ec3f4b6b772

libdpkg: Turn status file parser errors on field blank lines into warnings
Regression introduced in commit e4cb12a710457b103a7544c4de5e9fc1b2bd24d8.

This has caused issues at least on Cydia installations. And it should
have been introduced as a lax parsing error, only failing for new
packages, and warn on already installed ones. Otherwise one cannot
easily recover after upgrading to a new dpkg on an affected system.

Reported-by: Jay Freeman <saurik@saurik.com>
Stable-Candidate: 1.17.x

lib/dpkg/dpkg-db.h
@@ -310,8 +310,10 @@ enum parsedbflags {
   pdb_ignoreolder		= DPKG_BIT(5),
   /** Perform laxer version parsing. */
   pdb_lax_version_parser	= DPKG_BIT(6),
+  /** Perform laxer control stanza parsing. */
+  pdb_lax_stanza_parser		= DPKG_BIT(9),
   /** Perform laxer parsing, used to transition to stricter parsing. */
-  pdb_lax_parser		= pdb_lax_version_parser,
+  pdb_lax_parser		= pdb_lax_stanza_parser | pdb_lax_version_parser,
   /** Close file descriptor on context destruction. */
   pdb_close_fd			= DPKG_BIT(7),
   /** Interpret filename ‘-’ as stdin. */

lib/dpkg/parse.c
@@ -673,10 +673,14 @@ parse_stanza(struct parsedb_state *ps, struct field_state *fs,
     fs->valuestart = ps->dataptr - 1;
     for (;;) {
       if (c == '\n' || c == MSDOS_EOF_CHAR) {
-        if (blank_line)
-          parse_error(ps,
-                      _("blank line in value of field '%.*s'"),
-                      fs->fieldlen, fs->fieldstart);
+        if (blank_line) {
+          if (ps->flags & pdb_lax_stanza_parser)
+            parse_warn(ps, _("blank line in value of field '%.*s'"),
+                       fs->fieldlen, fs->fieldstart);
+          else
+            parse_error(ps, _("blank line in value of field '%.*s'"),
+                        fs->fieldlen, fs->fieldstart);
+        }
         ps->lno++;
 
         if (parse_at_eof(ps))