27bb073c1f40014c4eafbe43f150a76a02a6c542
Guillem Jover <guillem@debian.org>
2017-02-08 05:04:34 +0100

    Dpkg::Shlibs::Objdump: Do not special case EM_SPARC32PLUS for NetBSD
    The affected code in NetBSD was bogus, and has been removed now. So
    there is no point in trying to special case the EM_SPARC32PLUS ELF
    machine ID depending on the ELF class, for something that should never
    happen.
    
    Ref: https//gnats.netbsd.org/51925

--- scripts/Dpkg/Shlibs/Objdump.pm
+++ scripts/Dpkg/Shlibs/Objdump.pm
@@ -156,6 +156,7 @@ my %elf_mach_map = (
     ELF_MACH_MN10300_CYGNUS()   => ELF_MACH_MN10300,
     ELF_MACH_OR1K_OLD()         => ELF_MACH_OR1K,
     ELF_MACH_S390_OLD()         => ELF_MACH_S390,
+    ELF_MACH_SPARC32PLUS()      => ELF_MACH_SPARC,
     ELF_MACH_SPARC64_OLD()      => ELF_MACH_SPARC64,
     ELF_MACH_XTENSA_OLD()       => ELF_MACH_XTENSA,
 );
@@ -209,16 +210,6 @@ sub get_format {
     # Unpack the endianness and size dependent fields.
     my $tmpl = "x16(S2Lx[${elf_word}3]L)${elf_endian}";
     @elf{qw(type mach version flags)} = unpack $tmpl, $header;
-
-    # XXX: We need to special case ELF_MACH_SPARC32PLUS, because NetBSD
-    # treats it differently depending on the ELF bits.
-    if ($elf{mach} == ELF_MACH_SPARC32PLUS) {
-        if ($elf{bits} == ELF_BITS_32) {
-            $elf{mach} = ELF_MACH_SPARC;
-        } else {
-            $elf{mach} = ELF_MACH_SPARC64;
-        }
-    }
 
     # Canonicalize the machine ID.
     $elf{mach} = $elf_mach_map{$elf{mach}} // $elf{mach};
