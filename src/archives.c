guillemj/dpkg

commit 05a8ddb482419d110571679a29bcd25e3c2b036d

dpkg: Fix logic on reinstallation of previously configured shared conffiles
When checking for the validity of the Conffiles database field, do not
just assume that a package that is just in stat_unpacked has never
before been configured. Instead check if the last configured version
is the same as the currently extracted version, which will mean the
values in the Conffiles field are valid.

Closes: #684776

src/archives.c
@@ -194,7 +194,14 @@ md5hash_prev_conffile(struct pkginfo *pkg, char *oldhash, const char *oldname,
       continue;
     /* The hash in the Conffiles is only meaningful if the package
      * configuration has been at least tried. */
-    if (otherpkg->status <= stat_unpacked)
+    if (otherpkg->status < stat_unpacked)
+      continue;
+    /* If we are reinstalling, even if the other package is only unpacked,
+     * we can always make use of the Conffiles hash value from an initial
+     * installation, if that happened at all. */
+    if (otherpkg->status == stat_unpacked &&
+        dpkg_version_compare(&otherpkg->installed.version,
+                             &otherpkg->configversion) != 0)
       continue;
     for (conff = otherpkg->installed.conffiles; conff; conff = conff->next) {
       if (conff->obsolete)

commit 9507afd811a3d73822dc6a6b110bd602b82aad0b

dpkg: Fix digest inference for shared conffiles
The code in charge of inferring the digest for a conffile was
checking the owning package status twice, and ignoring conffiles
with a status lower than "unpacked" even if they had been configured
previously and their md5sums were valid.

This was causing bogus takeover unpack errors.

Fixes: commit 05a8ddb482419d110571679a29bcd25e3c2b036d
Closes: #861217
Stable-Candidate: 1.17.x

src/archives.c
@@ -228,14 +228,10 @@ md5hash_prev_conffile(struct pkginfo *pkg, char *oldhash, const char *oldname,
   for (otherpkg = &pkg->set->pkg; otherpkg; otherpkg = otherpkg->arch_next) {
     if (otherpkg == pkg)
       continue;
-    /* The hash in the Conffiles is only meaningful if the package
-     * configuration has been at least tried. */
-    if (otherpkg->status < PKG_STAT_UNPACKED)
-      continue;
     /* If we are reinstalling, even if the other package is only unpacked,
      * we can always make use of the Conffiles hash value from an initial
      * installation, if that happened at all. */
-    if (otherpkg->status == PKG_STAT_UNPACKED &&
+    if (otherpkg->status <= PKG_STAT_UNPACKED &&
         dpkg_version_compare(&otherpkg->installed.version,
                              &otherpkg->configversion) != 0)
       continue;