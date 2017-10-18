e16940da1af9f62f877840fb71e49eafea8a64fa
Guillem Jover <guillem@debian.org>
2017-10-19 01:39:14 +0200

    dpkg-buildpackage: Setup and check rootcommand only if needed
    When building in R³ mode or when the user requested to run a rule as
    root we should setup the rootcommand, otherwise we should ignore it.
    
    Reported-by: Niels Thykier <niels@thykier.net>
    (cherry picked from commit b4e46fab018e3d5ae813642b20b955d8a5327b1e)

--- scripts/dpkg-buildpackage.pl
+++ scripts/dpkg-buildpackage.pl
@@ -369,22 +369,6 @@ if ($noclean) {
     $checkbuilddep = 0 if build_is(BUILD_SOURCE);
 }
 
-if ($< == 0) {
-    warning(g_('using a gain-root-command while being root')) if (@rootcommand);
-} else {
-    push @rootcommand, 'fakeroot' unless @rootcommand;
-}
-
-if (@rootcommand and not find_command($rootcommand[0])) {
-    if ($rootcommand[0] eq 'fakeroot' and $< != 0) {
-        error(g_("fakeroot not found, either install the fakeroot\n" .
-                 'package, specify a command with the -r option, ' .
-                 'or run this as root'));
-    } else {
-        error(g_("gain-root-command '%s' not found"), $rootcommand[0]);
-    }
-}
-
 if ($call_target_as_root and @call_target == 0) {
     error(g_('option %s is only meaningful with option %s'),
           '--as-root', '--rules-target');
@@ -439,6 +423,10 @@ my $dir = basename($cwd);
 my $changelog = changelog_parse();
 my $ctrl = Dpkg::Control::Info->new();
 
+# Check whether we are doing some kind of rootless build, and sanity check
+# the fields values.
+my %rules_requires_root = parse_rules_requires_root($ctrl->get_source());
+
 my $pkg = mustsetvar($changelog->{source}, g_('source package'));
 my $version = mustsetvar($changelog->{version}, g_('source version'));
 my $v = Dpkg::Version->new($version);
@@ -461,10 +449,6 @@ if ($changedby) {
 
 # <https://reproducible-builds.org/specs/source-date-epoch/>
 $ENV{SOURCE_DATE_EPOCH} ||= $changelog->{timestamp} || time;
-
-# Check whether we are doing some kind of rootless build, and sanity check
-# the fields values.
-my %rules_requires_root = parse_rules_requires_root($ctrl->get_source());
 
 my @arch_opts;
 push @arch_opts, ('--host-arch', $host_arch) if $host_arch;
@@ -687,6 +671,24 @@ sub mustsetvar {
     return $var;
 }
 
+sub setup_rootcommand {
+    if ($< == 0) {
+        warning(g_('using a gain-root-command while being root')) if @rootcommand;
+    } else {
+        push @rootcommand, 'fakeroot' unless @rootcommand;
+    }
+
+    if (@rootcommand and not find_command($rootcommand[0])) {
+        if ($rootcommand[0] eq 'fakeroot' and $< != 0) {
+            error(g_("fakeroot not found, either install the fakeroot\n" .
+                     'package, specify a command with the -r option, ' .
+                     'or run this as root'));
+        } else {
+            error(g_("gain-root-command '%s' not found"), $rootcommand[0]);
+        }
+    }
+}
+
 sub parse_rules_requires_root {
     my $ctrl = shift;
 
@@ -717,6 +719,10 @@ sub parse_rules_requires_root {
             error(g_('field %s contains duplicate keyword %s'),
                         'Rules-Requires-Root', $keyword);
         }
+    }
+
+    if ($call_target_as_root or not exists $rrr{no}) {
+        setup_rootcommand();
     }
 
     if ($keywords_base > 1 or $keywords_base and $keywords_impl) {
