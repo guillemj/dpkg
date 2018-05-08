guillemj/dpkg

commit 6f903af181057a046a1344aadca552b113a2c2ee

dpkg-buildpackage: Validate OpenPGP signing key IDs
We should not accept short key IDs, and warn about long key IDs.
Recommend to use fingerprints.

scripts/dpkg-buildpackage.pl
@@ -477,6 +477,8 @@ if (build_has_any(BUILD_ARCH_DEP)) {
 my $pv = "${pkg}_$sversion";
 my $pva = "${pkg}_${sversion}_$arch";
 
+signkey_validate();
+
 if (not $signcommand) {
     $signsource = 0;
     $signbuildinfo = 0;
@@ -808,6 +810,21 @@ sub update_files_field {
     my $file_regex = qr/$md5sum_regex\s+\d+\s+(\S+\s+\S+\s+\Q$filename\E)/;
 
     $ctrl->{'Files'} =~ s/^$file_regex$/$md5sum $size $1/m;
+}
+
+sub signkey_validate {
+    # Make sure this is an hex keyid.
+    return unless $signkey =~ m/^(?:0x)?([[:xdigit:]]+)$/;
+
+    my $keyid = $1;
+
+    if (length $keyid <= 8) {
+        error(g_('short OpenPGP key IDs are broken; ' .
+                 'please use key fingerprints instead'));
+    } elsif (length $keyid <= 16) {
+        warning(g_('long OpenPGP key IDs are strongly discouraged; ' .
+                   'please use key fingerprints instead'));
+    }
 }
 
 sub signfile {

commit df0a86de45006f9f2b2980a0ad2e81640271a685

dpkg-buildpackage: Avoid using an uninitialized variable
If DEB_SIGN_KEYID is not set in the environment or the signing key is
not passed via --sign-key, dpkg-buildpackage emits a perl warning.

Warned-by: perl
Fixes: commit 6f903af181057a046a1344aadca552b113a2c2ee
Based-on-patch-by: Sven Joachim <svenjoac@gmx.de>

scripts/dpkg-buildpackage.pl
@@ -813,6 +813,7 @@ sub update_files_field {
 }
 
 sub signkey_validate {
+    return unless defined $signkey;
     # Make sure this is an hex keyid.
     return unless $signkey =~ m/^(?:0x)?([[:xdigit:]]+)$/;
 