guillemj/dpkg

commit 293bd243a19149165fc4fd8830b16a51d471a5e9

dpkg-buildpackage: Add long options
Add new long options for any short option that is a useful configurable
candidate.

scripts/dpkg-buildpackage.pl
@@ -59,76 +59,78 @@ sub usage {
 'Usage: %s [<option>...]')
     . "\n\n" . g_(
 'Options:
-  --build=<type>[,...]
-                 specify the build <type>: full, source, binary, any, all
-                   (default is \'full\').
-  -F             normal full build (binaries and sources, default).
-  -g             source and arch-indep build.
-  -G             source and arch-specific build.
-  -b             binary-only, no source files.
-  -B             binary-only, only arch-specific files.
-  -A             binary-only, only arch-indep files.
-  -S             source-only, no binary files.
-  -nc            do not clean source tree (implies -b).
-  -tc            clean source tree when finished.
-  -D             check build dependencies and conflicts (default).
-  -d             do not check build dependencies and conflicts.
-  -P<profiles>   assume given build profiles as active (comma-separated list).
-  -R<rules>      rules file to execute (default is debian/rules).
-  -T<target>     call debian/rules <target> with the proper environment.
-      --as-root  ensure -T calls the target with root rights.
-  -j[<number>]   jobs to run simultaneously (passed to <rules>), forced mode.
-  -J[<number>]   jobs to run simultaneously (passed to <rules>), opt-in mode.
-  -r<gain-root-command>
-                 command to gain root privileges (default is fakeroot).
-  --check-command=<check-command>
-                 command to check the .changes file (no default).
-  --check-option=<opt>
-                 pass <opt> to <check-command>.
-  --hook-<hook-name>=<hook-command>
-                 set <hook-command> as the hook <hook-name>, known hooks:
-                   init preclean source build binary changes postclean
-                   check sign done
-  -p<sign-command>
-                 command to sign .dsc and/or .changes files
-                   (default is gpg2 or gpg).
-  -k<keyid>      the key to use for signing.
-  -ap            add pause before starting signature process.
-  -us            unsigned source package.
-  -uc            unsigned .changes file.
-      --force-sign
-                 force signing the resulting files.
-      --admindir=<directory>
-                 change the administrative directory.
-  -?, --help     show this help message.
-      --version  show the version.')
+      --build=<type>[,...]    specify the build <type>: full, source, binary,
+                                any, all (default is \'full\').
+  -F                          normal full build (source and binary; default).
+  -g                          source and arch-indep build.
+  -G                          source and arch-specific build.
+  -b                          binary-only, no source files.
+  -B                          binary-only, only arch-specific files.
+  -A                          binary-only, only arch-indep files.
+  -S                          source-only, no binary files.
+  -nc, --no-pre-clean         do not pre clean source tree (implies -b).
+       --pre-clean            pre clean source tree (default).
+  -tc, --post-clean           clean source tree when finished.
+  -D                          check build dependencies and conflicts (default).
+  -d                          do not check build dependencies and conflicts.
+      --[no-]check-builddeps  ditto.
+  -P, --build-profiles=<profiles>
+                              assume comma-separated build profiles as active.
+  -R, --rules-file=<rules>    rules file to execute (default is debian/rules).
+  -T, --rules-target=<target> call debian/rules <target>.
+      --as-root               ensure -T calls the target with root rights.
+  -j, --jobs[=<number>|auto]  jobs to run simultaneously (passed to <rules>),
+                                forced mode.
+  -J, --jobs-try[=<number>|auto]
+                              jobs to run simultaneously (passed to <rules>),
+                                opt-in mode.
+  -r, --root-command=<command>
+                              command to gain root rights (default is fakeroot).
+      --check-command=<command>
+                              command to check the .changes file (no default).
+      --check-option=<opt>    pass <opt> to check <command>.
+      --hook-<name>=<command> set <command> as the hook <name>, known hooks:
+                                init preclean source build binary changes
+                                postclean check sign done
+  -p, --sign-command=<command>
+                              command to sign .dsc and/or .changes files
+                                (default is gpg2 or gpg).
+  -k, --sign-key=<keyid>      the key to use for signing.
+  -ap, --sign-pause           add pause before starting signature process.
+  -us, --unsigned-source      unsigned source package.
+  -uc, --unsigned-changes     unsigned .changes file.
+      --force-sign            force signing the resulting files.
+      --admindir=<directory>  change the administrative directory.
+  -?, --help                  show this help message.
+      --version               show the version.')
     . "\n\n" . g_(
 'Options passed to dpkg-architecture:
-  -a, --host-arch <arch>    set the host Debian architecture.
-  -t, --host-type <type>    set the host GNU system type.
-      --target-arch <arch>  set the target Debian architecture.
-      --target-type <type>  set the target GNU system type.')
+  -a, --host-arch <arch>      set the host Debian architecture.
+  -t, --host-type <type>      set the host GNU system type.
+      --target-arch <arch>    set the target Debian architecture.
+      --target-type <type>    set the target GNU system type.')
     . "\n\n" . g_(
 'Options passed to dpkg-genchanges:
-  -si            source includes orig, if new upstream (default).
-  -sa            source includes orig, always.
-  -sd            source is diff and .dsc only.
-  -v<version>    changes since version <version>.
-  -m<maint>      maintainer for package is <maint>.
-  -e<maint>      maintainer for release is <maint>.
-  -C<descfile>   changes are described in <descfile>.
-      --changes-option=<opt>
-                 pass option <opt> to dpkg-genchanges.')
+  -si                         source includes orig, if new upstream (default).
+  -sa                         source includes orig, always.
+  -sd                         source is diff and .dsc only.
+  -v<version>                 changes since version <version>.
+  -m, --release-by=<maint>    maintainer for this release is <maint>.
+  -e, --build-by=<maint>      maintainer for this build is <maint>.
+  -C<descfile>                changes are described in <descfile>.
+      --changes-option=<opt>  pass option <opt> to dpkg-genchanges.')
     . "\n\n" . g_(
 'Options passed to dpkg-source:
-  -sn            force Debian native source format.
-  -s[sAkurKUR]   see dpkg-source for explanation.
-  -z<level>      compression level to use for source.
-  -Z<compressor> compression to use for source (gz|xz|bzip2|lzma).
-  -i[<regex>]    ignore diffs of files matching regex.
-  -I[<pattern>]  filter out files when building tarballs.
-      --source-option=<opt>
-                 pass option <opt> to dpkg-source.
+  -sn                         force Debian native source format.
+  -s[sAkurKUR]                see dpkg-source for explanation.
+  -z, --compression-level=<level>
+                              compression level to use for source.
+  -Z, --compression=<compressor>
+                              compression to use for source (gz|xz|bzip2|lzma).
+  -i, --diff-ignore[=<regex>] ignore diffs of files matching <regex>.
+  -I, --tar-ignore[=<pattern>]
+                              filter out files when building tarballs.
+      --source-option=<opt>   pass option <opt> to dpkg-source.
 '), $Dpkg::PROGNAME;
 }
 
@@ -196,13 +198,13 @@ while (@ARGV) {
 	push @source_opts, $1;
     } elsif (/^--changes-option=(.*)$/) {
 	push @changes_opts, $1;
-    } elsif (/^-j(\d*|auto)$/) {
+    } elsif (/^(?:-j|--jobs=)(\d*|auto)$/) {
 	$parallel = $1 || '';
 	$parallel_force = 1;
-    } elsif (/^-J(\d*|auto)$/) {
+    } elsif (/^(?:-J|--jobs-try=)(\d*|auto)$/) {
 	$parallel = $1 || '';
 	$parallel_force = 0;
-    } elsif (/^-r(.*)$/) {
+    } elsif (/^(?:-r|--root-command=)(.*)$/) {
 	my $arg = $1;
 	@rootcommand = split /\s+/, $arg;
     } elsif (/^--check-command=(.*)$/) {
@@ -216,10 +218,12 @@ while (@ARGV) {
 	usageerr(g_('missing hook %s command'), $hook_name)
 	    if not defined $hook_cmd;
 	$hook{$hook_name} = $hook_cmd;
-    } elsif (/^-p(.*)$/) {
+    } elsif (/^(?:-p|--sign-command=)(.*)$/) {
 	$signcommand = $1;
-    } elsif (/^-k(.*)$/) {
+    } elsif (/^(?:-k|--sign-key=)(.*)$/) {
 	$signkey = $1;
+    } elsif (/^--(no-)?check-builddeps$/) {
+	$checkbuilddep = !(defined $1 and $1 eq 'no-');
     } elsif (/^-([dD])$/) {
 	$checkbuilddep = ($1 eq 'D');
     } elsif (/^--ignore-builtin-builddeps$/) {
@@ -229,24 +233,28 @@ while (@ARGV) {
 	warning(g_('-s%s is deprecated; always using gpg style interface'), $1);
     } elsif (/^--force-sign$/) {
 	$signforce = 1;
-    } elsif (/^-us$/) {
+    } elsif (/^-us$/ or /^--unsigned-source$/) {
 	$signsource = 0;
-    } elsif (/^-uc$/) {
+    } elsif (/^-uc$/ or /^--unsigned-changes$/) {
 	$signchanges = 0;
-    } elsif (/^-ap$/) {
+    } elsif (/^-ap$/ or /^--sign-pausa$/) {
 	$signpause = 1;
     } elsif (/^-a$/ or /^--host-arch$/) {
 	$host_arch = shift;
     } elsif (/^-a(.*)$/ or /^--host-arch=(.*)$/) {
 	$host_arch = $1;
-    } elsif (/^-P(.*)$/) {
+    } elsif (/^-P(.*)$/ or /^--build-profiles=(.*)$/) {
 	my $arg = $1;
 	@build_profiles = split /,/, $arg;
     } elsif (/^-s[iad]$/) {
 	push @changes_opts, $_;
+    } elsif (/^--(?:compression-level|compression)=.+$/) {
+	push @source_opts, $_;
+    } elsif (/^--(?:diff-ignore|tar-ignore)(?:=.+)?$/) {
+	push @source_opts, $_;
     } elsif (/^-(?:s[nsAkurKUR]|[zZ].*|i.*|I.*)$/) {
 	push @source_opts, $_; # passed to dpkg-source
-    } elsif (/^-tc$/) {
+    } elsif (/^-tc$/ or /^--post-clean$/) {
 	$cleansource = 1;
     } elsif (/^-t$/ or /^--host-type$/) {
 	$host_type = shift; # Order DOES matter!
@@ -260,13 +268,15 @@ while (@ARGV) {
 	$target_type = shift;
     } elsif (/^--target-type=(.*)$/) {
 	$target_type = $1;
-    } elsif (/^(?:--target|-T)$/) {
+    } elsif (/^(?:--target|--rules-target|-T)$/) {
         $call_target = shift @ARGV;
-    } elsif (/^(?:--target=|-T)(.+)$/) {
+    } elsif (/^(?:--target=|--rules-target=|-T)(.+)$/) {
         $call_target = $1;
     } elsif (/^--as-root$/) {
         $call_target_as_root = 1;
-    } elsif (/^-nc$/) {
+    } elsif (/^--pre-clean$/) {
+	$noclean = 0;
+    } elsif (/^-nc$/ or /^--no-pre-clean$/) {
 	$noclean = 1;
     } elsif (/^--build=(.*)$/) {
         set_build_type_from_options($1, $_);
@@ -286,16 +296,16 @@ while (@ARGV) {
 	set_build_type(BUILD_FULL, $_);
     } elsif (/^-v(.*)$/) {
 	$since = $1;
-    } elsif (/^-m(.*)$/) {
+    } elsif (/^-m(.*)$/ or /^--release-by=(.*)$/) {
 	$maint = $1;
-    } elsif (/^-e(.*)$/) {
+    } elsif (/^-e(.*)$/ or /^--build-by=(.*)$/) {
 	$changedby = $1;
     } elsif (/^-C(.*)$/) {
 	$desc = $1;
     } elsif (m/^-[EW]$/) {
 	# Deprecated option
 	warning(g_('-E and -W are deprecated, they are without effect'));
-    } elsif (/^-R(.*)$/) {
+    } elsif (/^-R(.*)$/ or /^--rules-target=(.*)$/) {
 	my $arg = $1;
 	@debian_rules = split /\s+/, $arg;
     } else {

man/dpkg-buildpackage.1
@@ -38,12 +38,13 @@ variables (see \fBENVIRONMENT\fP), runs the \fBinit\fP hook, and calls
 has been used).
 .IP \fB2.\fP 3
 It checks that the build-dependencies and build-conflicts
-are satisfied (unless \fB\-d\fP is specified).
+are satisfied (unless \fB\-d\fP or \fB\-\-no\-check\-builddeps\fP is specified).
 .IP \fB3.\fP 3
 If a specific target has been selected with the \fB\-T\fP or \fB\-\-target\fP
 option, it calls that target and stops here. Otherwise it runs the
 \fBpreclean\fP hook and calls \fBfakeroot debian/rules clean\fP to
-clean the build-tree (unless \fB\-nc\fP is specified).
+clean the build-tree (unless \fB\-nc\fP or \fB\-\-no\-pre\-clean\fP
+is specified).
 .IP \fB4.\fP 3
 It runs the \fBsource\fP hook and calls \fBdpkg\-source \-b\fP to generate
 the source package (if a \fBsource\fP build has been requested with
@@ -66,8 +67,8 @@ generate a \fB.changes\fP file.
 Many \fBdpkg\-buildpackage\fP options are forwarded to
 \fBdpkg\-genchanges\fP.
 .IP \fB7.\fP 3
-It runs the \fBpostclean\fP hook and if \fB\-tc\fP is specified, it will
-call \fBfakeroot debian/rules clean\fP again.
+It runs the \fBpostclean\fP hook and if \fB\-tc\fP or \fB\-\-post\-clean\fP
+is specified, it will call \fBfakeroot debian/rules clean\fP again.
 .IP \fB8.\fP 3
 It calls \fBdpkg\-source \-\-after\-build\fP.
 .IP \fB9.\fP 3
@@ -76,8 +77,9 @@ It runs the \fBcheck\fP hook and calls a package checker for the
 with \fB\-\-check\-command\fP).
 .IP \fB10.\fP 3
 It runs the \fBsign\fP hook and calls \fBgpg2\fP or \fBgpg\fP to sign
-the \fB.dsc\fP file (if any, unless \fB\-us\fP is specified or on UNRELEASED
-builds), and the \fB.changes\fP file (unless \fB\-uc\fP is specified or on
+the \fB.dsc\fP file (if any, unless \fB\-us\fP or \fB\-\-unsigned\-source\fP
+is specified or on UNRELEASED builds), and the \fB.changes\fP file
+(unless \fB\-uc\fP or \fB\-\-unsigned\-changes\fP is specified or on
 UNRELEASED builds).
 .IP \fB11.\fP 3
 It runs the \fBdone\fP hook.
@@ -141,11 +143,13 @@ Equivalent to \fB\-\-build=full\fP, \fB\-\-build=source,binary\fP or
 .TQ
 .BI "\-\-target " target
 .TQ
-.BI \-T target
+.BR \-T ", " \-\-rules\-target= \fItarget\fP
 Calls \fBdebian/rules\fP \fItarget\fP after having setup the build
-environment and stops the package build process here (since dpkg 1.15.0).
+environment and stops the package build process here (since dpkg 1.15.0,
+long option since dpkg 1.18.8).
 If \fB\-\-as\-root\fP is also given, then the command is executed
-as root (see \fB\-r\fP). Note that known targets that are required to
+as root (see \fB\-\-root\-command\fP).
+Note that known targets that are required to
 be run as root do not need this option (i.e. the \fBclean\fP, \fBbinary\fP,
 \fBbinary\-arch\fP and \fBbinary\-indep\fP targets).
 .TP
@@ -163,9 +167,9 @@ Requires that the target be run with root rights.
 .TQ
 .BI \-C changes-description
 .TQ
-.BI \-m maintainer-address
+.BR \-m ", " \-\-release\-by= \fImaintainer-address\fP
 .TQ
-.BI \-e maintainer-address
+.BR \-e ", " \-\-build\-by= \fImaintainer-address\fP
 Passed unchanged to \fBdpkg\-genchanges\fP. See its manual page.
 .TP
 .BR \-a ", " \-\-host\-arch " \fIarchitecture\fP"
@@ -192,21 +196,22 @@ It can be
 used in place of \fB\-\-target\-arch\fP or as a complement to override the
 default GNU system type of the target Debian architecture.
 .TP
-.BR \-P \fIprofile\fP[ , ...]
-Specify the profile(s) we build, as a comma-separated list (since dpkg 1.17.2).
+.BR \-P ", " \-\-build\-profiles= \fIprofile\fP[ , ...]
+Specify the profile(s) we build, as a comma-separated list (since dpkg 1.17.2,
+long option since dpkg 1.18.8).
 The default
 behavior is to build for no specific profile. Also sets them (as a space
 separated list) as the \fBDEB_BUILD_PROFILES\fP environment variable which
 allows, for example, \fBdebian/rules\fP files to use this information for
 conditional builds.
 .TP
-.BR \-j [\fIjobs\fP|\fBauto\fP]
+.BR \-j ", " \-\-jobs [=\fIjobs\fP|\fBauto\fP]
 Number of jobs allowed to be run simultaneously, number of jobs matching
 the number of online processors if \fBauto\fP is specified
 (since dpkg 1.17.10), or unlimited number if \fIjobs\fP is not specified,
 equivalent to the
 .BR make (1)
-option of the same name (since dpkg 1.14.7).
+option of the same name (since dpkg 1.14.7, long option since dpkg 1.18.8).
 Will add itself to the \fB\%MAKEFLAGS\fP
 environment variable, which should cause all subsequent make
 invocations to inherit the option, thus forcing the parallel setting on
@@ -223,43 +228,48 @@ currently active processors, and as such will not get propagated to any
 child process. If the number of online processors cannot be inferred then
 the code will fallback to using an unlimited number.
 .TP
-.BR \-J [\fIjobs\fP|\fBauto\fP]
-This option (since dpkg 1.18.2) is equivalent to the \fB\-j\fP option
-except that it does not set the \fB\%MAKEFLAGS\fP environment variable,
-and as such it is safer to use with any package including those that are
-not parallel-build safe.
+.BR \-J ", " \-\-jobs\-try [=\fIjobs\fP|\fBauto\fP]
+This option (since dpkg 1.18.2, long option since dpkg 1.18.8) is equivalent
+to the \fB\-j\fP option except that it does not set the \fB\%MAKEFLAGS\fP
+environment variable, and as such it is safer to use with any package
+including those that are not parallel-build safe.
 .TP
-.B \-D
-Check build dependencies and conflicts; abort if unsatisfied. This is the
-default behavior.
+.BR \-D ", " \-\-check\-builddeps
+Check build dependencies and conflicts; abort if unsatisfied (long option
+since dpkg 1.18.8).
+This is the default behavior.
 .TP
-.B \-d
-Do not check build dependencies and conflicts.
+.BR \-d ", " \-\-no\-check\-builddeps
+Do not check build dependencies and conflicts (long option since dpkg 1.18.8).
 .TP
 .B \-\-ignore\-builtin\-builddeps
 Do not check built-in build dependencies and conflicts (since dpkg 1.18.2).
 These are the distribution specific implicit build dependencies usually
 required in a build environment, the so called Build-Essential package set.
 .TP
-.B \-nc
-Do not clean the source tree.
+.BR \-nc ", " \-\-no\-pre\-clean
+Do not clean the source tree (long option since dpkg 1.18.8).
 Implies \fB\-b\fP if nothing else has been selected among \fB\-F\fP,
 \fB\-g\fP, \fB\-G\fP, \fB\-B\fP, \fB\-A\fP or \fB\-S\fP.
 Implies \fB\-d\fP with \fB\-S\fP (since dpkg 1.18.0).
 .TP
-.B \-tc
+.BR \-\-pre\-clean
+Clean the source tree before building (since dpkg 1.18.8).
+.TP
+.BR \-tc ", " \-\-post\-clean
 Clean the source tree (using
 .I gain-root-command
 .BR "debian/rules clean" )
-after the package has been built.
+after the package has been built (long option since dpkg 1.18.8).
 .TP
-.BI \-r gain-root-command
+.BR \-r ", " \-\-root\-command= \fIgain-root-command\fP
 When
 .B dpkg\-buildpackage
 needs to execute part of the build process as root, it prefixes the
 command it executes with
 .I gain-root-command
-if one has been specified. Otherwise, if none has been specified,
+if one has been specified (long option since dpkg 1.18.8).
+Otherwise, if none has been specified,
 \fBfakeroot\fP will be used by default, if the command is present.
 .I gain-root-command
 should start with the name of a program on the
@@ -277,10 +287,11 @@ is not suitable, since it can only invoke the user's shell with
 .B \-c
 instead of passing arguments individually to the command to be run.
 .TP
-.BI \-R rules-file
+.BR \-R ", " \-\-rules-file= \fIrules-file\fP
 Building a Debian package usually involves invoking
 .B debian/rules
-as a command with several standard parameters (since dpkg 1.14.17).
+as a command with several standard parameters (since dpkg 1.14.17,
+long option since dpkg 1.18.8).
 With this option it's
 possible to use another program invocation to build the package (it can
 include space separated parameters).
@@ -339,34 +350,54 @@ The source package version (without the epoch).
 The upstream version.
 .RE
 .TP
-.BI \-p sign-command
+.BR \-p ", " \-\-sign\-command= \fIsign-command\fP
 When \fBdpkg\-buildpackage\fP needs to execute GPG to sign a source
 control (\fB.dsc\fP) file or a \fB.changes\fP file it will run
 \fIsign-command\fP (searching the \fBPATH\fP if necessary) instead of
-\fBgpg2\fP or \fBgpg\fP. \fIsign-command\fP will get all the arguments
+\fBgpg2\fP or \fBgpg\fP (long option since dpkg 1.18.8).
+\fIsign-command\fP will get all the arguments
 that \fBgpg2\fP or \fBgpg\fP would have gotten. \fIsign-command\fP
 should not contain spaces or any other shell metacharacters.
 .TP
-.BI \-k key-id
-Specify a key-ID to use when signing packages.
+.BR \-k ", " \-\-sign\-key= \fIkey-id\fP
+Specify a key-ID to use when signing packages (long option since dpkg 1.18.8).
 .TP
-.BR \-us
-Do not sign the source package.
+.BR \-us ", " \-\-unsigned\-source
+Do not sign the source package (long option since dpkg 1.18.8).
 .TP
-.BR \-uc
-Do not sign the \fB.changes\fP file.
+.BR \-uc ", " \-\-unsigned\-changes
+Do not sign the \fB.changes\fP file (long option since dpkg 1.18.8).
 .TP
 .BR \-\-force\-sign
-Force the signing of the resulting files (since dpkg 1.17.0),
-regardless of \fB\-us\fP or \fB\-uc\fP or other internal heuristics.
+Force the signing of the resulting files (since dpkg 1.17.0), regardless of
+\fB\-us\fP, \fB\-\-unsigned\-source\fP, \fB\-uc\fP, \fB\-\-unsigned\-changes\fP
+or other internal heuristics.
 .TP
-.BR \-i [\fIregex\fP]
+.BR \-sn
 .TQ
-.BR \-I [\fIpattern\fP]
+.BR \-ss
 .TQ
-.BR \-s [ nsAkurKUR ]
+.BR \-sA
 .TQ
-.BR \-z ", " \-Z
+.BR \-sk
+.TQ
+.BR \-su
+.TQ
+.BR \-sr
+.TQ
+.BR \-sK
+.TQ
+.BR \-sU
+.TQ
+.BR \-sR
+.TQ
+.BR \-i ", " \-\-diff\-ignore [=\fIregex\fP]
+.TQ
+.BR \-I ", " \-\-tar\-ignore [=\fIpattern\fP]
+.TQ
+.BR \-z ", " \-\-compression\-level= \fIlevel\fP
+.TQ
+.BR \-Z ", " \-\-compression= \fIcompressor\fP
 Passed unchanged to \fBdpkg\-source\fP. See its manual page.
 .TP
 .BI \-\-source\-option= opt
@@ -400,7 +431,7 @@ Overridden by the \fB\-\-check\-command\fP option.
 .B DEB_SIGN_KEYID
 If set, it will be used to sign the \fB.changes\fP and \fB.dsc\fP files
 (since dpkg 1.17.2).
-Overridden by the \fB\-k\fP option.
+Overridden by the \fB\-\-sign\-key\fP option.
 .TP
 .B DEB_BUILD_OPTIONS
 If set, and containing \fBnocheck\fP the \fBDEB_CHECK_COMMAND\fP variable

commit 5cd52673aabdf5eaa58181972119a41041fc85f2

dpkg-buildpackage: Fix --rules-file option parsing
Parse --rules-file instead of --rules-target, which made it a no-op.

Fixes: commit 293bd243a19149165fc4fd8830b16a51d471a5e9
Stable-Candidate: 1.18.x

scripts/dpkg-buildpackage.pl
@@ -346,7 +346,7 @@ while (@ARGV) {
     } elsif (m/^-[EW]$/) {
 	# Deprecated option
 	warning(g_('-E and -W are deprecated, they are without effect'));
-    } elsif (/^-R(.*)$/ or /^--rules-target=(.*)$/) {
+    } elsif (/^-R(.*)$/ or /^--rules-file=(.*)$/) {
 	my $arg = $1;
 	@debian_rules = split ' ', $arg;
     } else {