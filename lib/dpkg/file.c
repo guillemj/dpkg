guillemj/dpkg

commit 9f7e58acdf28043bce2dfaf24ba27bb878418658

Use internerr() or BUG() instead of assert()
The problem with assert() is that it does not print the contents of
the variables. It also can be disabled on NDEBUG. But we always want
these consistency checks no matter what, and they are not performance
sensitive anyway.

Enable -Wno-nonnull-compare so that we can keep doing run-time non-NULL
checks in functions, instead of just compile-time checks.

lib/dpkg/file.c
@@ -25,7 +25,6 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-#include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
@@ -79,7 +78,8 @@ file_unlock(int lockfd, const char *lock_desc)
 {
 	struct flock fl;
 
-	assert(lockfd >= 0);
+	if (lockfd < 0)
+		internerr("lock fd is %d < 0", lockfd);
 
 	file_lock_setup(&fl, F_UNLCK);
 

TODO
@@ -28,7 +28,6 @@ TODO
    - Do more unused header include removal.
    - Add needed includes to all header files.
    - Get rid of useless "unsigned" modifiers.
-   - Use internerr instead of assert, and print more meaningful messages.
    - Use enums for currently hardcoded literals (replacingfilesandsaid,
      filetriggers_edited, etc).
    - Do not use nfmalloc (and friends) for non in-core db memory.

debian/rules
@@ -3,7 +3,12 @@
 # Copyright © 2004 Scott James Remnant <scott@netsplit.com>
 # Copyright © 2006-2012 Guillem Jover <guillem@debian.org>
 
-WFLAGS := -Wall -Wextra -Wno-unused-parameter -Wno-missing-field-initializers
+WFLAGS := \
+	-Wall -Wextra \
+	-Wno-missing-field-initializers \
+	-Wno-nonnull-compare \
+	-Wno-unused-parameter \
+	$(nil)
 
 # Use the in-tree dpkg-buildflags
 dpkg_buildflags = \

debian/usertags
@@ -15,7 +15,8 @@ Generic tags
 usertag			description
 -------			-----------
 
-assert			Errors caused by assert().
+assert			Errors caused by assert() (obsoleted by internerr()
+			  usage).
 segfault		Errors that cause segmentation faults.
 conffile		Issues with configuration file handling.
 symlink			Issues related to symlink handling.

dpkg-split/join.c
@@ -21,7 +21,6 @@
 #include <config.h>
 #include <compat.h>
 
-#include <assert.h>
 #include <limits.h>
 #include <string.h>
 #include <fcntl.h>
@@ -120,7 +119,9 @@ do_join(const char *const *argv)
   refi= NULL;
   for (pq= queue; pq; pq= pq->nextinqueue)
     if (!refi || pq->info.thispartn < refi->thispartn) refi= &pq->info;
-  assert(refi);
+  if (refi == NULL)
+    internerr("empty deb part queue");
+
   partlist= nfmalloc(sizeof(struct partinfo*)*refi->maxpartn);
   for (i = 0; i < refi->maxpartn; i++)
     partlist[i] = NULL;

dselect/baselist.cc
@@ -25,7 +25,6 @@
 
 #include <sys/ioctl.h>
 
-#include <assert.h>
 #include <errno.h>
 #include <string.h>
 #include <termios.h>
@@ -155,7 +154,10 @@ baselist::draw_column_item(column &col, int y, const char *item)
 
 void baselist::setheights() {
   int y= ymax - (title_height + colheads_height + thisstate_height);
-  assert(y>=1);
+
+  if (y < 1)
+    internerr("widget y=%d < 1", y);
+
   if (showinfo==2 && y>=7) {
     list_height= 5;
     whatinfo_height= 1;

dselect/main.cc
@@ -26,7 +26,6 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-#include <assert.h>
 #include <errno.h>
 #include <limits.h>
 #if HAVE_LOCALE_H

dselect/methlist.cc
@@ -22,7 +22,6 @@
 #include <config.h>
 #include <compat.h>
 
-#include <assert.h>
 #include <errno.h>
 #include <string.h>
 #include <stdio.h>
@@ -129,10 +128,16 @@ methodlist::methodlist() : baselist(&methodlistbindings) {
 
   struct dselect_option *opt, **ip;
   for (opt=options, ip=table, nitems=0; opt; opt=opt->next, nitems++) {
-    if (opt == coption) { assert(newcursor==-1); newcursor= nitems; }
+    if (opt == coption) {
+      if (newcursor != -1)
+        internerr("multiple methods with same index");
+      newcursor = nitems;
+    }
     *ip++= opt;
   }
-  assert(nitems==noptions);
+  if (nitems != noptions)
+    internerr("inconsistent number of items: ntimes=%d != noptions=%d",
+              nitems, noptions);
 
   if (newcursor==-1) newcursor= 0;
   setcursor(newcursor);

commit 7f7eb8cc77e84dd03791f7317c38242e49760b49

libdpkg: Fix internerr format string
Do not print filename which is going to be NULL.

Fixes: commit 9f7e58acdf28043bce2dfaf24ba27bb878418658

lib/dpkg/file.c
@@ -203,7 +203,7 @@ file_show(const char *filename)
 	pid_t pid;
 
 	if (filename == NULL)
-		internerr("file '%s' does not exist", filename);
+		internerr("filename is NULL");
 
 	pid = subproc_fork();
 	if (pid == 0) {