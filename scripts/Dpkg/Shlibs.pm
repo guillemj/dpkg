guillemj/dpkg

commit 174a5bd2a080847d0ed901d269a2ba74476eba8b

Dpkg::Shlibs: Warn when using LD_LIBRARY_PATH with a descendent of cwd
This makes the deprecation explicit, so that we can start noticing
what is inappropriately setting this variable for a purpose it was
not designed for.

scripts/Dpkg/Shlibs.pm
@@ -90,8 +90,18 @@ sub setup_library_paths {
 
     # XXX: Deprecated. Update library paths with LD_LIBRARY_PATH.
     if ($ENV{LD_LIBRARY_PATH}) {
+        require Cwd;
+        my $cwd = Cwd::getcwd;
+
         foreach my $path (split /:/, $ENV{LD_LIBRARY_PATH}) {
             $path =~ s{/+$}{};
+
+            if (Cwd::realpath($path) =~ m/^\Q$cwd\E/) {
+                warning(g_('deprecated use of LD_LIBRARY_PATH with private ' .
+                           'library directory which interferes with ' .
+                           'cross-building, please use -l option instead'));
+            }
+
             # XXX: This should be added to @custom_librarypaths, but as this
             # is deprecated we do not care as the code will go away.
             push @system_librarypaths, $path;

commit 8fe2c1ebf907f325b6985f471a00843eca77992b

Dpkg::Shlibs: Ignore nonexistent directories present in LD_LIBRARY_PATH
We were trying to use realpath() on nonexistent directories, which means
we'd get undef. Instead just completely ignore nonexistent directories
from any processing.

Fixes: commit 174a5bd2a080847d0ed901d269a2ba74476eba8b

scripts/Dpkg/Shlibs.pm
@@ -96,7 +96,9 @@ sub setup_library_paths {
         foreach my $path (split /:/, $ENV{LD_LIBRARY_PATH}) {
             $path =~ s{/+$}{};
 
-            if (Cwd::realpath($path) =~ m/^\Q$cwd\E/) {
+            my $realpath = Cwd::realpath($path);
+            next unless defined $realpath;
+            if ($realpath =~ m/^\Q$cwd\E/) {
                 warning(g_('deprecated use of LD_LIBRARY_PATH with private ' .
                            'library directory which interferes with ' .
                            'cross-building, please use -l option instead'));