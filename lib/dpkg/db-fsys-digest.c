guillemj/dpkg

commit 1166bbb5f099580a0c1c26d0e29b1ae3fe11ad0f

libdpkg: Switch db-fsys to use the new file_slurp() function
This reduces code duplication.

lib/dpkg/db-fsys-digest.c
@@ -80,13 +80,14 @@ write_filehash_except(struct pkginfo *pkg, struct pkgbin *pkgbin,
 }
 
 static void
-parse_filehash_buffer(char *buf, char *buf_end,
+parse_filehash_buffer(struct varbuf *buf,
                       struct pkginfo *pkg, struct pkgbin *pkgbin)
 {
 	char *thisline, *nextline;
 	const char *pkgname = pkg_name(pkg, pnaw_nonambig);
+	const char *buf_end = buf->buf + buf->used;
 
-	for (thisline = buf; thisline < buf_end; thisline = nextline) {
+	for (thisline = buf->buf; thisline < buf_end; thisline = nextline) {
 		struct filenamenode *namenode;
 		char *endline, *hash_end, *filename;
 
@@ -124,7 +125,7 @@ parse_filehash_buffer(char *buf, char *buf_end,
 		      thisline, filename);
 
 		/* Add the file to the list. */
-		namenode = findnamenode(filename, fnn_nocopy);
+		namenode = findnamenode(filename, 0);
 		namenode->newhash = thisline;
 	}
 }
@@ -132,43 +133,19 @@ parse_filehash_buffer(char *buf, char *buf_end,
 void
 parse_filehash(struct pkginfo *pkg, struct pkgbin *pkgbin)
 {
-	static int fd;
 	const char *hashfile;
-	struct stat st;
+	struct varbuf buf = VARBUF_INIT;
+	struct dpkg_error err = DPKG_ERROR_INIT;
 
 	hashfile = pkg_infodb_get_file(pkg, pkgbin, HASHFILE);
 
-	fd = open(hashfile, O_RDONLY);
-	if (fd < 0) {
-		if (errno == ENOENT)
-			return;
+	if (file_slurp(hashfile, &buf, &err) < 0 && err.syserrno != ENOENT)
+		dpkg_error_print(&err,
+		                 _("loading control file '%s' for package '%s'"),
+		                 HASHFILE, pkg_name(pkg, pnaw_nonambig));
 
-		ohshite(_("cannot open control file '%s' for package '%s'"),
-		        HASHFILE, pkg_name(pkg, pnaw_nonambig));
-	}
+	if (buf.used > 0)
+		parse_filehash_buffer(&buf, pkg, pkgbin);
 
-	if (fstat(fd, &st) < 0)
-		ohshite(_("cannot stat control file '%s' for package '%s'"),
-		        HASHFILE, pkg_name(pkg, pnaw_nonambig));
-
-	if (!S_ISREG(st.st_mode))
-		ohshit(_("control file '%s' for package '%s' is not a regular file"),
-		       HASHFILE, pkg_name(pkg, pnaw_nonambig));
-
-	if (st.st_size > 0) {
-		char *buf, *buf_end;
-
-		buf = nfmalloc(st.st_size);
-		buf_end = buf + st.st_size;
-
-		if (fd_read(fd, buf, st.st_size) < 0)
-			ohshite(_("cannot read control file '%s' for package '%s'"),
-			        HASHFILE, pkg_name(pkg, pnaw_nonambig));
-
-		parse_filehash_buffer(buf, buf_end, pkg, pkgbin);
-	}
-
-	if (close(fd))
-		ohshite(_("cannot close control file '%s' for package '%s'"),
-		        HASHFILE, pkg_name(pkg, pnaw_nonambig));
+	varbuf_destroy(&buf);
 }

lib/dpkg/db-fsys-files.c
@@ -77,11 +77,11 @@ static enum pkg_filesdb_load_status saidread = PKG_FILESDB_LOAD_NONE;
 void
 ensure_packagefiles_available(struct pkginfo *pkg)
 {
-  static int fd;
   const char *filelistfile;
   struct fileinlist **lendp;
-  struct stat stat_buf;
-  char *loaded_list, *loaded_list_end, *thisline, *nextline, *ptr;
+  char *loaded_list_end, *thisline, *nextline, *ptr;
+  struct varbuf buf = VARBUF_INIT;
+  struct dpkg_error err = DPKG_ERROR_INIT;
 
   if (pkg->files_list_valid)
     return;
@@ -99,12 +99,11 @@ ensure_packagefiles_available(struct pkginfo *pkg)
 
   onerr_abort++;
 
-  fd= open(filelistfile,O_RDONLY);
+  if (file_slurp(filelistfile, &buf, &err) < 0) {
+    if (err.syserrno != ENOENT)
+      dpkg_error_print(&err, _("loading files list file for package '%s'"),
+                       pkg_name(pkg, pnaw_nonambig));
 
-  if (fd==-1) {
-    if (errno != ENOENT)
-      ohshite(_("unable to open files list file for package '%.250s'"),
-              pkg_name(pkg, pnaw_nonambig));
     onerr_abort--;
     if (pkg->status != PKG_STAT_CONFIGFILES &&
         dpkg_version_is_informative(&pkg->configversion)) {
@@ -117,26 +116,11 @@ ensure_packagefiles_available(struct pkginfo *pkg)
     return;
   }
 
-  push_cleanup(cu_closefd, ehflag_bombout, 1, &fd);
-
-  if (fstat(fd, &stat_buf))
-    ohshite(_("unable to stat files list file for package '%.250s'"),
-            pkg_name(pkg, pnaw_nonambig));
-
-  if (!S_ISREG(stat_buf.st_mode))
-    ohshit(_("files list for package '%.250s' is not a regular file"),
-           pkg_name(pkg, pnaw_nonambig));
-
-  if (stat_buf.st_size) {
-    loaded_list = nfmalloc(stat_buf.st_size);
-    loaded_list_end = loaded_list + stat_buf.st_size;
-
-    if (fd_read(fd, loaded_list, stat_buf.st_size) < 0)
-      ohshite(_("reading files list for package '%.250s'"),
-              pkg_name(pkg, pnaw_nonambig));
+  if (buf.used) {
+    loaded_list_end = buf.buf + buf.used;
 
     lendp = &pkg->files;
-    thisline = loaded_list;
+    thisline = buf.buf;
     while (thisline < loaded_list_end) {
       struct filenamenode *namenode;
 
@@ -154,15 +138,13 @@ ensure_packagefiles_available(struct pkginfo *pkg)
                pkg_name(pkg, pnaw_nonambig));
       *ptr = '\0';
 
-      namenode = findnamenode(thisline, fnn_nocopy);
+      namenode = findnamenode(thisline, 0);
       lendp = pkg_files_add_file(pkg, namenode, lendp);
       thisline = nextline;
     }
   }
-  pop_cleanup(ehflag_normaltidy); /* fd = open() */
-  if (close(fd))
-    ohshite(_("error closing files list file for package '%.250s'"),
-            pkg_name(pkg, pnaw_nonambig));
+
+  varbuf_destroy(&buf);
 
   onerr_abort--;
 

commit 04f63813361b97b7da13653d547736b00e852b90

libdpkg: Fix filesystem digest parsing
Allocate the digest into the non-freeing memory pool, as we have stopped
doing that for the entire loaded file with the switch to the file_slurp()
function. Otherwise when we release the varbuf the fsys nodes will be
pointing to garbage.

Fixes: commit 1166bbb5f099580a0c1c26d0e29b1ae3fe11ad0f
Reported-by: Sven Joachim <svenjoac@gmx.de>
Bisected-by: Sven Joachim <svenjoac@gmx.de>

lib/dpkg/db-fsys-digest.c
@@ -126,7 +126,7 @@ parse_filehash_buffer(struct varbuf *buf,
 
 		/* Add the file to the list. */
 		namenode = findnamenode(filename, 0);
-		namenode->newhash = thisline;
+		namenode->newhash = nfstrsave(thisline);
 	}
 }
 