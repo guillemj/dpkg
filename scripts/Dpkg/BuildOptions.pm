7605ee419fa482baeb8db5ff42e3163df6ac51bd
Guillem Jover <guillem@debian.org>
2019-02-22 01:42:13 +0100

    Dpkg::BuildOptions: Fix typo in POD

--- scripts/Dpkg/BuildOptions.pm
+++ scripts/Dpkg/BuildOptions.pm
@@ -164,7 +164,7 @@ Each feature is prefixed with a ‘B<+>’ or a ‘B<->’ character as a marker
 to enable or disable it. The special feature “B<all>” can be used to act
 on all known features.
 
-Unknown of malformed features will emit warnings.
+Unknown or malformed features will emit warnings.
 
 =cut
 
