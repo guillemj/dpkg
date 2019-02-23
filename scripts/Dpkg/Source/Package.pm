guillemj/dpkg

commit d1d35a56e401181b6d15baf474a7db21d3372a65

Dpkg::Source::Package, dpkg-source: Switch to new Dpkg::Source::Format module
This move all ad-hoc code to use the new Dpkg::Source::Format module.

scripts/Dpkg/Source/Package.pm
@@ -213,6 +213,7 @@ sub new {
     my $class = ref($this) || $this;
     my $self = {
         fields => Dpkg::Control->new(type => CTRL_PKG_SRC),
+        format => Dpkg::Source::Format->new(),
         options => {},
         checksums => Dpkg::Checksums->new(),
     };
@@ -287,34 +288,28 @@ sub initialize {
 sub upgrade_object_type {
     my ($self, $update_format) = @_;
     $update_format //= 1;
-    $self->{fields}{'Format'} //= '1.0';
-    my $format = $self->{fields}{'Format'};
 
-    if ($format =~ /^(\d+)(?:\.(\d+))?(?:\s+\(([a-z0-9]+)\))?$/) {
-        my ($major, $minor, $variant) = ($1, $2, $3);
+    my $format = $self->{fields}{'Format'} // '1.0';
+    my ($major, $minor, $variant) = $self->{format}->set($format);
 
-        my $module = "Dpkg::Source::Package::V$major";
-        $module .= '::' . ucfirst $variant if defined $variant;
-        eval qq{
-            pop \@INC if \$INC[-1] eq '.';
-            require $module;
-            \$minor = \$${module}::CURRENT_MINOR_VERSION;
-        };
-        if ($@) {
-            error(g_("source package format '%s' is not supported: %s"),
-                  $format, $@);
-        }
-
-        if ($update_format) {
-            $minor //= 0;
-            $self->{fields}{'Format'} = "$major.$minor";
-            $self->{fields}{'Format'} .= " ($variant)" if defined $variant;
-        }
-        $module->prerequisites() if $module->can('prerequisites');
-        bless $self, $module;
-    } else {
-        error(g_("source package format '%s' is invalid"), $format);
+    my $module = "Dpkg::Source::Package::V$major";
+    $module .= '::' . ucfirst $variant if defined $variant;
+    eval qq{
+        pop \@INC if \$INC[-1] eq '.';
+        require $module;
+        \$minor = \$${module}::CURRENT_MINOR_VERSION;
+    };
+    if ($@) {
+        error(g_("source package format '%s' is not supported: %s"),
+              $format, $@);
     }
+    if ($update_format) {
+        $self->{format}->set_from_parts($major, $minor, $variant);
+        $self->{fields}{'Format'} = $self->{format}->get();
+    }
+
+    $module->prerequisites() if $module->can('prerequisites');
+    bless $self, $module;
 }
 
 =item $p->get_filename()
@@ -548,10 +543,7 @@ sub extract {
         my $format_file = File::Spec->catfile($srcdir, 'format');
 	unless (-e $format_file) {
 	    mkdir($srcdir) unless -e $srcdir;
-	    open(my $format_fh, '>', $format_file)
-	        or syserr(g_('cannot write %s'), $format_file);
-	    print { $format_fh } $self->{fields}{'Format'} . "\n";
-	    close($format_fh);
+            $self->{format}->save($format_file);
 	}
     }
 

scripts/dpkg-source.pl
@@ -396,14 +396,10 @@ if ($options{opmode} =~ /^(build|print-format|(before|after)-build|commit)$/) {
 
     # Select the format to use
     if (not defined $build_format) {
-	if (-e "$dir/debian/source/format") {
-	    open(my $format_fh, '<', "$dir/debian/source/format")
-	        or syserr(g_('cannot read %s'), "$dir/debian/source/format");
-	    $build_format = <$format_fh>;
-	    chomp($build_format) if defined $build_format;
-	    error(g_('%s is empty'), "$dir/debian/source/format")
-		unless defined $build_format and length $build_format;
-	    close($format_fh);
+        my $format_file = "$dir/debian/source/format";
+        if (-e $format_file) {
+            my $format = Dpkg::Source::Format->new(filename => $format_file);
+            $build_format = $format->get();
 	} else {
 	    warning(g_('no source format specified in %s, ' .
 	               'see dpkg-source(1)'), 'debian/source/format')

commit 41628ced3ebbdb2ed2219b5320b6301aee7d65be

Dpkg::Source::Package: Add a missing use Dpkg::Source::Format
Closes: #921031
Fixes: commit d1d35a56e401181b6d15baf474a7db21d3372a65
Diagnosed-by: Ian Jackson <ijackson@chiark.greenend.org.uk>

scripts/Dpkg/Source/Package.pm
@@ -56,6 +56,7 @@ use Dpkg::Exit qw(run_exit_handlers);
 use Dpkg::Path qw(check_files_are_the_same find_command);
 use Dpkg::IPC;
 use Dpkg::Vendor qw(run_vendor_hook);
+use Dpkg::Source::Format;
 
 my $diff_ignore_default_regex = '
 # Ignore general backup files