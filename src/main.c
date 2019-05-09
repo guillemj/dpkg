guillemj/dpkg

commit 52427d9075b4b062fa0db16a70d2c2bda6407857

dpkg: Switch to set the default force option from the forceinfos array

src/main.c
@@ -743,6 +743,7 @@ int main(int argc, const char *const *argv) {
   dpkg_locales_init(PACKAGE);
   dpkg_program_init("dpkg");
   dpkg_options_load(DPKG, cmdinfos);
+  set_force_default(FORCE_ALL);
   dpkg_options_parse(&argv, cmdinfos, printforhelp);
 
   /* When running as root, make sure our primary group is also root, so

src/force.c
@@ -37,8 +37,8 @@
 
 #include "force.h"
 
-static int force_mask = FORCE_ALL;
-static int force_flags = FORCE_DOWNGRADE;
+static int force_mask;
+static int force_flags;
 
 enum forcetype {
 	FORCETYPE_DISABLED,
@@ -318,6 +318,18 @@ parse_force(const char *value, bool set)
 			break;
 		value = ++comma;
 	}
+}
+
+void
+set_force_default(int mask)
+{
+	const struct forceinfo *fip;
+
+	force_mask = mask;
+
+	for (fip = forceinfos; fip->name; fip++)
+		if (fip->type == FORCETYPE_ENABLED)
+			set_force(fip->flag);
 }
 
 void

src/force.h
@@ -67,6 +67,8 @@ void
 parse_force(const char *value, bool set);
 
 void
+set_force_default(int mask);
+void
 set_force_option(const struct cmdinfo *cip, const char *value);
 void
 reset_force_option(const struct cmdinfo *cip, const char *value);

commit 770aa74afb0fbf1c61605e4e2d0a1c27c3a847d6

dpkg: Set the force defaults before loading the config file
Otherwise we incorrectly override them.

Fixes: commit 52427d9075b4b062fa0db16a70d2c2bda6407857
Closes: #928671

src/main.c
@@ -742,8 +742,8 @@ int main(int argc, const char *const *argv) {
 
   dpkg_locales_init(PACKAGE);
   dpkg_program_init("dpkg");
-  dpkg_options_load(DPKG, cmdinfos);
   set_force_default(FORCE_ALL);
+  dpkg_options_load(DPKG, cmdinfos);
   dpkg_options_parse(&argv, cmdinfos, printforhelp);
 
   /* When running as root, make sure our primary group is also root, so