guillemj/dpkg

commit 438db7a31e074691e70e8ace7fff91631030624b

dpkg-deb, dpkg: Do not allow pathnames with embedded newlines
None of the deb toolchain supports embedded newlines in filenames, we
could try to escape them but then we'd need to add support in other
tools to unescape these too, and this does not sem worth the trouble.

Closes: #720761

dpkg-deb/build.c
@@ -194,6 +194,8 @@ file_treewalk_feed(const char *dir, int fd_out)
   /* We need to reorder the files so we can make sure that symlinks
    * will not appear before their target. */
   while ((fi = file_info_get(dir, pipefd[0])) != NULL) {
+    if (strchr(fi->fn, '\n'))
+      ohshit(_("newline not allowed in pathname '%s'"), fi->fn);
     if (S_ISLNK(fi->st.st_mode)) {
       file_info_list_append(&symlist, &symlist_end, fi);
     } else {

src/archives.c
@@ -658,6 +658,10 @@ tarobject(void *ctx, struct tar_entry *ti)
 
   tar_entry_update_from_system(ti);
 
+  /* Perform some sanity checks on the tar entry. */
+  if (strchr(ti->name, '\n'))
+    ohshit(_("newline not allowed in archive object name '%.255s'"), ti->name);
+
   /* Append to list of files.
    * The trailing ‘/’ put on the end of names in tarfiles has already
    * been stripped by tar_extractor(). */

commit 36644e7d3041f403ae18af45ff6b13a51a8378ae

dpkg-deb: Honor --nocheck when building packages with newlines in filenames
Fixes: commit 438db7a31e074691e70e8ace7fff91631030624b
Closes: #929727
Stable-Candidate: 1.18.x

dpkg-deb/build.c
@@ -168,7 +168,7 @@ file_treewalk_feed(const char *dir, int fd_out)
 
     nodename = str_fmt("./%s", virtname);
 
-    if (strchr(nodename, '\n'))
+    if (!nocheckflag && strchr(nodename, '\n'))
       ohshit(_("newline not allowed in pathname '%s'"), nodename);
 
     /* We need to reorder the files so we can make sure that symlinks