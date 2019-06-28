195edc4201f063a11ccdc10c523d671048353cca
Guillem Jover <guillem@debian.org>
2019-06-28 02:54:32 +0200

    dpkg-shlibdeps: Document split_soname() function
    Explain what each different pattern means, and what it maps to.
    
    Prompted-by: Christopher Crim <christopher.crim@quoininc.com>

--- scripts/dpkg-shlibdeps.pl
+++ scripts/dpkg-shlibdeps.pl
@@ -705,11 +705,15 @@ sub add_shlibs_dep {
 
 sub split_soname {
     my $soname = shift;
+
     if ($soname =~ /^(.*)\.so\.(.*)$/) {
+        # Shared library with stable <name>.so.<version> format.
 	return wantarray ? ($1, $2) : 1;
     } elsif ($soname =~ /^(.*)-(\d.*)\.so$/) {
+        # Shared library/module with unstable <name>-<version>.so format.
 	return wantarray ? ($1, $2) : 1;
     } else {
+        # Something else.
 	return wantarray ? () : 0;
     }
 }
