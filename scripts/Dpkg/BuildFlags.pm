36925e06749e495a475f6c42543b226d3f7ae27b
Guillem Jover <guillem@debian.org>
2019-07-30 04:45:57 +0200

    Dpkg::BuildFlags: Add new unset() method
    This makes it possible to unset variables, so that they do not get
    output by the various tools.
    
    Requested-by: Daniel Schepler <dschepler@gmail.com>

--- scripts/Dpkg/BuildFlags.pm
+++ scripts/Dpkg/BuildFlags.pm
@@ -18,7 +18,7 @@ package Dpkg::BuildFlags;
 use strict;
 use warnings;
 
-our $VERSION = '1.03';
+our $VERSION = '1.04';
 
 use Dpkg ();
 use Dpkg::Gettext;
@@ -210,6 +210,20 @@ sub load_config {
     $self->load_user_config();
     $self->load_environment_config();
     $self->load_maintainer_config();
+}
+
+=item $bf->unset($flag)
+
+Unset the build flag $flag, so that it will not be known anymore.
+
+=cut
+
+sub unset {
+    my ($self, $flag) = @_;
+
+    delete $self->{flags}->{$flag};
+    delete $self->{origin}->{$flag};
+    delete $self->{maintainer}->{$flag};
 }
 
 =item $bf->set($flag, $value, $source, $maint)
@@ -439,6 +453,10 @@ sub list {
 =back
 
 =head1 CHANGES
+
+=head2 Version 1.04 (dpkg 1.20.0)
+
+New method: $bf->unset().
 
 =head2 Version 1.03 (dpkg 1.16.5)
 
