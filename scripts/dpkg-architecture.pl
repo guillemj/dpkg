guillemj/dpkg

commit ba23abe9e93ffcdae09ec234208ea55478c3f5ac

dpkg-architecture: Fix handling of exec failing in --command
We should check exec's return value, and error out ourselves so that
we exit with a correct error code. We suppress the perl exec warning,
so that we do not get duplicate messages.

Reported-by: Helmut Grohne <helmut@subdivi.de>

scripts/dpkg-architecture.pl
@@ -344,7 +344,8 @@ if ($action eq 'list') {
     exit !debarch_is($v{DEB_HOST_ARCH}, $req_is_arch);
 } elsif ($action eq 'command') {
     @ENV{keys %v} = values %v;
-    exec @ARGV;
+    no warnings qw(exec);
+    exec @ARGV or syserr(g_('unable to execute %s'), "@ARGV");
 } elsif ($action eq 'query') {
     print "$v{$req_variable_to_print}\n";
 } elsif ($action eq 'list-known') {

commit ff589fd1d161dbea88dea82c7edee5ed62549c74

dpkg-architecture: Suppress perlcritic policy for «no warnings»
Fixes: commit ba23abe9e93ffcdae09ec234208ea55478c3f5ac

scripts/dpkg-architecture.pl
@@ -344,6 +344,7 @@ if ($action eq 'list') {
     exit !debarch_is($v{DEB_HOST_ARCH}, $req_is_arch);
 } elsif ($action eq 'command') {
     @ENV{keys %v} = values %v;
+    ## no critic (TestingAndDebugging::ProhibitNoWarnings)
     no warnings qw(exec);
     exec @ARGV or syserr(g_('unable to execute %s'), "@ARGV");
 } elsif ($action eq 'query') {