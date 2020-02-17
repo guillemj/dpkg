39decc4c215a2839f5d7964cfb0624d71aab878b
Guillem Jover <guillem@debian.org>
2020-02-17 19:46:41 +0100

    Dpkg::BuildAPI: New module
    This module implements the new dpkg-build-api interface that will be
    used to control gradual default changes avoiding disruption, where
    the specific packages can opt-in at their convenience.

---
+++ scripts/Dpkg/BuildAPI.pm
@@ -0,0 +1,142 @@
+# Copyright © 2020-2022 Guillem Jover <guillem@debian.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package Dpkg::BuildAPI 0.01;
+
+use strict;
+use warnings;
+
+our @EXPORT_OK = qw(
+    get_build_api
+    reset_build_api
+);
+
+use Exporter qw(import);
+
+use Dpkg::Gettext;
+use Dpkg::ErrorHandling;
+use Dpkg::BuildEnv;
+use Dpkg::Version;
+use Dpkg::Deps;
+
+use constant {
+    DEFAULT_BUILD_API => '0',
+    MAX_BUILD_API => '0',
+};
+
+my $build_api;
+
+=encoding utf8
+
+=head1 NAME
+
+Dpkg::BuildAPI - handle build API versions
+
+=head1 DESCRIPTION
+
+The Dpkg::BuildAPI module provides functions to fetch the current dpkg
+build API level.
+
+=head1 FUNCTIONS
+
+=over 4
+
+=item $level = get_build_api([$ctrl])
+
+Get the build API level, from the environment variable B<DPKG_BUILD_API>,
+or if not defined and a $ctrl C<Dpkg::Control::Info> object passed as an
+argument, from its build dependency fields. If no $ctrl object gets passed
+the previous value obtained is returned.
+
+=cut
+
+sub get_build_api {
+    my $ctrl = shift;
+
+    return $build_api if defined $build_api && ! defined $ctrl;
+
+    if (Dpkg::BuildEnv::has('DPKG_BUILD_API')) {
+        $build_api = Dpkg::BuildEnv::get('DPKG_BUILD_API');
+    } elsif (defined $ctrl) {
+        my $src = $ctrl->get_source();
+        my @dep_list = deps_concat(map {
+            $src->{$_ }
+        } qw(Build-Depends Build-Depends-Indep Build-Depends-Arch));
+
+        my $deps = deps_parse(@dep_list,
+            build_dep => 1,
+            reduce_restrictions => 1,
+        );
+
+        if (not defined $deps) {
+            $build_api = DEFAULT_BUILD_API;
+            return $build_api;
+        }
+
+        deps_iterate($deps, sub {
+            my $dep = shift;
+
+            return 1 if $dep->{package} ne 'dpkg-build-api';
+
+            if (! defined $dep->{relation} || $dep->{relation} ne REL_EQ) {
+                error(g_('dpkg build API level needs an exact version'));
+            }
+
+            if (defined $build_api and $build_api ne $dep->{version}) {
+                error(g_('dpkg build API level with conflicting versions: %s vs %s'),
+                      $build_api, $dep->{version});
+            }
+
+            $build_api = $dep->{version};
+
+            return 1;
+        });
+    }
+
+    $build_api //= DEFAULT_BUILD_API;
+
+    if ($build_api !~ m/^[0-9]+$/) {
+        error(g_("invalid dpkg build API level '%s'"), $build_api);
+    }
+
+    if ($build_api > MAX_BUILD_API) {
+        error(g_("dpkg build API level '%s' greater than max '%s'"),
+              $build_api, MAX_BUILD_API);
+    }
+
+    return $build_api;
+}
+
+=item reset_build_api()
+
+Reset the cached build API level.
+
+=cut
+
+sub reset_build_api {
+    $build_api = undef;
+}
+
+=back
+
+=head1 CHANGES
+
+=head2 Version 0.xx
+
+This is a private module.
+
+=cut
+
+1;
---
+++ scripts/t/Dpkg_BuildAPI.t
@@ -0,0 +1,93 @@
+#!/usr/bin/perl
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use strict;
+use warnings;
+
+use Test::More tests => 17;
+use Test::Dpkg qw(:paths);
+
+$ENV{DEB_HOST_ARCH} = 'amd64';
+
+use Dpkg::Control;
+use Dpkg::Control::Info;
+
+BEGIN {
+    use_ok('Dpkg::BuildAPI', qw(get_build_api reset_build_api));
+}
+
+my $datadir = test_get_data_path();
+
+sub test_load_ctrl {
+    my $file = shift;
+
+    my $ctrl = Dpkg::Control::Info->new(type => CTRL_INFO_SRC);
+    $ctrl->load("$datadir/$file");
+
+    return $ctrl;
+}
+
+sub test_parse_env_invalid {
+    my ($value, $desc) = @_;
+
+    my $api;
+
+    eval {
+        local $ENV{DPKG_BUILD_API} = $value;
+        reset_build_api();
+        $api = get_build_api();
+    };
+
+    ok($@, "failed to parse build API $desc: $@");
+    is($api, undef, "parsing invalid build API $desc returns undef");
+}
+
+sub test_parse_ctrl_invalid {
+    my ($file, $desc) = @_;
+
+    my $ctrl = test_load_ctrl($file);
+    my $api;
+
+    eval {
+        reset_build_api();
+        $api = get_build_api($ctrl);
+    };
+    ok($@, "failed to parse build API $desc from $file: $@");
+    is($api, undef, "parsing invalid build API $desc from $file returns undef");
+}
+
+sub test_parse_ctrl {
+    my ($file, $exp_api, $desc) = @_;
+
+    my $ctrl = test_load_ctrl($file);
+
+    reset_build_api();
+    my $api = get_build_api($ctrl);
+    is($api, $exp_api, "build API $desc matches");
+}
+
+test_parse_env_invalid('aa', 'text level');
+test_parse_env_invalid('999999999', 'out of range');
+
+test_parse_ctrl_invalid('ctrl-api-desync', 'multiple build API levels out of sync');
+test_parse_ctrl_invalid('ctrl-api-no-ver', 'build API with no level');
+test_parse_ctrl_invalid('ctrl-api-rel-noeq', 'API level with non-eq operator');
+test_parse_ctrl_invalid('ctrl-api-gt-max', 'API level > max');
+test_parse_ctrl_invalid('ctrl-api-no-int', 'API level is not an integer');
+
+test_parse_ctrl('ctrl-api-default', 0, 'default API level');
+test_parse_ctrl('ctrl-api-explicit', 0, 'explicit API level');
+
+# TODO: Add more test cases.
