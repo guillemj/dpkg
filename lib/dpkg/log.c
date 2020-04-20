guillemj/dpkg

commit bbb52d7a9a8061920414634af22d76b7f02466eb

libdpkg: Create the logfile with correct permissions
We switch from fopen(3) to open(2) so that we get more control when
opening the log file and can explicitly give the corrent mode to use.

Remove the code from the postinst, as it should now be unnecessary.

lib/dpkg/log.c
@@ -21,11 +21,14 @@
 #include <config.h>
 #include <compat.h>
 
+#include <sys/types.h>
+#include <sys/stat.h>
+
 #include <errno.h>
 #include <time.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <stdarg.h>
-#include <stdio.h>
 
 #include <dpkg/i18n.h>
 #include <dpkg/dpkg.h>
@@ -38,7 +41,7 @@ void
 log_message(const char *fmt, ...)
 {
 	static struct varbuf log;
-	static FILE *logfd = NULL;
+	static int logfd = -1;
 	char time_str[20];
 	time_t now;
 	va_list args;
@@ -46,27 +49,31 @@ log_message(const char *fmt, ...)
 	if (!log_file)
 		return;
 
-	if (!logfd) {
-		logfd = fopen(log_file, "a");
-		if (!logfd) {
+	if (logfd < 0) {
+		logfd = open(log_file, O_CREAT | O_APPEND, 0644);
+		if (logfd < 0) {
 			notice(_("could not open log '%s': %s"),
 			       log_file, strerror(errno));
 			log_file = NULL;
 			return;
 		}
-		setlinebuf(logfd);
-		setcloexec(fileno(logfd), log_file);
+		setcloexec(logfd, log_file);
 	}
-
-	va_start(args, fmt);
-	varbuf_reset(&log);
-	varbuf_vprintf(&log, fmt, args);
-	va_end(args);
 
 	time(&now);
 	strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S",
 	         localtime(&now));
-	fprintf(logfd, "%s %s\n", time_str, log.buf);
+
+	va_start(args, fmt);
+	varbuf_reset(&log);
+	varbuf_add_str(&log, time_str);
+	varbuf_add_char(&log, ' ');
+	varbuf_vprintf(&log, fmt, args);
+	varbuf_add_char(&log, '\n');
+	varbuf_end_str(&log);
+	va_end(args);
+
+	fd_write(logfd, log.buf, log.size);
 }
 
 struct pipef {

debian/dpkg.postinst
@@ -14,23 +14,9 @@ create_database() {
     done
 }
 
-
-# Create log file and set default permissions if possible
-create_logfile() {
-    logfile=$DPKG_ROOT/var/log/dpkg.log
-
-    if [ ! -f "$logfile" ]; then
-        touch "$logfile"
-        chmod 644 "$logfile"
-        chown root:root "$logfile" 2>/dev/null || chown 0:0 "$logfile"
-    fi
-}
-
-
 case "$1" in
     configure)
 	create_database
-	create_logfile
 	;;
 
     abort-upgrade|abort-deconfigure|abort-remove)

commit a2793c9621c864a959c71b9ab964dfe496d9a1c1

libdpkg: Open the log file as write-only instead of read-only
Otherwise we end up not writing anything to the log file.

Fixes: commit bbb52d7a9a8061920414634af22d76b7f02466eb
Closes: #953684
Signed-off-by: Guillem Jover <guillem@debian.org>

lib/dpkg/log.c
@@ -50,7 +50,7 @@ log_message(const char *fmt, ...)
 		return;
 
 	if (logfd < 0) {
-		logfd = open(log_file, O_CREAT | O_APPEND, 0644);
+		logfd = open(log_file, O_WRONLY | O_CREAT | O_APPEND, 0644);
 		if (logfd < 0) {
 			notice(_("could not open log '%s': %s"),
 			       log_file, strerror(errno));