8c59d393220ab34cd7a6ee3791f05ae15d3cf44b
Helmut Grohne <helmut@subdivi.de>
2020-05-20 18:35:08 +0200

    dpkg-realpath: Add support for DPKG_ROOT
    [guillem@debian.org:
      - Hook the canonicalize function into the script.
      - Document the DPKG_ROOT usage in the man page. ]
    
    Signed-off-by: Guillem Jover <guillem@debian.org>

--- scripts/dpkg-realpath.sh
+++ scripts/dpkg-realpath.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #
+# Copyright © 2020 Helmut Grohne <helmut@subdivi.de>
 # Copyright © 2020 Guillem Jover <guillem@debian.org>
 #
 # This program is free software; you can redistribute it and/or modify
@@ -45,6 +46,73 @@ Options:
 END
 }
 
+canonicalize() {
+  local src="$1"
+  local root="$2"
+  local loop=0
+  local result="$root"
+  local dst
+
+  if [ "${src#"$root"}" = "$src" ]; then
+    error "link not within root"
+  fi
+  # Remove prefixed root dir.
+  src=${src#"$root"}
+  # Remove prefixed slashes.
+  while [ "$src" != "${src#/}" ]; do
+     src=${src#/}
+  done
+  while [ -n "$src" ]; do
+    loop=$((loop + 1))
+    if [ "$loop" -gt 25 ]; then
+      error "too many levels of symbolic links"
+    fi
+    # Get the first directory component.
+    prefix=${src%%/*}
+    # Remove the first directory component from src.
+    src=${src#"$prefix"}
+    # Remove prefixed slashes.
+    while [ "$src" != "${src#/}" ]; do
+      src=${src#/}
+    done
+    # Resolve the first directory component.
+    if [ "$prefix" = . ]; then
+      # Ignore, stay at the same directory.
+      :
+    elif [ "$prefix" = .. ]; then
+      # Go up one directory.
+      result=${result%/*}
+      if [ "${result#"$root"}" = "$result" ]; then
+        result="$root"
+      fi
+    elif [ -h "$result/$prefix" ]; then
+      # Resolve the symlink within $result.
+      dst=$(readlink "$result/$prefix")
+      case "$dst" in
+      /*)
+        # Absolute pathname, reset result back to $root.
+        result=$root
+        src="$dst${src:+/$src}"
+        # Remove prefixed slashes.
+        while [ "$src" != "${src#/}" ]; do
+          src=${src#/}
+        done
+        ;;
+      *)
+        # Relative pathname.
+        src="$dst${src:+/$src}"
+        ;;
+      esac
+    else
+      # Otherwise append the prefix.
+      result="$result/$prefix"
+    fi
+  done
+  # We are done, print the resolved pathname, w/o $root.
+  result="${result#"$root"}"
+  echo "${result:-/}"
+}
+
 setup_colors
 
 [ $# -eq 1 ] || badusage "missing pathname"
@@ -73,6 +141,6 @@ while [ $# -ne 0 ]; do
   shift
 done
 
-realpath "$pathname"
+canonicalize "$pathname" "${DPKG_ROOT:-}"
 
 exit 0
