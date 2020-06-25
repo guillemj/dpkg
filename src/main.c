guillemj/dpkg

commit e0e096046a8919a30b70d6bfd0443f25ceab3cdc

Add Protected field support
This field is intended to make it possible to move several of the
current packages marked as Essential, so that they can be removed on
installations where these do not make sense being installed.

Protected packages have some of the properties of Essential, but not
all. These are intended to be used mostly for packages that are involved
in booting the system.

src/main.c
@@ -128,7 +128,7 @@ usage(const struct cmdinfo *ci, const char *value)
 
   printf(_(
 "Assertable features: support-predepends, working-epoch, long-filenames,\n"
-"  multi-conrep, multi-arch, versioned-provides.\n"
+"  multi-conrep, multi-arch, versioned-provides protected-field.\n"
 "\n"));
 
   printf(_(
@@ -567,6 +567,7 @@ static const struct cmdinfo cmdinfos[]= {
   ACTION( "assert-multi-conrep",             0,  act_assertmulticonrep,    assertmulticonrep ),
   ACTION( "assert-multi-arch",               0,  act_assertmultiarch,      assertmultiarch ),
   ACTION( "assert-versioned-provides",       0,  act_assertverprovides,    assertverprovides ),
+  ACTION( "assert-protected-field",          0,  act_assert_protected,     assert_protected ),
   ACTION( "add-architecture",                0,  act_arch_add,             arch_add        ),
   ACTION( "remove-architecture",             0,  act_arch_remove,          arch_remove     ),
   ACTION( "print-architecture",              0,  act_printarch,            printarch   ),

Makefile.am
@@ -48,6 +48,7 @@ dist_doc_DATA = \
 	doc/README.api \
 	doc/README.feature-removal-schedule \
 	doc/frontend.txt \
+	doc/protected-field.txt \
 	doc/rootless-builds.txt \
 	doc/triggers.txt \
 	$(nil)

debian/dpkg-dev.docs
@@ -1,3 +1,4 @@
 usr/share/doc/dpkg/frontend.txt
+usr/share/doc/dpkg/protected-field.txt
 usr/share/doc/dpkg/rootless-builds.txt
 usr/share/doc/dpkg/triggers.txt

lib/dpkg/dpkg-db.h
@@ -108,6 +108,8 @@ struct pkgbin {
   struct dependency *depends;
   /** The ‘essential’ flag, true = yes, false = no (absent). */
   bool essential;
+  /** The ‘protected’ flag, true = yes, false = no (absent). */
+  bool is_protected;
   enum pkgmultiarch multiarch;
   const struct dpkg_arch *arch;
   /** The following is the "pkgname:archqual" cached string, if this was a

lib/dpkg/parse.c
@@ -54,6 +54,7 @@ const struct fieldinfo fieldinfos[]= {
   /* Note: Capitalization of field name strings is important. */
   { FIELD("Package"),          f_name,            w_name                                     },
   { FIELD("Essential"),        f_boolean,         w_booleandefno,   PKGIFPOFF(essential)     },
+  { FIELD("Protected"),        f_boolean,         w_booleandefno,   PKGIFPOFF(is_protected)  },
   { FIELD("Status"),           f_status,          w_status                                   },
   { FIELD("Priority"),         f_priority,        w_priority                                 },
   { FIELD("Section"),          f_section,         w_section                                  },

lib/dpkg/pkg.c
@@ -99,6 +99,7 @@ void
 pkgbin_blank(struct pkgbin *pkgbin)
 {
 	pkgbin->essential = false;
+	pkgbin->is_protected = false;
 	pkgbin->depends = NULL;
 	pkgbin->pkgname_archqual = NULL;
 	pkgbin->description = NULL;

man/deb-control.pod
@@ -116,12 +116,18 @@ distribution policy.
 
 The approximate total size of the package's installed files, in KiB units.
 
+=item B<Protected:> B<yes>|B<no>
+
+This field is usually only needed when the answer is B<yes>.
+It denotes a package that is required for proper booting of the system.
+L<dpkg(1)> or any other installation tool will not allow a B<Protected>
+package to be removed (at least not without using one of the force options).
+
 =item B<Essential:> B<yes>|B<no>
 
-This field is usually only needed when the answer is B<yes>. It denotes
-a package that is required for proper operation of the system. Dpkg
-or any other installation tool will not allow an
-B<Essential>
+This field is usually only needed when the answer is B<yes>.
+It denotes a package that is required for proper operation of the system.
+L<dpkg(1)> or any other installation tool will not allow an B<Essential>
 package to be removed (at least not without using one of the force options).
 
 =item B<Build-Essential:> B<yes>|B<no>

man/deb-src-control.pod
@@ -371,6 +371,8 @@ B<Build-Profiles> field, then this binary package is generated if and
 only if the condition expressed by the conjunctive normal form expression
 evaluates to true.
 
+=item B<Protected:> B<Byes>|B<no>
+
 =item B<Essential:> B<yes>|B<no>
 
 =item B<Build-Essential:> B<yes>|B<no>

commit 6dd2a4e569ba0a852a91d5fc3c315c56fe61e903

dpkg: Add missing coma in assertable list
Reported-by: Sven Joachim <svenjoac@gmx.de>
Fixes: commit e0e096046a8919a30b70d6bfd0443f25ceab3cdc

src/main.c
@@ -128,7 +128,7 @@ usage(const struct cmdinfo *ci, const char *value)
 
   printf(_(
 "Assertable features: support-predepends, working-epoch, long-filenames,\n"
-"  multi-conrep, multi-arch, versioned-provides protected-field.\n"
+"  multi-conrep, multi-arch, versioned-provides, protected-field.\n"
 "\n"));
 
   printf(_(