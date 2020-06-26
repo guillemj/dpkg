guillemj/dpkg

commit 584d2a5575627f34dd796b550b09c590d34a79ae

dpkg-realpath: Add new program
This program will be used by dpkg-maintscript-helper and any maintainter
script that needs a realpath that can handle pathnames relative to the
dpkg root directory.

scripts/dpkg-realpath.sh
@@ -1,0 +1,78 @@
+#!/bin/sh
+#
+# Copyright © 2020 Guillem Jover <guillem@debian.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+set -e
+
+PROGNAME=$(basename "$0")
+version="unknown"
+
+PKGDATADIR=scripts/sh
+
+. "$PKGDATADIR/dpkg-error.sh"
+
+show_version()
+{
+  cat <<END
+Debian $PROGNAME version $version.
+
+This is free software; see the GNU General Public License version 2 or
+later for copying conditions. There is NO warranty.
+END
+}
+
+show_usage()
+{
+  cat <<END
+Usage: $PROGNAME [<option>...] <pathname>
+
+Options:
+       --version      Show the version.
+  -?,  --help         Show this help message.
+END
+}
+
+setup_colors
+
+[ $# -eq 1 ] || badusage "missing pathname"
+
+while [ $# -ne 0 ]; do
+  case "$1" in
+  --version)
+    show_version
+    exit 0
+    ;;
+  --help|-\?)
+    show_usage
+    exit 0
+    ;;
+  --)
+    shift
+    pathname="$1"
+    ;;
+  --*)
+    badusage "unknown option: $1"
+    ;;
+  *)
+    pathname="$1"
+    ;;
+  esac
+  shift
+done
+
+realpath "$pathname"
+
+exit 0

debian/dpkg.manpages
@@ -2,6 +2,7 @@ usr/share/man/*/dpkg-deb.1
 usr/share/man/*/dpkg-divert.1
 usr/share/man/*/dpkg-maintscript-helper.1
 usr/share/man/*/dpkg-query.1
+usr/share/man/*/dpkg-realpath.1
 usr/share/man/*/dpkg-split.1
 usr/share/man/*/dpkg-statoverride.1
 usr/share/man/*/dpkg-trigger.1

man/Makefile.am
@@ -42,6 +42,7 @@ man_MANS = \
 	dpkg-name.1 \
 	dpkg-parsechangelog.1 \
 	dpkg-query.1 \
+	dpkg-realpath.1 \
 	dpkg-scanpackages.1 \
 	dpkg-scansources.1 \
 	dpkg-shlibdeps.1 \
@@ -115,6 +116,7 @@ EXTRA_DIST = \
 	dpkg-name.pod \
 	dpkg-parsechangelog.pod \
 	dpkg-query.pod \
+	dpkg-realpath.pod \
 	dpkg-scanpackages.pod \
 	dpkg-scansources.pod \
 	dpkg-shlibdeps.pod \

man/dpkg-realpath.pod
@@ -1,0 +1,59 @@
+# dpkg manual page - dpkg-realpath(1)
+#
+# Copyright © 2020 Guillem Jover <guillem@debian.org>
+#
+# This is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+=encoding utf8
+
+=head1 NAME
+
+dpkg-realpath - print the resolved pathname
+
+=head1 SYNOPSIS
+
+B<dpkg-realpath> [I<option>...] I<pathname>
+
+=head1 DESCRIPTION
+
+B<dpkg-realpath> is a tool to resolve a pathname.
+
+=head1 OPTIONS
+
+=over
+
+=item B<-?>, B<--help>
+
+Show the usage message and exit.
+
+=item B<--version>
+
+Show the version and exit.
+
+=back
+
+=head1 ENVIRONMENT
+
+=over
+
+=item B<DPKG_COLORS>
+
+Sets the color mode.
+The currently accepted values are: B<auto> (default), B<always> and B<never>.
+
+=back
+
+=head1 SEE ALSO
+
+L<realpath(1)>.

man/po/po4a.cfg
@@ -48,6 +48,7 @@
 [type:pod] dpkg-name.pod $lang:$lang/dpkg-name.pod
 [type:pod] dpkg-parsechangelog.pod $lang:$lang/dpkg-parsechangelog.pod
 [type:pod] dpkg-query.pod $lang:$lang/dpkg-query.pod
+[type:pod] dpkg-realpath.pod $lang:$lang/dpkg-realpath.pod
 [type:pod] dpkg-scanpackages.pod $lang:$lang/dpkg-scanpackages.pod
 [type:pod] dpkg-scansources.pod $lang:$lang/dpkg-scansources.pod
 [type:pod] dpkg-shlibdeps.pod $lang:$lang/dpkg-shlibdeps.pod

scripts/.gitignore
@@ -12,6 +12,7 @@ dpkg-maintscript-helper
 dpkg-mergechangelogs
 dpkg-name
 dpkg-parsechangelog
+dpkg-realpath
 dpkg-scanpackages
 dpkg-scansources
 dpkg-shlibdeps

scripts/Makefile.am
@@ -16,6 +16,7 @@ bin_SCRIPTS = \
 	dpkg-mergechangelogs \
 	dpkg-name \
 	dpkg-parsechangelog \
+	dpkg-realpath \
 	dpkg-scanpackages \
 	dpkg-scansources \
 	dpkg-shlibdeps \
@@ -36,6 +37,7 @@ EXTRA_DIST = \
 	dpkg-mergechangelogs.pl \
 	dpkg-name.pl \
 	dpkg-parsechangelog.pl \
+	dpkg-realpath.sh \
 	dpkg-scanpackages.pl \
 	dpkg-scansources.pl \
 	dpkg-shlibdeps.pl \

t/shellcheck.t
@@ -38,6 +38,7 @@ my @files = qw(
     debian/dpkg.cron.daily
     debian/dpkg.postrm
     scripts/dpkg-maintscript-helper.sh
+    scripts/dpkg-realpath.sh
 );
 my @shellcheck_opts = (
     '--exclude=SC1090', # Allow non-constant source.

commit d462d716908250024e9487ac69b0ed69880eec95

dpkg-realpath: Check pathname argument after options parsing
Fixes: commit 584d2a5575627f34dd796b550b09c590d34a79ae

scripts/dpkg-realpath.sh
@@ -117,8 +117,6 @@ canonicalize() {
 
 setup_colors
 
-[ $# -eq 1 ] || badusage "missing pathname"
-
 while [ $# -ne 0 ]; do
   case "$1" in
   --instdir|--root)
@@ -152,6 +150,8 @@ while [ $# -ne 0 ]; do
   esac
   shift
 done
+
+[ -n "$pathname" ] || badusage "missing pathname"
 
 canonicalize "$pathname" "${DPKG_ROOT:-}"
 