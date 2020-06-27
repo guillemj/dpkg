guillemj/dpkg

commit 934741712f54226c26d909f6210e17164cc8936c

dpkg: Add a new --robot option to be used with --version
This option will make --version print its output in a machine parseable
format.

src/main.c
@@ -58,11 +58,15 @@
 static void DPKG_ATTR_NORET
 printversion(const struct cmdinfo *ci, const char *value)
 {
-  printf(_("Debian '%s' package management program version %s.\n"),
-         DPKG, PACKAGE_RELEASE);
-  printf(_(
+  if (f_robot) {
+    printf("%s", PACKAGE_RELEASE);
+  } else {
+    printf(_("Debian '%s' package management program version %s.\n"),
+           DPKG, PACKAGE_RELEASE);
+    printf(_(
 "This is free software; see the GNU General Public License version 2 or\n"
 "later for copying conditions. There is NO warranty.\n"));
+  }
 
   m_output(stdout, _("<standard output>"));
 
@@ -163,6 +167,7 @@ usage(const struct cmdinfo *ci, const char *value)
 "  --no-force-...|--refuse-...\n"
 "                             Stop when problems encountered.\n"
 "  --abort-after <n>          Abort after encountering <n> errors.\n"
+"  --robot                    Use machine-readable output on some commands.\n"
 "\n"), ADMINDIR);
 
   printf(_(
@@ -189,6 +194,7 @@ static const char printforhelp[] = N_(
 "\n"
 "Options marked [*] produce a lot of output - pipe it through 'less' or 'more' !");
 
+int f_robot = 0;
 int f_pending=0, f_recursive=0, f_alsoselect=1, f_skipsame=0, f_noact=0;
 int f_autodeconf=0, f_nodebsig=0;
 int f_triggers = 0;
@@ -596,6 +602,7 @@ static const struct cmdinfo cmdinfos[]= {
   { "no-also-select",    'N', 0, &f_alsoselect, NULL,      NULL,    0 },
   { "skip-same-version", 'E', 0, &f_skipsame,   NULL,      NULL,    1 },
   { "auto-deconfigure",  'B', 0, &f_autodeconf, NULL,      NULL,    1 },
+  { "robot",             0,   0, &f_robot,      NULL,      NULL,    1 },
   { "root",              0,   1, NULL,          NULL,      set_root,      0 },
   { "abort-after",       0,   1, &errabort,     NULL,      set_integer,   0 },
   { "admindir",          0,   1, NULL,          &admindir, NULL,          0 },

man/dpkg.pod
@@ -512,6 +512,9 @@ Give help about debugging options.
 
 Display B<dpkg> version information.
 
+When used with B<--robot>, the output will be the program version number
+in a dotted numerical format, with no newline.
+
 =item B<dpkg-deb actions>
 
 See B<dpkg-deb>(1) for more information about the following actions,
@@ -999,6 +1002,15 @@ For conffile changes where I<decision> is either B<install> or
 B<keep>.
 
 =back
+
+=item B<--robot>
+
+Use a machine-readable output format. This provides an interface for programs
+that need to parse the output of some of the commands that do not otherwise
+emit a machine-readable output format. No localization will be used, and the
+output will be modified to make it easier to parse.
+
+The only currently supported command is B<--version>.
 
 =item B<--no-pager>
 

src/main.h
@@ -122,6 +122,7 @@ enum action {
 
 extern const char *const statusstrings[];
 
+extern int f_robot;
 extern int f_pending, f_recursive, f_alsoselect, f_skipsame, f_noact;
 extern int f_autodeconf, f_nodebsig;
 extern int f_triggers;

commit d06eaaa3b7a16a3636e278f8f467cb4e5dfab836

dpkg: Do not include the architecture with --robot --version
Fixes: commit 934741712f54226c26d909f6210e17164cc8936c

src/main.c
@@ -59,7 +59,7 @@ static void DPKG_ATTR_NORET
 printversion(const struct cmdinfo *ci, const char *value)
 {
   if (f_robot) {
-    printf("%s", PACKAGE_RELEASE);
+    printf("%s", PACKAGE_VERSION);
   } else {
     printf(_("Debian '%s' package management program version %s.\n"),
            DPKG, PACKAGE_RELEASE);