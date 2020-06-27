guillemj/dpkg

commit a74a91310260efe55cc986506fe208ae2776a45a

build: Require po4a >= 0.59
Supporting older versions resulted impractical, as we could not use
the refactored addenda specification anyway. Also 0.58 had broken
--srcdir support, and the new 0.59 has a nice addenda mode=eof which
will simplify the switch to POD. So let's just require the newer
version so that we can make use of the new features.

m4/dpkg-progs.m4
@@ -40,26 +40,29 @@ AC_DEFUN([DPKG_PROG_PO4A], [
   AC_REQUIRE([AM_NLS])
   AC_REQUIRE([DPKG_PROG_PERL])
   AC_ARG_VAR([PO4A], [po4a program])
+  m4_define([_PO4A_MIN_VERSION], [0.59])
+  AC_CACHE_CHECK([for po4a >= _PO4A_MIN_VERSION], [ac_cv_path_PO4A], [
+    AC_PATH_PROGS_FEATURE_CHECK([PO4A], [po4a], [
+      po4aversion=$(LC_ALL=C $ac_path_PO4A --version \
+                      | sed -ne 's/^po4a version \(.*\)\.$/\1/p')
+      AS_VERSION_COMPARE([$po4aversion], [_PO4A_MIN_VERSION],
+                         [po4acheck=no], [po4acheck=yes], [po4acheck=yes])
+      AS_IF([test "x$po4acheck" = "xyes"], [
+        ac_cv_path_PO4A=$ac_path_PO4A ac_path_PO4A_found=:
+      ])
+    ], [
+      AC_MSG_ERROR([cannot find po4a >= _PO4A_MIN_VERSION])
+    ])
+  ])
+  AC_SUBST([PO4A], [$ac_cv_path_PO4A])
+
   AC_CHECK_PROGS([PO4A], [po4a])
   AS_IF([test "$USE_NLS" = "yes" && test -n "$PO4A"], [
     USE_PO4A=yes
-    po4a_use_wrap=$($PERL -MLocale::Po4a::Po -e \
-                    'my $porefs = Locale::Po4a::Po->new()->{options}{porefs};
-                     print "yes" if $porefs =~ /,nowrap$/')
   ], [
     USE_PO4A=no
   ])
   AC_SUBST([USE_PO4A])
-
-  # Starting with po4a 0.58 the --porefs option does not accept the wrap
-  # modifiers, but earlier versions defaulted to nowrap, so we have to handle
-  # the backward incompatible change to support older and newer versions.
-  AS_IF([test "x$po4a_use_wrap" = "xyes"], [
-    PO4A_POREFS="file,wrap"
-  ], [
-    PO4A_POREFS="file"
-  ])
-  AC_SUBST([PO4A_POREFS])
 ])# DPKG_PROG_PO4A
 
 # DPKG_PROG_POD2MAN

README
@@ -56,7 +56,7 @@ the source should be roughly equivalent to the distributed tar source.
 
 To enable translated documentation this software will be needed:
 
-  po4a >= 0.43 (>= 0.58 for addenda inclusion)
+  po4a >= 0.59
 
 
 Building from tar source

debian/control
@@ -14,7 +14,7 @@ Build-Depends:
 # Needed for ITS support.
  gettext (>= 0.19.7),
 # Needed for --porefs defaults.
- po4a (>= 0.58),
+ po4a (>= 0.59),
  zlib1g-dev,
  libbz2-dev,
  liblzma-dev,

man/Makefile.am
@@ -147,9 +147,8 @@ PO4A_V = $(PO4A_V_@AM_V@)
 PO4A_V_ = $(PO4A_V_@AM_DEFAULT_V@)
 PO4A_V_0 = @echo "  PO4A   $@";
 
-PO4A_POREFS = @PO4A_POREFS@
 PO4A_OPTS = --previous --srcdir $(srcdir) --destdir $(builddir) --no-backups \
-            --porefs $(PO4A_POREFS) --msgmerge-opt=--add-location=file \
+            --porefs file --msgmerge-opt=--add-location=file \
             --package-name dpkg-man --package-version $(PACKAGE_VERSION) \
             --copyright-holder "$(PACKAGE_COPYRIGHT_HOLDER)" \
             --msgid-bugs-address $(PACKAGE_BUGREPORT)

commit edc10b4b9e2632340ad131ad48bef31c2254a5b1

build: Do not fail if po4a is not found, and search for it just once
A missing po4a should not be fatal, as it was before. And we should not
override the initial feature check with the standard check, which was
left over by mistake when the new check was introduced.

Fixes: commit a74a91310260efe55cc986506fe208ae2776a45a

m4/dpkg-progs.m4
@@ -51,13 +51,12 @@ AC_DEFUN([DPKG_PROG_PO4A], [
         ac_cv_path_PO4A=$ac_path_PO4A ac_path_PO4A_found=:
       ])
     ], [
-      AC_MSG_ERROR([cannot find po4a >= _PO4A_MIN_VERSION])
+      ac_cv_path_PO4A=no
     ])
   ])
   AC_SUBST([PO4A], [$ac_cv_path_PO4A])
 
-  AC_CHECK_PROGS([PO4A], [po4a])
-  AS_IF([test "$USE_NLS" = "yes" && test -n "$PO4A"], [
+  AS_IF([test "$USE_NLS" = "yes" && test "$ac_cv_path_PO4A" != "no"], [
     USE_PO4A=yes
   ], [
     USE_PO4A=no