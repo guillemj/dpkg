guillemj/dpkg

commit 7b2d550c64191597827350fa447bf9caf15b749a

Dpkg::OpenPGP: Use a temporary directory for the GnuPG homedir
In import_key() when calling gpg we need to specify a temporary homedir
to make sure we do not write to the user home directory, which might be
read-only or non-existent.

Reported-by: Mattia Rizzolo <mattia@debian.org>

scripts/Dpkg/OpenPGP.pm
@@ -20,6 +20,7 @@ use warnings;
 
 use POSIX qw(:sys_wait_h);
 use Exporter qw(import);
+use File::Temp;
 use File::Copy;
 
 use Dpkg::Gettext;
@@ -98,6 +99,10 @@ sub import_key {
                 $asc);
         return;
     }
+
+    my $gpghome = File::Temp->newdir("dpkg-import-key.XXXXXXXX", TMPDIR => 1);
+
+    push @exec, '--homedir', $gpghome;
     push @exec, '--no-options', '--no-default-keyring', '-q', '--import';
     push @exec, '--keyring', $opts{keyring};
     push @exec, $asc;

commit 0b9db34e3efabbbbbd11f253ac84d66d84f437d8

Dpkg::OpenPGP: Do not use interpolated string for a literal
Fixes: commit 7b2d550c64191597827350fa447bf9caf15b749a

scripts/Dpkg/OpenPGP.pm
@@ -100,7 +100,7 @@ sub import_key {
         return;
     }
 
-    my $gpghome = File::Temp->newdir("dpkg-import-key.XXXXXXXX", TMPDIR => 1);
+    my $gpghome = File::Temp->newdir('dpkg-import-key.XXXXXXXX', TMPDIR => 1);
 
     push @exec, '--homedir', $gpghome;
     push @exec, '--no-options', '--no-default-keyring', '-q', '--import';