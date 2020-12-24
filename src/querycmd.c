guillemj/dpkg

commit 33899e9378790f0bda984feb09860899eab96331

libdpkg, dpkg-query: Optimize db-fsys:Files loading
We load either the entire db-fsys for all packages, possibly optimized
per platform (such as by using fiemap), or the specific ones for the
requested packages.

This also fixes a problematic cast removing the constness of a variable.

src/querycmd.c
@@ -541,6 +541,12 @@ list_files(const char *const *argv)
 }
 
 static void
+pkg_array_load_db_fsys(struct pkg_array *array, struct pkginfo *pkg, void *pkg_data)
+{
+  ensure_packagefiles_available(pkg);
+}
+
+static void
 pkg_array_show_item(struct pkg_array *array, struct pkginfo *pkg, void *pkg_data)
 {
   struct pkg_format_node *fmt = pkg_data;
@@ -555,6 +561,7 @@ showpackages(const char *const *argv)
   struct pkg_array array;
   struct pkginfo *pkg;
   struct pkg_format_node *fmt;
+  bool fmt_needs_db_fsys;
   int i;
   int rc = 0;
 
@@ -566,6 +573,8 @@ showpackages(const char *const *argv)
     return rc;
   }
 
+  fmt_needs_db_fsys = pkg_format_needs_db_fsys(fmt);
+
   if (!opt_loadavail)
     modstatdb_open(msdbrw_readonly);
   else
@@ -575,6 +584,8 @@ showpackages(const char *const *argv)
   pkg_array_sort(&array, pkg_sorter_by_nonambig_name_arch);
 
   if (!*argv) {
+    if (fmt_needs_db_fsys)
+      ensure_allinstfiles_available();
     for (i = 0; i < array.n_pkgs; i++) {
       pkg = array.pkgs[i];
       if (pkg->status == PKG_STAT_NOTINSTALLED)
@@ -582,6 +593,8 @@ showpackages(const char *const *argv)
       pkg_format_show(fmt, pkg, &pkg->installed);
     }
   } else {
+    if (fmt_needs_db_fsys)
+      pkg_array_foreach(&array, pkg_array_load_db_fsys, NULL);
     rc = pkg_array_match_patterns(&array, pkg_array_show_item, fmt, argv);
   }
 

lib/dpkg/pkg-format.c
@@ -302,21 +302,16 @@ virt_fsys_last_modified(struct varbuf *vb,
 	varbuf_printf(vb, "%ld", st.st_mtime);
 }
 
+/*
+ * This function requires the caller to have loaded the package fsys metadata,
+ * otherwise it will do nothing.
+ */
 static void
 virt_fsys_files(struct varbuf *vb,
                 const struct pkginfo *pkg, const struct pkgbin *pkgbin,
                 enum fwriteflags flags, const struct fieldinfo *fip)
 {
 	struct fsys_namenode_list *node;
-
-	/* XXX: This cast is so wrong on so many levels, but the alternatives
-	 * are apparently worse. We might need to end up removing the const
-	 * from the arguments.
-	 *
-	 * Ideally loading the entire fsys db would be cheaper, and stored
-	 * in a single file, so we could do it unconditionally, before any
-	 * formatting. */
-	ensure_packagefiles_available((struct pkginfo *)pkg);
 
 	if (!pkg->files_list_valid)
 		return;
@@ -381,6 +376,21 @@ static const struct fieldinfo virtinfos[] = {
 	{ FIELD("source:Upstream-Version"), NULL, virt_source_upstream_version },
 	{ NULL },
 };
+
+bool
+pkg_format_needs_db_fsys(const struct pkg_format_node *head)
+{
+	const struct pkg_format_node *node;
+
+	for (node = head; node; node = node->next) {
+		if (node->type != PKG_FORMAT_FIELD)
+			continue;
+		if (strcmp(node->data, "db-fsys:Files") == 0)
+			return true;
+	}
+
+	return false;
+}
 
 static void
 pkg_format_item(struct varbuf *vb,

lib/dpkg/pkg-format.h
@@ -35,6 +35,9 @@ DPKG_BEGIN_DECLS
 
 struct pkg_format_node;
 
+bool
+pkg_format_needs_db_fsys(const struct pkg_format_node *head);
+
 struct pkg_format_node *pkg_format_parse(const char *fmt,
                                          struct dpkg_error *err);
 void pkg_format_free(struct pkg_format_node *head);

commit 6f3840adb488cb31a537af608eaa8a6e00dc5813

dpkg-query: Do not print database loading progress
The output for these commands is part of their interface, and we are not
supposed to print any progress output, or that breaks the expected
result.

Fixes: commit 33899e9378790f0bda984feb09860899eab96331
Closes: #977240

src/querycmd.c
@@ -585,7 +585,7 @@ showpackages(const char *const *argv)
 
   if (!*argv) {
     if (fmt_needs_db_fsys)
-      ensure_allinstfiles_available();
+      ensure_allinstfiles_available_quiet();
     for (i = 0; i < array.n_pkgs; i++) {
       pkg = array.pkgs[i];
       if (pkg->status == PKG_STAT_NOTINSTALLED)