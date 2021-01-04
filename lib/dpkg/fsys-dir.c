guillemj/dpkg

commit f13aebc8a05da51a54da55d55b023ca8199a6471

libdpkg: Fix single-instance memory leak with fsys dir
This variable is usually set up once at the beginning of the execution
and then never changed again.

Warned-by: gcc ASAN

lib/dpkg/fsys-dir.c
@@ -29,6 +29,7 @@
 #include <dpkg/fsys.h>
 
 static const char *fsys_dir = "";
+static char *fsys_dir_alloc;
 
 /**
  * Set current on-disk filesystem root directory.
@@ -45,22 +46,23 @@ static const char *fsys_dir = "";
 const char *
 dpkg_fsys_set_dir(const char *dir)
 {
-	char *new_dir;
-
 	if (dir == NULL) {
 		const char *env;
 
 		env = getenv("DPKG_ROOT");
 		if (env)
 			dir = env;
-		else
-			dir = "";
 	}
 
-	new_dir = m_strdup(dir);
-	path_trim_slash_slashdot(new_dir);
+	free(fsys_dir_alloc);
 
-	fsys_dir = new_dir;
+	if (dir == NULL) {
+		fsys_dir = "";
+		fsys_dir_alloc = NULL;
+	} else {
+		fsys_dir = fsys_dir_alloc = m_strdup(dir);
+		path_trim_slash_slashdot(fsys_dir_alloc);
+	}
 
 	return fsys_dir;
 }

commit 4dae5986f96d5b1d52947e9e35a92d2e713ef0e1

libdpkg: Fix passing the allocated instdir back into fsys_set_dir()
The code was free()ing the previously allocated string too early, then
trying to strdup() it. Instead use an intermediate to free() the old
pointer before overwriting it with the new one.

Fixes: commit f13aebc8a05da51a54da55d55b023ca8199a6471
Changelog: silent

lib/dpkg/fsys-dir.c
@@ -46,6 +46,8 @@ static char *fsys_dir_alloc;
 const char *
 dpkg_fsys_set_dir(const char *dir)
 {
+	char *fsys_dir_new;
+
 	if (dir == NULL) {
 		const char *env;
 
@@ -54,15 +56,16 @@ dpkg_fsys_set_dir(const char *dir)
 			dir = env;
 	}
 
-	free(fsys_dir_alloc);
-
 	if (dir == NULL) {
 		fsys_dir = "";
-		fsys_dir_alloc = NULL;
+		fsys_dir_new = NULL;
 	} else {
-		fsys_dir = fsys_dir_alloc = m_strdup(dir);
-		path_trim_slash_slashdot(fsys_dir_alloc);
+		fsys_dir = fsys_dir_new = m_strdup(dir);
+		path_trim_slash_slashdot(fsys_dir_new);
 	}
+
+	free(fsys_dir_alloc);
+	fsys_dir_alloc = fsys_dir_new;
 
 	return fsys_dir;
 }

lib/dpkg/t/t-fsys-dir.c
@@ -29,11 +29,17 @@
 static void
 test_fsys_dir(void)
 {
+	const char *newdir;
 	char *dir;
 
 	test_str(dpkg_fsys_get_dir(), ==, "");
 
-	dpkg_fsys_set_dir("/testdir");
+	newdir = dpkg_fsys_set_dir("/testdir");
+	test_str(newdir, ==, "/testdir");
+	test_str(dpkg_fsys_get_dir(), ==, "/testdir");
+
+	newdir = dpkg_fsys_set_dir(newdir);
+	test_str(newdir, ==, "/testdir");
 	test_str(dpkg_fsys_get_dir(), ==, "/testdir");
 
 	dir = dpkg_fsys_get_path("testfile");
@@ -59,7 +65,7 @@ test_fsys_dir(void)
 
 TEST_ENTRY(test)
 {
-	test_plan(7);
+	test_plan(10);
 
 	test_fsys_dir();
 }