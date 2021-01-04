guillemj/dpkg

commit 5f454c08c5e3190dce3256019b4adef93dc62f99

dpkg-realpath: Do not expect pathname to be prefixed with root directory
This made its usage cumbersome as then the root directory needs to be
specified twice. It also diverges from the other commands as they do
not expect the root directory to be prefixed, which would make this a
rather unexpected interface.

scripts/dpkg-maintscript-helper.sh
@@ -410,7 +410,7 @@ dir_to_symlink() {
             -f "$DPKG_ROOT$PATHNAME/.dpkg-staging-dir" \) -o \
          \( -h "$DPKG_ROOT$PATHNAME" -a \
             \( "$(readlink "$DPKG_ROOT$PATHNAME")" = "$SYMLINK_TARGET" -o \
-               "$(dpkg-realpath "$DPKG_ROOT$PATHNAME")" = "$SYMLINK_TARGET" \) \) ] &&
+               "$(dpkg-realpath "$PATHNAME")" = "$SYMLINK_TARGET" \) \) ] &&
        dpkg --compare-versions -- "$2" le-nl "$LASTVERSION"; then
       abort_dir_to_symlink "$PATHNAME"
     fi

scripts/dpkg-realpath.sh
@@ -55,11 +55,6 @@ canonicalize() {
   local result="$root"
   local dst
 
-  if [ "${src#"$root"}" = "$src" ]; then
-    error "link not within root"
-  fi
-  # Remove prefixed root dir.
-  src=${src#"$root"}
   # Remove prefixed slashes.
   while [ "$src" != "${src#/}" ]; do
      src=${src#/}
@@ -155,6 +150,9 @@ while [ $# -ne 0 ]; do
 done
 
 [ -n "$pathname" ] || badusage "missing pathname"
+if [ "${pathname#"$DPKG_ROOT"}" != "$pathname" ]; then
+  error "link includes root prefix"
+fi
 
 canonicalize "$pathname"
 

commit 96b3a902eaeda2869d8346e6c4b32bc1af065f4e

dpkg-m-h: Do not pass DPKG_ROOT prefixed pathmames to dpkg-realpath
This case was missed when changing the semantics for dpkg-realpath.

Fixes: commit 5f454c08c5e3190dce3256019b4adef93dc62f99

scripts/dpkg-maintscript-helper.sh
@@ -559,7 +559,7 @@ symlink_match()
   local SYMLINK_TARGET="$2"
 
   [ "$(readlink "$DPKG_ROOT$SYMLINK")" = "$SYMLINK_TARGET" ] || \
-  [ "$(dpkg-realpath "$DPKG_ROOT$SYMLINK")" = "$SYMLINK_TARGET" ]
+  [ "$(dpkg-realpath "$SYMLINK")" = "$SYMLINK_TARGET" ]
 }
 
 usage() {