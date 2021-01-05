guillemj/dpkg

commit b7556b76e81c4b02d14dd030cf128503f237f922

libdpkg: Make sure we do not walk on rootless trees
This should not happen with the current code, but we can be defensive
about it anyway.

Warned-by: coverity

lib/dpkg/treewalk.c
@@ -437,6 +437,10 @@ treewalk_next(struct treeroot *tree)
 {
 	struct treenode *node;
 
+	/* Handle rootless trees, such as uninitialized or fully traversed. */
+	if (tree->rootnode == NULL)
+		return NULL;
+
 	node = tree->curnode;
 
 	/* Handle end of tree. */

commit 2be0e88b61c5a6d2d15860eb68100248c105eab3

libdpkg: Reset the rootnode to NULL after free()ing it
We are testing whether rootnode is NULL, in part to placate code
analyzers not understanding the structure of the code, and to make
it more robust, so after free()ing it we should reset it to NULL.

Fixes: commit b7556b76e81c4b02d14dd030cf128503f237f922
Warned-by: coverity
Changelog: silent

lib/dpkg/treewalk.c
@@ -495,6 +495,7 @@ treewalk_next(struct treeroot *tree)
 
 		if (tree->curdir == NULL) {
 			treenode_free_node(tree->rootnode);
+			tree->rootnode = NULL;
 			break;
 		}
 	}