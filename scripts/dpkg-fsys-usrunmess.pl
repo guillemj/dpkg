guillemj/dpkg

commit cf55e9f07217749dee34125be2bb0407fde75374

dpkg-fsys-usrunmess: Do not fail when removing lingering directories
These directories might contain untracked files, if they are not empty,
failing the whole script will be worse. Keep track of them, and print a
summary at the end of the run.

Stable-Candidate: 1.20.x

scripts/dpkg-fsys-usrunmess.pl
@@ -341,17 +341,21 @@ foreach my $dir (keys %deferred_dirnames) {
     $batch_size = 0;
 }
 
+my @dirs_linger;
+
 if (not $opt_noact) {
     foreach my $dirname (reverse sort keys %deferred_dirnames) {
-        rmdir $dirname
-            or sysfatal("cannot remove shadow directory $dirname");
+        next if rmdir $dirname;
+        warning("cannot remove shadow directory $dirname: $!");
+
+        push @dirs_linger, $dirname;
     }
 }
 
 if (not $opt_noact) {
     debug("cleaning up shadow root dir = $sroot");
     rmdir $sroot
-        or sysfatal("cannot remove shadow directory $sroot");
+        or warning("cannot remove shadow directory $sroot: $!");
 }
 
 #
@@ -402,6 +406,13 @@ if (not $opt_noact) {
 }
 
 print "\n";
+
+if (@dirs_linger) {
+    warning("lingering directories that could not be removed:");
+    foreach my $dir (@dirs_linger) {
+        warning("  $dir");
+    }
+}
 
 print "Done, hierarchy unmessed, congrats!\n";
 print "Rebooting now is very strongly advised.\n";

man/dpkg-fsys-usrunmess.pod
@@ -110,6 +110,10 @@ B<Note>: When running the program from some shells such as L<bash(1)> or
 L<zsh(1)>, after executing it, you might need to request the shell to
 forget all remembered executable locations with for example C<hash -r>.
 
+B<Note>: Some directories might linger after the migration in case they
+contain untracked files. A list is printed once the script has finished
+for further investigation.
+
 B<Warning>: Note that this operation has the potential to render the system
 unusable or broken in case of a sudden crash or reboot, unexpected state of
 the system, or possible bugs in the script. Be prepared with recovery media

commit 2165119fe4d81207e49a3296d14e1a2cb64b93f1

dpkg-fsys-usrunmess: Do not use interpolated strings for literals
Fixes: commit cf55e9f07217749dee34125be2bb0407fde75374
Fixes: commit a335ee075be83eb7f69e85644b9ece40bc29a6a5

scripts/dpkg-fsys-usrunmess.pl
@@ -441,15 +441,15 @@ if (not $opt_noact) {
 
     # Restore the selections we saved initially.
     open my $altfh, '|-', qw(update-alternatives --set-selections)
-        or fatal("cannot restore alternatives state");
+        or fatal('cannot restore alternatives state');
     print { $altfh } $_ foreach @selections;
-    close $altfh or fatal("cannot restore alternatives state");
+    close $altfh or fatal('cannot restore alternatives state');
 }
 
 print "\n";
 
 if (@dirs_linger) {
-    warning("lingering directories that could not be removed:");
+    warning('lingering directories that could not be removed:');
     foreach my $dir (@dirs_linger) {
         warning("  $dir");
     }