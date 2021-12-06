guillemj/dpkg

commit 5fbe26662ee96b1005fde6ee1f32e694594ec1a9

dpkg-buildpackage: When printing build type match the extension exactly
We need to anchor the regex at the end of the string, otherwise we get
partial matches due to substrings.

Closes: #989824

scripts/dpkg-buildpackage.pl
@@ -880,7 +880,7 @@ sub signfile {
 sub fileomitted {
     my ($files, $regex) = @_;
 
-    return $files !~ /$regex/
+    return $files !~ /$regex$/
 }
 
 sub describe_build {

commit 5c9bfddc1794690dff7f8c8c6086a017da0da599

dpkg-buildpackage: Fix build description due to improper multiline match
The string passed is a multiline, so we need to use take that into
account when matching regexes anchored at the end of line.

Warned-by: devscripts test-suite
Fixes: commit 5fbe26662ee96b1005fde6ee1f32e694594ec1a9

scripts/dpkg-buildpackage.pl
@@ -909,7 +909,7 @@ sub signfile {
 sub fileomitted {
     my ($files, $regex) = @_;
 
-    return $files !~ /$regex$/
+    return $files !~ m/$regex$/m
 }
 
 sub describe_build {