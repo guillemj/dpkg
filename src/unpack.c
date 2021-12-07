guillemj/dpkg

commit 20e181fc2030385488e3db2d4cc3ce4114f0a498

dpkg: Fix conffile removal-on-upgrade handling
We need to check for file conflicts during conffile removal, otherwise
we might end up removing files from any other packages that has started
owning them since we targeted the pathname for removal.

We remove the conffiles during upgrade, as configure is a local
action that does not load all file lists files, so we cannot check for
file conflicts.

Closes: #995387
Stable-Candidate: 1.20.x

src/unpack.c
@@ -593,13 +593,109 @@ pkg_infodb_update(struct pkginfo *pkg, char *cidir, char *cidirrest)
 }
 
 static void
+pkg_remove_conffile_on_upgrade(struct pkginfo *pkg, struct fsys_namenode *namenode)
+{
+  struct pkginfo *otherpkg;
+  struct fsys_namenode *usenode;
+  struct fsys_node_pkgs_iter *iter;
+  struct varbuf cdr = VARBUF_INIT;
+  struct varbuf cdrext = VARBUF_INIT;
+  struct varbuf_state cdrext_state;
+  char currenthash[MD5HASHLEN + 1];
+  int rc;
+
+  usenode = namenodetouse(namenode, pkg, &pkg->installed);
+
+  rc = conffderef(pkg, &cdr, usenode->name);
+  if (rc == -1) {
+    debug(dbg_conffdetail, "%s: '%s' conffile dereference error: %s", __func__,
+          namenode->name, strerror(errno));
+    namenode->oldhash = EMPTYHASHFLAG;
+    return;
+  }
+
+  varbuf_reset(&cdrext);
+  varbuf_add_str(&cdrext, cdr.buf);
+  varbuf_end_str(&cdrext);
+
+  varbuf_snapshot(&cdrext, &cdrext_state);
+
+  iter = fsys_node_pkgs_iter_new(namenode);
+  while ((otherpkg = fsys_node_pkgs_iter_next(iter))) {
+    debug(dbg_conffdetail, "%s: namenode '%s' owned by other %s?",
+          __func__, namenode->name, pkg_name(otherpkg, pnaw_always));
+
+    if (otherpkg == pkg)
+      continue;
+
+    debug(dbg_conff, "%s: namenode '%s' owned by other %s, remove-on-upgrade ignored",
+         __func__, namenode->name, pkg_name(otherpkg, pnaw_always));
+    return;
+  }
+  fsys_node_pkgs_iter_free(iter);
+
+  /* Remove DPKGDISTEXT variant if still present. */
+  varbuf_rollback(&cdrext, &cdrext_state);
+  varbuf_add_str(&cdrext, DPKGDISTEXT);
+  varbuf_end_str(&cdrext);
+
+  if (unlink(cdrext.buf) < 0 && errno != ENOENT)
+    warning(_("%s: failed to remove '%.250s': %s"),
+            pkg_name(pkg, pnaw_nonambig), cdrext.buf,
+            strerror(errno));
+
+  md5hash(pkg, currenthash, cdr.buf);
+
+  /* Has it been already removed (e.g. by local admin)? */
+  if (strcmp(currenthash, NONEXISTENTFLAG) == 0)
+    return;
+
+  /* For unmodified conffiles, we just remove them. */
+  if (strcmp(currenthash, namenode->oldhash) == 0) {
+    printf(_("Removing obsolete conffile %s ...\n"), cdr.buf);
+    if (unlink(cdr.buf) < 0 && errno != ENOENT)
+      warning(_("%s: failed to remove '%.250s': %s"),
+              pkg_name(pkg, pnaw_nonambig), cdr.buf, strerror(errno));
+    return;
+  }
+
+  /* Otherwise, preserve the modified conffile. */
+  varbuf_rollback(&cdrext, &cdrext_state);
+  varbuf_add_str(&cdrext, DPKGOLDEXT);
+  varbuf_end_str(&cdrext);
+
+  printf(_("Obsolete conffile '%s' has been modified by you.\n"), cdr.buf);
+  printf(_("Saving as %s ...\n"), cdrext.buf);
+  if (rename(cdr.buf, cdrext.buf) < 0)
+    warning(_("%s: cannot rename obsolete conffile '%s' to '%s': %s"),
+            pkg_name(pkg, pnaw_nonambig),
+            cdr.buf, cdrext.buf, strerror(errno));
+}
+
+static void
 pkg_remove_old_files(struct pkginfo *pkg,
                      struct fsys_namenode_queue *newfiles_queue,
                      struct fsys_namenode_queue *newconffiles)
 {
   struct fsys_hash_rev_iter rev_iter;
+  struct fsys_namenode_list *cfile;
   struct fsys_namenode *namenode;
   struct stat stab, oldfs;
+
+  /* Before removing any old files, we try to remove obsolete conffiles that
+   * have been requested to be removed during upgrade. These conffiles are
+   * not tracked as part of the package file lists, so removing them here
+   * means we will get their parent directories removed if not present in the
+   * new package without needing to do anything special ourselves. */
+  for (cfile = newconffiles->head; cfile; cfile = cfile->next) {
+    debug(dbg_conffdetail, "%s: removing conffile '%s' for %s?", __func__,
+          cfile->namenode->name, pkg_name(pkg, pnaw_always));
+
+    if (!(cfile->namenode->flags & FNNF_RM_CONFF_ON_UPGRADE))
+      continue;
+
+    pkg_remove_conffile_on_upgrade(pkg, cfile->namenode);
+  }
 
   fsys_hash_rev_iter_init(&rev_iter, pkg->files);
 
@@ -646,7 +742,6 @@ pkg_remove_old_files(struct pkginfo *pkg,
       }
     } else {
       struct fsys_namenode_list *sameas = NULL;
-      struct fsys_namenode_list *cfile;
       static struct file_ondisk_id empty_ondisk_id;
       struct varbuf cfilename = VARBUF_INIT;
 

src/configure.c
@@ -401,42 +401,6 @@ deferred_configure_conffile(struct pkginfo *pkg, struct conffile *conff)
 	cdr2rest = cdr2.buf + strlen(cdr.buf);
 	/* From now on we can just strcpy(cdr2rest, extension); */
 
-	if (conff->remove_on_upgrade) {
-		/* Remove DPKGDISTEXT variant if still present. */
-		strcpy(cdr2rest, DPKGDISTEXT);
-		if (unlink(cdr2.buf) < 0 && errno != ENOENT)
-			warning(_("%s: failed to remove '%.250s': %s"),
-			        pkg_name(pkg, pnaw_nonambig), cdr2.buf,
-			        strerror(errno));
-
-		/* Has it been already removed (e.g. by local admin)? */
-		if (strcmp(currenthash, NONEXISTENTFLAG) == 0)
-			return;
-
-		/* For unmodified conffiles, we just remove them. */
-		if (strcmp(currenthash, conff->hash) == 0) {
-			printf(_("Removing obsolete conffile %s ...\n"),
-			       cdr.buf);
-			if (unlink(cdr.buf) < 0 && errno != ENOENT)
-				warning(_("%s: failed to remove '%.250s': %s"),
-				        pkg_name(pkg, pnaw_nonambig), cdr.buf,
-				        strerror(errno));
-			return;
-		}
-
-		/* Otherwise, preserve the modified conffile. */
-		strcpy(cdr2rest, DPKGOLDEXT);
-		printf(_("Obsolete conffile '%s' has been modified by you.\n"),
-		       cdr.buf);
-		printf(_("Saving as %s ...\n"), cdr2.buf);
-		if (rename(cdr.buf, cdr2.buf) < 0)
-			warning(_("%s: cannot rename obsolete conffile '%s' "
-			          "to '%s': %s"),
-			        pkg_name(pkg, pnaw_nonambig),
-			        cdr.buf, cdr2.buf, strerror(errno));
-		return;
-	}
-
 	strcpy(cdr2rest, DPKGNEWEXT);
 	/* If the .dpkg-new file is no longer there, ignore this one. */
 	if (lstat(cdr2.buf, &stab)) {
@@ -696,7 +660,7 @@ deferred_configure(struct pkginfo *pkg)
 		 * ‘*.dpkg-new’ no longer exists we assume that we've
 		 * already processed this one. */
 		for (conff = pkg->installed.conffiles; conff; conff = conff->next) {
-			if (conff->obsolete)
+			if (conff->obsolete || conff->remove_on_upgrade)
 				continue;
 			deferred_configure_conffile(pkg, conff);
 		}

tests/t-conffile-declarative-removal/.gitignore
@@ -1,0 +1,1 @@
+pkg-conffile-owns-symlink/test-dir/test-conffile-to-be-removed

tests/t-conffile-declarative-removal/Makefile
@@ -1,6 +1,15 @@
-TESTS_DEB := pkg-conffile-v1 pkg-conffile-v2
+TESTS_DEB := pkg-conffile-v1 pkg-conffile-v2 \
+             pkg-conffile-owns-filename pkg-conffile-owns-symlink
 
 include ../Test.mk
+
+build-hook:
+	ln -sf test-conffile-owned \
+	       pkg-conffile-owns-symlink/test-dir/test-conffile-to-be-removed
+
+clean-hook:
+	$(RM) pkg-conffile-owns-symlink/test-dir/test-conffile-to-be-removed
+	$(RM) pkg-conffile-removal-present.deb
 
 test-case:
 	# Conffile marked for removal but present, cannot be installed.
@@ -9,31 +18,36 @@ test-case:
 	$(call pkg_is_not_installed,pkg-conffile-removal-present)
 	$(DPKG_PURGE) pkg-conffile-removal-present
 
-	# Unmodified case.
+	# Unmodified case, one invocation.
 	$(DPKG_INSTALL) pkg-conffile-v1.deb
 	test -f "$(DPKG_INSTDIR)/test-dir/test-conffile-to-be-removed"
+	test ! -f "$(DPKG_INSTDIR)/test-dir/test-conffile-to-be-removed.dpkg-old"
 	$(DPKG_INSTALL) pkg-conffile-v2.deb
 	test ! -f "$(DPKG_INSTDIR)/test-dir/test-conffile-to-be-removed"
+	test ! -f "$(DPKG_INSTDIR)/test-dir/test-conffile-to-be-removed.dpkg-old"
 
 	# Reset.
 	$(DPKG_PURGE) pkg-conffile
 	test ! -f "$(DPKG_INSTDIR)/test-dir/test-conffile-to-be-removed"
+	test ! -f "$(DPKG_INSTDIR)/test-dir/test-conffile-to-be-removed.dpkg-old"
 
 	# Unmodified case; but force it to happen via two invocations.
 	$(DPKG_INSTALL) pkg-conffile-v1.deb
 	test -f "$(DPKG_INSTDIR)/test-dir/test-conffile-to-be-removed"
-	$(DPKG_UNPACK) pkg-conffile-v2.deb
-	# The old file is still present when unpacked.
-	test -f "$(DPKG_INSTDIR)/test-dir/test-conffile-to-be-removed"
 	test ! -f "$(DPKG_INSTDIR)/test-dir/test-conffile-to-be-removed.dpkg-old"
+	$(DPKG_UNPACK) pkg-conffile-v2.deb
+	# The old conffile is removed during unpack.
+	test ! -f "$(DPKG_INSTDIR)/test-dir/test-conffile-to-be-removed"
+	test ! -f "$(DPKG_INSTDIR)/test-dir/test-conffile-to-be-removed.dpkg-old"
+	# After configure, the file is still removed.
 	$(DPKG_CONFIGURE) pkg-conffile
-	# After configure, the file is removed.
 	test ! -f "$(DPKG_INSTDIR)/test-dir/test-conffile-to-be-removed"
 	test ! -f "$(DPKG_INSTDIR)/test-dir/test-conffile-to-be-removed.dpkg-old"
 
 	# Reset.
 	$(DPKG_PURGE) pkg-conffile
 	test ! -f "$(DPKG_INSTDIR)/test-dir/test-conffile-to-be-removed"
+	test ! -f "$(DPKG_INSTDIR)/test-dir/test-conffile-to-be-removed.dpkg-old"
 
 	# Modified; should store .dpkg-old.
 	$(DPKG_INSTALL) pkg-conffile-v1.deb
@@ -43,7 +57,45 @@ test-case:
 	test ! -f "$(DPKG_INSTDIR)/test-dir/test-conffile-to-be-removed"
 	test -f "$(DPKG_INSTDIR)/test-dir/test-conffile-to-be-removed.dpkg-old"
 
+	# Reset.
+	$(DPKG_PURGE) pkg-conffile
+	test ! -f "$(DPKG_INSTDIR)/test-dir/test-conffile-to-be-removed"
+	$(RM) "$(DPKG_INSTDIR)/test-dir/test-conffile-to-be-removed.dpkg-old"
+
+	# Queue a remove on upgrade, and move ownership.
+	$(DPKG_INSTALL) pkg-conffile-v1.deb
+	test -f "$(DPKG_INSTDIR)/test-dir/test-conffile-to-be-removed"
+	$(DPKG_INSTALL) pkg-conffile-v2.deb
+	test ! -f "$(DPKG_INSTDIR)/test-dir/test-conffile-to-be-removed"
+	$(DPKG_INSTALL) pkg-conffile-owns-filename.deb
+	test -f "$(DPKG_INSTDIR)/test-dir/test-conffile-to-be-removed"
+	# Upgrade the package again, should not remove unowned conffile.
+	$(DPKG_INSTALL) pkg-conffile-v2.deb
+	test -f "$(DPKG_INSTDIR)/test-dir/test-conffile-to-be-removed"
+	test ! -f "$(DPKG_INSTDIR)/test-dir/test-conffile-to-be-removed.dpkg-old"
+
+	# Reset.
+	$(DPKG_PURGE) pkg-conffile
+	$(DPKG_PURGE) pkg-conffile-owns
+	test ! -f "$(DPKG_INSTDIR)/test-dir/test-conffile-to-be-removed"
+	test ! -f "$(DPKG_INSTDIR)/test-dir/test-conffile-to-be-removed.dpkg-old"
+
+	# Queue a remove on upgrade, and move ownership via symlink.
+	$(DPKG_INSTALL) pkg-conffile-v1.deb
+	test -f "$(DPKG_INSTDIR)/test-dir/test-conffile-to-be-removed"
+	$(DPKG_INSTALL) pkg-conffile-v2.deb
+	test ! -f "$(DPKG_INSTDIR)/test-dir/test-conffile-to-be-removed"
+	test ! -f "$(DPKG_INSTDIR)/test-dir/test-conffile-to-be-removed.dpkg-old"
+	$(DPKG_INSTALL) pkg-conffile-owns-symlink.deb
+	test -f "$(DPKG_INSTDIR)/test-dir/test-conffile-owned"
+	test -L "$(DPKG_INSTDIR)/test-dir/test-conffile-to-be-removed"
+	# Upgrade the package again, should not remove unowned conffile.
+	$(DPKG_INSTALL) pkg-conffile-v2.deb
+	test -f "$(DPKG_INSTDIR)/test-dir/test-conffile-owned"
+	test -L "$(DPKG_INSTDIR)/test-dir/test-conffile-to-be-removed"
+
 test-clean:
 	$(DPKG_PURGE) pkg-conffile-removal-present
+	$(DPKG_PURGE) pkg-conffile-owns
 	$(DPKG_PURGE) pkg-conffile
 	$(BEROOT) $(RM) -rf "$(DPKG_INSTDIR)/test-dir"

tests/t-conffile-declarative-removal/pkg-conffile-owns-filename/DEBIAN/conffiles
@@ -1,0 +1,1 @@
+/test-dir/test-conffile-to-be-removed

tests/t-conffile-declarative-removal/pkg-conffile-owns-filename/DEBIAN/control
@@ -1,0 +1,7 @@
+Package: pkg-conffile-owns
+Version: 1
+Section: test
+Priority: extra
+Maintainer: Dpkg Developers <debian-dpkg@lists.debian.org>
+Architecture: all
+Description: test package - owns conffile to be removed

tests/t-conffile-declarative-removal/pkg-conffile-owns-symlink/DEBIAN/conffiles
@@ -1,0 +1,1 @@
+/test-dir/test-conffile-owned

tests/t-conffile-declarative-removal/pkg-conffile-owns-symlink/DEBIAN/control
@@ -1,0 +1,7 @@
+Package: pkg-conffile-owns
+Version: 1
+Section: test
+Priority: extra
+Maintainer: Dpkg Developers <debian-dpkg@lists.debian.org>
+Architecture: all
+Description: test package - owns conffile to be removed via symlink

commit 29f3a8dafd07191bb132c471e4a44379553384d4

dpkg: Fix memory leak in remove-on-upgrade handling
Fixes: commit 20e181fc2030385488e3db2d4cc3ce4114f0a498
Warned-by: coverity
Stable-Candidate: 1.20.x

src/unpack.c
@@ -630,6 +630,7 @@ pkg_remove_conffile_on_upgrade(struct pkginfo *pkg, struct fsys_namenode *nameno
 
     debug(dbg_conff, "%s: namenode '%s' owned by other %s, remove-on-upgrade ignored",
          __func__, namenode->name, pkg_name(otherpkg, pnaw_always));
+    fsys_node_pkgs_iter_free(iter);
     return;
   }
   fsys_node_pkgs_iter_free(iter);