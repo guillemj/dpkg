guillemj/dpkg

commit 863af2464d7bf20ad458ee51e2d187a9845214ca

build: Add zlib-ng support
The zlib-ng library is a fork of zlib with its code modernized and lots
of optimizations for various modern CPUs giving it a substantial
performance boost.

This library provides either an API (and in theory ABI) compatible
interface, or a cleaned up API with namespaced and with better types.

We add support for the latter, via a compatibility header mapping symbol
names.

lib/compat/compat-zlib.h
@@ -1,0 +1,43 @@
+/*
+ * libcompat - system compatibility library
+ * compat-zlib.h - zlib compatibility declarations
+ *
+ * Copyright © 2021 Guillem Jover <guillem@debian.org>
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef COMPAT_ZLIB_H
+#define COMPAT_ZLIB_H
+
+#ifdef WITH_LIBZ_NG
+#include <zlib-ng.h>
+#else
+#ifdef WITH_LIBZ
+#include <zlib.h>
+#endif
+#endif
+
+#ifdef WITH_LIBZ_NG
+/* Compatibility symbols for zlib-ng. */
+#define gzdopen		zng_gzdopen
+#define gzopen		zng_gzopen
+#define gzread		zng_gzread
+#define gzwrite		zng_gzwrite
+#define gzerror		zng_gzerror
+#define gzclose		zng_gzclose
+#define zError		zng_zError
+#endif
+
+#endif /* COMPAT_ZLIB_H */

lib/dpkg/compress.c
@@ -29,8 +29,8 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-#ifdef WITH_LIBZ
-#include <zlib.h>
+#if defined(WITH_LIBZ) || defined(WITH_LIBZ_NG)
+#include <compat-zlib.h>
 #endif
 #ifdef WITH_LIBLZMA
 #include <lzma.h>
@@ -47,7 +47,9 @@
 #include <dpkg/buffer.h>
 #include <dpkg/command.h>
 #include <dpkg/compress.h>
-#if !defined(WITH_LIBZ) || !defined(WITH_LIBLZMA) || !defined(WITH_LIBBZ2)
+#if !(defined(WITH_LIBZ_NG) || defined(WITH_LIBZ)) || \
+    !defined(WITH_LIBLZMA) || \
+    !defined(WITH_LIBBZ2)
 #include <dpkg/subproc.h>
 
 static void DPKG_ATTR_SENTINEL
@@ -145,7 +147,7 @@ fixup_gzip_params(struct compress_params *params)
 		params->type = COMPRESSOR_TYPE_NONE;
 }
 
-#ifdef WITH_LIBZ
+#if defined(WITH_LIBZ_NG) || defined(WITH_LIBZ)
 static void
 decompress_gzip(int fd_in, int fd_out, const char *desc)
 {

m4/dpkg-libs.m4
@@ -69,10 +69,23 @@ AC_DEFUN([DPKG_WITH_COMPRESS_LIB], [
 ])# DPKG_WITH_COMPRESS_LIB
 
 # DPKG_LIB_Z
-# -------------
-# Check for z library.
+# ----------
+# Check for z-ng and z libraries.
 AC_DEFUN([DPKG_LIB_Z], [
   DPKG_WITH_COMPRESS_LIB([z], [zlib.h], [gzdopen])
+  DPKG_WITH_COMPRESS_LIB([z-ng], [zlib-ng.h], [zng_gzdopen])
+
+  # If we have been requested the stock zlib, override the auto-detection.
+  AS_IF([test "x$with_libz" != "xyes" && test "x$have_libz_ng" = "xyes"], [
+    AC_DEFINE([WITH_GZFILEOP], [yes],
+      [Define to yes to use zlib-ng gzFile IO support])
+    Z_LIBS=$Z_NG_LIBS
+    have_libz_impl="yes (zlib-ng)"
+  ], [test "x$have_libz" = "xyes"], [
+    have_libz_impl="yes (zlib)"
+  ], [
+    have_libz_impl="no"
+  ])
 ])# DPKG_LIB_Z
 
 # DPKG_LIB_LZMA

configure.ac
@@ -285,7 +285,7 @@ Configuration:
     libkvm  . . . . . . . . . . . : ${have_libkvm:-no}
     libselinux  . . . . . . . . . : $have_libselinux
     libmd . . . . . . . . . . . . : $have_libmd
-    libz  . . . . . . . . . . . . : $have_libz
+    libz  . . . . . . . . . . . . : $have_libz_impl
     liblzma . . . . . . . . . . . : $have_liblzma
     libbz2  . . . . . . . . . . . : $have_libbz2
     libcurses . . . . . . . . . . : ${have_libcurses:-no}

lib/compat/Makefile.am
@@ -36,6 +36,7 @@ endif
 libcompat_la_SOURCES = \
 	empty.c \
 	compat.h \
+	compat-zlib.h \
 	gettext.h \
 	# EOL
 

commit ccf91bef46858f62be45e3417785e047e7ce85b1

build: Fix zlib implementation selection
We need to select only one of the possibly available zlib
implementations. Turn the code to use a pre-processor "enum", by
defining the various values as different macros, that we will select
from a variable macro.

Changelog: silent
Fixes: commit 863af2464d7bf20ad458ee51e2d187a9845214ca

lib/compat/compat-zlib.h
@@ -21,15 +21,13 @@
 #ifndef COMPAT_ZLIB_H
 #define COMPAT_ZLIB_H
 
-#ifdef WITH_LIBZ_NG
+#if USE_LIBZ_IMPL == USE_LIBZ_IMPL_ZLIB_NG
 #include <zlib-ng.h>
-#else
-#ifdef WITH_LIBZ
+#elif USE_LIBZ_IMPL == USE_LIBZ_IMPL_ZLIB
 #include <zlib.h>
 #endif
-#endif
 
-#ifdef WITH_LIBZ_NG
+#if USE_LIBZ_IMPL == USE_LIBZ_IMPL_ZLIB_NG
 /* Compatibility symbols for zlib-ng. */
 #define gzdopen		zng_gzdopen
 #define gzopen		zng_gzopen

lib/dpkg/compress.c
@@ -33,7 +33,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-#if defined(WITH_LIBZ) || defined(WITH_LIBZ_NG)
+#if USE_LIBZ_IMPL != USE_LIBZ_IMPL_NONE
 #include <compat-zlib.h>
 #endif
 #ifdef WITH_LIBLZMA
@@ -51,7 +51,7 @@
 #include <dpkg/buffer.h>
 #include <dpkg/command.h>
 #include <dpkg/compress.h>
-#if !(defined(WITH_LIBZ_NG) || defined(WITH_LIBZ)) || \
+#if USE_LIBZ_IMPL == USE_LIBZ_IMPL_NONE || \
     !defined(WITH_LIBLZMA) || \
     !defined(WITH_LIBBZ2)
 #include <dpkg/subproc.h>
@@ -154,7 +154,7 @@ fixup_gzip_params(struct compress_params *params)
 		params->type = COMPRESSOR_TYPE_NONE;
 }
 
-#if defined(WITH_LIBZ_NG) || defined(WITH_LIBZ)
+#if USE_LIBZ_IMPL != USE_LIBZ_IMPL_NONE
 static void
 decompress_gzip(struct compress_params *params, int fd_in, int fd_out,
                 const char *desc)

m4/dpkg-libs.m4
@@ -75,17 +75,29 @@ AC_DEFUN([DPKG_LIB_Z], [
   DPKG_WITH_COMPRESS_LIB([z], [zlib.h], [gzdopen])
   DPKG_WITH_COMPRESS_LIB([z-ng], [zlib-ng.h], [zng_gzdopen])
 
+  AC_DEFINE([USE_LIBZ_IMPL_NONE], [0],
+            [Define none as 0 for the zlib implementation enum])
+  AC_DEFINE([USE_LIBZ_IMPL_ZLIB], [1],
+            [Define zlib as 1 for the zlib implementation enum])
+  AC_DEFINE([USE_LIBZ_IMPL_ZLIB_NG], [2],
+            [Define zlib-ng as 2 for the zlib implementation enum])
+
   # If we have been requested the stock zlib, override the auto-detection.
   AS_IF([test "x$with_libz" != "xyes" && test "x$have_libz_ng" = "xyes"], [
     AC_DEFINE([WITH_GZFILEOP], [yes],
       [Define to yes to use zlib-ng gzFile IO support])
     Z_LIBS=$Z_NG_LIBS
+    use_libz_impl="USE_LIBZ_IMPL_ZLIB_NG"
     have_libz_impl="yes (zlib-ng)"
   ], [test "x$have_libz" = "xyes"], [
+    use_libz_impl="USE_LIBZ_IMPL_ZLIB"
     have_libz_impl="yes (zlib)"
   ], [
+    use_libz_impl="USE_LIBZ_IMPL_NONE"
     have_libz_impl="no"
   ])
+  AC_DEFINE_UNQUOTED([USE_LIBZ_IMPL], [$use_libz_impl],
+                     [Define to the zlib implementation to use])
 ])# DPKG_LIB_Z
 
 # DPKG_LIB_LZMA