guillemj/dpkg

commit 536a38582a2d92da84f82d850e22f581b00d2082

libdpkg: Ignore not-installed packages for source related virtual fields
The source name and source versions are not available for not-installed
packages, so we should not try to print anything as that will end up
being bogus, or produce parsing problems for version related virtual
fields.

Closes: #972580

lib/dpkg/pkg-format.c
@@ -333,6 +333,9 @@ virt_source_package(struct varbuf *vb,
 	const char *name;
 	size_t len;
 
+	if (pkg->status == PKG_STAT_NOTINSTALLED)
+		return;
+
 	name = pkgbin->source;
 	if (name == NULL)
 		name = pkg->set->name;
@@ -347,6 +350,9 @@ virt_source_version(struct varbuf *vb,
                     const struct pkginfo *pkg, const struct pkgbin *pkgbin,
                     enum fwriteflags flags, const struct fieldinfo *fip)
 {
+	if (pkg->status == PKG_STAT_NOTINSTALLED)
+		return;
+
 	varbuf_add_source_version(vb, pkg, pkgbin);
 }
 
@@ -356,6 +362,9 @@ virt_source_upstream_version(struct varbuf *vb,
                              enum fwriteflags flags, const struct fieldinfo *fip)
 {
 	struct dpkg_version version;
+
+	if (pkg->status == PKG_STAT_NOTINSTALLED)
+		return;
 
 	pkg_source_version(&version, pkg, pkgbin);
 

commit 560574b55a543d3d0a0f4e8abf0ae42f46d2a2f1

libdpkg: Do not restrict source:* virtual fields to installed packages
Initially the code was not handling missing data properly. It got changed
in two ways, in commit 536a38582a2d92da84f82d850e22f581b00d2082 it was
first restricted to only operate on installed packages, which regressed
support for dpkg-deb show format, and then the code was made robust
against such missing data (which was really the only correct and necessary
part to the fix) in commit 9022f9e0ffc58425c6798ff62e797f4026a29fe9.

Fixes: commit 536a38582a2d92da84f82d850e22f581b00d2082
Closes: #1004372
Stable-Candidate: 1.20.x

lib/dpkg/pkg-format.c
@@ -333,9 +333,6 @@ virt_source_package(struct varbuf *vb,
 	const char *name;
 	size_t len;
 
-	if (pkg->status == PKG_STAT_NOTINSTALLED)
-		return;
-
 	name = pkgbin->source;
 	if (name == NULL)
 		name = pkg->set->name;
@@ -350,9 +347,6 @@ virt_source_version(struct varbuf *vb,
                     const struct pkginfo *pkg, const struct pkgbin *pkgbin,
                     enum fwriteflags flags, const struct fieldinfo *fip)
 {
-	if (pkg->status == PKG_STAT_NOTINSTALLED)
-		return;
-
 	varbuf_add_source_version(vb, pkg, pkgbin);
 }
 
@@ -362,9 +356,6 @@ virt_source_upstream_version(struct varbuf *vb,
                              enum fwriteflags flags, const struct fieldinfo *fip)
 {
 	struct dpkg_version version;
-
-	if (pkg->status == PKG_STAT_NOTINSTALLED)
-		return;
 
 	pkg_source_version(&version, pkg, pkgbin);
 