guillemj/dpkg

commit 3a7db14e4a881d28fb73b29c126ea6f3ac4d0831

Dpkg::Compression: Use gzip --rsyncable unconditionally
Upstream gzip merged --rsyncable support in 1.7 (on 2016-04-26).
The comment and implementation do not reflect reality any longer and
we should be safe to use the option unconditionally on all systems.

scripts/Dpkg/Compression.pm
@@ -53,9 +53,10 @@ interact with the set of supported compression methods.
 =cut
 
 my $COMP = {
+    # Requires gzip >= 1.7 for the --rsyncable option.
     gzip => {
 	file_ext => 'gz',
-	comp_prog => [ 'gzip', '--no-name' ],
+	comp_prog => [ 'gzip', '--no-name', '--rsyncable' ],
 	decomp_prog => [ 'gunzip' ],
 	default_level => 9,
     },
@@ -78,24 +79,6 @@ my $COMP = {
 	default_level => 6,
     },
 };
-
-#
-# XXX: The gzip package in Debian at some point acquired a Debian-specific
-# --rsyncable option via a vendor patch. Which is not present in most of the
-# major distributions, dpkg downstream systems, nor gzip upstream, who have
-# stated they will most probably not accept it because people should be using
-# pigz instead.
-#
-# This option should have never been accepted in dpkg, ever. But removing it
-# now would probably cause demands for tarring and feathering. In addition
-# we cannot use the Dpkg::Vendor logic because that would cause circular
-# module dependencies. The whole affair is pretty disgusting really.
-#
-# Check the perl Config to discern Debian and hopefully derivatives too.
-#
-if ($Config{cf_by} eq 'Debian Project') {
-    push @{$COMP->{gzip}->{comp_prog}}, '--rsyncable';
-}
 
 my $default_compression = 'xz';
 my $default_compression_level = undef;

commit 0124692643c6bf25034dbb16e74205d911aa23e3

Dpkg::Compression: Conditionally use the gzip --rsyncable option
The gzip --rsyncable option is not universally supported, so we need to
conditionally use it. Ideally we would invoke «gzip --help» and check
whether the option is supported, but that would imply forking and
executing that process for any module that ends up loading this one,
which is not acceptable performance-wise. Instead we will approximate it
by Perl's osname, which is not ideal, but better than nothing.

Requires GNU gzip >= 1.7 for the --rsyncable option. On AIX GNU gzip
is too old. On the BSDs they use their own implementation based on zlib,
which does not currently support the --rsyncable option.

Fixes: commit 3a7db14e4a881d28fb73b29c126ea6f3ac4d0831
Fixes: https://rt.cpan.org/Ticket/Display.html?id=141805

scripts/Dpkg/Compression.pm
@@ -35,6 +35,7 @@ our @EXPORT = qw(
 
 use Exporter qw(import);
 use Config;
+use List::Util qw(any);
 
 use Dpkg::ErrorHandling;
 use Dpkg::Gettext;
@@ -53,10 +54,9 @@ interact with the set of supported compression methods.
 =cut
 
 my $COMP = {
-    # Requires gzip >= 1.7 for the --rsyncable option.
     gzip => {
 	file_ext => 'gz',
-	comp_prog => [ 'gzip', '--no-name', '--rsyncable' ],
+	comp_prog => [ 'gzip', '--no-name' ],
 	decomp_prog => [ 'gunzip' ],
 	default_level => 9,
     },
@@ -79,6 +79,20 @@ my $COMP = {
 	default_level => 6,
     },
 };
+
+# The gzip --rsyncable option is not universally supported, so we need to
+# conditionally use it. Ideally we would invoke 'gzip --help' and check
+# whether the option is supported, but that would imply forking and executing
+# that process for any module that ends up loading this one, which is not
+# acceptable performance-wise. Instead we will approximate it by osname, which
+# is not ideal, but better than nothing.
+#
+# Requires GNU gzip >= 1.7 for the --rsyncable option. On AIX GNU gzip is
+# too old. On the BSDs they use their own implementation based on zlib,
+# which does not currently support the --rsyncable option.
+if (any { $Config{osname} eq $_ } qw(linux gnu)) {
+    push @{$COMP->{gzip}->{comp_prog}}, '--rsyncable';
+}
 
 my $default_compression = 'xz';
 my $default_compression_level = undef;