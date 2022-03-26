guillemj/dpkg

commit f0fa8190fe02bb9db8168ae6a67f592f135b06c2

Dpkg::Control::FieldsCore: Do not execute code at import time

scripts/Dpkg/Control/FieldsCore.pm
@@ -584,6 +584,10 @@ our %FIELDS = (
     },
 );
 
+my @src_dep_fields = qw(build-depends build-depends-arch build-depends-indep
+    build-conflicts build-conflicts-arch build-conflicts-indep);
+my @bin_dep_fields = qw(pre-depends depends recommends suggests enhances
+    conflicts breaks replaces provides built-using);
 my @src_checksums_fields = qw(checksums-md5 checksums-sha1 checksums-sha256);
 my @bin_checksums_fields = qw(md5sum sha1 sha256);
 
@@ -592,15 +596,30 @@ our %FIELD_ORDER = (
         qw(package package-type source version built-using kernel-version
         built-for-profiles auto-built-package architecture subarchitecture
         installer-menu-item build-essential essential origin bugs
-        maintainer installed-size), map { lc } &field_list_pkg_dep(),
+        maintainer installed-size), @bin_dep_fields,
+        qw(section priority multi-arch homepage description tag task)
+    ],
+    CTRL_INDEX_PKG() => [
+        qw(package package-type source version built-using kernel-version
+        built-for-profiles auto-built-package architecture subarchitecture
+        installer-menu-item build-essential essential origin bugs
+        maintainer installed-size), @bin_dep_fields,
+        qw(filename size), @bin_checksums_fields,
         qw(section priority multi-arch homepage description tag task)
     ],
     CTRL_PKG_SRC() => [
         qw(format source binary architecture version origin maintainer
         uploaders homepage standards-version vcs-browser
         vcs-arch vcs-bzr vcs-cvs vcs-darcs vcs-git vcs-hg vcs-mtn
-        vcs-svn testsuite testsuite-triggers), map { lc } &field_list_src_dep(),
+        vcs-svn testsuite testsuite-triggers), @src_dep_fields,
         qw(package-list), @src_checksums_fields, qw(files)
+    ],
+    CTRL_INDEX_SRC() => [
+        qw(format package binary architecture version priority section
+        origin maintainer uploaders homepage standards-version vcs-browser
+        vcs-arch vcs-bzr vcs-cvs vcs-darcs vcs-git vcs-hg vcs-mtn vcs-svn
+        testsuite testsuite-triggers), @src_dep_fields,
+        qw(package-list directory), @src_checksums_fields, qw(files)
     ],
     CTRL_FILE_BUILDINFO() => [
         qw(format source binary architecture version binary-only-changes),
@@ -644,15 +663,6 @@ our %FIELD_ORDER = (
         qw(license comment)
     ],
 );
-# Order for CTRL_INDEX_PKG is derived from CTRL_PKG_DEB
-$FIELD_ORDER{CTRL_INDEX_PKG()} = [ @{$FIELD_ORDER{CTRL_PKG_DEB()}} ];
-&field_insert_before(CTRL_INDEX_PKG, 'section', 'filename', 'size', @bin_checksums_fields);
-# Order for CTRL_INDEX_SRC is derived from CTRL_PKG_SRC
-$FIELD_ORDER{CTRL_INDEX_SRC()} = [ @{$FIELD_ORDER{CTRL_PKG_SRC()}} ];
-@{$FIELD_ORDER{CTRL_INDEX_SRC()}} = map { $_ eq 'source' ? 'package' : $_ }
-                                  @{$FIELD_ORDER{CTRL_PKG_SRC()}};
-&field_insert_after(CTRL_INDEX_SRC, 'version', 'priority', 'section');
-&field_insert_before(CTRL_INDEX_SRC, 'checksums-md5', 'directory');
 
 =encoding utf8
 

commit 83ceb3f325ff208f0ed86ccad39ea256d2ff0194

Dpkg::Control::Fields: Remove duplicate *Built-Using from field order lists
These fields got refactored into a common dependency fields list, but
the Built-Using field did not get removed from the original order lists,
and ended up being duplicated. The Static-Built-Using field got added
next to each previous Built-Using instance, which ended up being
duplicated too.

Fixes: commit f0fa8190fe02bb9db8168ae6a67f592f135b06c2
Reported-by: David Kalnischkies <donkult@debian.org>

scripts/Dpkg/Control/FieldsCore.pm
@@ -755,8 +755,6 @@ our %FIELD_ORDER = (
             package-type
             source
             version
-            built-using
-            static-built-using
             kernel-version
             built-for-profiles
             auto-built-package
@@ -816,8 +814,6 @@ our %FIELD_ORDER = (
             package-type
             source
             version
-            built-using
-            static-built-using
             kernel-version
             built-for-profiles
             auto-built-package

scripts/t/Dpkg_Control_Fields.t
@@ -182,8 +182,6 @@ my %fields = (
                 Package-Type
                 Source
                 Version
-                Built-Using
-                Static-Built-Using
                 Kernel-Version
                 Built-For-Profiles
                 Auto-Built-Package
@@ -246,8 +244,6 @@ my %fields = (
                 Package-Type
                 Source
                 Version
-                Built-Using
-                Static-Built-Using
                 Kernel-Version
                 Built-For-Profiles
                 Auto-Built-Package