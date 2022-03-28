guillemj/dpkg

commit 3a017177110da9f54668b614f78aeb98bf73796d

libdpkg: Merge nicknames table into fieldinfos
The current code first does a lookup to see if the fieldname is in the
nicknames table, if found fixes up the fieldname to the new alias, and
then always does a lookup for the normalized name in fieldinfos table.

This penalizes the common case (i.e. non-obsolete fieldnames)
substantially. Also moving the nicknames into the fieldinfos table will
allow to print correct fieldnames on error as those are not mangled any
more, and to provide new wrapper parse functions that print warnings for
the obsolete fieldnames.

lib/dpkg/parse.c
@@ -64,7 +64,6 @@ const struct fieldinfo fieldinfos[]= {
   { FIELD("Multi-Arch"),       f_multiarch,       w_multiarch,      PKGIFPOFF(multiarch)     },
   { FIELD("Source"),           f_charfield,       w_charfield,      PKGIFPOFF(source)        },
   { FIELD("Version"),          f_version,         w_version,        PKGIFPOFF(version)       },
-  { FIELD("Revision"),         f_revision,        w_null                                     },
   { FIELD("Config-Version"),   f_configversion,   w_configversion                            },
   { FIELD("Replaces"),         f_dependency,      w_dependency,     dep_replaces             },
   { FIELD("Provides"),         f_dependency,      w_dependency,     dep_provides             },
@@ -84,17 +83,13 @@ const struct fieldinfo fieldinfos[]= {
   { FIELD("Triggers-Pending"), f_trigpend,        w_trigpend                                 },
   { FIELD("Triggers-Awaited"), f_trigaw,          w_trigaw                                   },
   /* Note that aliases are added to the nicknames table. */
-  {  NULL                                                                             }
-};
-
-static const struct nickname nicknames[] = {
-  /* Note: Capitalization of these strings is important. */
-  { NICK("Recommended"),      .canon = "Recommends" },
-  { NICK("Optional"),         .canon = "Suggests" },
-  { NICK("Class"),            .canon = "Priority" },
-  { NICK("Package-Revision"), .canon = "Revision" },
-  { NICK("Package_Revision"), .canon = "Revision" },
-  { .nick = NULL }
+  { FIELD("Revision"),         f_revision,        w_null                                     },
+  { FIELD("Recommended"),      f_dependency,      w_null                                     },
+  { FIELD("Optional"),         f_dependency,      w_null                                     },
+  { FIELD("Class"),            f_priority,        w_null                                     },
+  { FIELD("Package-Revision"), f_revision,        w_null                                     },
+  { FIELD("Package_Revision"), f_revision,        w_null                                     },
+  { NULL                                                                                     }
 };
 
 /**
@@ -116,18 +111,8 @@ pkg_parse_field(struct parsedb_state *ps, struct field_state *fs,
                 void *parse_obj)
 {
   struct pkg_parse_object *pkg_obj = parse_obj;
-  const struct nickname *nick;
   const struct fieldinfo *fip;
   int *ip;
-
-  for (nick = nicknames; nick->nick; nick++)
-    if (nick->nicklen == (size_t)fs->fieldlen &&
-        strncasecmp(nick->nick, fs->fieldstart, fs->fieldlen) == 0)
-      break;
-  if (nick->nick) {
-    fs->fieldstart = nick->canon;
-    fs->fieldlen = strlen(fs->fieldstart);
-  }
 
   for (fip = fieldinfos, ip = fs->fieldencountered; fip->name; fip++, ip++)
     if (fip->namelen == (size_t)fs->fieldlen &&

lib/dpkg/parsedump.h
@@ -158,14 +158,6 @@ void parse_ensure_have_field(struct parsedb_state *ps,
 
 #define MSDOS_EOF_CHAR '\032' /* ^Z */
 
-#define NICK(name) .nick = name, .nicklen = sizeof(name) - 1
-
-struct nickname {
-  const char *nick;
-  const char *canon;
-  size_t nicklen;
-};
-
 extern const struct fieldinfo fieldinfos[];
 
 /** @} */

commit 4ddb46ac28df4b290b9055e20268b850a019385e

libdpkg: Update code comment for obsolete fields
The comment was still referencing the old nicknames structure.

Fixes: commit 3a017177110da9f54668b614f78aeb98bf73796d

lib/dpkg/parse.c
@@ -84,7 +84,9 @@ const struct fieldinfo fieldinfos[]= {
   { FIELD("Description"),      f_charfield,       w_charfield,      PKGIFPOFF(description)   },
   { FIELD("Triggers-Pending"), f_trigpend,        w_trigpend                                 },
   { FIELD("Triggers-Awaited"), f_trigaw,          w_trigaw                                   },
-  /* Note that aliases are added to the nicknames table. */
+
+  /* The following are the obsolete fields that get remapped to their
+   * modern forms, while emitting an obsolescence warning. */
   { FIELD("Recommended"),      f_obs_dependency,  w_null,           dep_recommends           },
   { FIELD("Optional"),         f_obs_dependency,  w_null,           dep_suggests             },
   { FIELD("Class"),            f_obs_class,       w_null                                     },