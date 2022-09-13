b6b89baa413fe9f37c5df5fbe1bba4f5f7f758b1
Guillem Jover <guillem@debian.org>
2022-09-13 23:41:55 +0200

    scripts/mk: On dpkg-build-api >= 1 include buildtools.mk in default.mk

--- scripts/mk/default.mk
+++ scripts/mk/default.mk
@@ -4,6 +4,9 @@
 dpkg_datadir = $(srcdir)/mk
 include $(dpkg_datadir)/architecture.mk
 include $(dpkg_datadir)/buildapi.mk
+ifeq ($(call dpkg_build_api_ge,1),yes)
+include $(dpkg_datadir)/buildtools.mk
+endif
 include $(dpkg_datadir)/buildflags.mk
 include $(dpkg_datadir)/buildopts.mk
 include $(dpkg_datadir)/pkg-info.mk
