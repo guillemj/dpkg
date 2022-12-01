guillemj/dpkg

commit 36a7770f03e9e4d1865d25ad74b27d82c07b6f84

dpkg-genbuildinfo: Add new can-execute-cross-built-programs tainted flag
Whether we can execute a cross-built program depends on whether the
CPU and the operating system have such capability, including in some
cases doing so via emulation, such as qemu hooked via something like
binfmt on Linux. If the system is or not capable of running such
programs, might affect the built artifacts and what tests run or not.

[guillem@debian.org:
 - Use File::Temp instead of tmpnam() and push_exit_handler().
 - Set a taint flag instead of a new field.
 - Pass -w to gcc to avoid warnings.
 - Refactor into a function.
 - Style fixes. ]

Closes: #1011191

scripts/dpkg-genbuildinfo.pl
@@ -28,13 +28,19 @@ use warnings;
 use List::Util qw(any);
 use Cwd;
 use File::Basename;
+use File::Temp;
 use POSIX qw(:fcntl_h :locale_h strftime);
 
 use Dpkg ();
 use Dpkg::Gettext;
 use Dpkg::Checksums;
 use Dpkg::ErrorHandling;
-use Dpkg::Arch qw(get_build_arch get_host_arch debarch_eq);
+use Dpkg::IPC;
+use Dpkg::Arch qw(
+    get_build_arch
+    get_host_arch
+    debarch_eq debarch_to_gnutriplet
+);
 use Dpkg::Build::Types;
 use Dpkg::Build::Info qw(get_build_env_allowed);
 use Dpkg::BuildOptions;
@@ -247,8 +253,51 @@ sub collect_installed_builddeps {
     return $installed_deps;
 }
 
+sub is_cross_executable {
+    my $host_arch = get_host_arch();
+    my $build_arch = get_build_arch();
+
+    return if $host_arch eq $build_arch;
+
+    # If we are cross-compiling, record whether it was possible to execute
+    # the host architecture by cross-compiling and executing a small
+    # host-arch binary.
+    my $CC = debarch_to_gnutriplet($host_arch) . '-gcc';
+    my $crossprog = 'int main() { write(1, "ok", 2); return 0; }';
+    my ($stdout, $stderr) = ('', '');
+    my $tmpfh = File::Temp->new();
+    spawn(
+        exec => [ $CC, '-w', '-x', 'c', '-o', $tmpfh->filename, '-' ],
+        from_string => \$crossprog,
+        to_string => \$stdout,
+        error_to_string => \$stderr,
+        wait_child => 1,
+        nocheck => 1,
+    );
+    if ($?) {
+       print { *STDOUT } $stdout;
+       print { *STDERR } $stderr;
+       subprocerr("$CC -w -x c -");
+    }
+    close $tmpfh;
+    spawn(
+        exec => [ $tmpfh->filename ],
+        error_to_file => '/dev/null',
+        to_string => \$stdout,
+        wait_child => 1,
+        nocheck => 1,
+    );
+
+    return 1 if $? == 0 && $stdout eq 'ok';
+    return 0;
+}
+
 sub get_build_tainted_by {
     my @tainted = run_vendor_hook('build-tainted-by');
+
+    if (is_cross_executable()) {
+        push @tainted, 'can-execute-cross-built-programs';
+    }
 
     return @tainted;
 }

man/deb-buildinfo.pod
@@ -211,6 +211,13 @@ The system has programs under I</usr/local/bin> or I</usr/local/sbin>.
 
 The system has libraries, either static or shared under I</usr/local/lib>.
 
+=item B<can-execute-cross-built-programs>
+
+The system can execute cross built programs, either directly or via some
+emulation layer.
+
+Since dpkg 1.21.10.
+
 =back
 
 =item B<Installed-Build-Depends:> (required)

commit 2bec078d3be86aef7106f67f7e7a919c5e53ed3e

dpkg-genbuildinfo: Do not fail if there is no cross-comiler
When bootstrapping an architecture, or even on circumstances where we
might be cross-compiling using a language other than C/C++ we cannot
expect such cross-compiler to be present. In those cases simply ignore
the taint check.

Fixes: commit 36a7770f03e9e4d1865d25ad74b27d82c07b6f84
Closes: #1025273

scripts/dpkg-genbuildinfo.pl
@@ -36,6 +36,7 @@ use Dpkg::Gettext;
 use Dpkg::Checksums;
 use Dpkg::ErrorHandling;
 use Dpkg::IPC;
+use Dpkg::Path qw(find_command);
 use Dpkg::Arch qw(
     get_build_arch
     get_host_arch
@@ -263,6 +264,12 @@ sub is_cross_executable {
     # the host architecture by cross-compiling and executing a small
     # host-arch binary.
     my $CC = debarch_to_gnutriplet($host_arch) . '-gcc';
+
+    # If we do not have a cross-compiler, we might be in the process of
+    # building one or cross-compiling using a language other than C/C++,
+    # and aborting the build is then not very useful.
+    return if ! find_command($CC);
+
     my $crossprog = <<~'CROSSPROG';
         #include <unistd.h>
         int main() { write(1, "ok", 2); return 0; }