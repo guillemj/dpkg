guillemj/dpkg

commit 99c9cdbf1b9650ef5025c3c06ea918ef3007888e

libdpkg: Allow missing status and available databases
This makes it possible to bootstrap an installation w/o requiring
external knowledge of the dpkg internals, as the databases will be
created on write when necessary. We can then remove the code setting
them up from the dpkg postinst.

Closes: #647911

lib/dpkg/dpkg-db.h
@@ -350,13 +350,16 @@ enum parsedbflags {
   pdb_close_fd			= DPKG_BIT(7),
   /** Interpret filename ‘-’ as stdin. */
   pdb_dash_is_stdin		= DPKG_BIT(8),
+  /** Allow empty/missing files. */
+  pdb_allow_empty		= DPKG_BIT(9),
 
   /* Standard operations. */
 
-  pdb_parse_status		= pdb_lax_parser | pdb_weakclassification,
+  pdb_parse_status		= pdb_lax_parser | pdb_weakclassification |
+				  pdb_allow_empty,
   pdb_parse_update		= pdb_parse_status | pdb_single_stanza,
   pdb_parse_available		= pdb_recordavailable | pdb_rejectstatus |
-				  pdb_lax_parser,
+				  pdb_lax_parser | pdb_allow_empty,
   pdb_parse_binary		= pdb_recordavailable | pdb_rejectstatus |
 				  pdb_single_stanza,
 };

Makefile.am
@@ -73,7 +73,6 @@ EXTRA_DIST = \
 	debian/dpkg.docs \
 	debian/dpkg.install \
 	debian/dpkg.manpages \
-	debian/dpkg.postinst \
 	debian/dpkg.postrm \
 	debian/dpkg.logrotate \
 	debian/dpkg.lintian-overrides \

debian/dpkg.postinst
@@ -1,32 +1,0 @@
-#!/bin/sh
-# See deb-postinst(5).
-
-set -e
-
-# Create the database files if they don't already exist
-create_database() {
-    admindir=${DPKG_ADMINDIR:-/var/lib/dpkg}
-
-    for file in diversions statoverride status; do
-	if [ ! -f "$admindir/$file" ]; then
-	    touch "$admindir/$file"
-	fi
-    done
-}
-
-case "$1" in
-    configure)
-	create_database
-	;;
-
-    abort-upgrade|abort-deconfigure|abort-remove)
-	;;
-
-    *)
-	echo "$0 called with unknown argument '$1'" 1>&2
-	exit 1
-	;;
-esac
-
-#DEBHELPER#
-exit 0

lib/dpkg/parse.c
@@ -28,6 +28,7 @@
 #include <sys/mman.h>
 #endif
 
+#include <errno.h>
 #include <fcntl.h>
 #include <string.h>
 #include <unistd.h>
@@ -531,6 +532,9 @@ parsedb_new(const char *filename, int fd, enum parsedbflags flags)
   ps->flags = flags;
   ps->fd = fd;
   ps->lno = 0;
+  ps->data = NULL;
+  ps->dataptr = NULL;
+  ps->endptr = NULL;
   ps->pkg = NULL;
   ps->pkgbin = NULL;
 
@@ -551,7 +555,7 @@ parsedb_open(const char *filename, enum parsedbflags flags)
     return parsedb_new(filename, STDIN_FILENO, flags);
 
   fd = open(filename, O_RDONLY);
-  if (fd == -1)
+  if (fd == -1 && !(errno == ENOENT && (flags & pdb_allow_empty)))
     ohshite(_("failed to open package info file '%.255s' for reading"),
             filename);
 
@@ -569,6 +573,9 @@ void
 parsedb_load(struct parsedb_state *ps)
 {
   struct stat st;
+
+  if (ps->fd < 0 && (ps->flags & pdb_allow_empty))
+      return;
 
   if (fstat(ps->fd, &st) == -1)
     ohshite(_("can't stat package info file '%.255s'"), ps->filename);
@@ -735,7 +742,7 @@ parsedb_close(struct parsedb_state *ps)
   if (ps->flags & pdb_close_fd) {
     pop_cleanup(ehflag_normaltidy);
 
-    if (close(ps->fd))
+    if (ps->fd >= 0 && close(ps->fd) < 0)
       ohshite(_("failed to close after read: '%.255s'"), ps->filename);
   }
 
@@ -765,6 +772,9 @@ parsedb_parse(struct parsedb_state *ps, struct pkginfo **donep)
   int fieldencountered[array_count(fieldinfos)];
   int pdone;
   struct field_state fs;
+
+  if (ps->data == NULL && (ps->flags & pdb_allow_empty))
+    return 0;
 
   memset(&fs, 0, sizeof(fs));
   fs.fieldencountered = fieldencountered;

t/shellcheck.t
@@ -36,7 +36,6 @@ my @files = qw(
     get-version
     run-script
     debian/dpkg.cron.daily
-    debian/dpkg.postinst
     debian/dpkg.postrm
     scripts/dpkg-maintscript-helper.sh
 );

commit 6b4fdaa805b66d897785bb2d88de32a99d990138

libdpkg: Fix pdb_lax_stanza_parser and pdb_allow_empty aliased values
The pdb_lax_stanza_parser constant was assigned the last value to avoid
renumbering all other enum constants, but because it was placed
unordered it was predictably not future-proof. Later on a new constant
was assigned as the last enum constant reusing that same value, causing
the current aliasing.

This ends up currently being a non-issue, as all places where we pass
one of the constants we also pass the other.

Fixes: commit 99c9cdbf1b9650ef5025c3c06ea918ef3007888e

lib/dpkg/dpkg-db.h
@@ -346,15 +346,15 @@ enum parsedbflags {
   /** Perform laxer version parsing. */
   pdb_lax_version_parser	= DPKG_BIT(6),
   /** Perform laxer control stanza parsing. */
-  pdb_lax_stanza_parser		= DPKG_BIT(9),
+  pdb_lax_stanza_parser		= DPKG_BIT(7),
   /** Perform laxer parsing, used to transition to stricter parsing. */
   pdb_lax_parser		= pdb_lax_stanza_parser | pdb_lax_version_parser,
   /** Close file descriptor on context destruction. */
-  pdb_close_fd			= DPKG_BIT(7),
+  pdb_close_fd			= DPKG_BIT(8),
   /** Interpret filename ‘-’ as stdin. */
-  pdb_dash_is_stdin		= DPKG_BIT(8),
+  pdb_dash_is_stdin		= DPKG_BIT(9),
   /** Allow empty/missing files. */
-  pdb_allow_empty		= DPKG_BIT(9),
+  pdb_allow_empty		= DPKG_BIT(10),
 
   /* Standard operations. */
 