guillemj/dpkg

commit df7627acfadca528e52147cc777fa01b2ed802b4

Dpkg::Vendor::Ubuntu: Handle LTO by default using new feature overrides

scripts/Dpkg/Vendor/Ubuntu.pm
@@ -24,6 +24,8 @@ use warnings;
 
 our $VERSION = '0.01';
 
+use List::Util qw(any);
+
 use Dpkg::ErrorHandling;
 use Dpkg::Gettext;
 use Dpkg::Control::Types;
@@ -114,6 +116,10 @@ sub set_build_features {
 
     require Dpkg::Arch;
     my $arch = Dpkg::Arch::get_host_arch();
+
+    if (any { $_ eq $arch } qw(amd64 arm64 ppc64el s390x)) {
+        $flags->set_feature('optimize', 'lto', 1);
+    }
 
     if ($arch eq 'ppc64el' && $flags->get_option_value('optimize-level') != 0) {
         $flags->set_option_value('optimize-level', 3);

scripts/t/Dpkg_BuildFlags_Ubuntu.t
@@ -16,7 +16,7 @@
 use strict;
 use warnings;
 
-use Test::More tests => 16;
+use Test::More tests => 18;
 
 BEGIN {
     use_ok('Dpkg::BuildFlags');
@@ -33,6 +33,15 @@ sub test_optflag
     }
 }
 
+sub test_ltoflag
+{
+    my $bf = shift;
+
+    # Test the LTO flags enabled by default on some arches.
+    ok($bf->get('LDFLAGS') =~ m/-flto=auto -ffat-lto-objects/,
+        "LDFLAGS contains LTO flags on $ENV{DEB_HOST_ARCH}");
+}
+
 my $bf;
 
 # Force loading the Dpkg::Vendor::Ubuntu module.
@@ -43,6 +52,7 @@ $ENV{DEB_HOST_ARCH} = 'amd64';
 $bf = Dpkg::BuildFlags->new();
 
 test_optflag($bf, '-O2');
+test_ltoflag($bf);
 
 # Test the overlaid Ubuntu-specific linker flag.
 ok($bf->get('LDFLAGS') =~ m/-Wl,-Bsymbolic-functions/,
@@ -53,5 +63,6 @@ $ENV{DEB_HOST_ARCH} = 'ppc64el';
 $bf = Dpkg::BuildFlags->new();
 
 test_optflag($bf, '-O3');
+test_ltoflag($bf);
 
 1;

commit c5e6822d598a34d93001450c282c33f22b18efde

Dpkg::Vendor::Ubuntu: Fix lto feature to honor DEB_BUILD_OPTIONS
The recently added lto handling for Ubuntu, did not take into account
that the DEB_BUILD_OPTIONS and DEB_BUILD_MAINT_OPTIONS environment
variables are parsed and their options applied just after setting the
defaults, and before applying any arch-specific mask.

We add a new init_build_features() internal method, which sits between
these two actions, so that the Ubuntu vendor module can modify the
defaults before any user or maintainer override are applied.

Fixes: commit df7627acfadca528e52147cc777fa01b2ed802b4
Fixes: https://bugs.launchpad.net/bugs/2002582

scripts/Dpkg/Vendor/Debian.pm
@@ -94,6 +94,10 @@ sub run_hook {
     }
 }
 
+sub init_build_features {
+    my ($self, $use_feature, $builtin_feature) = @_;
+}
+
 sub set_build_features {
     my ($self, $flags) = @_;
 
@@ -138,6 +142,8 @@ sub set_build_features {
             pie => 1,
         },
     );
+
+    $self->init_build_features(\%use_feature, \%builtin_feature);
 
     ## Setup
 

scripts/Dpkg/Vendor/Ubuntu.pm
@@ -109,6 +109,19 @@ sub run_hook {
 }
 
 # Override Debian default features.
+sub init_build_features {
+    my ($self, $use_feature, $builtin_feature) = @_;
+
+    $self->SUPER::init_build_features($use_feature, $builtin_feature);
+
+    require Dpkg::Arch;
+    my $arch = Dpkg::Arch::get_host_arch();
+
+    if (any { $_ eq $arch } qw(amd64 arm64 ppc64el s390x)) {
+        $use_feature->{optimize}{lto} = 1;
+    }
+}
+
 sub set_build_features {
     my ($self, $flags) = @_;
 
@@ -116,10 +129,6 @@ sub set_build_features {
 
     require Dpkg::Arch;
     my $arch = Dpkg::Arch::get_host_arch();
-
-    if (any { $_ eq $arch } qw(amd64 arm64 ppc64el s390x)) {
-        $flags->set_feature('optimize', 'lto', 1);
-    }
 
     if ($arch eq 'ppc64el' && $flags->get_option_value('optimize-level') != 0) {
         $flags->set_option_value('optimize-level', 3);