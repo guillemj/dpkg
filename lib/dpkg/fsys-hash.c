c317a33cf53abd5d82d0570df3a532f13c35530b
Guillem Jover <guillem@debian.org>
2023-02-13 23:47:06 +0100

    libdpkg: Move early return into the loop
    The only way the outer condition can trigger is if the inner condition
    makes it break out of the loop. Simplify by moving the early return
    inside the loop
    
    Warned-by: cppcheck

--- lib/dpkg/fsys-hash.c
+++ lib/dpkg/fsys-hash.c
@@ -89,11 +89,9 @@ fsys_hash_find_node(const char *name, enum fsys_hash_find_flags flags)
 			          (*pointerp)->name);
 
 		if (strcmp((*pointerp)->name + 1, name) == 0)
-			break;
+			return *pointerp;
 		pointerp = &(*pointerp)->next;
 	}
-	if (*pointerp)
-		return *pointerp;
 
 	if (flags & FHFF_NONE)
 		return NULL;
