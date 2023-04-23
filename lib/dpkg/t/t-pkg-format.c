guillemj/dpkg

commit 560574b55a543d3d0a0f4e8abf0ae42f46d2a2f1

libdpkg: Do not restrict source:* virtual fields to installed packages
Initially the code was not handling missing data properly. It got changed
in two ways, in commit 536a38582a2d92da84f82d850e22f581b00d2082 it was
first restricted to only operate on installed packages, which regressed
support for dpkg-deb show format, and then the code was made robust
against such missing data (which was really the only correct and necessary
part to the fix) in commit 9022f9e0ffc58425c6798ff62e797f4026a29fe9.

Fixes: commit 536a38582a2d92da84f82d850e22f581b00d2082
Closes: #1004372
Stable-Candidate: 1.20.x

lib/dpkg/pkg-format.c
@@ -333,9 +333,6 @@ virt_source_package(struct varbuf *vb,
 	const char *name;
 	size_t len;
 
-	if (pkg->status == PKG_STAT_NOTINSTALLED)
-		return;
-
 	name = pkgbin->source;
 	if (name == NULL)
 		name = pkg->set->name;
@@ -350,9 +347,6 @@ virt_source_version(struct varbuf *vb,
                     const struct pkginfo *pkg, const struct pkgbin *pkgbin,
                     enum fwriteflags flags, const struct fieldinfo *fip)
 {
-	if (pkg->status == PKG_STAT_NOTINSTALLED)
-		return;
-
 	varbuf_add_source_version(vb, pkg, pkgbin);
 }
 
@@ -362,9 +356,6 @@ virt_source_upstream_version(struct varbuf *vb,
                              enum fwriteflags flags, const struct fieldinfo *fip)
 {
 	struct dpkg_version version;
-
-	if (pkg->status == PKG_STAT_NOTINSTALLED)
-		return;
 
 	pkg_source_version(&version, pkg, pkgbin);
 

commit 2c4814f7cb676c335c6372284ac95b7fc986effb

libdpkg: Handle missing Version when formatting source:Upstream-Version
When dealing with virtual packages (such as the ones from Provides, or
for not-installed Recommends or Suggests), there might be no version
field at all, so we cannot assume we can use these when filling in the
version information in the fallback code handling the missing Source
field.

The commit that removed the check for not-installed packages missed that
this instance had not been protected, as the code in varbufversion() is
doing the correct thing, but unfortunately we are not using that because
we need to get at the upstream part, so we need to also check explicitly
for a non-NULL upstream part. This was causing a segfault.

Fixes: commit 560574b55a543d3d0a0f4e8abf0ae42f46d2a2f1
Stable-Candidates: 1.20.x 1.21.x
Reported-by: John Scott <jscott@posteo.net>

lib/dpkg/pkg-format.c
@@ -359,7 +359,8 @@ virt_source_upstream_version(struct varbuf *vb,
 
 	pkg_source_version(&version, pkg, pkgbin);
 
-	varbuf_add_str(vb, version.version);
+	if (version.version)
+		varbuf_add_str(vb, version.version);
 	varbuf_end_str(vb);
 }
 

lib/dpkg/t/t-pkg-format.c
@@ -44,6 +44,17 @@ prep_pkg(struct pkgset *set, struct pkginfo *pkg)
 }
 
 static void
+prep_virtpkg(struct pkgset *set, struct pkginfo *pkg)
+{
+	pkg_blank(pkg);
+
+	pkgset_blank(set);
+	pkgset_link_pkg(set, pkg);
+
+	set->name = "test-virt";
+}
+
+static void
 test_field(struct pkginfo *pkg, const char *fmt, const char *exp)
 {
 	struct pkg_format_node *head;
@@ -96,6 +107,12 @@ test_pkg_format_virtual_fields(void)
 
 	test_field(&pkg, "${binary:Summary}",
 	                 "short synopsis -- some package");
+
+	prep_virtpkg(&pkgset, &pkg);
+	test_field(&pkg, "${source:Package}_${source:Version}",
+	                 "test-virt_");
+	test_field(&pkg, "${source:Upstream-Version}",
+	                 "");
 }
 
 static void
@@ -116,7 +133,7 @@ test_pkg_format_virtual_fields_db_fsys(void)
 
 TEST_ENTRY(test)
 {
-	test_plan(20);
+	test_plan(24);
 
 	test_pkg_format_real_fields();
 	test_pkg_format_virtual_fields();