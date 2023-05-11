guillemj/dpkg

commit 309123b9e76a44cd8e394d87295e2d244d7e354c

scripts: Unpack function arguments
Unpack the arguments into variables to make the core more readable, and
avoid aliasing issues.

Warned-by: perlcritic
Fixes: Subroutines::RequireArgUnpacking

scripts/Dpkg/Source/Quilt.pm
@@ -271,8 +271,9 @@ sub get_series_file {
 }
 
 sub get_db_file {
-    my $self = shift;
-    return File::Spec->catfile($self->{dir}, '.pc', @_);
+    my ($self, $file) = @_;
+
+    return File::Spec->catfile($self->{dir}, '.pc', $file);
 }
 
 sub get_db_dir {
@@ -281,8 +282,9 @@ sub get_db_dir {
 }
 
 sub get_patch_file {
-    my $self = shift;
-    return File::Spec->catfile($self->{dir}, 'debian', 'patches', @_);
+    my ($self, $file) = @_;
+
+    return File::Spec->catfile($self->{dir}, 'debian', 'patches', $file);
 }
 
 sub get_patch_dir {

scripts/Dpkg/Compression/FileHandle.pm
@@ -209,9 +209,10 @@ sub READLINE {
 }
 
 sub OPEN {
-    my ($self) = shift;
-    if (scalar(@_) == 2) {
-	my ($mode, $filename) = @_;
+    my ($self, @args) = @_;
+
+    if (scalar @args == 2) {
+        my ($mode, $filename) = @args;
 	$self->set_filename($filename);
 	if ($mode eq '>') {
 	    $self->_open_for_write();
@@ -229,10 +230,10 @@ sub OPEN {
 }
 
 sub CLOSE {
-    my ($self) = shift;
+    my ($self, @args) = @_;
     my $ret = 1;
     if (defined *$self->{file}) {
-	$ret = *$self->{file}->close(@_) if *$self->{file}->opened();
+        $ret = *$self->{file}->close(@args) if *$self->{file}->opened();
     } else {
 	$ret = 0;
     }
@@ -241,34 +242,39 @@ sub CLOSE {
 }
 
 sub FILENO {
-    my ($self) = shift;
-    return *$self->{file}->fileno(@_) if defined *$self->{file};
+    my ($self, @args) = @_;
+
+    return *$self->{file}->fileno(@args) if defined *$self->{file};
     return;
 }
 
 sub EOF {
     # Since perl 5.12, an integer parameter is passed describing how the
     # function got called, just ignore it.
-    my ($self, $param) = (shift, shift);
-    return *$self->{file}->eof(@_) if defined *$self->{file};
+    my ($self, $param, @args) = @_;
+
+    return *$self->{file}->eof(@args) if defined *$self->{file};
     return 1;
 }
 
 sub SEEK {
-    my ($self) = shift;
-    return *$self->{file}->seek(@_) if defined *$self->{file};
+    my ($self, @args) = @_;
+
+    return *$self->{file}->seek(@args) if defined *$self->{file};
     return 0;
 }
 
 sub TELL {
-    my ($self) = shift;
-    return *$self->{file}->tell(@_) if defined *$self->{file};
+    my ($self, @args) = @_;
+
+    return *$self->{file}->tell(@args) if defined *$self->{file};
     return -1;
 }
 
 sub BINMODE {
-    my ($self) = shift;
-    return *$self->{file}->binmode(@_) if defined *$self->{file};
+    my ($self, @args) = @_;
+
+    return *$self->{file}->binmode(@args) if defined *$self->{file};
     return;
 }
 

scripts/Dpkg/Control/Changelog.pm
@@ -48,9 +48,9 @@ Create a new empty set of changelog related fields.
 =cut
 
 sub new {
-    my $this = shift;
+    my ($this, @args) = @_;
     my $class = ref($this) || $this;
-    my $self = Dpkg::Control->new(type => CTRL_CHANGELOG, @_);
+    my $self = Dpkg::Control->new(type => CTRL_CHANGELOG, @args);
     return bless $self, $class;
 }
 

scripts/Dpkg/Control/HashCore.pm
@@ -179,9 +179,9 @@ Prints an error message and dies on syntax parse errors.
 =cut
 
 sub parse_error {
-    my ($self, $file, $msg) = (shift, shift, shift);
+    my ($self, $file, $msg, @args) = @_;
 
-    $msg = sprintf($msg, @_) if (@_);
+    $msg = sprintf $msg, @args if @args;
     error(g_('syntax error in %s at line %d: %s'), $file, $., $msg);
 }
 

scripts/Dpkg/Control/HashCore/Tie.pm
@@ -64,10 +64,10 @@ Return a reference to a tied hash implementing storage of simple
 =cut
 
 sub new {
-    my $class = shift;
+    my ($class, @args) = @_;
     my $hash = {};
 
-    tie %{$hash}, $class, @_; ## no critic (Miscellanea::ProhibitTies)
+    tie %{$hash}, $class, @args; ## no critic (Miscellanea::ProhibitTies)
     return $hash;
 }
 

scripts/Dpkg/Deps/Multiple.pm
@@ -56,9 +56,9 @@ Creates a new object.
 =cut
 
 sub new {
-    my $this = shift;
+    my ($this, @deps) = @_;
     my $class = ref($this) || $this;
-    my $self = { list => [ @_ ] };
+    my $self = { list => [ @deps ] };
 
     bless $self, $class;
     return $self;
@@ -84,9 +84,9 @@ Adds new dependency objects at the end of the list.
 =cut
 
 sub add {
-    my $self = shift;
+    my ($self, @deps) = @_;
 
-    push @{$self->{list}}, @_;
+    push @{$self->{list}}, @deps;
 }
 
 =item $dep->get_deps()

scripts/Dpkg/ErrorHandling.pm
@@ -189,9 +189,9 @@ sub _typename_prefix
 
 sub report(@)
 {
-    my ($type, $msg) = (shift, shift);
+    my ($type, $msg, @args) = @_;
 
-    $msg = sprintf($msg, @_) if (@_);
+    $msg = sprintf $msg, @args if @args;
 
     my $progname = _progname_prefix();
     my $typename = _typename_prefix($type);
@@ -201,39 +201,51 @@ sub report(@)
 
 sub debug
 {
-    my $level = shift;
-    print report(REPORT_DEBUG, @_) if $level <= $debug_level;
+    my ($level, @args) = @_;
+
+    print report(REPORT_DEBUG, @args) if $level <= $debug_level;
 }
 
 sub info($;@)
 {
-    print { $info_fh } report(REPORT_INFO, @_) if not $quiet_warnings;
+    my @args = @_;
+
+    print { $info_fh } report(REPORT_INFO, @args) if not $quiet_warnings;
 }
 
 sub notice
 {
-    warn report(REPORT_NOTICE, @_) if not $quiet_warnings;
+    my @args = @_;
+
+    warn report(REPORT_NOTICE, @args) if not $quiet_warnings;
 }
 
 sub warning($;@)
 {
-    warn report(REPORT_WARN, @_) if not $quiet_warnings;
+    my @args = @_;
+
+    warn report(REPORT_WARN, @args) if not $quiet_warnings;
 }
 
 sub syserr($;@)
 {
-    my $msg = shift;
-    die report(REPORT_ERROR, "$msg: $!", @_);
+    my ($msg, @args) = @_;
+
+    die report(REPORT_ERROR, "$msg: $!", @args);
 }
 
 sub error($;@)
 {
-    die report(REPORT_ERROR, @_);
+    my @args = @_;
+
+    die report(REPORT_ERROR, @args);
 }
 
 sub errormsg($;@)
 {
-    print { *STDERR } report(REPORT_ERROR, @_);
+    my @args = @_;
+
+    print { *STDERR } report(REPORT_ERROR, @args);
 }
 
 sub printcmd
@@ -245,9 +257,9 @@ sub printcmd
 
 sub subprocerr(@)
 {
-    my ($p) = (shift);
+    my ($p, @args) = @_;
 
-    $p = sprintf($p, @_) if (@_);
+    $p = sprintf $p, @args if @args;
 
     require POSIX;
 
@@ -264,11 +276,11 @@ sub subprocerr(@)
 
 sub usageerr(@)
 {
-    my ($msg) = (shift);
+    my ($msg, @args) = @_;
 
     state $printforhelp = g_('Use --help for program usage information.');
 
-    $msg = sprintf($msg, @_) if (@_);
+    $msg = sprintf $msg, @args if @args;
     warn report(REPORT_ERROR, $msg);
     warn "\n$printforhelp\n";
     exit(2);

scripts/Dpkg/Source/Package.pm
@@ -621,9 +621,9 @@ sub before_build {
 }
 
 sub build {
-    my $self = shift;
+    my ($self, @args) = @_;
 
-    $self->do_build(@_);
+    $self->do_build(@args);
 }
 
 sub after_build {
@@ -652,9 +652,9 @@ sub add_file {
 }
 
 sub commit {
-    my $self = shift;
+    my ($self, @args) = @_;
 
-    $self->do_commit(@_);
+    $self->do_commit(@args);
 }
 
 sub do_commit {

commit 5678aa632dfd20ec83e1d0eacad75bb4a4ef44a2

Dpkg::Source::Quilt: Switch get_db_file() to be based off get_db_dir()
Do not use get_db() with no argument as a means to get the db directory,
instead reverse the implementation around and use get_db_dir() from
get_db_file() instead, which makes more sense, and fixes a perl warning
introduced when the code changed to properly unpack arguments.

Fixes: commit 309123b9e76a44cd8e394d87295e2d244d7e354c

scripts/Dpkg/Source/Quilt.pm
@@ -63,7 +63,7 @@ sub new {
 
 sub setup_db {
     my $self = shift;
-    my $db_dir = $self->get_db_file();
+    my $db_dir = $self->get_db_dir();
     if (not -d $db_dir) {
         mkdir $db_dir or syserr(g_('cannot mkdir %s'), $db_dir);
     }
@@ -271,12 +271,13 @@ sub get_series_file {
 sub get_db_file {
     my ($self, $file) = @_;
 
-    return File::Spec->catfile($self->{dir}, '.pc', $file);
+    return File::Spec->catfile($self->get_db_dir(), $file);
 }
 
 sub get_db_dir {
     my $self = shift;
-    return $self->get_db_file();
+
+    return File::Spec->catfile($self->{dir}, '.pc');
 }
 
 sub get_patch_file {