guillemj/dpkg

commit 59268c3f1098114c0dc27b2129882faa96897a1a

build: Add support for compiler sanitizer flags
Add a new configure option to detect and enable these compiler flags and
use them when running the unit and functional tests from the CI.

m4/dpkg-compiler.m4
@@ -139,6 +139,36 @@ AC_DEFUN([DPKG_COMPILER_WARNINGS], [
   ])
 ])
 
+# DPKG_CHECK_COMPILER_SANITIZER
+# -----------------------------
+# Check whether the compiler sanitizer options are supported.
+AC_DEFUN([DPKG_CHECK_COMPILER_SANITIZER], [
+  DPKG_CHECK_COMPILER_FLAG([-fsanitize=address])
+  DPKG_CHECK_COMPILER_FLAG([-fsanitize=leak])
+  DPKG_CHECK_COMPILER_FLAG([-fsanitize=undefined])
+])
+
+# DPKG_COMPILER_SANITIZER
+# -----------------------
+# Add configure option to enable compiler sanitizer support options.
+AC_DEFUN([DPKG_COMPILER_SANITIZER], [
+  AC_ARG_ENABLE([compiler-sanitizer],
+    [AS_HELP_STRING([--enable-compiler-sanitizer],
+      [Enable compiler sanitizer support])],
+    [], [enable_compiler_sanitizer=no])
+
+  AS_IF([test "x$enable_compiler_sanitizer" = "xyes"], [
+    DPKG_CHECK_COMPILER_SANITIZER
+    AC_LANG_PUSH([C++])
+    DPKG_CHECK_COMPILER_SANITIZER
+    AC_LANG_POP([C++])
+
+    LDFLAGS="$DPKG_COMPILER_CFLAGS $LDFLAGS"
+    CFLAGS="$DPKG_COMPILER_CFLAGS $CFLAGS"
+    CXXFLAGS="$DPKG_COMPILER_CXXFLAGS $CXXFLAGS"
+  ])
+])
+
 # DPKG_COMPILER_OPTIMIZATIONS
 # ---------------------------
 # Add configure option to disable optimizations.

.gitlab-ci.yml
@@ -21,7 +21,7 @@ unit-tests:
   stage: test
   script:
     - eatmydata apt -P pkg.dpkg.author-testing build-dep -qq -y .
-    - ./configure
+    - ./configure --enable-compiler-sanitizer
     - make authorcheck TESTSUITEFLAGS=--verbose TEST_PARALLEL=$(nproc)
 
 # Test whether the unit tests pass on a VPATH build.
@@ -44,7 +44,7 @@ shared-tests:
 func-tests:
   stage: test
   script:
-    - ./configure
+    - ./configure --enable-compiler-sanitizer
     - make
     - cd tests
     - echo "DPKG_BUILDTREE = $(realpath ..)" >>.pkg-tests.conf

configure.ac
@@ -220,6 +220,7 @@ DPKG_USE_DISK_PREALLOCATE
 # Checks for the build machinery.
 AC_DEFINE([LIBDPKG_VOLATILE_API], [1], [Acknowledge the volatility of the API.])
 DPKG_COMPILER_WARNINGS
+DPKG_COMPILER_SANITIZER
 DPKG_COMPILER_OPTIMIZATIONS
 DPKG_LINKER_OPTIMIZATIONS
 DPKG_ARCHITECTURE

scripts/t/dpkg_buildpackage.t
@@ -87,6 +87,9 @@ my $tmpl_rules = <<'TMPL_RULES';
 DI := debian/${binary-name-all}
 DA := debian/${binary-name-any}
 
+# fakeroot confuses ASAN link order check.
+export ASAN_OPTIONS = verify_asan_link_order=0
+
 clean:
 	rm -f debian/files
 	rm -rf $(DI) $(DA)

commit 36cfc2394b4363af68f0a0755e9330817320e2c4

build: Fix sanitizer compiler options selected
We should not pass -fsanitize=leak as that is already part of
-fsanitize=address. To get better stack traces we should pass
-fno-omit-frame-pointer.

Fixes: commit 59268c3f1098114c0dc27b2129882faa96897a1a
Changelog: silent

m4/dpkg-compiler.m4
@@ -146,8 +146,8 @@ AC_DEFUN([DPKG_COMPILER_WARNINGS], [
 # -----------------------------
 # Check whether the compiler sanitizer options are supported.
 AC_DEFUN([DPKG_CHECK_COMPILER_SANITIZER], [
+  DPKG_CHECK_COMPILER_FLAG([-fno-omit-frame-pointer])
   DPKG_CHECK_COMPILER_FLAG([-fsanitize=address])
-  DPKG_CHECK_COMPILER_FLAG([-fsanitize=leak])
   DPKG_CHECK_COMPILER_FLAG([-fsanitize=undefined])
 ])
 