guillemj/dpkg

commit db1de20385ff04579ad3cb421e3dcc62f1df13f4

src: Use DEBEXT instead of hard-coding ".deb"

src/split/main.c
@@ -87,7 +87,7 @@ usage(const char *const *argv)
 "      --root <directory>           Use <directory> instead of %s.\n"
 "  -S, --partsize <size>            In KiB, for -s (default is 450).\n"
 "  -o, --output <file>              Filename, for -j (default is\n"
-"                                     <package>_<version>_<arch>.deb).\n"
+"                                     <package>_<version>_<arch>" DEBEXT ").\n"
 "  -Q, --npquiet                    Be quiet when -a is not a part.\n"
 "      --msdos                      Generate 8.3 filenames.\n"
 "\n"), ADMINDIR, PARTSDIR, ADMINDIR, "/");

src/main/archives.c
@@ -1545,7 +1545,7 @@ archivefiles(const char *const *argv)
 
         /* Check if it looks like a .deb file. */
         nodename = treenode_get_pathname(node);
-        if (strcmp(nodename + strlen(nodename) - 4, ".deb") != 0)
+        if (strcmp(nodename + strlen(nodename) - 4, DEBEXT) != 0)
           continue;
 
         arglist = m_realloc(arglist, sizeof(char *) * (nfiles + 2));

src/split/split.c
@@ -199,11 +199,11 @@ mksplit(const char *file_src, const char *prefix, off_t maxpartsize,
 
 			refname = str_fmt("%dof%d", curpart, nparts);
 			prefix_max = max(8 - strlen(refname), 0);
-			varbuf_printf(&file_dst, "%s/%.*s%.8s.deb",
+			varbuf_printf(&file_dst, "%s/%.*s%.8s" DEBEXT,
 			              prefixdir, prefix_max, prefix, refname);
 			free(refname);
 		} else {
-			varbuf_printf(&file_dst, "%s.%dof%d.deb",
+			varbuf_printf(&file_dst, "%s.%dof%d" DEBEXT,
 			              prefix, curpart, nparts);
 		}
 

commit e8ce6340e7729278331faf7133c609466a27f002

dpkg-split: Revert switch to use DEBEXT for --help output
We cannot use string concatenation via a macro in strings marked for
translation that end up being parsed by xgettext, as the resulting
msgid strings end up truncated.

Restore the previous hardcoding, as using a format string makes the
string to translate harder to understand, and instead we'll probably
need to add a new macro to only contain the actual extension w/o the
dot and use it via a format string, and unify all other messages too.

Reported-by: Sven Joachim <svenjoac@gmx.de>
Fixes: commit db1de20385ff04579ad3cb421e3dcc62f1df13f4
Changelog: silent

src/split/main.c
@@ -87,7 +87,7 @@ usage(const char *const *argv)
 "      --root <directory>           Use <directory> instead of %s.\n"
 "  -S, --partsize <size>            In KiB, for -s (default is 450).\n"
 "  -o, --output <file>              Filename, for -j (default is\n"
-"                                     <package>_<version>_<arch>" DEBEXT ").\n"
+"                                     <package>_<version>_<arch>.deb).\n"
 "  -Q, --npquiet                    Be quiet when -a is not a part.\n"
 "      --msdos                      Generate 8.3 filenames.\n"
 "\n"), ADMINDIR, PARTSDIR, ADMINDIR, "/");