guillemj/dpkg

commit c03aeaa372a1507ec6835c8613388f84c56228d1

build: Check whether we need to linkg against libtinfo explicitly
Depending on the system, linking against the curses shared library
will automatically expose the tinfo symbols (either via a linker script
or similar). On some other system we need to explicitly link against
tinfo.

m4/dpkg-libs.m4
@@ -192,6 +192,14 @@ AC_DEFUN([DPKG_LIB_CURSES], [
   ], [
     _DPKG_CHECK_LIB_CURSES_NARROW()
   ])
+  dnl Check whether linking against the curses library also exposes the tinfo
+  dnl symbols, otherwise explicitly link against it.
+  dpkg_save_curses_LIBS=$LIBS
+  LIBS="$CURSES_LIBS"
+  AC_SEARCH_LIBS([tigetstr], [tinfo], [
+    CURSES_LIBS="${CURSES_LIBS:+$CURSES_LIBS }-ltinfo"
+  ])
+  LIBS=$dpkg_save_curses_LIBS
   AS_IF([test "x$have_curses_header" != "xyes"], [
     AC_MSG_FAILURE([curses header not found])
   ])

commit 8a60f64945e2d88149b1c112caeb0b60e62955d8

build: Only link against libtinfo if needed
If the check determines the symbol we look for is already available,
then we should not link against the libtinfo library unconditionally.
Instead move the assignment outside the AC_SEARCH_LIBS, and check
for whether the function is not available at all or is builtin, and
only if these do not apply link against the library autoconf has
determined to use.

Fixes: commit c03aeaa372a1507ec6835c8613388f84c56228d1
Changelog: silent

m4/dpkg-libs.m4
@@ -196,10 +196,12 @@ AC_DEFUN([DPKG_LIB_CURSES], [
   dnl symbols, otherwise explicitly link against it.
   dpkg_save_curses_LIBS=$LIBS
   LIBS="$CURSES_LIBS"
-  AC_SEARCH_LIBS([tigetstr], [tinfo], [
-    CURSES_LIBS="${CURSES_LIBS:+$CURSES_LIBS }-ltinfo"
-  ])
+  AC_SEARCH_LIBS([tigetstr], [tinfo])
   LIBS=$dpkg_save_curses_LIBS
+  AS_IF([test "x$ac_cv_search_tigetstr" != "xnone required" && \
+         test "x$ac_cv_search_tigetstr" != "xno"], [
+    CURSES_LIBS="${CURSES_LIBS:+$CURSES_LIBS }$ac_cv_search_tigetstr"
+  ])
   AS_IF([test "x$have_curses_header" != "xyes"], [
     AC_MSG_FAILURE([curses header not found])
   ])