guillemj/dpkg

commit 292655415c34db23e6d5014b6f71d0060f5811e4

dpkg-shlibdeps: On dpkg-build-api >= 1 require -l instead of LD_LIBRARY_PATH

scripts/dpkg-shlibdeps.pl
@@ -41,6 +41,7 @@ use Dpkg::Shlibs::Objdump;
 use Dpkg::Shlibs::SymbolFile;
 use Dpkg::Substvars;
 use Dpkg::Arch qw(get_host_arch);
+use Dpkg::BuildAPI qw(get_build_api);
 use Dpkg::Deps;
 use Dpkg::Control::Info;
 use Dpkg::Control::Fields;
@@ -76,6 +77,10 @@ my @pkg_dir_to_ignore = ();
 my $host_arch = get_host_arch();
 
 my (@pkg_shlibs, @pkg_symbols, @pkg_root_dirs);
+
+my $control = Dpkg::Control::Info->new();
+# Initialize build API level.
+get_build_api($control);
 
 my ($stdout, %exec);
 foreach (@ARGV) {
@@ -158,7 +163,6 @@ if (-d 'debian') {
     push @pkg_root_dirs, keys %uniq;
 }
 
-my $control = Dpkg::Control::Info->new();
 my $fields = $control->get_source();
 my $bd_value = deps_concat($fields->{'Build-Depends'}, $fields->{'Build-Depends-Arch'});
 my $build_deps = deps_parse($bd_value, build_dep => 1, reduce_restrictions => 1);

man/dpkg-build-api.pod
@@ -51,6 +51,11 @@ build-dependencies.
 
 =over
 
+=item *
+
+B<dpkg-shlibdeps> no longer uses the B<LD_LIBRARY_PATH> environment variable.
+The B<-l> option should be used instead.
+
 =back
 
 =item v0

scripts/Dpkg/Shlibs.pm
@@ -49,6 +49,7 @@ use File::Spec;
 use Dpkg::Gettext;
 use Dpkg::ErrorHandling;
 use Dpkg::Shlibs::Objdump;
+use Dpkg::BuildAPI qw(get_build_api);
 use Dpkg::Path qw(resolve_symlink canonpath);
 use Dpkg::Arch qw(get_build_arch get_host_arch :mappers);
 
@@ -112,10 +113,16 @@ sub setup_library_paths {
             my $realpath = Cwd::realpath($path);
             next unless defined $realpath;
             if ($realpath =~ m/^\Q$cwd\E/) {
-                warning(g_('deprecated use of LD_LIBRARY_PATH with private ' .
-                           'library directory which interferes with ' .
-                           'cross-building, please use -l option instead'));
+                if (get_build_api() >= 1) {
+                    error(g_('use -l option instead of LD_LIBRARY_PATH'));
+                } else {
+                    warning(g_('deprecated use of LD_LIBRARY_PATH with private ' .
+                               'library directory which interferes with ' .
+                               'cross-building, please use -l option instead'));
+                }
             }
+
+            next if get_build_api() >= 1;
 
             # XXX: This should be added to @custom_librarypaths, but as this
             # is deprecated we do not care as the code will go away.

scripts/dpkg-gensymbols.pl
@@ -24,6 +24,7 @@ use warnings;
 use Dpkg ();
 use Dpkg::Arch qw(get_host_arch);
 use Dpkg::Package;
+use Dpkg::BuildAPI qw(get_build_api);
 use Dpkg::Shlibs qw(get_library_paths);
 use Dpkg::Shlibs::Objdump;
 use Dpkg::Shlibs::SymbolFile;
@@ -156,8 +157,10 @@ if (not defined($sourceversion)) {
     my $changelog = changelog_parse();
     $sourceversion = $changelog->{'Version'};
 }
+my $control = Dpkg::Control::Info->new();
+# Initialize the build API level.
+get_build_api($control);
 if (not defined($oppackage)) {
-    my $control = Dpkg::Control::Info->new();
     my @packages = map { $_->{'Package'} } $control->get_packages();
     if (@packages == 0) {
 	error(g_('no package stanza found in control info'));

commit a4ed77d833c3e0f3699ca25c7376f24ecac4dd80

dpkg-shlibdeps: Do not require debian/control when parsing CLI options
Now that we are not registering the private library directories while
parsing the options, but instead we queue them for later. We can move
the parsing of debian/control, after the option parsing too, so that
we do not require it, which made the file to be required from the
current working directory even for options such as --version or --help.

Fixes: commit 292655415c34db23e6d5014b6f71d0060f5811e4
Closes: #1055536

scripts/dpkg-shlibdeps.pl
@@ -79,10 +79,6 @@ my $host_arch = get_host_arch();
 
 my (@pkg_shlibs, @pkg_symbols, @pkg_root_dirs);
 
-my $control = Dpkg::Control::Info->new();
-# Initialize build API level.
-get_build_api($control);
-
 my ($stdout, %exec);
 foreach (@ARGV) {
     if (m/^-T(.*)$/) {
@@ -163,6 +159,10 @@ if (-d 'debian') {
     my %uniq = map { guess_pkg_root_dir($_) => 1 } (@pkg_symbols, @pkg_shlibs);
     push @pkg_root_dirs, keys %uniq;
 }
+
+my $control = Dpkg::Control::Info->new();
+# Initialize build API level.
+get_build_api($control);
 
 foreach my $libdir (@priv_lib_dirs) {
     Dpkg::Shlibs::add_library_dir($libdir);