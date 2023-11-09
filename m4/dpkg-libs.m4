guillemj/dpkg

commit d7e7c430d1dd1b26666cf8f22989ebc110a4a2cf

libcompat: Remove local setexecfilecon() and require libselinux 2.3
The needed libselinux version was released in 2014, there's been enough
time for people to catch up.

Changelog: internal

m4/dpkg-libs.m4
@@ -103,7 +103,7 @@ AC_DEFUN([DPKG_LIB_SELINUX], [
     [AS_HELP_STRING([--with-libselinux],
       [use selinux library to set security contexts])],
     [], [with_libselinux=check])
-  SELINUX_MIN_VERSION=2.0.99
+  SELINUX_MIN_VERSION=2.3
   have_libselinux="no"
   AS_IF([test "x$with_libselinux" != "xno"], [
     PKG_CHECK_MODULES([SELINUX], [libselinux >= $SELINUX_MIN_VERSION], [
@@ -116,9 +116,8 @@ AC_DEFUN([DPKG_LIB_SELINUX], [
           AC_MSG_FAILURE([libselinux header not found])
         ])
       ])
-      AC_CHECK_LIB([selinux], [setexecfilecon], [
-        AC_DEFINE([HAVE_SETEXECFILECON], [1],
-          [Define to 1 if SELinux setexecfilecon is present])
+      AC_CHECK_LIB([selinux], [setexecfilecon], [], [
+        AC_MSG_FAILURE([libselinux does not support setexecfilecon()])
       ])
     ], [
       AS_IF([test "x$with_libselinux" != "xcheck"], [
@@ -127,8 +126,6 @@ AC_DEFUN([DPKG_LIB_SELINUX], [
     ])
   ])
   AM_CONDITIONAL([WITH_LIBSELINUX], [test "x$have_libselinux" = "xyes"])
-  AM_CONDITIONAL([HAVE_SETEXECFILECON],
-    [test "x$ac_cv_lib_selinux_setexecfilecon" = "xyes"])
 ])# DPKG_LIB_SELINUX
 
 # _DPKG_CHECK_LIB_CURSES_NARROW

lib/compat/Makefile.am
@@ -25,10 +25,6 @@ if HAVE_SYS_ERRLIST
 libcompat_test_la_SOURCES += strerror.c
 endif
 
-if WITH_LIBSELINUX
-libcompat_test_la_SOURCES += selinux.c
-endif
-
 libcompat_la_SOURCES = \
 	empty.c \
 	compat.h \
@@ -72,12 +68,6 @@ endif
 
 if !HAVE_STRSIGNAL
 libcompat_la_SOURCES += strsignal.c
-endif
-
-if WITH_LIBSELINUX
-if !HAVE_SETEXECFILECON
-libcompat_la_SOURCES += selinux.c
-endif
 endif
 
 if !HAVE_C99_SNPRINTF

lib/compat/compat.h
@@ -176,10 +176,6 @@ int alphasort(const void *a, const void *b);
 int unsetenv(const char *x);
 #endif
 
-#if TEST_LIBCOMPAT || !defined(HAVE_SETEXECFILECON)
-int setexecfilecon(const char *filename, const char *fallback_type);
-#endif
-
 #ifdef __cplusplus
 }
 #endif

lib/compat/selinux.c
@@ -1,92 +1,0 @@
-/*
- * libcompat - system compatibility library
- *
- * Based on code from libselinux, Public Domain.
- * Copyright © 2014 Guillem Jover <guillem@debian.org>
- *
- * This is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
-#include <config.h>
-
-#include <string.h>
-#include <stdlib.h>
-
-/* We need to disarm the libselinux declaration to avoid a redundant
- * declaration. */
-#if TEST_LIBCOMPAT
-#define setexecfilecon setexecfilecon_libselinux
-#endif
-#include <selinux/selinux.h>
-#include <selinux/context.h>
-#if TEST_LIBCOMPAT
-#undef setexecfilecon
-#endif
-
-#include "compat.h"
-
-int
-setexecfilecon(const char *filename, const char *fallback)
-{
-	int rc;
-	char *curcon = NULL, *newcon = NULL, *filecon = NULL;
-	security_class_t seclass;
-	context_t tmpcon = NULL;
-
-	if (is_selinux_enabled() < 1)
-		return 0;
-
-	rc = getcon(&curcon);
-	if (rc < 0)
-		goto out;
-
-	rc = getfilecon(filename, &filecon);
-	if (rc < 0)
-		goto out;
-
-	seclass = string_to_security_class("process");
-	if (seclass == 0)
-		goto out;
-
-	rc = security_compute_create(curcon, filecon, seclass, &newcon);
-	if (rc < 0)
-		goto out;
-
-	if (strcmp(curcon, newcon) == 0) {
-		/* No default transition, use fallback for now. */
-		rc = -1;
-		tmpcon = context_new(curcon);
-		if (tmpcon == NULL)
-			goto out;
-		if (context_type_set(tmpcon, fallback))
-			goto out;
-		freecon(newcon);
-		newcon = strdup(context_str(tmpcon));
-		if (newcon == NULL)
-			goto out;
-	}
-
-	rc = setexeccon(newcon);
-
-out:
-	if (rc < 0 && security_getenforce() == 0)
-		rc = 0;
-
-	context_free(tmpcon);
-	freecon(newcon);
-	freecon(curcon);
-	freecon(filecon);
-
-	return rc;
-}

commit db6cf585c8ab6ac2e03b8a2f6c961c07be584efa

build: Replace action-if-found with : for extra libselinux check
If we do not replace this argument with a no-op, then we get the library
added into LIBS, which means we overlink for things that are not really
using libselinux.

Fixes: commit d7e7c430d1dd1b26666cf8f22989ebc110a4a2cf

m4/dpkg-libs.m4
@@ -146,7 +146,7 @@ AC_DEFUN([DPKG_LIB_SELINUX], [
           AC_MSG_FAILURE([libselinux header not found])
         ])
       ])
-      AC_CHECK_LIB([selinux], [setexecfilecon], [], [
+      AC_CHECK_LIB([selinux], [setexecfilecon], [:], [
         AC_MSG_FAILURE([libselinux does not support setexecfilecon()])
       ])
     ], [