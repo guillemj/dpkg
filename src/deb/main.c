guillemj/dpkg

commit b499f2688349694d24c4a4e53d461deeae826f5c

dpkg-deb: Force deb format 0.939000 to use gzip compression
The format is defined as using this compression, force its use.

src/deb/main.c
@@ -305,6 +305,9 @@ int main(int argc, const char *const *argv) {
     badusage(_("unsupported deb format '%d.%d' with non-uniform compression"),
              deb_format.major, deb_format.minor);
 
+  if (deb_format.major == 0)
+    compress_params = compress_params_deb0;
+
   if (opt_uniform_compression &&
       (compress_params.type != COMPRESSOR_TYPE_NONE &&
        compress_params.type != COMPRESSOR_TYPE_GZIP &&

src/at/deb-format.at
@@ -32,7 +32,7 @@ cp $top_srcdir/ChangeLog.old pkg-old-templ/
 chmod 0644 pkg-old-templ/ChangeLog.old
 chmod -R u+w pkg-old-templ
 find pkg-old-templ | xargs touch -t 197001010100.00
-dpkg-deb --deb-format=0.939000 --root-owner-group -Zgzip -b pkg-old-templ >/dev/null
+dpkg-deb --deb-format=0.939000 --root-owner-group -b pkg-old-templ >/dev/null
 
 dpkg-deb --ctrl-tarfile pkg-old-templ.deb >ctrl.tar
 dpkg-deb --fsys-tarfile pkg-old-templ.deb >fsys.tar

commit de5e15f26100b379430a3d5071d199791ca37cf5

dpkg-deb: Fix up compressor parameters for default legacy format
When using the gzip command as a compressor backend the code ended up
passing «-c-1» because the default compression level was set to -1,
and we were not fixing it up.

Move the compressor fix up call after we have set compressor_params.

Fixes: commit b499f2688349694d24c4a4e53d461deeae826f5c

src/deb/main.c
@@ -298,15 +298,15 @@ int main(int argc, const char *const *argv) {
 
   if (!cipaction) badusage(_("need an action option"));
 
-  if (!compressor_check_params(&compress_params, &err))
-    badusage(_("invalid compressor parameters: %s"), err.str);
-
   if (!opt_uniform_compression && deb_format.major == 0)
     badusage(_("unsupported deb format '%d.%d' with non-uniform compression"),
              deb_format.major, deb_format.minor);
 
   if (deb_format.major == 0)
     compress_params = compress_params_deb0;
+
+  if (!compressor_check_params(&compress_params, &err))
+    badusage(_("invalid compressor parameters: %s"), err.str);
 
   if (opt_uniform_compression &&
       (compress_params.type != COMPRESSOR_TYPE_NONE &&