61f824cf17ca87db571f2754207726aae78986cd
Nicolas Boulenguez <nicolas@debian.org>
2024-04-09 05:31:16 +0200

    scripts/mk: Search once for parrallel= in DEB_BUILD_OPTIONS
    Previously each expansion of DEB_BUILD_OPTION_PARALLEL was parsing
    DEB_BUILD_OPTIONS.
    
    Signed-off-by: Guillem Jover <guillem@debian.org>

--- scripts/mk/buildopts.mk
+++ scripts/mk/buildopts.mk
@@ -4,12 +4,16 @@
 # Defines the following variables:
 #
 #   DEB_BUILD_OPTION_PARALLEL: the argument for the parallel=N option.
+#     $(DEB_BUILD_OPTIONS)                "parallel=2"  "parallel="  ""
+#     $(DEB_BUILD_OPTION_PARALLEL)        "2"           ""           unset
+#     $(DEB_BUILD_OPTION_PARALLEL:%=-j%)  "-j2"         ""           ""
 
 ifndef dpkg_buildopts_mk_included
 dpkg_buildopts_mk_included = yes
 
-ifneq (,$(filter parallel=%,$(DEB_BUILD_OPTIONS)))
-  DEB_BUILD_OPTION_PARALLEL = $(patsubst parallel=%,%,$(filter parallel=%,$(DEB_BUILD_OPTIONS)))
+dpkg_buildopts_parallel := $(filter parallel=%,$(DEB_BUILD_OPTIONS))
+ifdef dpkg_buildopts_parallel
+  DEB_BUILD_OPTION_PARALLEL = $(patsubst parallel=%,%,$(dpkg_buildopts_parallel))
 endif
 
 endif # dpkg_buildopts_mk_included
