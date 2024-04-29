guillemj/dpkg

commit e5c856f7368a71a6dbcb8fb1fd318f836a246f78

libdpkg: Add support for DPKG_NLS environment variable
This implements the DPKG_NLS environment variable support that all other
perl scripts already support, unifying the interface. This also covers
the case where a frontend might want to be translated but get
untranslated messages from dpkg itself.

lib/dpkg/i18n.c
@@ -21,6 +21,8 @@
 #include <config.h>
 #include <compat.h>
 
+#include <stdbool.h>
+
 #include <dpkg/i18n.h>
 
 #ifdef HAVE_USELOCALE
@@ -30,10 +32,27 @@
 static locale_t dpkg_C_locale;
 #endif
 
+static bool
+dpkg_use_nls(void)
+{
+	const char *env;
+
+	/* We mimic the behavior of the Dpkg::Gettext perl module. */
+	env = getenv("DPKG_NLS");
+	if (env == NULL)
+		return true;
+
+	if (strcmp(env, "0") == 0 || env[0] == '\0')
+		return false;
+
+	return true;
+}
+
 void
 dpkg_locales_init(const char *package)
 {
-	setlocale(LC_ALL, "");
+	if (dpkg_use_nls())
+		setlocale(LC_ALL, "");
 	bindtextdomain(package, LOCALEDIR);
 	textdomain(package);
 

man/dpkg-deb.pod
@@ -394,6 +394,12 @@ Sets the color mode (since dpkg 1.18.5).
 The currently accepted values are: B<auto> (default), B<always> and
 B<never>.
 
+=item B<DPKG_NLS>
+
+If set, it will be used to decide whether to activate Native Language Support,
+also known as internationalization (or i18n) support (since dpkg 1.22.7).
+The accepted values are: B<0> and B<1> (default).
+
 =item B<TMPDIR>
 
 If set, B<dpkg-deb> will use it as the directory in which to create

man/dpkg-divert.pod
@@ -202,6 +202,12 @@ Sets the color mode (since dpkg 1.18.5).
 The currently accepted values are: B<auto> (default), B<always> and
 B<never>.
 
+=item B<DPKG_NLS>
+
+If set, it will be used to decide whether to activate Native Language Support,
+also known as internationalization (or i18n) support (since dpkg 1.22.7).
+The accepted values are: B<0> and B<1> (default).
+
 =back
 
 =head1 FILES

man/dpkg-query.pod
@@ -544,6 +544,12 @@ Sets the color mode (since dpkg 1.18.5).
 The currently accepted values are: B<auto> (default), B<always> and
 B<never>.
 
+=item B<DPKG_NLS>
+
+If set, it will be used to decide whether to activate Native Language Support,
+also known as internationalization (or i18n) support (since dpkg 1.22.7).
+The accepted values are: B<0> and B<1> (default).
+
 =back
 
 =head2 Internal environment

man/dpkg-realpath.pod
@@ -82,6 +82,12 @@ specified, it will be used as the filesystem root directory.
 Sets the color mode.
 The currently accepted values are: B<auto> (default), B<always> and B<never>.
 
+=item B<DPKG_NLS>
+
+If set, it will be used to decide whether to activate Native Language Support,
+also known as internationalization (or i18n) support (since dpkg 1.22.7).
+The accepted values are: B<0> and B<1> (default).
+
 =back
 
 =head1 SEE ALSO

man/dpkg-split.pod
@@ -290,6 +290,12 @@ Sets the color mode (since dpkg 1.18.5).
 The currently accepted values are: B<auto> (default), B<always> and
 B<never>.
 
+=item B<DPKG_NLS>
+
+If set, it will be used to decide whether to activate Native Language Support,
+also known as internationalization (or i18n) support (since dpkg 1.22.7).
+The accepted values are: B<0> and B<1> (default).
+
 =item B<SOURCE_DATE_EPOCH>
 
 If set, it will be used as the timestamp (as seconds since the epoch) in

man/dpkg-statoverride.pod
@@ -221,6 +221,12 @@ Sets the color mode (since dpkg 1.18.5).
 The currently accepted values are: B<auto> (default), B<always> and
 B<never>.
 
+=item B<DPKG_NLS>
+
+If set, it will be used to decide whether to activate Native Language Support,
+also known as internationalization (or i18n) support (since dpkg 1.22.7).
+The accepted values are: B<0> and B<1> (default).
+
 =back
 
 =head1 FILES

man/dpkg-trigger.pod
@@ -157,6 +157,12 @@ Sets the color mode (since dpkg 1.18.5).
 The currently accepted values are: B<auto> (default), B<always> and
 B<never>.
 
+=item B<DPKG_NLS>
+
+If set, it will be used to decide whether to activate Native Language Support,
+also known as internationalization (or i18n) support (since dpkg 1.22.7).
+The accepted values are: B<0> and B<1> (default).
+
 =back
 
 =head1 SEE ALSO

commit 489c0f4b2fcee6169640328fa8188174f2e4888e

libdpkg: Add missing header includes
Fixes: commit e5c856f7368a71a6dbcb8fb1fd318f836a246f78

lib/dpkg/i18n.c
@@ -22,6 +22,8 @@
 #include <compat.h>
 
 #include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include <dpkg/i18n.h>
 