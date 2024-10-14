3af30041f14fe67f66505e6df2f45f4a80042f3e
Guillem Jover <guillem@debian.org>
2024-10-14 13:13:35 +0200

    libdpkg: Cache root user/group to UID/GID mapping to speed up unpacking
    On glibc systems, each time we need to resolve from a user/group name to
    their uid/gid, which happens for most unpacked files, we end up opening,
    parsing and closing the passwd and group databases. If we cache the UID
    mapping for the "root" user and group, even though on some systems the
    root group is called either "wheel" (on macOS) or "system" (on AIX), we
    should skip a bunch of repeated and unnecessary work for every unpack.

--- lib/dpkg/sysuser.c
+++ lib/dpkg/sysuser.c
@@ -21,14 +21,55 @@
 #include <config.h>
 #include <compat.h>
 
+#include <string.h>
+
 #include <dpkg/sysuser.h>
+
+/* Cache UID 0 called "root" in most systems.
+ * Cache GID 0 called "root" in most systems,
+ *   or "wheel" on macOS,
+ *   or "system" on AIX,
+ *   although we do not cache the non-"root" ones.
+ */
+
+static struct passwd *root_pw;
+static struct group *root_gr;
+
+/*
+ * Copy a sysuser struct.
+ *
+ * Note: We only copy the portable subset that we care about, the rest will
+ * be zeroed out.
+ */
+static struct passwd *
+dpkg_sysuser_copy(struct passwd *pw_old)
+{
+	struct passwd *pw_new;
+
+	pw_new = m_calloc(1, sizeof(*pw_new));
+	pw_new->pw_name = m_strdup(pw_old->pw_name);
+	pw_new->pw_uid = pw_old->pw_uid;
+	pw_new->pw_gid = pw_old->pw_gid;
+	pw_new->pw_gecos = m_strdup(pw_old->pw_gecos);
+	pw_new->pw_dir = m_strdup(pw_old->pw_dir);
+	pw_new->pw_shell = m_strdup(pw_old->pw_shell);
+
+	return pw_new;
+}
 
 struct passwd *
 dpkg_sysuser_from_name(const char *uname)
 {
 	struct passwd *pw;
+	bool is_root = strcmp(uname, "root") == 0;
+
+	if (is_root && root_pw != NULL)
+		return root_pw;
 
 	pw = getpwnam(uname);
+
+	if (is_root)
+		root_pw = dpkg_sysuser_copy(pw);
 
 	return pw;
 }
@@ -39,12 +80,45 @@ dpkg_sysuser_from_uid(uid_t uid)
 	return getpwuid(uid);
 }
 
+/*
+ * Copy a sysgroup struct.
+ *
+ * Note: We only copy the portable subset that we care about, the rest will
+ * be zeroed out.
+ */
+static struct group *
+dpkg_sysgroup_copy(struct group *gr_old)
+{
+	struct group *gr_new;
+	size_t n;
+
+	gr_new = m_calloc(1, sizeof(*gr_new));
+	gr_new->gr_name = m_strdup(gr_old->gr_name);
+	gr_new->gr_gid = gr_old->gr_gid;
+
+	for (n = 0; gr_old->gr_mem[n]; n++)
+		;
+
+	gr_new->gr_mem = m_calloc(n + 1, sizeof(char *));
+	for (n = 0; gr_old->gr_mem[n]; n++)
+		gr_new->gr_mem[n] = m_strdup(gr_old->gr_mem[n]);
+
+	return gr_new;
+}
+
 struct group *
 dpkg_sysgroup_from_name(const char *gname)
 {
 	struct group *gr;
+	bool is_root = strcmp(gname, "root") == 0;
+
+	if (is_root && root_gr != NULL)
+		return root_gr;
 
 	gr = getgrnam(gname);
+
+	if (is_root)
+		root_gr = dpkg_sysgroup_copy(gr);
 
 	return gr;
 }
