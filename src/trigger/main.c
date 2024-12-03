guillemj/dpkg

commit faebdfb31c2f354562c348760aa38539d5c948a4

src: Do not name option variables as no<thing>
This makes following the defaults, and code flow harder. Use the
positive form instead.

src/trigger/main.c
@@ -94,7 +94,7 @@ usage(const char *const *argv)
 	return 0;
 }
 
-static int opt_noact;
+static int opt_act;
 static int opt_await = 1;
 static const char *opt_bypackage;
 
@@ -194,7 +194,7 @@ do_trigger(const char *const *argv)
 	trigdef_set_methods(&tdm_add);
 
 	tduf = TDUF_NO_LOCK_OK;
-	if (!opt_noact)
+	if (opt_act)
 		tduf |= TDUF_WRITE | TDUF_WRITE_IF_EMPTY;
 	tdus = trigdef_update_start(tduf);
 	if (tdus >= 0) {
@@ -244,7 +244,7 @@ static const struct cmdinfo cmdinfos[] = {
 	{ "by-package",      'f', 1, NULL,       &opt_bypackage },
 	{ "await",           0,   0, &opt_await, NULL,       NULL, 1 },
 	{ "no-await",        0,   0, &opt_await, NULL,       NULL, 0 },
-	{ "no-act",          0,   0, &opt_noact, NULL,       NULL, 1 },
+	{ "no-act",          0,   0, &opt_act,   NULL,       NULL, 0 },
 	{  NULL  }
 };
 

src/deb/main.c
@@ -141,7 +141,7 @@ static const char printforhelp[] =
      "Type dpkg --help for help about installing and deinstalling packages.");
 
 int opt_debug = 0;
-int opt_nocheck = 0;
+int opt_check = 1;
 int opt_verbose = 0;
 int opt_root_owner_group = 0;
 int opt_uniform_compression = 1;
@@ -266,7 +266,7 @@ static const struct cmdinfo cmdinfos[]= {
   { "deb-format",    0,   1, NULL,           NULL,         set_deb_format   },
   { "debug",         'D', 0, &opt_debug,     NULL,         NULL,          1 },
   { "verbose",       'v', 0, &opt_verbose,   NULL,         NULL,          1 },
-  { "nocheck",       0,   0, &opt_nocheck,   NULL,         NULL,          1 },
+  { "nocheck",       0,   0, &opt_check,     NULL,         NULL,          0 },
   { "root-owner-group",    0, 0, &opt_root_owner_group,    NULL, NULL,    1 },
   { "threads-max",   0,   1, NULL,           NULL,         set_threads_max  },
   { "uniform-compression", 0, 0, &opt_uniform_compression, NULL, NULL,    1 },

src/main/main.c
@@ -193,9 +193,14 @@ static const char printforhelp[] = N_(
 "\n"
 "Options marked [*] produce a lot of output - pipe it through 'less' or 'more' !");
 
+int f_act = 1;
+int f_alsoselect = 1;
+int f_autodeconf = 0;
+int f_debsig = 1;
+int f_pending = 0;
+int f_recursive = 0;
 int f_robot = 0;
-int f_pending=0, f_recursive=0, f_alsoselect=1, f_skipsame=0, f_noact=0;
-int f_autodeconf=0, f_nodebsig=0;
+int f_skipsame = 0;
 int f_triggers = 0;
 
 int errabort = 50;
@@ -343,7 +348,7 @@ can_invoke_hooks(enum action action)
   if (!is_invoke_action(action))
     return false;
 
-  if (f_noact)
+  if (!f_act)
     return false;
 
   if (in_force(FORCE_NON_ROOT))
@@ -584,11 +589,11 @@ static const struct cmdinfo cmdinfos[]= {
   { "log",               0,   1, NULL,          &log_file, NULL,    0 },
   { "pending",           'a', 0, &f_pending,    NULL,      NULL,    1 },
   { "recursive",         'R', 0, &f_recursive,  NULL,      NULL,    1 },
-  { "no-act",            0,   0, &f_noact,      NULL,      NULL,    1 },
-  { "dry-run",           0,   0, &f_noact,      NULL,      NULL,    1 },
-  { "simulate",          0,   0, &f_noact,      NULL,      NULL,    1 },
+  { "no-act",            0,   0, &f_act,        NULL,      NULL,    0 },
+  { "dry-run",           0,   0, &f_act,        NULL,      NULL,    0 },
+  { "simulate",          0,   0, &f_act,        NULL,      NULL,    0 },
   { "no-pager",          0,   0, NULL,          NULL,      set_no_pager,  0 },
-  { "no-debsig",         0,   0, &f_nodebsig,   NULL,      NULL,    1 },
+  { "no-debsig",         0,   0, &f_debsig,     NULL,      NULL,    0 },
   /* Alias ('G') for --refuse. */
   {  NULL,               'G', 0, NULL,          NULL,      reset_force_option, FORCE_DOWNGRADE },
   { "selected-only",     'O', 0, &f_alsoselect, NULL,      NULL,    0 },

src/deb/build.c
@@ -169,7 +169,7 @@ file_treewalk_feed(const char *dir, int fd_out)
 
     nodename = str_fmt("./%s", virtname);
 
-    if (!opt_nocheck && strchr(nodename, '\n'))
+    if (opt_check && strchr(nodename, '\n'))
       ohshit(_("newline not allowed in pathname '%s'"), nodename);
 
     /* We need to reorder the files so we can make sure that symlinks
@@ -583,7 +583,7 @@ do_build(const char *const *argv)
   ctrldir = str_fmt("%s/%s", dir, BUILDCONTROLDIR);
 
   /* Perform some sanity checks on the to-be-build package. */
-  if (opt_nocheck) {
+  if (!opt_check) {
     if (debar == NULL)
       ohshit(_("target is directory - cannot skip control file check"));
     warning(_("not checking contents of control area"));

src/deb/dpkg-deb.h
@@ -40,7 +40,7 @@ extern int opt_verbose;
 extern int opt_root_owner_group;
 extern int opt_uniform_compression;
 extern int opt_debug;
-extern int opt_nocheck;
+extern int opt_check;
 extern const char *opt_showformat;
 
 extern struct deb_version deb_format;

src/main/archives.c
@@ -1498,7 +1498,7 @@ archivefiles(const char *const *argv)
 
   trigproc_install_hooks();
 
-  if (f_noact)
+  if (!f_act)
     msdbflags = msdbrw_readonly;
   else if (cipaction->arg_int == act_avail)
     msdbflags = msdbrw_readonly | msdbrw_available_write;

src/main/configure.c
@@ -626,7 +626,7 @@ deferred_configure(struct pkginfo *pkg)
 
 	trig_activate_packageprocessing(pkg);
 
-	if (f_noact) {
+	if (!f_act) {
 		pkg_set_status(pkg, PKG_STAT_INSTALLED);
 		ensure_package_clientdata(pkg);
 		pkg->clientdata->istobe = PKG_ISTOBE_NORMAL;

src/main/main.h
@@ -71,9 +71,14 @@ struct perpackagestate {
 
 extern const char *const statusstrings[];
 
+extern int f_act;
+extern int f_alsoselect;
+extern int f_autodeconf;
+extern int f_debsig;
+extern int f_pending;
+extern int f_recursive;
 extern int f_robot;
-extern int f_pending, f_recursive, f_alsoselect, f_skipsame, f_noact;
-extern int f_autodeconf, f_nodebsig;
+extern int f_skipsame;
 extern int f_triggers;
 
 extern bool abort_processing;

commit d3195b7f618ce0aa8c6b09178c6175460742c22d

dpkg-trigger: Initialize opt_act to 1
After inverting the sense of the boolean variable, we should have
initialized it to 1, from the previous implicit 0, otherwise we default
to dry-run mode.

Fixes: commit faebdfb31c2f354562c348760aa38539d5c948a4
Changelog: silent

src/trigger/main.c
@@ -94,7 +94,7 @@ usage(const char *const *argv)
 	return 0;
 }
 
-static int opt_act;
+static int opt_act = 1;
 static int opt_await = 1;
 static const char *opt_bypackage;
 