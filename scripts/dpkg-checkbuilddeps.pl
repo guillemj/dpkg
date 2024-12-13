04e4a8129e62a058951f53ff5c349071bab96c95
Guillem Jover <guillem@debian.org>
2024-12-13 04:13:15 +0100

    dpkg-checkbuilddeps: Improve unmet dependency error messages
    Lower case them to conform to the overall project error messages style,
    and expand the error for build conflicts to also mention these are
    unmet.

--- scripts/dpkg-checkbuilddeps.pl
+++ scripts/dpkg-checkbuilddeps.pl
@@ -138,11 +138,11 @@ if ($bc_value) {
 }
 
 if (@unmet) {
-    errormsg(g_('Unmet build dependencies: %s'),
+    errormsg(g_('unmet build dependencies: %s'),
              join(' ', map { $_->output() } @unmet));
 }
 if (@conflicts) {
-    errormsg(g_('Build conflicts: %s'),
+    errormsg(g_('unmet build conflicts: %s'),
              join(' ', map { $_->output() } @conflicts));
 }
 exit 1 if @unmet || @conflicts;
