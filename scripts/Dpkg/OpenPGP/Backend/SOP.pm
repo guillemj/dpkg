guillemj/dpkg

commit 589485b1242da5debca487307759783511456d9b

Dpkg::OpenPGP::Backend::SOP: Add gosop as an alternative SOP implementation
The new version fixes the remaining issue that allows the code to use it
as a backend.

scripts/Dpkg/OpenPGP/Backend/SOP.pm
@@ -56,7 +56,7 @@ use parent qw(Dpkg::OpenPGP::Backend);
 #   Ref: https://gitlab.com/dkg/openpgp-stateless-cli/-/issues/42
 
 sub DEFAULT_CMD {
-    return [ qw(sqop rsop pgpainless-cli) ];
+    return [ qw(sqop rsop gosop pgpainless-cli) ];
 }
 
 sub _sop_exec

debian/control
@@ -122,8 +122,8 @@ Recommends:
  build-essential,
  gcc | c-compiler,
  fakeroot,
- gnupg | sq | sqop | rsop | pgpainless-cli | gpg-sq,
- gpgv | sq | sqop | rsop | pgpainless-cli | gpgv-sq,
+ gnupg | sq | sqop | rsop | gosop | pgpainless-cli | gpg-sq,
+ gpgv | sq | sqop | rsop | gosop | pgpainless-cli | gpgv-sq,
 # Used by dpkg-mergechangelogs.
  libalgorithm-merge-perl,
 Suggests:
@@ -137,6 +137,8 @@ Breaks:
  sqop (<< 0.27.2~),
 # Uses required SOP features, w/o requiring a hard dependency on rsop.
  rsop (<< 0.4.0~),
+# Uses required SOP features, w/o requiring a hard dependency on gosop.
+ gosop (<< 1.1.0~),
 # Uses required SOP features, w/o requiring a hard dependency on pgpainless.
  pgpainless-cli (<< 1.3.13~),
  libsop-java-java (<< 4.0.7~),
@@ -170,8 +172,8 @@ Recommends:
  xz-utils (>= 5.4.0),
 Suggests:
  debian-keyring,
- gnupg | sq | sqop | rsop | pgpainless-cli | gpg-sq,
- gpgv | sq | sqop | rsop | pgpainless-cli | gpgv-sq,
+ gnupg | sq | sqop | rsop | gosop | pgpainless-cli | gpg-sq,
+ gpgv | sq | sqop | rsop | gosop | pgpainless-cli | gpgv-sq,
  gcc | c-compiler,
  binutils,
  patch,
@@ -194,6 +196,8 @@ Breaks:
  sqop (<< 0.27.2~),
 # Uses required SOP features, w/o requiring a hard dependency on rsop.
  rsop (<< 0.4.0~),
+# Uses required SOP features, w/o requiring a hard dependency on gosop.
+ gosop (<< 1.1.0~),
 # Uses required SOP features, w/o requiring a hard dependency on pgpainless.
  pgpainless-cli (<< 1.3.13~),
  libsop-java-java (<< 4.0.7~),

scripts/Test/Dpkg.pm
@@ -214,6 +214,9 @@ my %openpgp_command = (
     'rsop' => {
         backend => 'sop',
     },
+    'gosop' => {
+        backend => 'sop',
+    },
     'pgpainless-cli' => {
         backend => 'sop',
     },

commit 310da86e14d6c1659a808c3fc6299a6ecef93f4e

Dpkg::OpenPGP::Backend: Remove references to fixed gosop issues
These were blockers for the gosop inclusion, but were already fixed
when the gosop support was added here. Remove the references.

Fixes: commit 589485b1242da5debca487307759783511456d9b
Changelog: silent

scripts/Dpkg/OpenPGP/Backend/SOP.pm
@@ -42,10 +42,6 @@ use Dpkg::OpenPGP::ErrorCodes;
 
 use parent qw(Dpkg::OpenPGP::Backend);
 
-# - Once "gosop" implements inline-verify and inline-sign, add as alternative.
-#   Ref: https://github.com/ProtonMail/gosop/issues/6
-# - Once "gosop" can handle big keyrings.
-#   Ref: https://github.com/ProtonMail/gosop/issues/25
 # - Once "hop" implements the new SOP draft, add as alternative.
 #   Ref: https://salsa.debian.org/clint/hopenpgp-tools/-/issues/4
 # - Once the SOP MR !23 is finalized and merged, implement a way to select