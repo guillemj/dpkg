guillemj/dpkg

commit 80c5c9d43896eef795a988cb080ff31ed8776172

dpkg-buildpackage: Reword hint on unsatisfied dependencies
Guiding unsuspecting users to disable the dependency checks will in most
cases not do what people expect, when what they should be doing instead
is to install missing dependencies and remove conflicting ones.

Closes: #817955

scripts/dpkg-buildpackage.pl
@@ -676,7 +676,7 @@ if ($checkbuilddep) {
         subprocerr('dpkg-checkbuilddeps');
     } elsif (WEXITSTATUS($?)) {
 	warning(g_('build dependencies/conflicts unsatisfied; aborting'));
-        hint(g_('use -d flag to override'));
+        hint(g_('satify build dependencies with your package manager frontend'));
 	exit 3;
     }
 }

commit c6464c6fce3f36cf5f1361b0a9848c523439d727

dpkg-buildpackage: Fix typo in hint
Fixes: commit 80c5c9d43896eef795a988cb080ff31ed8776172
Spotted-by: Matthias Urlichs <matthias@urlichs.de>
Changelog: silent

scripts/dpkg-buildpackage.pl
@@ -676,7 +676,7 @@ if ($checkbuilddep) {
         subprocerr('dpkg-checkbuilddeps');
     } elsif (WEXITSTATUS($?)) {
         errormsg(g_('build dependencies/conflicts unsatisfied; aborting'));
-        hint(g_('satify build dependencies with your package manager frontend'));
+        hint(g_('satisfy build dependencies with your package manager frontend'));
 	exit 3;
     }
 }