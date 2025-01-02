guillemj/dpkg

commit 5ab30303e4d1aa99aaef75790321bdce65a9505c

dselect: Switch to use varbufs instead of sprintf in keybindings
Warned-by: gcc on OpenBSD and macOS
Changelog: internal

dselect/bindings.cc
@@ -71,16 +71,21 @@ keybindings::bind(int key, const char *action)
   return true;
 }
 
-const char *keybindings::find(const char *action) {
+varbuf
+keybindings::find(const char *action) {
   binding *b = bindings;
   while (b && strcmp(action, b->interp->action))
     b = b->next;
-  if (!b) return _("[not bound]");
+  if (!b)
+    return varbuf(_("[not bound]"));
+
   const char *n= key2name(b->key);
-  if (n) return n;
-  static char buf[50];
-  sprintf(buf,_("[unk: %d]"),b->key);
-  return buf;
+  if (n) return
+    varbuf(n);
+
+  varbuf unknown;
+  unknown.set_fmt(_("[unk: %d]"), b->key);
+  return unknown;
 }
 
 const keybindings::interpretation *keybindings::operator()(int key) {

dselect/baselist.cc
@@ -336,11 +336,11 @@ void baselist::refreshinfo() {
       wprintw(whatinfowin,_("  -- %d%%, press "),
               (infotopofscreen + info_height) * 100 / infolines);
       if (infotopofscreen + info_height < infolines) {
-        wprintw(whatinfowin,_("%s for more"), bindings->find("iscrollon"));
+        wprintw(whatinfowin, _("%s for more"), bindings->find("iscrollon").str());
         if (infotopofscreen) waddstr(whatinfowin, ", ");
       }
       if (infotopofscreen)
-        wprintw(whatinfowin, _("%s to go back"),bindings->find("iscrollback"));
+        wprintw(whatinfowin, _("%s to go back"), bindings->find("iscrollback").str());
       waddch(whatinfowin,'.');
     }
     wnoutrefresh(whatinfowin);

dselect/bindings.h
@@ -62,7 +62,7 @@ struct keybindings {
   bool bind(const char *name, const char *action)
   { return bind(name2key(name), action); }
   const interpretation *operator()(int key);
-  const char *find(const char *action);
+  varbuf find(const char *action);
 
   void describestart() { iterate=descriptions; }
   const char **describenext();

commit 32feead3450bc3d1f3b2cbd343fa87c75aeca702

dselect: Fix formatting for return in keybindings::find()
Fixes: commit 5ab30303e4d1aa99aaef75790321bdce65a9505c
Changelog: internal

dselect/bindings.cc
@@ -80,8 +80,8 @@ keybindings::find(const char *action) {
     return varbuf(_("[not bound]"));
 
   const char *n= key2name(b->key);
-  if (n) return
-    varbuf(n);
+  if (n)
+    return varbuf(n);
 
   varbuf unknown;
   unknown.set_fmt(_("[unk: %d]"), b->key);