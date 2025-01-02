guillemj/dpkg

commit c11a69207d031b7d2260dd0108b91b27b22dea1e

dselect: Switch to use varbufs instead of sprintf for searchstring
Warned-by: gcc on OpenBSD and macOS
Changelog: internal

dselect/basecmds.cc
@@ -83,12 +83,15 @@ void baselist::kd_redraw() {
 }
 
 void baselist::kd_searchagain() {
-  if (!searchstring[0]) { beep(); return; }
+  if (searchstring.len()) {
+    beep();
+    return;
+  }
   dosearch();
 }
 
 bool
-baselist::checksearch(char *str)
+baselist::checksearch(varbuf &str)
 {
   return true;
 }
@@ -96,42 +99,42 @@ baselist::checksearch(char *str)
 bool
 baselist::matchsearch(int index)
 {
-  int lendiff, searchlen, i;
+  int lendiff, i;
   const char *name;
 
   name = itemname(index);
   if (!name)
     return false;	/* Skip things without a name (separators). */
 
-  searchlen=strlen(searchstring);
-  lendiff = strlen(name) - searchlen;
+  lendiff = strlen(name) - searchstring.len();
   for (i=0; i<=lendiff; i++)
-    if (strncasecmp(name + i, searchstring, searchlen) == 0)
+    if (strncasecmp(name + i, searchstring.str(), searchstring.len()) == 0)
       return true;
 
   return false;
 }
 
 void baselist::kd_search() {
-  char newsearchstring[128];
-  strcpy(newsearchstring,searchstring);
+  varbuf newsearchstring(searchstring);
+
   werase(querywin);
   mvwaddstr(querywin,0,0, _("Search for ? "));
   echo();
-  if (wgetnstr(querywin,newsearchstring,sizeof(newsearchstring)-1) == ERR)
-    searchstring[0]= 0;
+  if (wgetnstr(querywin, newsearchstring.buf, newsearchstring.size - 1) == ERR)
+    searchstring.reset();
   resize_window();
   noecho();
   if (whatinfo_height) { touchwin(whatinfowin); refreshinfo(); }
   else if (info_height) { touchwin(infopad); refreshinfo(); }
   else if (thisstate_height) redrawthisstate();
   else { touchwin(listpad); refreshlist(); }
-  if (newsearchstring[0]) {
+  if (newsearchstring.len()) {
     if (!checksearch(newsearchstring)) {
       beep();
       return;
     }
-    strcpy(searchstring,newsearchstring);
+
+    searchstring = newsearchstring;
     dosearch();
   } else
     kd_searchagain();

dselect/baselist.cc
@@ -280,7 +280,7 @@ baselist::baselist(keybindings *kb) {
   ldrawnend = 0;
   showinfo= 1;
 
-  searchstring[0]= 0;
+  searchstring.reset();
 }
 
 void baselist::itd_keys() {
@@ -308,7 +308,7 @@ void baselist::itd_keys() {
 void baselist::dosearch() {
   int offset, index;
   debug(dbg_general, "baselist[%p]::dosearch(); searchstring='%s'",
-        this, searchstring);
+        this, searchstring.str());
   for (offset = 1, index = max(topofscreen, cursorline + 1);
        offset<nitems;
        offset++, index++) {

dselect/dselect.h
@@ -104,7 +104,7 @@ protected:
   int topofscreen, leftofscreen, cursorline;
   int infotopofscreen, infolines;
   varbuf whatinfovb;
-  char searchstring[128];
+  varbuf searchstring;
 
   virtual void setheights();
   void unsizes();
@@ -131,7 +131,7 @@ protected:
   virtual const char *itemname(int index) =0;
   virtual const struct helpmenuentry *helpmenulist() =0;
 
-  virtual bool checksearch(char *str);
+  virtual bool checksearch(varbuf &str);
   virtual bool matchsearch(int index);
   void wordwrapinfo(int offset, const char *string);
 

dselect/pkglist.cc
@@ -486,7 +486,7 @@ perpackagestate::free(bool recursive)
 packagelist::~packagelist() {
   debug(dbg_general, "packagelist[%p]::~packagelist()", this);
 
-  if (searchstring[0])
+  if (searchstring.len())
     regfree(&searchfsm);
 
   discardheadings();
@@ -507,28 +507,28 @@ packagelist::~packagelist() {
 }
 
 bool
-packagelist::checksearch(char *rx)
+packagelist::checksearch(varbuf &rx)
 {
   int rc, opt = REG_NOSUB;
   int pos;
 
-  if (str_is_unset(rx))
+  if (rx.len() == 0)
     return false;
 
   searchdescr = false;
-  if (searchstring[0]) {
+  if (searchstring.len()) {
     regfree(&searchfsm);
-    searchstring[0]=0;
+    searchstring.reset();
   }
 
   /* look for search options */
-  for (pos = strlen(rx) - 1; pos >= 0; pos--)
+  for (pos = rx.len() - 1; pos >= 0; pos--)
     if ((rx[pos] == '/') && ((pos == 0) || (rx[pos - 1] != '\\')))
       break;
 
   if (pos >= 0) {
     rx[pos++] = '\0';
-    if (strcspn(rx + pos, "di") != 0) {
+    if (strcspn(rx.str() + pos, "di") != 0) {
       displayerror(_("invalid search option given"));
       return false;
     }
@@ -542,7 +542,7 @@ packagelist::checksearch(char *rx)
    }
   }
 
-  rc = regcomp(&searchfsm, rx, opt);
+  rc = regcomp(&searchfsm, rx.str(), opt);
   if (rc != 0) {
     displayerror(_("error in regular expression"));
     return false;

dselect/pkglist.h
@@ -166,7 +166,7 @@ protected:
   int deselect_one_of(pkginfo *er, pkginfo *ed, dependency *dep);
 
   // Define these virtuals
-  bool checksearch(char *str) override;
+  bool checksearch(varbuf &str) override;
   bool matchsearch(int index) override;
   void redraw1itemsel(int index, int selected) override;
   void redrawcolheads() override;

commit 78814a4dec2b0c974dc822ca39637c5920356ce0

dselect: Fix logic inversion in repeat search keybinding
This was making the repeat search not work at all.

Fixes: commit c11a69207d031b7d2260dd0108b91b27b22dea1e
Changelog: internal

dselect/basecmds.cc
@@ -85,7 +85,7 @@ void baselist::kd_redraw() {
 }
 
 void baselist::kd_searchagain() {
-  if (searchstring.len()) {
+  if (!searchstring.len()) {
     beep();
     return;
   }