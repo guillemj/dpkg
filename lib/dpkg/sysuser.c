guillemj/dpkg

commit 850bef3ef2817227ecea47370ee124f9f3bfcf82

libdpkg: Add support for sysuser/sysgroup specific to each chroot
When operating on a chroot via --root, we should always parse the
passwd and group files from within that chroot, and not from the
host system.

We now parse the passwd and group files explicitly, so that we can
use the pathname of our choosing. The defaults are /etc/passwd and
/etc/group which can be overridden with the new environment variables
DPKG_PATH_PASSWD and DPKG_PATH_GROUP, all relative to the --root
pathname. Although we currently hardcode these paths, which we
previously delegated to the libc, these are pretty standard locations
and this was not working for chroots anyway. We might eventually make
this selectable at configure time, but for now the defaults and the
environment variables should be good enough.

Closes: #1033051

lib/dpkg/sysuser.c
@@ -2,7 +2,7 @@
  * libdpkg - Debian packaging suite library routines
  * sysuser.c - system user and group handling
  *
- * Copyright © 2023 Guillem Jover <guillem@debian.org>
+ * Copyright © 2023-2025 Guillem Jover <guillem@debian.org>
  *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,9 +21,17 @@
 #include <config.h>
 #include <compat.h>
 
+#include <errno.h>
+#include <stdbool.h>
+#include <stdlib.h>
 #include <string.h>
 
+#include <dpkg/string.h>
+#include <dpkg/fsys.h>
 #include <dpkg/sysuser.h>
+
+#define DPKG_PATH_PASSWD "/etc/passwd"
+#define DPKG_PATH_GROUP "/etc/group"
 
 /* Cache UID 0 called "root" in most systems.
  * Cache GID 0 called "root" in most systems,
@@ -34,6 +42,37 @@
 
 static struct passwd *root_pw;
 static struct group *root_gr;
+
+static FILE *
+dpkg_sysdb_open(const char *envvar, const char *default_pathname)
+{
+	const char *pathname;
+	char *root_pathname;
+	FILE *fp;
+
+	pathname = getenv(envvar);
+	if (str_is_unset(pathname))
+		pathname = default_pathname;
+	root_pathname = dpkg_fsys_get_path(pathname);
+
+	fp = fopen(root_pathname, "r");
+	free(root_pathname);
+
+	if (fp == NULL) {
+		if (errno != ENOENT)
+			return NULL;
+
+		/*
+		 * If there was no such file, we assume we might be in a
+		 * bootstrapping scenario where the filesystem is empty,
+		 * and we try to fallback potentially to the non-chroot
+		 * file, which happens to be the historic behavior.
+		 */
+		fp = fopen(default_pathname, "r");
+	}
+
+	return fp;
+}
 
 /*
  * Copy a sysuser struct.
@@ -57,6 +96,31 @@ dpkg_sysuser_copy(struct passwd *pw_old)
 	return pw_new;
 }
 
+typedef bool dpkg_sysuser_match_func(struct passwd *pw, const void *match);
+
+static struct passwd *
+dpkg_sysuser_find(dpkg_sysuser_match_func *matcher, const void *match)
+{
+	FILE *fp;
+	struct passwd *pw;
+
+	fp = dpkg_sysdb_open("DPKG_PATH_PASSWD", DPKG_PATH_PASSWD);
+	while ((pw = fgetpwent(fp)))
+		if (matcher(pw, match))
+			break;
+	fclose(fp);
+
+	return pw;
+}
+
+static bool
+dpkg_sysuser_match_uname(struct passwd *pw, const void *match)
+{
+	const char *uname = match;
+
+	return strcmp(pw->pw_name, uname) == 0;
+}
+
 struct passwd *
 dpkg_sysuser_from_name(const char *uname)
 {
@@ -66,18 +130,26 @@ dpkg_sysuser_from_name(const char *uname)
 	if (is_root && root_pw != NULL)
 		return root_pw;
 
-	pw = getpwnam(uname);
+	pw = dpkg_sysuser_find(dpkg_sysuser_match_uname, uname);
 
-	if (is_root)
+	if (is_root && pw)
 		root_pw = dpkg_sysuser_copy(pw);
 
 	return pw;
 }
 
+static bool
+dpkg_sysuser_match_uid(struct passwd *pw, const void *match)
+{
+	const uid_t *uid = match;
+
+	return pw->pw_uid == *uid;
+}
+
 struct passwd *
 dpkg_sysuser_from_uid(uid_t uid)
 {
-	return getpwuid(uid);
+	return dpkg_sysuser_find(dpkg_sysuser_match_uid, &uid);
 }
 
 /*
@@ -106,6 +178,31 @@ dpkg_sysgroup_copy(struct group *gr_old)
 	return gr_new;
 }
 
+typedef bool dpkg_sysgroup_match_func(struct group *gr, const void *match);
+
+static struct group *
+dpkg_sysgroup_find(dpkg_sysgroup_match_func *matcher, const void *match)
+{
+	FILE *fp;
+	struct group *gr;
+
+	fp = dpkg_sysdb_open("DPKG_PATH_GROUP", DPKG_PATH_GROUP);
+	while ((gr = fgetgrent(fp)))
+		if (matcher(gr, match))
+			break;
+	fclose(fp);
+
+	return gr;
+}
+
+static bool
+dpkg_sysgroup_match_gname(struct group *gr, const void *match)
+{
+	const char *gname = match;
+
+	return strcmp(gr->gr_name, gname) == 0;
+}
+
 struct group *
 dpkg_sysgroup_from_name(const char *gname)
 {
@@ -115,16 +212,24 @@ dpkg_sysgroup_from_name(const char *gname)
 	if (is_root && root_gr != NULL)
 		return root_gr;
 
-	gr = getgrnam(gname);
+	gr = dpkg_sysgroup_find(dpkg_sysgroup_match_gname, gname);
 
-	if (is_root)
+	if (is_root && gr)
 		root_gr = dpkg_sysgroup_copy(gr);
 
 	return gr;
 }
 
+static bool
+dpkg_sysgroup_match_gid(struct group *gr, const void *match)
+{
+	const gid_t *gid = match;
+
+	return gr->gr_gid == *gid;
+}
+
 struct group *
 dpkg_sysgroup_from_gid(gid_t gid)
 {
-	return getgrgid(gid);
+	return dpkg_sysgroup_find(dpkg_sysgroup_match_gid, &gid);
 }

debian/copyright
@@ -32,7 +32,7 @@ Copyright:
  Copyright © 2004-2005, 2007-2010 Canonical Ltd.
  Copyright © 2004-2005 Scott James Remnant <scott@netsplit.com>
  Copyright © 2006-2008 Frank Lichtenheld <djpig@debian.org>
- Copyright © 2006-2024 Guillem Jover <guillem@debian.org>
+ Copyright © 2006-2025 Guillem Jover <guillem@debian.org>
  Copyright © 2006-2007 Nicolas François <nicolas.francois@centraliens.net>
  Copyright © 2007 Don Armstrong <don@donarmstrong.com>
  Copyright © 2007 Colin Watson <cjwatson@debian.org>

lib/dpkg/Makefile.am
@@ -234,6 +234,7 @@ test_programs = \
 	t/t-error \
 	t/t-string \
 	t/t-strvec \
+	t/t-sysuser \
 	t/t-file \
 	t/t-buffer \
 	t/t-meminfo \

lib/dpkg/t/.gitignore
@@ -35,6 +35,7 @@ t-progname
 t-string
 t-strvec
 t-subproc
+t-sysuser
 t-tar
 t-test
 t-test-skip

lib/dpkg/t/t-sysuser.c
@@ -1,0 +1,162 @@
+/*
+ * libdpkg - Debian packaging suite library routines
+ * t-sysuser.c - test sysuser handling code
+ *
+ * Copyright © 2025 Guillem Jover <guillem@debian.org>
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <config.h>
+#include <compat.h>
+
+#include <dpkg/test.h>
+#include <dpkg/file.h>
+#include <dpkg/sysuser.h>
+
+#define test_user(name, pass, uid, gid, gecos, dir, shell) \
+	do { \
+		test_fail(pw == NULL); \
+		if (pw == NULL) \
+			continue; \
+		test_str(pw->pw_name, ==, name); \
+		test_str(pw->pw_passwd, ==, pass); \
+		test_pass(pw->pw_uid == uid); \
+		test_pass(pw->pw_gid == gid); \
+		test_str(pw->pw_gecos, ==, gecos); \
+		test_str(pw->pw_dir, ==, dir); \
+		test_str(pw->pw_shell, ==, shell); \
+	} while (0)
+
+#define test_user_from_name(name, pass, uid, gid, gecos, dir, shell) \
+	pw = dpkg_sysuser_from_name(name); \
+	test_user(name, pass, uid, gid, gecos, dir, shell); \
+	/* EOM. */
+
+#define test_user_from_uid(name, pass, uid, gid, gecos, dir, shell) \
+	pw = dpkg_sysuser_from_uid(uid); \
+	test_user(name, pass, uid, gid, gecos, dir, shell); \
+	/* EOM. */
+
+static void
+test_sysuser(void)
+{
+	char *passwdfile, *passwdfile_abs;
+	struct passwd *pw;
+
+	passwdfile = test_data_file("sysuser/passwd.base");
+	passwdfile_abs = file_realpath(passwdfile);
+	setenv("DPKG_PATH_PASSWD", passwdfile_abs, 1);
+	free(passwdfile_abs);
+	free(passwdfile);
+
+	test_user_from_name("root", "x", 0, 0, "root", "/root", "/bin/sh");
+	test_user_from_name("daemon", "x", 1, 1, "daemon", "/usr/sbin", "/usr/sbin/nologin");
+	test_user_from_name("bin", "x", 2, 2, "bin", "/bin", "/usr/sbin/nologin");
+	test_user_from_name("sys", "x", 3, 3, "sys", "/dev", "/usr/sbin/nologin");
+	test_user_from_name("sync", "x", 4, 65534, "sync", "/bin", "/bin/sync");
+	test_user_from_name("_sysuser", "x", 100, 65534, "", "/nonexistent", "/usr/sbin/nologin");
+	test_user_from_name("someuser", "x", 1000, 1000, "Some User,,,", "/home/someuser", "/bin/bash");
+	test_user_from_name("otheruser", "x", 1001, 1001, "Other User,,,", "/home/otheruser", "/bin/sh");
+	pw = dpkg_sysuser_from_name("invented");
+	test_pass(pw == NULL);
+
+	test_user_from_uid("root", "x", 0, 0, "root", "/root", "/bin/sh");
+	test_user_from_uid("daemon", "x", 1, 1, "daemon", "/usr/sbin", "/usr/sbin/nologin");
+	test_user_from_uid("bin", "x", 2, 2, "bin", "/bin", "/usr/sbin/nologin");
+	test_user_from_uid("sys", "x", 3, 3, "sys", "/dev", "/usr/sbin/nologin");
+	test_user_from_uid("sync", "x", 4, 65534, "sync", "/bin", "/bin/sync");
+	test_user_from_uid("_sysuser", "x", 100, 65534, "", "/nonexistent", "/usr/sbin/nologin");
+	test_user_from_uid("someuser", "x", 1000, 1000, "Some User,,,", "/home/someuser", "/bin/bash");
+	test_user_from_uid("otheruser", "x", 1001, 1001, "Other User,,,", "/home/otheruser", "/bin/sh");
+	pw = dpkg_sysuser_from_uid(12345);
+	test_pass(pw == NULL);
+}
+
+#define test_group(name, pass, gid, mem) \
+	do { \
+		test_fail(gr == NULL); \
+		if (gr == NULL) \
+			continue; \
+		test_str(gr->gr_name, ==, name); \
+		test_str(gr->gr_passwd, ==, pass); \
+		test_pass(gr->gr_gid == gid); \
+	} while (0)
+
+#define test_group_from_name(name, pass, gid, mem) \
+	gr = dpkg_sysgroup_from_name(name); \
+	test_group(name, pass, gid, mem); \
+	/* EOM. */
+
+#define test_group_from_gid(name, pass, gid, mem) \
+	gr = dpkg_sysgroup_from_gid(gid); \
+	test_group(name, pass, gid, mem); \
+	/* EOM. */
+
+static void
+test_sysgroup(void)
+{
+	char *groupfile, *groupfile_abs;
+	struct group *gr;
+
+	groupfile = test_data_file("sysuser/group.base");
+	groupfile_abs = file_realpath(groupfile);
+	setenv("DPKG_PATH_GROUP", groupfile_abs, 1);
+	free(groupfile_abs);
+	free(groupfile);
+
+	test_group_from_name("root", "x", 0, NULL);
+	test_group_from_name("daemon", "x", 1, NULL);
+	test_group_from_name("bin", "x", 2, NULL);
+	test_group_from_name("sys", "x", 3, NULL);
+	test_group_from_name("adm", "x", 4, NULL);
+	test_group_from_name("users", "x", 100, NULL);
+	test_str(gr->gr_mem[0], ==, "someuser");
+	test_str(gr->gr_mem[1], ==, "otheruser");
+	test_pass(gr->gr_mem[2] == NULL);
+	test_group_from_name("someuser", "x", 1000, NULL);
+	test_str(gr->gr_mem[0], ==, "someuser");
+	test_pass(gr->gr_mem[1] == NULL);
+	test_group_from_name("otheruser", "x", 1001, NULL);
+	test_str(gr->gr_mem[0], ==, "otheruser");
+	test_pass(gr->gr_mem[1] == NULL);
+	gr = dpkg_sysgroup_from_name("invented");
+	test_pass(gr == NULL);
+
+	test_group_from_gid("root", "x", 0, NULL);
+	test_group_from_gid("daemon", "x", 1, NULL);
+	test_group_from_gid("bin", "x", 2, NULL);
+	test_group_from_gid("sys", "x", 3, NULL);
+	test_group_from_gid("adm", "x", 4, NULL);
+	test_group_from_gid("users", "x", 100, NULL);
+	test_str(gr->gr_mem[0], ==, "someuser");
+	test_str(gr->gr_mem[1], ==, "otheruser");
+	test_pass(gr->gr_mem[2] == NULL);
+	test_group_from_gid("someuser", "x", 1000, NULL);
+	test_str(gr->gr_mem[0], ==, "someuser");
+	test_pass(gr->gr_mem[1] == NULL);
+	test_group_from_gid("otheruser", "x", 1001, NULL);
+	test_str(gr->gr_mem[0], ==, "otheruser");
+	test_pass(gr->gr_mem[1] == NULL);
+	gr = dpkg_sysgroup_from_gid(12345);
+	test_pass(gr == NULL);
+}
+
+TEST_ENTRY(test)
+{
+	test_plan(210);
+
+	test_sysuser();
+	test_sysgroup();
+}

man/dpkg-deb.pod
@@ -408,6 +408,22 @@ If set, it will be used to decide whether to activate Native Language Support,
 also known as internationalization (or i18n) support (since dpkg 1.22.7).
 The accepted values are: B<0> and B<1> (default).
 
+=item B<DPKG_PATH_PASSWD>
+
+Sets the pathname for the system F<passwd> file.
+This path is relative to the B<--root> directory.
+The default is F</etc/passwd>.
+
+Since dpkg 1.22.16.
+
+=item B<DPKG_PATH_GROUP>
+
+Sets the pathname for the system F<group> file.
+This path is relative to the B<--root> directory.
+The default is F</etc/group>.
+
+Since dpkg 1.22.16.
+
 =item B<TMPDIR>
 
 If set, B<dpkg-deb> will use it as the directory in which to create

man/dpkg-statoverride.pod
@@ -227,6 +227,22 @@ If set, it will be used to decide whether to activate Native Language Support,
 also known as internationalization (or i18n) support (since dpkg 1.22.7).
 The accepted values are: B<0> and B<1> (default).
 
+=item B<DPKG_PATH_PASSWD>
+
+Sets the pathname for the system F<passwd> file.
+This path is relative to the B<--root> directory.
+The default is F</etc/passwd>.
+
+Since dpkg 1.22.16.
+
+=item B<DPKG_PATH_GROUP>
+
+Sets the pathname for the system F<group> file.
+This path is relative to the B<--root> directory.
+The default is F</etc/group>.
+
+Since dpkg 1.22.16.
+
 =back
 
 =head1 FILES

man/dpkg.pod
@@ -1397,6 +1397,22 @@ specified, it will be used as the B<dpkg> administrative directory
 Set by a package manager frontend to notify dpkg that it should not acquire
 the frontend lock (since dpkg 1.19.1).
 
+=item B<DPKG_PATH_PASSWD>
+
+Sets the pathname for the system F<passwd> file.
+This path is relative to the B<--root> directory.
+The default is F</etc/passwd>.
+
+Since dpkg 1.22.16.
+
+=item B<DPKG_PATH_GROUP>
+
+Sets the pathname for the system F<group> file.
+This path is relative to the B<--root> directory.
+The default is F</etc/group>.
+
+Since dpkg 1.22.16.
+
 =back
 
 =head2 Internal environment

commit 6f494470644fbedac6242de053af27d520d9928a

libdpkg: Do not crash on missing sysuser or sysgroup system database files
The check for a NULL out of the file open was lost during a last minute
refactoring. This causes dpkg to crash when the system does not have yet
a sysuser (by default /etc/passwd) or sysgroup (by default /etc/group)
database files on disk yet.

Fixes: commit 850bef3ef2817227ecea47370ee124f9f3bfcf82
Reported-by: Marc Haber <mh+debian-packages@zugschlus.de>

lib/dpkg/sysuser.c
@@ -105,6 +105,8 @@ dpkg_sysuser_find(dpkg_sysuser_match_func *matcher, const void *match)
 	struct passwd *pw;
 
 	fp = dpkg_sysdb_open("DPKG_PATH_PASSWD", DPKG_PATH_PASSWD);
+	if (fp == NULL)
+		return NULL;
 	while ((pw = fgetpwent(fp)))
 		if (matcher(pw, match))
 			break;
@@ -187,6 +189,8 @@ dpkg_sysgroup_find(dpkg_sysgroup_match_func *matcher, const void *match)
 	struct group *gr;
 
 	fp = dpkg_sysdb_open("DPKG_PATH_GROUP", DPKG_PATH_GROUP);
+	if (fp == NULL)
+		return NULL;
 	while ((gr = fgetgrent(fp)))
 		if (matcher(gr, match))
 			break;