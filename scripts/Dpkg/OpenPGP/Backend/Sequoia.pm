guillemj/dpkg

commit d231432f71925be6c6b31f47c397c6d2a8ac9f1d

Dpkg::OpenPGP::Backend::Sequoia: Add a hint about authorizing own keys
These would ideally be emitted by sq itself, but for now this hint is
better than nothing, and might direct users to a solution that might not
be obvious.

scripts/Dpkg/OpenPGP/Backend/Sequoia.pm
@@ -144,7 +144,13 @@ sub inline_sign
     push @opts, '--output', $inlinesigned;
 
     my $rc = $self->_sq_exec('sign', @opts, $data);
-    return OPENPGP_KEY_CANNOT_SIGN if $rc;
+    if ($rc) {
+        # XXX: Ideally sq would emit this kind of hint itself when it knows it
+        #      might apply, but for now this is probably better than nothing.
+        hint(g_('imported own keys might be missing ownership information, try:'));
+        hint(g_('  sq pki link authorize --cert FINGERPRINT --all --unconstrained'));
+        return OPENPGP_KEY_CANNOT_SIGN;
+    }
     return OPENPGP_OK;
 }
 

commit 613695f741bb06fcb16a906de506ac3cb1d3321d

Dpkg::OpenPGP::Backend::Sequoia: Add missing Dpkg::Gettext import
Closes: #1099866
Fixes: commit d231432f71925be6c6b31f47c397c6d2a8ac9f1d
Signed-off-by: Yuri Konotopov <ykonotopov@gnome.org>
Signed-off-by: Guillem Jover <guillem@debian.org>

scripts/Dpkg/OpenPGP/Backend/Sequoia.pm
@@ -36,6 +36,7 @@ use warnings;
 use POSIX qw(:sys_wait_h);
 
 use Dpkg::ErrorHandling;
+use Dpkg::Gettext;
 use Dpkg::IPC;
 use Dpkg::OpenPGP::ErrorCodes;
 