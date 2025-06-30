guillemj/dpkg

commit 1a06485a77983e2471ec761a0f9bf02a921a8761

dpkg-buildtree: Add new command is-rootless
This new command can be used to query whether the build tree requires
root to build or can be built with no root privileges.

This is useful for build frontends that might need to know whether they
have to install additional gain-root-command packages to be able to
build the sources.

Suggested-by: Niels Thykier <niels@thykier.net>

scripts/Dpkg/BuildTree.pm
@@ -103,6 +103,24 @@ sub clean {
     return;
 }
 
+=item $bt->needs_root()
+
+Check whether the build tree needs root to build.
+
+=cut
+
+sub needs_root {
+    my $self = shift;
+
+    require Dpkg::Control::Info;
+    require Dpkg::BuildDriver;
+
+    my $ctrl = Dpkg::Control::Info->new();
+    my $bd = Dpkg::BuildDriver->new(ctrl => $ctrl);
+
+    return $bd->need_build_task('build', 'binary');
+}
+
 =back
 
 =head1 CHANGES

scripts/Makefile.am
@@ -274,6 +274,7 @@ test_scripts = \
 	t/Dpkg_Dist_Files.t \
 	t/dpkg_source.t \
 	t/dpkg_buildpackage.t \
+	t/dpkg_buildtree.t \
 	t/dpkg_mergechangelogs.t \
 	t/mk.t \
 	# EOL
@@ -388,6 +389,11 @@ test_data = \
 	t/dpkg_buildpackage/test-source_0_full.hook \
 	t/dpkg_buildpackage/test-source_0_source.changes \
 	t/dpkg_buildpackage/test-source_0_source.hook \
+	t/dpkg_buildtree/src-build-api-v0/debian/control \
+	t/dpkg_buildtree/src-build-api-v1/debian/control \
+	t/dpkg_buildtree/src-rrr-binary-targets/debian/control \
+	t/dpkg_buildtree/src-rrr-missing/debian/control \
+	t/dpkg_buildtree/src-rrr-no/debian/control \
 	t/dpkg_mergechangelogs/ch-a \
 	t/dpkg_mergechangelogs/ch-b \
 	t/dpkg_mergechangelogs/ch-badver-a \

scripts/t/dpkg_buildtree.t
@@ -1,0 +1,71 @@
+#!/usr/bin/perl
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use strict;
+use warnings;
+
+use Test::More;
+use Test::Dpkg qw(:paths);
+
+use File::Spec::Functions qw(rel2abs);
+
+use Dpkg::IPC;
+use Dpkg::BuildTree;
+
+plan tests => 5;
+
+my $srcdir = rel2abs($ENV{srcdir} || '.');
+my $datadir = "$srcdir/t/dpkg_buildtree";
+
+$ENV{$_} = rel2abs($ENV{$_}) foreach qw(DPKG_DATADIR DPKG_ORIGINS_DIR);
+
+my %is_rootless = (
+    'src-build-api-v0' => 0,
+    'src-build-api-v1' => 1,
+    'src-rrr-binary-targets' => 0,
+    'src-rrr-missing' => 0,
+    'src-rrr-no' => 1,
+);
+
+sub test_is_rootless
+{
+    my $test = shift;
+    my $dirname = "$datadir/$test";
+
+    my $stderr;
+    my $res;
+
+    chdir $dirname;
+    spawn(
+        exec => [
+            $ENV{PERL}, "$srcdir/dpkg-buildtree.pl",
+            'is-rootless'
+        ],
+        error_to_string => \$stderr,
+        wait_child => 1,
+        nocheck => 1,
+    );
+    $res = $? == 0 ? 1 : 0;
+    chdir $datadir;
+
+    return $res;
+}
+
+foreach my $test (sort keys %is_rootless) {
+    my $exp = $is_rootless{$test};
+    my $res = test_is_rootless($test);
+
+    is($res, $exp, "dpkg-buildtree is-rootless on $test not $exp");
+}

scripts/t/dpkg_buildtree/src-build-api-v0/debian/control
@@ -1,0 +1,6 @@
+Source: source-build-api-v0
+Build-Depends:
+ dpkg-build-api (= 0),
+
+Package: binary
+Architecture: all

scripts/t/dpkg_buildtree/src-build-api-v1/debian/control
@@ -1,0 +1,6 @@
+Source: source-build-api-v1
+Build-Depends:
+ dpkg-build-api (= 1),
+
+Package: binary
+Architecture: all

scripts/t/dpkg_buildtree/src-rrr-binary-targets/debian/control
@@ -1,0 +1,5 @@
+Source: source-rrr-binary-targets
+Rules-Requires-Root: binary-targets
+
+Package: binary
+Architecture: all

scripts/t/dpkg_buildtree/src-rrr-missing/debian/control
@@ -1,0 +1,4 @@
+Source: source-rrr-missing
+
+Package: binary
+Architecture: all

scripts/t/dpkg_buildtree/src-rrr-no/debian/control
@@ -1,0 +1,5 @@
+Source: source-rrr-no
+Rules-Requires-Root: no
+
+Package: binary
+Architecture: all

commit bdf018b3448d9977e9dc1920d2a07ae5ccb23d04

Dpkg::BuildTree: Fix needs_root() for R³ with implementation specific keywords
The «dpkg-buildtree is-rootless» action was implemented on top of
existing logic used by the dpkg Perl modules, but those were only
concerned about dpkg specific keywords in R³.

According to the specification, any implementation specific keyword
implies that some form of root is necessary. And given that
«dpkg-buildtree is-rootless» is now being used for example by sbuild to
decide whether to install fakeroot, giving a wrong result can result in
build failures.

Ref: #1107971
Fixes: commit 1a06485a77983e2471ec761a0f9bf02a921a8761
Stable-Candidate: 1.22.x

scripts/Dpkg/BuildTree.pm
@@ -117,7 +117,8 @@ sub needs_root {
     my $ctrl = Dpkg::Control::Info->new();
     my $bd = Dpkg::BuildDriver->new(ctrl => $ctrl);
 
-    return $bd->need_build_task('build', 'binary');
+    return $bd->need_build_task('build', 'binary') ||
+           defined $ENV{DEB_GAIN_ROOT_CMD};
 }
 
 =back

scripts/Makefile.am
@@ -387,6 +387,11 @@ test_data = \
 	t/dpkg_buildtree/src-build-api-v0/debian/control \
 	t/dpkg_buildtree/src-build-api-v1/debian/control \
 	t/dpkg_buildtree/src-rrr-binary-targets/debian/control \
+	t/dpkg_buildtree/src-rrr-dpkg-target-binary/debian/control \
+	t/dpkg_buildtree/src-rrr-dpkg-target-custom/debian/control \
+	t/dpkg_buildtree/src-rrr-dpkg-target-subcommand/debian/control \
+	t/dpkg_buildtree/src-rrr-dpkg-unknown-keyword/debian/control \
+	t/dpkg_buildtree/src-rrr-impl-specific-keyword/debian/control \
 	t/dpkg_buildtree/src-rrr-missing/debian/control \
 	t/dpkg_buildtree/src-rrr-no/debian/control \
 	t/dpkg_mergechangelogs/ch-a \

scripts/t/dpkg_buildtree.t
@@ -18,12 +18,13 @@ use v5.36;
 use Test::More;
 use Test::Dpkg qw(:paths);
 
+use POSIX;
 use File::Spec::Functions qw(rel2abs);
 
 use Dpkg::IPC;
 use Dpkg::BuildTree;
 
-plan tests => 5;
+plan tests => 10;
 
 my $srcdir = rel2abs($ENV{srcdir} || '.');
 my $datadir = "$srcdir/t/dpkg_buildtree";
@@ -34,6 +35,11 @@ my %is_rootless = (
     'src-build-api-v0' => 1,
     'src-build-api-v1' => 1,
     'src-rrr-binary-targets' => 0,
+    'src-rrr-dpkg-target-binary' => 255,
+    'src-rrr-dpkg-target-custom' => 0,
+    'src-rrr-dpkg-target-subcommand' => 0,
+    'src-rrr-dpkg-unknown-keyword' => 255,
+    'src-rrr-impl-specific-keyword' => 0,
     'src-rrr-missing' => 1,
     'src-rrr-no' => 1,
 );
@@ -56,7 +62,11 @@ sub test_is_rootless
         wait_child => 1,
         nocheck => 1,
     );
-    $res = $? == 0 ? 1 : 0;
+    if (POSIX::WIFEXITED($?) && POSIX::WEXITSTATUS($?) == 255) {
+        $res = 255;
+    } else {
+        $res = $? == 0 ? 1 : 0;
+    }
     chdir $datadir;
 
     return $res;
@@ -66,5 +76,5 @@ foreach my $test (sort keys %is_rootless) {
     my $exp = $is_rootless{$test};
     my $res = test_is_rootless($test);
 
-    is($res, $exp, "dpkg-buildtree is-rootless on $test not $exp");
+    is($res, $exp, "dpkg-buildtree is-rootless on $test $res == $exp");
 }

scripts/t/dpkg_buildtree/src-rrr-dpkg-target-binary/debian/control
@@ -1,0 +1,5 @@
+Source: source-rrr-dpkg-target-binary
+Rules-Requires-Root: dpkg/target/binary
+
+Package: binary
+Architecture: all

scripts/t/dpkg_buildtree/src-rrr-dpkg-target-custom/debian/control
@@ -1,0 +1,5 @@
+Source: source-rrr-dpkg-target-custom
+Rules-Requires-Root: dpkg/target/custom
+
+Package: binary
+Architecture: all

scripts/t/dpkg_buildtree/src-rrr-dpkg-target-subcommand/debian/control
@@ -1,0 +1,5 @@
+Source: source-rrr-dpkg-target-subcommand
+Rules-Requires-Root: dpkg/target-subcommand
+
+Package: binary
+Architecture: all

scripts/t/dpkg_buildtree/src-rrr-dpkg-unknown-keyword/debian/control
@@ -1,0 +1,5 @@
+Source: source-rrr-dpkg-unknown-keyword
+Rules-Requires-Root: dpkg/unknown-keyword
+
+Package: binary
+Architecture: all

scripts/t/dpkg_buildtree/src-rrr-impl-specific-keyword/debian/control
@@ -1,0 +1,5 @@
+Source: source-rrr-impl-specific-keyword
+Rules-Requires-Root: impl/specific-keyword
+
+Package: binary
+Architecture: all