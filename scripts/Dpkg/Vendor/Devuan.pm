bb84915b93dd39b2f5faa202f9c6efd429b4b2ac
Guillem Jover <guillem@debian.org>
2025-07-30 01:07:02 +0200

    Dpkg::Vendor::Devuan: Use .pgp keyrings instead of .gpg ones
    The .gpg keyrings are now considered deprecated and provided via
    compatibility symlinks. Use the .pgp ones which avoids an indirection,
    and makes the code future proof, for when the old ones disappear.

--- scripts/Dpkg/Vendor/Devuan.pm
+++ scripts/Dpkg/Vendor/Devuan.pm
@@ -38,12 +38,12 @@ sub run_hook {
     my ($self, $hook, @params) = @_;
 
     if ($hook eq 'package-keyrings') {
-        return ('/usr/share/keyrings/devuan-keyring.gpg',
-                '/usr/share/keyrings/devuan-maintainers.gpg');
+        return ('/usr/share/keyrings/devuan-keyring.pgp',
+                '/usr/share/keyrings/devuan-maintainers.pgp');
     } elsif ($hook eq 'archive-keyrings') {
-        return ('/usr/share/keyrings/devuan-archive-keyring.gpg');
+        return ('/usr/share/keyrings/devuan-archive-keyring.pgp');
     } elsif ($hook eq 'archive-keyrings-historic') {
-        return ('/usr/share/keyrings/devuan-archive-removed-keys.gpg');
+        return ('/usr/share/keyrings/devuan-archive-removed-keys.pgp');
     } elsif ($hook eq 'extend-patch-header') {
         my ($textref, $ch_info) = @params;
         if ($ch_info->{'Closes'}) {
