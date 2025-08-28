guillemj/dpkg

commit 40cd7dd9d16c3bfaf090e15bea0a0f91360cdb68

Add vendor specific support for fuzzy source vs version nativeness
There's been an ongoing push to distort the native source package
concept, where this is a source of both accidental source package
construction, where even warnings have been shown to not be enough.

It has been argued this is a requirement for mainly two broad cases,
one is for native metapackages tracking non-native packages, where
the desire is to use the same versioning scheme (but with no version
remapping), and for either CI or VCS based workflows, where some of
the people involved either consider none of the proposed alternative
solutions acceptable, or consider that source packages are obsolete
and should not even be used anymore anyway, and undermining them is
clearly an acceptable price that others should pay.

This sets us back into making the packaging and its concepts more
difficult to grasp and handle, both for existing maintainers and
newcomers, and also for tool maintainers.

We need to add the notion of fuzzy nativeness, where a native source
package does not have a coherent native source version, so that vendors
that are forcing this sloppiness through, can have it without affecting
the default non-vendor case. Unfortunately this affects Debian and
derivatives. Which means using a method like Dpkg::Version->is_native()
has unreliable and non-portable semantics, so we deprecate that now
as well, and will be removed in the future.

Closes: #737634

scripts/Dpkg/Version.pm
@@ -32,7 +32,7 @@ them.
 
 =cut
 
-package Dpkg::Version 1.03;
+package Dpkg::Version 1.04;
 
 use strict;
 use warnings;
@@ -186,9 +186,28 @@ sub revision {
 
 Returns true if the version is native, false if it has a revision.
 
+B<Note>: On some vendors, dpkg has been made incoherent, and a native
+source package format can end up having a non-native version, adding
+to the confusion of the whole concept. And while this method would
+be fine to be used on vendors that have a coherent native source concept,
+this is not a pattern that will be portably relied on. Thus this method
+is being deprecated.
+
 =cut
 
 sub is_native {
+    my $self = shift;
+    warnings::warning('deprecated',
+        'using Dpkg::Version->is_native() has been made incoherent and ' .
+        'confusing on some dpkg vendors; it is deprecated as not having ' .
+        'portable semantics anymore');
+    return $self->__is_native();
+}
+
+# Internal symbol for dpkg project use only, no API guarantees provided.
+# While a new method could be provided such as has_revision() (and
+# has_epoch()), the intent and semantics would be similar.
+sub __is_native {
     my $self = shift;
     return $self->{no_revision};
 }
@@ -480,6 +499,10 @@ sub version_check {
 =back
 
 =head1 CHANGES
+
+=head2 Version 1.04 (dpkg 1.23.0)
+
+Deprecated method: $v->is_native().
 
 =head2 Version 1.03 (dpkg 1.20.0)
 

man/deb-buildinfo.pod
@@ -116,6 +116,11 @@ It may also include a Debian revision number (for non-native packages).
 The exact format and sorting algorithm are described in
 L<deb-version(7)>.
 
+B<Note>: On some vendors,
+dpkg has been made to permit native sources with non-native versions,
+making this incoherent and adding to the confusion of the concept,
+where in addition this tends to be a trap for accidental mistakes.
+
 =item B<Binary-Only-Changes:>
 
 =item S< >I<changelog-entry>

man/deb-control.pod
@@ -78,6 +78,11 @@ The exact format and sorting algorithm
 are described in
 L<deb-version(7)>.
 
+B<Note>: On some vendors,
+dpkg has been made to permit native sources with non-native versions,
+making this incoherent and adding to the confusion of the concept,
+where in addition this tends to be a trap for accidental mistakes.
+
 =item B<Maintainer:> I<fullname-email> (recommended)
 
 Should be in the format “Joe Bloggs E<lt>jbloggs@foo.comE<gt>”, and is typically

man/dpkg-source.pod
@@ -371,6 +371,11 @@ Optionally the original tarball might be accompanied by a detached
 upstream signature B<.orig.tar.gz.asc>, extraction
 supported since dpkg 1.18.5.
 
+B<Note>: On some vendors,
+dpkg has been made to permit native sources with non-native versions,
+making this incoherent and adding to the confusion of the concept,
+where in addition this tends to be a trap for accidental mistakes.
+
 B<Extracting>
 
 Extracting a native package is a simple extraction of the single
@@ -573,6 +578,11 @@ It supports all compression methods and
 will ignore by default any VCS specific files and directories
 as well as many temporary files (see default value associated to
 B<-I> option in the B<--help> output).
+
+B<Note>: On some vendors,
+dpkg has been made to permit native sources with non-native versions,
+making this incoherent and adding to the confusion of the concept,
+where in addition this tends to be a trap for accidental mistakes.
 
 =head2 Format: 3.0 (quilt)
 

man/dsc.pod
@@ -107,6 +107,11 @@ It may also include a Debian revision number (for non-native packages).
 The exact format and sorting algorithm are described in
 L<deb-version(7)>.
 
+B<Note>: On some vendors,
+dpkg has been made to permit native sources with non-native versions,
+making this incoherent and adding to the confusion of the concept,
+where in addition this tends to be a trap for accidental mistakes.
+
 =item B<Origin:> I<name>
 
 The name of the distribution this package is originating from.

scripts/Dpkg/Source/Package/V1.pm
@@ -49,6 +49,7 @@ use Dpkg::Source::Patch;
 use Dpkg::Exit qw(push_exit_handler pop_exit_handler);
 use Dpkg::Source::Functions qw(erasedir);
 use Dpkg::Source::Package::V3::Native;
+use Dpkg::Vendor qw(run_vendor_hook);
 
 use parent qw(Dpkg::Source::Package);
 
@@ -205,7 +206,7 @@ sub do_extract {
         if ($tarfile =~ m/\.orig\.tar\.gz$/) {
             # We only need to warn on this branch, because of the $native reset
             # below, otherwise the V3::Native module will handle the warning.
-            if (!$v->is_native()) {
+            if (!$v->__is_native()) {
                 warning(g_('native package version may not have a revision'));
             }
 
@@ -214,7 +215,7 @@ sub do_extract {
             $native = 0;
         }
     } else {
-        if ($v->is_native()) {
+        if ($v->__is_native()) {
             warning(g_('non-native package version does not contain a revision'))
         }
     }
@@ -377,11 +378,15 @@ sub do_build {
     my $v = Dpkg::Version->new($self->{fields}->{'Version'});
     if ($sourcestyle =~ m/[kpursKPUR]/) {
         error(g_('non-native package version does not contain a revision'))
-            if $v->is_native();
+            if $v->__is_native();
     } else {
-        # TODO: This will become fatal in the near future.
-        warning(g_('native package version may not have a revision'))
-            unless $v->is_native();
+        if (!$v->__is_native) {
+            if (run_vendor_hook('has-fuzzy-native-source')) {
+                warning(g_('native package version may not have a revision'));
+            } else {
+                error(g_('native package version may not have a revision'));
+            }
+        }
     }
 
     my ($dirname, $dirbase) = fileparse($dir);

scripts/Dpkg/Source/Package/V2.pm
@@ -220,7 +220,7 @@ sub do_extract {
     }
 
     my $v = Dpkg::Version->new($fields->{'Version'});
-    if ($v->is_native()) {
+    if ($v->__is_native()) {
         warning(g_('non-native package version does not contain a revision'))
     }
 
@@ -353,7 +353,7 @@ sub can_build {
 
     my $v = Dpkg::Version->new($self->{fields}->{'Version'});
     return (0, g_('non-native package version does not contain a revision'))
-        if $v->is_native();
+        if $v->__is_native();
 
     return 1 if $self->find_original_tarballs(include_supplementary => 0);
     return 1 if $self->{options}{create_empty_orig} and

scripts/Dpkg/Source/Package/V3/Native.pm
@@ -44,6 +44,7 @@ use Dpkg::Exit qw(push_exit_handler pop_exit_handler);
 use Dpkg::Version;
 use Dpkg::Source::Archive;
 use Dpkg::Source::Functions qw(erasedir);
+use Dpkg::Vendor qw(run_vendor_hook);
 
 use parent qw(Dpkg::Source::Package);
 
@@ -71,7 +72,7 @@ sub do_extract {
     error(g_('no tarfile in Files field')) unless $tarfile;
 
     my $v = Dpkg::Version->new($fields->{'Version'});
-    if (!$v->is_native()) {
+    if (!$v->__is_native()) {
         warning(g_('native package version may not have a revision'));
     }
 
@@ -92,8 +93,13 @@ sub can_build {
     my ($self, $dir) = @_;
 
     my $v = Dpkg::Version->new($self->{fields}->{'Version'});
-    return (0, g_('native package version may not have a revision'))
-        unless $v->is_native();
+    if (!$v->__is_native()) {
+        if (run_vendor_hook('has-fuzzy-native-source')) {
+            warning(g_('native package version may not have a revision'));
+        } else {
+            return (0, g_('native package version may not have a revision'));
+        }
+    }
 
     return 1;
 }

commit 65bb5a4c267349df23102577517e698ac2ded90c

Dpkg::Version: Fix typo in function call
The function name is warnings::warnif() not warnings::warning().

Fixes: commit 40cd7dd9d16c3bfaf090e15bea0a0f91360cdb68
Changelog: silent

scripts/Dpkg/Version.pm
@@ -197,7 +197,7 @@ is being deprecated.
 
 sub is_native {
     my $self = shift;
-    warnings::warning('deprecated',
+    warnings::warnif('deprecated',
         'using Dpkg::Version->is_native() has been made incoherent and ' .
         'confusing on some dpkg vendors; it is deprecated as not having ' .
         'portable semantics anymore');