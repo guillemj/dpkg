ba13bb4418e93e7f5bf92b2981f50c1797fea99c
Guillem Jover <guillem@debian.org>
2025-12-06 02:28:38 +0100

    scripts/mk: Switch to use GNU make intcmp instead of relying on shell
    The intcmp function was added in GNU make 4.4, which was not present
    in Debian at the time this code was written. Switch to it to simplify
    the code and to avoid spawning multiple processes.
    
    Prompted-by: Sean Whitton <spwhitton@spwhitton.name>
    Ref: https://lists.debian.org/debian-devel/2025/12/msg00039.html

--- scripts/mk/buildapi.mk
+++ scripts/mk/buildapi.mk
@@ -11,9 +11,6 @@ dpkg_buildapi_mk_included = yes
 # Default API level when not set.
 DPKG_BUILD_API ?= $(shell dpkg-buildapi)
 
-# We could use only built-in GNU make functions, but that seems too much
-# complexity given no integer operators, given that we currently have to
-# fetch the build API level anyway.
-dpkg_build_api_ge = $(shell test "$(DPKG_BUILD_API)" -ge "$(1)" && echo yes)
+dpkg_build_api_ge = $(intcmp $(DPKG_BUILD_API),$(1),no,yes,yes)
 
 endif # dpkg_buildapi_mk_included
