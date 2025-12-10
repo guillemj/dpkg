guillemj/dpkg

commit 15535caaa48d0c36e199816a6ddf61f18c79eeca

Use invalid instead of illegal in symbol names and output messages
All these instances refer to strings or data formats that are not valid,
and never to things that have any legal (in law terms) connotation. Make
this clear by using the correct word.

scripts/Dpkg/Arch.pm
@@ -30,7 +30,7 @@ to import specific symbol subsets.
 
 =cut
 
-package Dpkg::Arch 1.03;
+package Dpkg::Arch 1.04;
 
 use v5.36;
 
@@ -45,6 +45,7 @@ our @EXPORT_OK = qw(
     debarch_is
     debarch_is_wildcard
     debarch_is_illegal
+    debarch_is_invalid
     debarch_is_concerned
     debarch_to_abiattrs
     debarch_to_cpubits
@@ -87,6 +88,7 @@ our %EXPORT_TAGS = (
         debarch_is
         debarch_is_wildcard
         debarch_is_illegal
+        debarch_is_invalid
         debarch_is_concerned
     ) ],
 );
@@ -598,7 +600,7 @@ sub debarch_is_wildcard
     return 0;
 }
 
-=item $bool = debarch_is_illegal($arch, %opts)
+=item $bool = debarch_is_invalid($arch, %opts)
 
 Validate an architecture name.
 
@@ -615,7 +617,7 @@ otherwise negated architectures are allowed.
 
 =cut
 
-sub debarch_is_illegal
+sub debarch_is_invalid
 {
     my ($arch, %opts) = @_;
     my $arch_regex = qr/[a-zA-Z0-9][a-zA-Z0-9-]*/;
@@ -625,6 +627,22 @@ sub debarch_is_illegal
     } else {
         return $arch !~ m/^!?$arch_regex$/;
     }
+}
+
+=item $bool = debarch_is_illegal($arch, %opts)
+
+This is a deprecated alias for L<debarch_is_invalid>.
+
+=cut
+
+sub debarch_is_illegal
+{
+    my ($arch, %opts) = @_;
+
+    warnings::warnif('deprecated',
+        'Dpkg::Arch::debarch_is_illegal() is deprecated, ' .
+        'use Dpkg::Arch::debarch_is_invalid() instead');
+    return debarch_is_invalid($arch, %opts);
 }
 
 =item $bool = debarch_is_concerned($arch, @arches)
@@ -685,7 +703,7 @@ sub debarch_list_parse
     my @arch_list = split ' ', $arch_list;
 
     foreach my $arch (@arch_list) {
-        if (debarch_is_illegal($arch, %opts)) {
+        if (debarch_is_invalid($arch, %opts)) {
             error(g_("'%s' is not a legal architecture in list '%s'"),
                   $arch, $arch_list);
         }
@@ -699,6 +717,12 @@ sub debarch_list_parse
 =back
 
 =head1 CHANGES
+
+=head2 Version 1.04 (dpkg 1.23.0)
+
+New functions: debarch_is_invalid().
+
+Deprecated functions: debarch_is_illegal().
 
 =head2 Version 1.03 (dpkg 1.19.1)
 

scripts/dpkg-source.pl
@@ -360,7 +360,7 @@ if ($options{opmode} =~ /^(build|print-format|(before|after)-build|commit)$/) {
                     for my $a (split(/\s+/, $v)) {
                         error(g_("'%s' is not a legal architecture string " .
                                  "in package '%s'"), $a, $p)
-                            if debarch_is_illegal($a);
+                            if debarch_is_invalid($a);
                         error(g_('architecture %s only allowed on its ' .
                                  "own (list for package %s is '%s')"),
                               $a, $p, $a)

lib/dpkg/ar.c
@@ -150,7 +150,7 @@ dpkg_ar_member_get_size(struct dpkg_ar *ar, struct dpkg_ar_hdr *arh)
 }
 
 bool
-dpkg_ar_member_is_illegal(struct dpkg_ar_hdr *arh)
+dpkg_ar_member_is_invalid(struct dpkg_ar_hdr *arh)
 {
 	return memcmp(arh->ar_fmag, DPKG_AR_FMAG, sizeof(arh->ar_fmag)) != 0;
 }

lib/dpkg/ar.h
@@ -97,7 +97,7 @@ dpkg_ar_close(struct dpkg_ar *ar);
 void
 dpkg_ar_normalize_name(struct dpkg_ar_hdr *arh);
 bool
-dpkg_ar_member_is_illegal(struct dpkg_ar_hdr *arh);
+dpkg_ar_member_is_invalid(struct dpkg_ar_hdr *arh);
 
 void
 dpkg_ar_put_magic(struct dpkg_ar *ar);

lib/dpkg/arch.c
@@ -55,7 +55,7 @@
  * @param name The architecture name to verify.
  */
 const char *
-dpkg_arch_name_is_illegal(const char *name)
+dpkg_arch_name_is_invalid(const char *name)
 {
 	static char buf[150];
 	const char *p = name;
@@ -130,7 +130,7 @@ dpkg_arch_new(const char *name, enum dpkg_arch_type type)
  *
  * Create a new structure for the architecture if it is not yet known from
  * the system, in that case it will have type == DPKG_ARCH_UNKNOWN, if the
- * architecture is illegal it will have type == DPKG_ARCH_ILLEGAL, if name
+ * architecture is invalid it will have type == DPKG_ARCH_INVALID, if name
  * is an empty string it will have type == DPKG_ARCH_EMPTY, and if it is
  * NULL then it will have type == DPKG_ARCH_NONE.
  *
@@ -153,8 +153,8 @@ dpkg_arch_find(const char *name)
 		last_arch = arch;
 	}
 
-	if (dpkg_arch_name_is_illegal(name))
-		type = DPKG_ARCH_ILLEGAL;
+	if (dpkg_arch_name_is_invalid(name))
+		type = DPKG_ARCH_INVALID;
 	else
 		type = DPKG_ARCH_UNKNOWN;
 
@@ -184,7 +184,7 @@ dpkg_arch_get(enum dpkg_arch_type type)
 		return &arch_item_all;
 	case DPKG_ARCH_NATIVE:
 		return &arch_item_native;
-	case DPKG_ARCH_ILLEGAL:
+	case DPKG_ARCH_INVALID:
 	case DPKG_ARCH_FOREIGN:
 	case DPKG_ARCH_UNKNOWN:
 		internerr("architecture type %d is not unique", type);

lib/dpkg/arch.h
@@ -37,7 +37,7 @@ DPKG_BEGIN_DECLS
 enum dpkg_arch_type {
 	DPKG_ARCH_NONE,
 	DPKG_ARCH_EMPTY,
-	DPKG_ARCH_ILLEGAL,
+	DPKG_ARCH_INVALID,
 	DPKG_ARCH_WILDCARD,
 	DPKG_ARCH_ALL,
 	DPKG_ARCH_NATIVE,
@@ -52,7 +52,7 @@ struct dpkg_arch {
 };
 
 const char *
-dpkg_arch_name_is_illegal(const char *name)
+dpkg_arch_name_is_invalid(const char *name)
 	DPKG_ATTR_NONNULL(1);
 struct dpkg_arch *
 dpkg_arch_find(const char *name);

lib/dpkg/dpkg-db.h
@@ -405,7 +405,7 @@ enum DPKG_ATTR_ENUM_FLAGS parsedbflags {
 };
 
 const char *
-pkg_name_is_illegal(const char *p);
+pkg_name_is_invalid(const char *p);
 
 const struct fieldinfo *
 find_field_info(const struct fieldinfo *fields, const char *fieldname);

lib/dpkg/fields.c
@@ -103,7 +103,7 @@ f_name(struct pkginfo *pkg, struct pkgbin *pkgbin,
 {
 	const char *e;
 
-	e = pkg_name_is_illegal(value);
+	e = pkg_name_is_invalid(value);
 	if (e != NULL)
 		parse_error(ps, _("invalid package name in '%s' field: %s"),
 		            fip->name, e);
@@ -220,9 +220,9 @@ f_architecture(struct pkginfo *pkg, struct pkgbin *pkgbin,
                const char *value, const struct fieldinfo *fip)
 {
 	pkgbin->arch = dpkg_arch_find(value);
-	if (pkgbin->arch->type == DPKG_ARCH_ILLEGAL)
+	if (pkgbin->arch->type == DPKG_ARCH_INVALID)
 		parse_warn(ps, _("'%s' is not a valid architecture name in '%s' field: %s"),
-		           value, fip->name, dpkg_arch_name_is_illegal(value));
+		           value, fip->name, dpkg_arch_name_is_invalid(value));
 }
 
 void
@@ -505,7 +505,7 @@ f_dependency(struct pkginfo *pkg, struct pkgbin *pkgbin,
 				              "package name expected"), fip->name);
 
 			varbuf_set_buf(&depname, depnamestart, depnamelength);
-			emsg = pkg_name_is_illegal(depname.buf);
+			emsg = pkg_name_is_invalid(depname.buf);
 			if (emsg)
 				parse_error(ps,
 				            _("'%s' field, invalid package name '%.255s': %s"),
@@ -552,8 +552,8 @@ f_dependency(struct pkginfo *pkg, struct pkgbin *pkgbin,
 				dop->arch_is_implicit = false;
 				dop->arch = dpkg_arch_find(arch.buf);
 
-				if (dop->arch->type == DPKG_ARCH_ILLEGAL)
-					emsg = dpkg_arch_name_is_illegal(arch.buf);
+				if (dop->arch->type == DPKG_ARCH_INVALID)
+					emsg = dpkg_arch_name_is_invalid(arch.buf);
 				if (emsg)
 					parse_error(ps, _("'%s' field, reference to '%.255s': "
 					                  "invalid architecture name '%.255s': %s"),
@@ -757,10 +757,10 @@ f_trigpend(struct pkginfo *pend, struct pkgbin *pkgbin,
 	while ((word = scan_word(&value))) {
 		const char *emsg;
 
-		emsg = trig_name_is_illegal(word);
+		emsg = trig_name_is_invalid(word);
 		if (emsg)
 			parse_error(ps,
-			            _("illegal pending trigger name '%.255s': %s"),
+			            _("invalid pending trigger name '%.255s': %s"),
 			            word, emsg);
 
 		if (!trig_note_pend_core(pend, nfstrsave(word)))
@@ -789,7 +789,7 @@ f_trigaw(struct pkginfo *aw, struct pkgbin *pkgbin,
 		pend = pkg_spec_parse_pkg(word, &err);
 		if (pend == NULL)
 			parse_error(ps,
-			            _("illegal package name in awaited trigger '%.255s': %s"),
+			            _("invalid package name in awaited trigger '%.255s': %s"),
 			            word, err.str);
 
 		if (!trig_note_aw(pend, aw))

commit d8ec15b13d03bdcb50aa38c4a9c6767a924432f1

Use valid instead of legal in output messages and code comments
All these instances refer to strings or data formats and their validity,
and never to things that have any legal (in law terms) connotation. Make
this clear by using the correct word.

Fixes: commit 15535caaa48d0c36e199816a6ddf61f18c79eeca

lib/dpkg/dbmodify.c
@@ -507,7 +507,7 @@ modstatdb_note_core(struct pkginfo *pkg)
 
 /*
  * Note: If anyone wants to set some triggers-pending, they must also
- * set status appropriately, or we will undo it. That is, it is legal
+ * set status appropriately, or we will undo it. That is, it is valid
  * to call this when pkg->status and pkg->trigpend_head disagree and
  * in that case pkg->status takes precedence and pkg->trigpend_head
  * will be adjusted.

scripts/Dpkg/Arch.pm
@@ -704,7 +704,7 @@ sub debarch_list_parse
 
     foreach my $arch (@arch_list) {
         if (debarch_is_invalid($arch, %opts)) {
-            error(g_("'%s' is not a legal architecture in list '%s'"),
+            error(g_("'%s' is not a valid architecture in list '%s'"),
                   $arch, $arch_list);
         }
     }

scripts/dpkg-source.pl
@@ -370,7 +370,7 @@ if ($options{opmode} =~ /^(build|print-format|(before|after)-build|commit)$/) {
                     push(@sourcearch, $v) unless $archadded{$v}++;
                 } else {
                     for my $a (split(/\s+/, $v)) {
-                        error(g_("'%s' is not a legal architecture string " .
+                        error(g_("'%s' is not a valid architecture string " .
                                  "in package '%s'"), $a, $p)
                             if debarch_is_invalid($a);
                         error(g_('architecture %s only allowed on its ' .