guillemj/dpkg

commit 9beeb2914bdcf90f60e731ac92e5a8a75833c37b

Dpkg::Changelog::Parse: Deprecate file option and rename it to filename
This matches the same option name used in all other function and methods
in the codebase, when we need to pass a filename. This avoids the
potential confusion of this being a file object or a handle.

scripts/Dpkg/Changelog/Parse.pm
@@ -28,7 +28,7 @@ of dpkg-parsechangelog.
 
 =cut
 
-package Dpkg::Changelog::Parse 2.01;
+package Dpkg::Changelog::Parse 2.02;
 
 use v5.36;
 
@@ -45,18 +45,18 @@ use Dpkg::ErrorHandling;
 use Dpkg::Control::Changelog;
 
 sub _changelog_detect_format {
-    my $file = shift;
+    my $filename = shift;
     my $format = 'debian';
 
     # Extract the format from the changelog file if possible
-    if ($file ne '-') {
+    if ($filename ne '-') {
         local $_;
 
-        open my $format_fh, '<', $file
-            or syserr(g_('cannot open file %s'), $file);
+        open my $format_fh, '<', $filename
+            or syserr(g_('cannot open file %s'), $filename);
         if (-s $format_fh > 4096) {
             seek $format_fh, -4096, 2
-                or syserr(g_('cannot seek into file %s'), $file);
+                or syserr(g_('cannot seek into file %s'), $filename);
         }
         while (<$format_fh>) {
             $format = $1 if m/\schangelog-format:\s+([0-9a-z]+)\W/;
@@ -94,15 +94,22 @@ Options:
 
 =over
 
-=item B<file>
+=item B<filename>
 
 Set the changelog file to parse.
 Defaults to F<debian/changelog>.
 
+=item B<file>
+
+Deprecated alias for B<filename>.
+It emits a deprecation warning, but if it is required for backwards
+compatibility it can be passed alongside B<filename> and the warning
+will not be emitted.
+
 =item B<label>
 
 Set the changelog name used in output messages.
-Defaults to $opts{file}.
+Defaults to $opts{filename}.
 
 =item B<compression>
 
@@ -132,11 +139,16 @@ sub changelog_parse {
     my (%opts) = @_;
 
     $opts{verbose} //= 1;
-    $opts{file} //= 'debian/changelog';
-    $opts{label} //= $opts{file};
-    $opts{changelogformat} //= _changelog_detect_format($opts{file});
+    if (exists $opts{file} && ! exists $opts{filename}) {
+        warnings::warnif('deprecated',
+            'Dpkg::Changelog::Parse::changelog_parse() option file is deprecated, ' .
+            'switch to use filename, or pass file it alongside it');
+    }
+    $opts{filename} //= $opts{file} // 'debian/changelog';
+    $opts{label} //= $opts{filename};
+    $opts{changelogformat} //= _changelog_detect_format($opts{filename});
     $opts{format} //= 'dpkg';
-    $opts{compression} //= $opts{file} ne 'debian/changelog';
+    $opts{compression} //= $opts{filename} ne 'debian/changelog';
 
     my @range_opts = qw(since until from to offset count reverse all);
     $opts{all} = 1 if exists $opts{all};
@@ -166,8 +178,8 @@ sub changelog_parse {
     );
 
     # Load and parse the changelog.
-    $changes->load($opts{file}, compression => $opts{compression})
-        or error(g_('fatal error occurred while parsing %s'), $opts{file});
+    $changes->load($opts{filename}, compression => $opts{compression})
+        or error(g_('fatal error occurred while parsing %s'), $opts{filename});
 
     # Get the output into several Dpkg::Control objects.
     my @res;
@@ -190,6 +202,12 @@ sub changelog_parse {
 =back
 
 =head1 CHANGES
+
+=head2 Version 2.02 (dpkg 1.23.0)
+
+New options: 'filename in changelog_parse().
+
+Deprecated options: 'file' in changelog_parse().
 
 =head2 Version 2.01 (dpkg 1.20.6)
 

scripts/Dpkg/Source/Package/V2.pm
@@ -690,7 +690,7 @@ AUTOGEN_HEADER
     }
 
     my $ch_info = changelog_parse(
-        file => $self->{options}{changelog_file},
+        filename => $self->{options}{changelog_file},
         offset => 0,
         count => 1,
     );

scripts/dpkg-genbuildinfo.pl
@@ -426,7 +426,7 @@ my $dist = Dpkg::Dist::Files->new();
 
 # Retrieve info from the current changelog entry.
 my %changelog_opts = (
-    file => $changelogfile,
+    filename => $changelogfile,
 );
 $changelog_opts{changelogformat} = $changelogformat if $changelogformat;
 my $changelog = changelog_parse(%changelog_opts);

scripts/dpkg-genchanges.pl
@@ -211,7 +211,7 @@ if (not defined $outputfile) {
 
 # Retrieve info from the current changelog entry
 my %changelog_opts = (
-    file => $changelogfile,
+    filename => $changelogfile,
 );
 $changelog_opts{changelogformat} = $changelogformat if $changelogformat;
 $changelog_opts{since} = $since if defined($since);

scripts/dpkg-gencontrol.pl
@@ -142,7 +142,7 @@ while (@ARGV) {
 
 umask 0o022; # ensure sane default permissions for created files
 my %changelog_opts = (
-    file => $changelogfile,
+    filename => $changelogfile,
 );
 $changelog_opts{changelogformat} = $changelogformat if $changelogformat;
 my $changelog = changelog_parse(%changelog_opts);

scripts/dpkg-parsechangelog.pl
@@ -92,9 +92,9 @@ while (@ARGV) {
     } elsif ($arg eq '--reverse') {
         $options{reverse} = 1;
     } elsif ($arg eq '-l' or $arg eq '--file') {
-        $options{file} = shift;
+        $options{filename} = shift;
         usageerr(g_('missing changelog filename'))
-            unless length $options{file};
+            unless length $options{filename};
     } elsif ($arg eq '-S' or $arg eq '--show-field') {
         $fieldname = shift;
     } elsif ($arg eq '-c' or $arg eq '--count' or $arg eq '-n') {

scripts/dpkg-source.pl
@@ -236,7 +236,7 @@ if ($options{opmode} =~ /^(build|print-format|(before|after)-build|commit)$/) {
     $controlfile ||= "$dir/debian/control";
 
     my %ch_options = (
-        file => $options{changelog_file},
+        filename => $options{changelog_file},
     );
     $ch_options{changelogformat} = $changelogformat if $changelogformat;
     my $changelog = changelog_parse(%ch_options);

commit 3c708d484f9e137bfbb8c6a237e53e46ef195ea9

perl: Fix grammar in warning messages
Fixes: commit 9beeb2914bdcf90f60e731ac92e5a8a75833c37b
Fixes: commit e1cd4e06dae34f1864f145382295ae6a0c6bde69
Changelog: silent

scripts/Dpkg/Changelog/Parse.pm
@@ -142,7 +142,7 @@ sub changelog_parse {
     if (exists $opts{file} && ! exists $opts{filename}) {
         warnings::warnif('deprecated',
             'Dpkg::Changelog::Parse::changelog_parse() option file is deprecated, ' .
-            'switch to use filename, or pass file it alongside it');
+            'switch to use filename, or pass file alongside it');
     }
     $opts{filename} //= $opts{file} // 'debian/changelog';
     $opts{label} //= $opts{filename};

scripts/Dpkg/IPC.pm
@@ -343,7 +343,7 @@ sub spawn {
         if (exists $opts{nocheck} && ! exists $opts{no_check}) {
             warnings::warnif('deprecated',
                 'Dpkg::IPC::spawn() option nocheck is deprecated, ' .
-                'either switch to no_check or pass it alongside it');
+                'either switch to no_check or pass nocheck alongside it');
         }
         wait_child($pid,
             no_check => $opts{no_check} // $opts{nocheck},
@@ -402,7 +402,7 @@ sub wait_child {
     if (exists $opts{nocheck} && ! exists $opts{no_check}) {
         warnings::warnif('deprecated',
             'Dpkg::IPC::wait_child() option nocheck is deprecated, ' .
-            'either switch to no_check or pass it alongside it');
+            'either switch to no_check or pass nocheck alongside it');
     }
     $opts{no_check} //= $opts{nocheck};
     croak 'no PID set, cannot wait end of process' unless $pid;