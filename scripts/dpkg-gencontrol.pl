guillemj/dpkg

commit e313ec63ddf60486db174be8acb4c36ee57925b5

scripts: Replace some POSIX imports with Fcntl module
The :errno_h stop being used, so we can switch to the more light weight
Fcntl module.

scripts/dpkg-gencontrol.pl
@@ -22,7 +22,7 @@
 use v5.36;
 
 use List::Util qw(none);
-use POSIX qw(:errno_h :fcntl_h);
+use Fcntl;
 use File::Find;
 
 use Dpkg ();

scripts/dpkg-distaddfile.pl
@@ -20,7 +20,7 @@
 
 use v5.36;
 
-use POSIX qw(:errno_h :fcntl_h);
+use Fcntl;
 
 use Dpkg ();
 use Dpkg::Gettext;

commit 33869fa2164945d51d97d2e56bf524499a82e20a

dpkg-gencontrol: Add a missing POSIX module require
Fixes: commit e313ec63ddf60486db174be8acb4c36ee57925b5
Changelog: silent

scripts/dpkg-gencontrol.pl
@@ -358,6 +358,9 @@ if ($binarypackage ne $sourcepackage || $verdiff) {
 }
 
 if (! defined($substvars->get('Installed-Size'))) {
+    # XXX: Switch to builtin::ceil() once we can use v5.40.
+    require POSIX;
+
     my $installed_size = 0;
     my %hardlink;
     my $scan_installed_size = sub {