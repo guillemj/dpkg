guillemj/dpkg

commit 7ef1a039771fc833ca0518e50cc68c21c8c6f872

Dpkg::BuildProfiles: Make parser more strict
Improve the parser to check for the actual intended syntax, and restrict
the allowed set of characters for build profile names, even though we
are still being lenient, at least for a transition period. But this set
might be further restricted in the future.

Closes: #1121657

scripts/Dpkg/BuildProfiles.pm
@@ -134,14 +134,20 @@ sub build_profile_is_invalid($string)
 
 Parses a build profiles specification, into an array of array references.
 
+It will die on invalid syntax.
+
 =cut
 
-sub parse_build_profiles {
-    my $string = shift;
+sub parse_build_profiles($string)
+{
+    if (build_profile_is_invalid($string)) {
+        error(g_("'%s' is not a valid build profile restriction formula"),
+              $string);
+    }
 
-    $string =~ s/^\s*<\s*(.*)\s*>\s*$/$1/;
+    my @restrictions = $string =~ m{$restriction_list_regex}g;
 
-    return map { [ split ' ' ] } split /\s*>\s+<\s*/, $string;
+    return map { [ split ' ' ] } @restrictions;
 }
 
 =item evaluate_restriction_formula(\@formula, \@profiles)

scripts/t/Dpkg_BuildProfiles.t
@@ -15,7 +15,7 @@
 
 use v5.36;
 
-use Test::More tests => 17;
+use Test::More tests => 23;
 
 use ok 'Dpkg::BuildProfiles', qw(
     build_profile_is_invalid
@@ -73,6 +73,24 @@ is_deeply([ parse_build_profiles('<nocheck nodoc> <stage1>') ], $formula,
     is_deeply([ get_build_profiles() ], [ qw(cross nodoc profile.name) ],
         'get active build profiles from environment');
 }
+
+eval { parse_build_profiles('<invalid> + <characters>') };
+ok($@, 'fail to parse build profiles with invalid characters');
+
+eval { parse_build_profiles('missing angle brackets') };
+ok($@, 'fail to parse build profiles with missing opening/closing angles brackets');
+
+eval { parse_build_profiles('<missing angle brackets <here too') };
+ok($@, 'fail to parse build profiles with missing closing angles brackets 1');
+
+eval { parse_build_profiles('<missing angle brackets <here too>') };
+ok($@, 'fail to parse build profiles with missing closing angles brackets 2');
+
+eval { parse_build_profiles('missing angle> brackets>') };
+ok($@, 'fail to parse build profiles with missing opening angles brackets 1');
+
+eval { parse_build_profiles('missing angle> <brackets>') };
+ok($@, 'fail to parse build profiles with missing opening angles brackets 2');
 
 set_build_profiles(qw(nocheck stage1));
 is_deeply([ get_build_profiles() ], [ qw(nocheck stage1) ],

commit b9190f1ce4eff987cd37852527332fff4c8c9e75

Dpkg::BuildProfiles: Add missing Dpkg::Gettext and Dpkg::ErrorHandling imports
Closes: #1123515
Fixes: commit 7ef1a039771fc833ca0518e50cc68c21c8c6f872

scripts/Dpkg/BuildProfiles.pm
@@ -41,6 +41,8 @@ our @EXPORT_OK = qw(
 use Exporter qw(import);
 use List::Util qw(any);
 
+use Dpkg::Gettext;
+use Dpkg::ErrorHandling;
 use Dpkg::BuildEnv;
 
 my $cache_profiles;