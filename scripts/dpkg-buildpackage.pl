guillemj/dpkg

commit 37cf54ce95bf274278b2eeb47a49a4b3b3840612

scripts: Parse and validate all Changed-By and Maintainer field inputs
We should not only validate the Maintainer field from the control and
changelog files, but also what comes from the command line for the
Maintainer field and the Changed-By field.

Closes: #1126507

scripts/dpkg-buildpackage.pl
@@ -42,6 +42,7 @@ use Dpkg::Compression;
 use Dpkg::Checksums;
 use Dpkg::Package;
 use Dpkg::Version;
+use Dpkg::Email::Address;
 use Dpkg::Control;
 use Dpkg::Control::Info;
 use Dpkg::Changelog::Parse;
@@ -404,9 +405,11 @@ while (@ARGV) {
     } elsif (/^-v(.*)$/) {
         $since = $1;
     } elsif (/^-m(.*)$/ or /^--(?:source|build)-by=(.*)$/) {
-        $maint = $1;
+        my $email = Dpkg::Email::Address->new($1);
+        $maint = $email->as_string();
     } elsif (/^-e(.*)$/ or /^--(?:changed|release)-by=(.*)$/) {
-        $changedby = $1;
+        my $email = Dpkg::Email::Address->new($1);
+        $changedby = $email->as_string();
     } elsif (/^-C(.*)$/) {
         $desc = $1;
     } elsif (m/^-[EW]$/) {
@@ -557,7 +560,14 @@ if ($changedby) {
 } elsif ($maint) {
     $maintainer = $maint;
 } else {
-    $maintainer = mustsetvar($changelog->{maintainer}, g_('source changed by'));
+    my $email = mustsetvar($changelog->{maintainer}, g_('source changed by'));
+    eval {
+        my $addr = Dpkg::Email::Address->new($email);
+        $maintainer = $addr->as_string();
+    } or do {
+        error(g_('cannot parse maintainer email address "%s" from changelog entry'),
+              $email);
+    }
 }
 
 # <https://reproducible-builds.org/specs/source-date-epoch/>

scripts/dpkg-genchanges.pl
@@ -37,6 +37,7 @@ use Dpkg::Compression;
 use Dpkg::Control::Info;
 use Dpkg::Control::Fields;
 use Dpkg::Control;
+use Dpkg::Email::Address;
 use Dpkg::Substvars;
 use Dpkg::Package;
 use Dpkg::Changelog::Parse;
@@ -178,9 +179,11 @@ while (@ARGV) {
         $substvars->load($1) if -e $1;
         $substvars_loaded = 1;
     } elsif (m/^-m(.*)$/s) {
-        $forcemaint = $1;
+        my $addr = Dpkg::Email::Address->new($1);
+        $forcemaint = $addr->as_string();
     } elsif (m/^-e(.*)$/s) {
-        $forcechangedby = $1;
+        my $addr = Dpkg::Email::Address->new($1);
+        $forcechangedby = $addr->as_string();
     } elsif (m/^-F([0-9a-z]+)$/) {
         $changelogformat = $1;
     } elsif (m/^-D([^\=:]+)[=:](.*)$/s) {
@@ -436,7 +439,8 @@ foreach my $f (keys %{$changelog}) {
     if ($f eq 'Source') {
         set_source_name($v);
     } elsif ($f eq 'Maintainer') {
-        $fields->{'Changed-By'} = $v;
+        my $addr = Dpkg::Email::Address->new($v);
+        $fields->{'Changed-By'} = $addr->as_string();
     } else {
         field_transfer_single($changelog, $fields, $f);
     }

commit 839df8f9226bac8455ee23014c179c17d812ae50

scripts: Do not fail on empty Maintainer field from parsed changelog
While this is invalid syntax (according to the documentation), we have
accepted these kinds of entries up to now, and this was an unintentional
change. Modify the code to handle them for now, while there's discussion
on how to improve the notion of unfinalized changelog entries.

Fixes: commit 37cf54ce95bf274278b2eeb47a49a4b3b3840612
Closes: #1130119

scripts/dpkg-buildpackage.pl
@@ -559,7 +559,7 @@ if ($changedby) {
     $maintainer = $changedby;
 } elsif ($maint) {
     $maintainer = $maint;
-} else {
+} elsif (length $changelog->{maintainer}) {
     my $email = mustsetvar($changelog->{maintainer}, g_('source changed by'));
     eval {
         my $addr = Dpkg::Email::Address->new($email);

scripts/dpkg-genchanges.pl
@@ -439,8 +439,10 @@ foreach my $f (keys %{$changelog}) {
     if ($f eq 'Source') {
         set_source_name($v);
     } elsif ($f eq 'Maintainer') {
-        my $addr = Dpkg::Email::Address->new($v);
-        $fields->{'Changed-By'} = $addr->as_string();
+        if (length $v) {
+            my $addr = Dpkg::Email::Address->new($v);
+            $fields->{'Changed-By'} = $addr->as_string();
+        }
     } else {
         field_transfer_single($changelog, $fields, $f);
     }