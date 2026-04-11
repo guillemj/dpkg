guillemj/dpkg

commit a9b22de4db4d42417c4cac74e7e537f48284e731

perl: Remove function prototypes
These have many issues, perform implicit context coercion which can
be rather surprising for callers, and are incompatible with function
signatures, which we'll be using once the minimum Perl version
supporting them has been bumped in our coding style baseline.

scripts/Dpkg/Version.pm
@@ -265,7 +265,7 @@ If $a or $b are not valid version numbers, it dies with an error.
 
 =cut
 
-sub version_compare($$) {
+sub version_compare {
     my ($a, $b) = @_;
     my $va = Dpkg::Version->new($a, check => 1);
     defined($va) || error(g_('%s is not a valid version'), "$a");
@@ -285,7 +285,7 @@ have an input string containing the operator.
 
 =cut
 
-sub version_compare_relation($$$) {
+sub version_compare_relation {
     my ($a, $op, $b) = @_;
     my $res = version_compare($a, $b);
 
@@ -314,7 +314,7 @@ they are obsolete aliases of ">=" and "<=".
 
 =cut
 
-sub version_normalize_relation($) {
+sub version_normalize_relation {
     my $op = shift;
 
     warning('relation %s is deprecated: use %s or %s',
@@ -362,7 +362,7 @@ sub _version_order {
     }
 }
 
-sub version_compare_string($$) {
+sub version_compare_string {
     my @a = map { _version_order($_) } split(//, shift);
     my @b = map { _version_order($_) } split(//, shift);
     while (1) {
@@ -386,7 +386,7 @@ $a is earlier than $b, 0 if they are equal and 1 if $a is later than $b.
 
 =cut
 
-sub version_compare_part($$) {
+sub version_compare_part {
     my @a = version_split_digits(shift);
     my @b = version_split_digits(shift);
     while (1) {
@@ -414,7 +414,7 @@ return ("1", ".", "024", "~beta", "1", "+svn", "234").
 
 =cut
 
-sub version_split_digits($) {
+sub version_split_digits {
     my $version = shift;
 
     return split /(?<=\d)(?=\D)|(?<=\D)(?=\d)/, $version;
@@ -430,7 +430,7 @@ contains a description of the problem with the $version scalar.
 
 =cut
 
-sub version_check($) {
+sub version_check {
     my $version = shift;
     my $str;
     if (defined $version) {

dselect/methods/Dselect/Method.pm
@@ -57,7 +57,7 @@ if ($@) {
 
 our %CONFIG;
 
-sub yesno($$) {
+sub yesno {
     my ($d, $msg) = @_;
 
     my ($res, $r);

dselect/methods/ftp/install.pl
@@ -205,7 +205,7 @@ foreach my $site (@{$CONFIG{site}}) {
 
 my $dldir = $CONFIG{dldir};
 # md5sum
-sub md5sum($) {
+sub md5sum {
     my $fn = shift;
     my $m = qx(md5sum $fn);
     $m = (split(' ', $m))[0];
@@ -310,7 +310,7 @@ if($totsize == 0) {
     }
 }
 
-sub download() {
+sub download {
  my $i = 0;
 
  foreach my $site (@{$CONFIG{site}}) {
@@ -447,7 +447,7 @@ my %files; # package-version => files...
 # check a deb or split deb file
 # return 1 if it a deb file, 2 if it is a split deb file
 # else 0
-sub chkdeb($) {
+sub chkdeb {
     my ($fn) = @_;
     # check to see if it is a .deb file
     if (!system "dpkg-deb --info $fn >/dev/null 2>&1 && dpkg-deb --contents $fn >/dev/null 2>&1") {
@@ -457,7 +457,7 @@ sub chkdeb($) {
     }
     return 0;
 }
-sub getdebinfo($) {
+sub getdebinfo {
     my ($fn) = @_;
     my $type = chkdeb($fn);
     my ($pkg, $ver);
@@ -484,7 +484,7 @@ sub getdebinfo($) {
 }
 
 # process deb file to make sure we only keep latest versions
-sub prcdeb($$) {
+sub prcdeb {
     my ($dir, $fn) = @_;
     my ($pkg, $ver) = getdebinfo($fn);
     if(!defined($pkg) || !defined($ver)) {
@@ -511,7 +511,7 @@ sub prcdeb($$) {
     }
 }
 
-sub prcfile() {
+sub prcfile {
     my ($fn) = $_;
     if (-f $fn and $fn ne '.') {
         my $dir = '.';

dselect/methods/ftp/setup.pl
@@ -108,7 +108,7 @@ if (! $CONFIG{done}) {
 edit_config('ftp', $methdir);
 
 my $ftp;
-sub download() {
+sub download {
  foreach (@{$CONFIG{site}}) {
     $ftp = do_connect(ftpsite => $_->[0],
                       ftpdir => $_->[1],

scripts/Dpkg/Arch.pm
@@ -122,7 +122,7 @@ from the environment.
 
 =cut
 
-sub get_raw_build_arch()
+sub get_raw_build_arch
 {
     state $build_arch;
 
@@ -149,7 +149,7 @@ if available.
 
 =cut
 
-sub get_build_arch()
+sub get_build_arch
 {
     return Dpkg::BuildEnv::get('DEB_BUILD_ARCH') || get_raw_build_arch();
 }
@@ -157,7 +157,7 @@ sub get_build_arch()
 {
     my %cc_host_gnu_type;
 
-    sub get_host_gnu_type()
+    sub get_host_gnu_type
     {
         my $CC = $ENV{CC} || 'gcc';
 
@@ -191,7 +191,7 @@ from the environment.
 
 =cut
 
-sub get_raw_host_arch()
+sub get_raw_host_arch
 {
     state $host_arch;
 
@@ -231,7 +231,7 @@ if available.
 
 =cut
 
-sub get_host_arch()
+sub get_host_arch
 {
     return Dpkg::BuildEnv::get('DEB_HOST_ARCH') || get_raw_host_arch();
 }
@@ -242,7 +242,7 @@ Get an array with all currently known Debian architectures.
 
 =cut
 
-sub get_valid_arches()
+sub get_valid_arches
 {
     _load_cputable();
     _load_ostable();
@@ -303,7 +303,7 @@ sub _load_ostable
     });
 }
 
-sub _load_abitable()
+sub _load_abitable
 {
     _load_table('abitable', sub {
         if (m/^(?!\#)(\S+)\s+(\S+)/) {
@@ -312,7 +312,7 @@ sub _load_abitable()
     });
 }
 
-sub _load_tupletable()
+sub _load_tupletable
 {
     _load_cputable();
 
@@ -340,7 +340,7 @@ sub _load_tupletable()
     });
 }
 
-sub debtuple_to_gnutriplet(@)
+sub debtuple_to_gnutriplet
 {
     my ($abi, $libc, $os, $cpu) = @_;
 
@@ -353,7 +353,7 @@ sub debtuple_to_gnutriplet(@)
     return join('-', $cputable{$cpu}, $ostable{"$abi-$libc-$os"});
 }
 
-sub gnutriplet_to_debtuple($)
+sub gnutriplet_to_debtuple
 {
     my $gnu = shift;
     return unless defined($gnu);
@@ -389,7 +389,7 @@ Map a GNU triplet into a Debian multiarch triplet.
 
 =cut
 
-sub gnutriplet_to_multiarch($)
+sub gnutriplet_to_multiarch
 {
     my $gnu = shift;
     my ($cpu, $cdr) = split(/-/, $gnu, 2);
@@ -407,14 +407,14 @@ Map a Debian architecture into a Debian multiarch triplet.
 
 =cut
 
-sub debarch_to_multiarch($)
+sub debarch_to_multiarch
 {
     my $arch = shift;
 
     return gnutriplet_to_multiarch(debarch_to_gnutriplet($arch));
 }
 
-sub debtuple_to_debarch(@)
+sub debtuple_to_debarch
 {
     my ($abi, $libc, $os, $cpu) = @_;
 
@@ -429,7 +429,7 @@ sub debtuple_to_debarch(@)
     }
 }
 
-sub debarch_to_debtuple($)
+sub debarch_to_debtuple
 {
     my $arch = shift;
 
@@ -464,7 +464,7 @@ Map a Debian architecture into a GNU triplet.
 
 =cut
 
-sub debarch_to_gnutriplet($)
+sub debarch_to_gnutriplet
 {
     my $arch = shift;
 
@@ -477,14 +477,14 @@ Map a GNU triplet into a Debian architecture.
 
 =cut
 
-sub gnutriplet_to_debarch($)
+sub gnutriplet_to_debarch
 {
     my $gnu = shift;
 
     return debtuple_to_debarch(gnutriplet_to_debtuple($gnu));
 }
 
-sub debwildcard_to_debtuple($)
+sub debwildcard_to_debtuple
 {
     my $arch = shift;
     my @tuple = split /-/, $arch, 4;
@@ -504,7 +504,7 @@ sub debwildcard_to_debtuple($)
     }
 }
 
-sub debarch_to_abiattrs($)
+sub debarch_to_abiattrs
 {
     my $arch = shift;
     my ($abi, $libc, $os, $cpu) = debarch_to_debtuple($arch);
@@ -518,7 +518,7 @@ sub debarch_to_abiattrs($)
     }
 }
 
-sub debarch_to_cpubits($)
+sub debarch_to_cpubits
 {
     my $arch = shift;
     my $cpu;
@@ -539,7 +539,7 @@ Debian architecture. No wildcard matching is performed.
 
 =cut
 
-sub debarch_eq($$)
+sub debarch_eq
 {
     my ($a, $b) = @_;
 
@@ -560,7 +560,7 @@ architecture wildcard.
 
 =cut
 
-sub debarch_is($$)
+sub debarch_is
 {
     my ($real, $alias) = @_;
 
@@ -587,7 +587,7 @@ Evaluate whether a Debian architecture is an architecture wildcard.
 
 =cut
 
-sub debarch_is_wildcard($)
+sub debarch_is_wildcard
 {
     my $arch = shift;
 

scripts/Dpkg/Checksums.pm
@@ -76,7 +76,7 @@ Returns the list of supported checksums algorithms.
 
 =cut
 
-sub checksums_get_list() {
+sub checksums_get_list {
     my @list = sort keys %{$CHECKSUMS};
     return @list;
 }
@@ -88,7 +88,7 @@ supported. The checksum algorithm is case-insensitive.
 
 =cut
 
-sub checksums_is_supported($) {
+sub checksums_is_supported {
     my $alg = shift;
     return exists $CHECKSUMS->{lc($alg)};
 }
@@ -104,7 +104,7 @@ whether the checksum algorithm is considered cryptographically strong.
 
 =cut
 
-sub checksums_get_property($$) {
+sub checksums_get_property {
     my ($alg, $property) = @_;
 
     return unless checksums_is_supported($alg);

scripts/Dpkg/Control/FieldsCore.pm
@@ -1048,7 +1048,7 @@ except the first of each word (words are separated by a hyphen in field names).
 
 =cut
 
-sub field_capitalize($) {
+sub field_capitalize {
     my $field = lc(shift);
 
     # Use known fields first.
@@ -1064,7 +1064,7 @@ Returns true if the field is official and known.
 
 =cut
 
-sub field_is_official($) {
+sub field_is_official {
     my $field = lc shift;
 
     return exists $FIELDS{$field};
@@ -1083,7 +1083,7 @@ Undef is returned for non-official fields.
 
 =cut
 
-sub field_is_allowed_in($@) {
+sub field_is_allowed_in {
     my ($field, @types) = @_;
     $field = lc $field;
 
@@ -1117,7 +1117,7 @@ added to $to otherwise.
 
 =cut
 
-sub field_transfer_single($$;$) {
+sub field_transfer_single {
     my ($from, $to, $field) = @_;
     if (not defined $field) {
         warnings::warnif('deprecated',
@@ -1164,7 +1164,7 @@ Returns the list of fields that have been added to $to.
 
 =cut
 
-sub field_transfer_all($$) {
+sub field_transfer_all {
     my ($from, $to) = @_;
     my (@res, $res);
     foreach my $k (keys %$from) {
@@ -1182,7 +1182,7 @@ The list might be empty for types where the order does not matter much.
 
 =cut
 
-sub field_ordered_list($) {
+sub field_ordered_list {
     my $type = shift;
 
     if (exists $FIELD_ORDER{$type}) {
@@ -1205,7 +1205,7 @@ Dpkg::Package::pkg_name_is_illegal() and Dpkg::Version::version_check().
 
 =cut
 
-sub field_parse_binary_source($) {
+sub field_parse_binary_source {
     my $ctrl = shift;
     my $ctrl_type = $ctrl->get_type();
 
@@ -1244,7 +1244,7 @@ Debian package.
 
 =cut
 
-sub field_list_src_dep() {
+sub field_list_src_dep {
     my @list = map {
         $FIELDS{$_}{name}
     } sort {
@@ -1264,7 +1264,7 @@ the stronger to the weaker.
 
 =cut
 
-sub field_list_pkg_dep() {
+sub field_list_pkg_dep {
     my @list = map {
         $FIELDS{$_}{name}
     } sort {
@@ -1285,7 +1285,7 @@ Breaks, ...). Returns undef for fields which are not dependencies.
 
 =cut
 
-sub field_get_dep_type($) {
+sub field_get_dep_type {
     my $field = lc shift;
 
     return unless exists $FIELDS{$field};
@@ -1300,7 +1300,7 @@ FIELD_SEP_SPACE, FIELD_SEP_COMMA or FIELD_SEP_LINE.
 
 =cut
 
-sub field_get_sep_type($) {
+sub field_get_sep_type {
     my $field = lc shift;
 
     return $FIELDS{$field}{separator} if exists $FIELDS{$field}{separator};
@@ -1314,7 +1314,7 @@ types. %opts is optional.
 
 =cut
 
-sub field_register($$;@) {
+sub field_register {
     my ($field, $types, %opts) = @_;
 
     $field = lc $field;
@@ -1336,7 +1336,7 @@ Return true if the field was inserted, otherwise false.
 
 =cut
 
-sub field_insert_after($$@) {
+sub field_insert_after {
     my ($type, $field, @fields) = @_;
 
     return 0 if not exists $FIELD_ORDER{$type};
@@ -1358,7 +1358,7 @@ Return true if the field was inserted, otherwise false.
 
 =cut
 
-sub field_insert_before($$@) {
+sub field_insert_before {
     my ($type, $field, @fields) = @_;
 
     return 0 if not exists $FIELD_ORDER{$type};

scripts/Dpkg/ErrorHandling.pm
@@ -186,7 +186,7 @@ sub _typename_prefix
     return report_pretty(report_name($type), report_color($type));
 }
 
-sub report(@)
+sub report
 {
     my ($type, $msg, @args) = @_;
 
@@ -205,7 +205,7 @@ sub debug
     print report(REPORT_DEBUG, @args) if $level <= $debug_level;
 }
 
-sub info($;@)
+sub info
 {
     my @args = @_;
 
@@ -219,28 +219,28 @@ sub notice
     warn report(REPORT_NOTICE, @args) if not $quiet_warnings;
 }
 
-sub warning($;@)
+sub warning
 {
     my @args = @_;
 
     warn report(REPORT_WARN, @args) if not $quiet_warnings;
 }
 
-sub syserr($;@)
+sub syserr
 {
     my ($msg, @args) = @_;
 
     die report(REPORT_ERROR, "$msg: $!", @args);
 }
 
-sub error($;@)
+sub error
 {
     my @args = @_;
 
     die report(REPORT_ERROR, @args);
 }
 
-sub errormsg($;@)
+sub errormsg
 {
     my @args = @_;
 
@@ -254,7 +254,7 @@ sub printcmd
     print { *STDERR } report_pretty(" @cmd\n", report_color(REPORT_COMMAND));
 }
 
-sub subprocerr(@)
+sub subprocerr
 {
     my ($p, @args) = @_;
 
@@ -273,7 +273,7 @@ sub subprocerr(@)
     }
 }
 
-sub usageerr(@)
+sub usageerr
 {
     my ($msg, @args) = @_;
 

commit f4941036b04e787c2803d6359802d6725d2476c6

Dpkg::Version: Add prototypes for sort functions
We need prototypes for these functions so that they get the $a and $b
variables automatically set by sort, otherwise they get no arguments at
all.

Analysis-by: Oron Peled <Oron.Peled@harmonicinc.com>
Fixes: commit a9b22de4db4d42417c4cac74e7e537f48284e731
Closes: #1131556
Stable-Candidate: 1.22.x

scripts/Dpkg/Version.pm
@@ -301,7 +301,8 @@ If $a or $b are not valid version numbers, it dies with an error.
 
 =cut
 
-sub version_compare {
+sub version_compare :prototype($$)
+{
     my ($a, $b) = @_;
     my $va = Dpkg::Version->new($a, check => 1);
     defined($va) || error(g_('%s is not a valid version'), "$a");
@@ -398,7 +399,8 @@ sub _version_order {
     }
 }
 
-sub version_compare_string {
+sub version_compare_string :prototype($$)
+{
     my @a = map { _version_order($_) } split(//, shift);
     my @b = map { _version_order($_) } split(//, shift);
     while (1) {
@@ -423,7 +425,8 @@ $a is earlier than $b, 0 if they are equal and 1 if $a is later than $b.
 
 =cut
 
-sub version_compare_part {
+sub version_compare_part :prototype($$)
+{
     my @a = version_split_digits(shift);
     my @b = version_split_digits(shift);
     while (1) {

scripts/t/Dpkg_Version.t
@@ -15,7 +15,7 @@
 
 use v5.36;
 
-use Test::More tests => 1752;
+use Test::More tests => 1755;
 
 use IPC::Cmd qw(can_run);
 
@@ -173,6 +173,67 @@ foreach my $case (@tests) {
         }
     }
 }
+
+my @versions_unsorted;
+my @versions_expected;
+my @versions_sorted;
+
+@versions_unsorted = qw(
+    4:4-4
+    5.0abc
+    0.0-0.0alpha0
+    10.100.1-1
+    0~999.999zeta
+    0:1.0-0
+);
+@versions_expected = qw(
+    0~999.999zeta
+    0.0-0.0alpha0
+    0:1.0-0
+    5.0abc
+    10.100.1-1
+    4:4-4
+);
+@versions_sorted = sort version_compare @versions_unsorted;
+is_deeply(\@versions_sorted, \@versions_expected, 'sort version_compare');
+
+@versions_unsorted = qw(
+    4
+    5.0abc
+    0.0alpha0
+    10.100.1
+    0~999.999zeta
+    1.0
+);
+@versions_expected = qw(
+    0~999.999zeta
+    0.0alpha0
+    1.0
+    4
+    5.0abc
+    10.100.1
+);
+@versions_sorted = sort version_compare_part @versions_unsorted;
+is_deeply(\@versions_sorted, \@versions_expected, 'sort version_compare_part');
+
+@versions_unsorted = qw(
+    10a10
+    ~zyx999
+    Z0z
+    ~000
+    aa5AA
+    ~abc
+);
+@versions_expected = qw(
+    ~000
+    ~abc
+    ~zyx999
+    10a10
+    Z0z
+    aa5AA
+);
+@versions_sorted = sort version_compare_string @versions_unsorted;
+is_deeply(\@versions_sorted, \@versions_expected, 'sort version_compare_string');
 
 __DATA__
 1.0-1 2.0-2 -1