guillemj/dpkg

commit 7d54287521ac4e1b2061d4a92467c634525436d3

dpkg-deb: Be more robust against truncated ar archives
Track the current ar archive position and what we are expected to be
reading, and error out if we expect more data than what is available,
before trying to unpack anything.

Reported-by: Yashashree Gund <yash_gund@live.com>

src/deb/extract.c
@@ -135,8 +135,11 @@ extracthalf(const char *debar, const char *dir,
 		read_fail(rc, debar, _("archive magic version number"));
 
 	if (strcmp(versionbuf, DPKG_AR_MAGIC) == 0) {
+		off_t ar_pos = 0;
 		int adminmember = -1;
 		bool header_done = false;
+
+		ar_pos += strlen(DPKG_AR_MAGIC);
 
 		ctrllennum = 0;
 		for (;;) {
@@ -146,6 +149,8 @@ extracthalf(const char *debar, const char *dir,
 			rc = fd_read(ar->fd, &arh, sizeof(arh));
 			if (rc != sizeof(arh))
 				read_fail(rc, debar, _("archive member header"));
+
+			ar_pos += sizeof(arh);
 
 			if (dpkg_ar_member_is_invalid(&arh))
 				ohshit(_("file '%s' is corrupt - bad archive header magic"),
@@ -167,6 +172,8 @@ extracthalf(const char *debar, const char *dir,
 					read_fail(rc, debar, _("archive information header member"));
 				infobuf[memberlen] = '\0';
 
+				ar_pos += ar_member_size;
+
 				if (strchr(infobuf, '\n') == NULL)
 					ohshit(_("archive has no newlines in header"));
 				errstr = deb_version_parse(&version, infobuf);
@@ -186,6 +193,8 @@ extracthalf(const char *debar, const char *dir,
 				if (fd_skip(ar->fd, ar_member_size, &err) < 0)
 					ohshit(_("cannot skip archive member from '%s': %s"),
 					       ar->name, err.str);
+
+				ar_pos += ar_member_size;
 			} else {
 				if (strncmp(arh.ar_name, ADMINMEMBER, strlen(ADMINMEMBER)) == 0) {
 					const char *extension = arh.ar_name + strlen(ADMINMEMBER);
@@ -229,8 +238,19 @@ extracthalf(const char *debar, const char *dir,
 					if (fd_skip(ar->fd, ar_member_size, &err) < 0)
 						ohshit(_("cannot skip archive member from '%s': %s"),
 						       ar->name, err.str);
+
+					ar_pos += ar_member_size;
 				} else {
 					/* Yes! - found it. */
+					off_t ar_new_pos = ar_pos + ar_member_size;
+
+					if (ar_new_pos < ar_pos)
+						ohshit(_("archive '%s' contains an overflowing member '%.*s' size"),
+						       ar->name, (int)sizeof(arh.ar_name), arh.ar_name);
+					if (ar_new_pos > ar->size)
+						ohshit(_("archive '%s' is truncated or corrupt, "
+						         "expected more data than available (%jd > %jd)"),
+						       ar->name, ar_new_pos, ar->size);
 					break;
 				}
 			}

commit a658782c1af35da6620b2eb05b90faaf8ab4b10e

dpkg-deb: Do not fail on non-seekable archives
We have documented that we support non-seekable archives for some time,
and this was a regression introduced in 1.23.7. Disable the robustness
check if the input is not seekable.

Add regression tests to avoid this happening in the future.

Fixes: commit 7d54287521ac4e1b2061d4a92467c634525436d3
Closes: #1132051

lib/dpkg/ar.c
@@ -52,6 +52,7 @@ dpkg_ar_fdopen(const char *filename, int fd)
 	ar->size = st.st_size;
 	ar->time = st.st_mtime;
 	ar->fd = fd;
+	ar->is_seekable = lseek(fd, 0, SEEK_CUR) == 0;
 
 	return ar;
 }

lib/dpkg/ar.h
@@ -67,6 +67,7 @@ struct dpkg_ar {
 	intmax_t time;
 	off_t size;
 	int fd;
+	bool is_seekable;
 };
 
 /**

src/Makefile.am
@@ -159,6 +159,7 @@ TESTSUITE_AT += $(srcdir)/at/deb-format.at
 TESTSUITE_AT += $(srcdir)/at/deb-fields.at
 TESTSUITE_AT += $(srcdir)/at/deb-content.at
 TESTSUITE_AT += $(srcdir)/at/deb-split.at
+TESTSUITE_AT += $(srcdir)/at/deb-streaming.at
 TESTSUITE_AT += $(srcdir)/at/realpath.at
 TESTSUITE_AT += $(srcdir)/at/divert.at
 TESTSUITE_AT += $(srcdir)/at/trigger.at

src/at/deb-streaming.at
@@ -1,0 +1,44 @@
+AT_SETUP([dpkg-deb .deb streaming])
+AT_KEYWORDS([dpkg-deb deb streaming])
+
+DPKG_GEN_CONTROL([pkg-streaming])
+AT_CHECK([
+dpkg-deb --root-owner-group -b pkg-streaming
+], [0], [ignore])
+
+AT_CHECK([
+cat pkg-streaming.deb | dpkg-deb --info -
+], [0], [ new Debian package, version 2.0.
+ size 0 bytes: control archive=328 bytes.
+     171 bytes,     7 lines      control
+ Package: pkg-streaming
+ Version: 0.0-1
+ Section: test
+ Priority: extra
+ Maintainer: Dpkg Developers <debian-dpkg@lists.debian.org>
+ Architecture: all
+ Description: test package
+])
+
+AT_CHECK([
+cat pkg-streaming.deb | dpkg-deb --show -
+], [0], [pkg-streaming	0.0-1
+])
+
+AT_CHECK([
+cat pkg-streaming.deb | dpkg-deb --field - Package
+], [0], [pkg-streaming
+])
+
+AT_CHECK([
+cat pkg-streaming.deb | dpkg-deb --ctrl-tarfile - | $TAR tf -
+], [0], [./
+./control
+])
+
+AT_CHECK([
+cat pkg-streaming.deb | dpkg-deb --fsys-tarfile - | $TAR tf -
+], [0], [./
+])
+
+AT_CLEANUP

src/at/testsuite.at
@@ -6,6 +6,7 @@ AT_BANNER([Binary .deb packages])
 m4_include([deb-format.at])
 m4_include([deb-content.at])
 m4_include([deb-fields.at])
+m4_include([deb-streaming.at])
 
 AT_TESTED([dpkg-split])
 AT_BANNER([Split .deb packages])

src/deb/extract.c
@@ -241,7 +241,8 @@ extracthalf(const char *debar, const char *dir,
 					if (ar_new_pos < ar_pos)
 						ohshit(_("archive '%s' contains an overflowing member '%.*s' size"),
 						       ar->name, (int)sizeof(arh.ar_name), arh.ar_name);
-					if (ar_new_pos > ar->size)
+					if (ar->is_seekable &&
+					    ar_new_pos > ar->size)
 						ohshit(_("archive '%s' is truncated or corrupt, "
 						         "expected more data than available (%jd > %jd)"),
 						       ar->name, ar_new_pos, ar->size);