guillemj/dpkg

commit 35c5fb5b42f2a48f14adc80d47ca392263fd7260

perl: Switch to use File::stat when we need to access its members
This incurs only a small performance penalty of around 5%, where the
calls switched should not be performance sensitive anyway, in contrast
to the amount of other work they need to do.

This should make the code way more clear.

scripts/dpkg-name.pl
@@ -21,6 +21,7 @@
 use v5.36;
 
 use List::Util qw(none);
+use File::stat ();
 use File::Basename;
 use File::Path qw(make_path);
 
@@ -81,11 +82,10 @@ sub fileexists
 sub filesame
 {
     my ($a, $b) = @_;
-    my @sta = stat($a);
-    my @stb = stat($b);
+    my $sta = File::stat::stat($a);
+    my $stb = file::stat::stat($b);
 
-    # Same device and inode numbers.
-    return (@sta and @stb and $sta[0] == $stb[0] and $sta[1] == $stb[1]);
+    return $sta && $stb && $sta->dev == $stb->dev && $sta->ino == $stb->dev;
 }
 
 sub getfields

dselect/methods/ftp/update.pl
@@ -28,6 +28,8 @@ if ($@) {
     exit 1;
 }
 
+use File::stat ();
+
 use Dselect::Method;
 use Dselect::Method::Ftp;
 
@@ -122,11 +124,13 @@ sub download {
                 $must_get = 1;
             } else {
                 # Else check last modification date.
-                my @pack_stat = stat($file);
-                if ($newest_pack_date > $pack_stat[9]) {
+                my $st = File::stat::stat($file);
+                if (! defined $st) {
+                    $must_get = 1;
+                } elsif ($newest_pack_date > $st->mtime) {
 #                   print "Packages has changed; must get it.\n";
                     $must_get = 1;
-                } elsif ($newest_pack_date < $pack_stat[9]) {
+                } elsif ($newest_pack_date < $st->mtime) {
                     print " Our file is newer than theirs; skipping.\n";
                 } else {
                     print " Already up-to-date; skipping.\n";

scripts/Dpkg/BuildDriver/DebianRules.pm
@@ -33,6 +33,7 @@ package Dpkg::BuildDriver::DebianRules 0.01;
 use v5.36;
 
 use Errno qw(ENOENT);
+use File::stat ();
 
 use Dpkg ();
 use Dpkg::Gettext;
@@ -225,15 +226,15 @@ sub pre_check {
     if (@{$self->{debian_rules}} == 1) {
         my $rules = $self->{debian_rules}[0];
 
-        my @sb = lstat $rules;
-        if (@sb == 0) {
+        my $st = File::stat::lstat($rules);
+        if (! defined $st) {
             syserr(g_('cannot stat %s'), $rules) if $! != ENOENT;
             warning(g_('%s does not exist'), $rules);
-        } elsif (-f _) {
-            if (! -x _) {
+        } elsif (-f $st) {
+            if (! -x $st) {
                 warning(g_('%s is not executable; fixing that'), $rules);
 
-                chmod($sb[2] | 0o111, $rules)
+                chmod $st->mode | 0o111, $rules
                     or syserr(g_('cannot make %s executable'), $rules);
             }
         } else {

scripts/Dpkg/Path.pm
@@ -45,6 +45,7 @@ our @EXPORT_OK = qw(
 
 use Exporter qw(import);
 use Errno qw(ENOENT);
+use File::stat ();
 use File::Spec;
 use File::Find;
 use Cwd qw(realpath);
@@ -138,17 +139,16 @@ sub check_files_are_the_same {
     my ($file1, $file2, $resolve_symlink) = @_;
 
     return 1 if $file1 eq $file2;
-    return 0 if ((! -e $file1) || (! -e $file2));
-    my (@stat1, @stat2);
+    my ($st1, $st2);
     if ($resolve_symlink) {
-        @stat1 = stat($file1);
-        @stat2 = stat($file2);
+        $st1 = File::stat::stat($file1);
+        $st2 = File::stat::stat($file2);
     } else {
-        @stat1 = lstat($file1);
-        @stat2 = lstat($file2);
+        $st1 = File::stat::lstat($file1);
+        $st2 = File::stat::lstat($file2);
     }
-    my $result = ($stat1[0] == $stat2[0]) && ($stat1[1] == $stat2[1]);
-    return $result;
+    return 0 if ! defined $st1 || ! defined $st2;
+    return $st1->dev == $st2->dev && $st1->ino == $st2->ino;
 }
 
 

scripts/Dpkg/Source/Archive.pm
@@ -34,6 +34,7 @@ use v5.36;
 
 use Carp;
 use Errno qw(ENOENT);
+use File::stat ();
 use File::Temp;
 use File::Basename qw(basename);
 use File::Spec;
@@ -205,33 +206,25 @@ sub extract {
             my $relpath = File::Spec->abs2rel($File::Find::name, $tmpdir);
             my $destpath = File::Spec->catfile($dest, $relpath);
 
-            my ($mode, $atime, $mtime);
-            lstat $File::Find::name
+            my $src_st = File::stat::lstat($File::Find::name)
                 or syserr(g_('cannot get source pathname %s metadata'), $File::Find::name);
-            ((undef) x 2, $mode, (undef) x 5, $atime, $mtime) = lstat _;
-            my $src_is_dir = -d _;
-
-            my $dest_exists = 1;
-            if (not lstat $destpath) {
-                if ($! == ENOENT) {
-                    $dest_exists = 0;
-                } else {
-                    syserr(g_('cannot get target pathname %s metadata'), $destpath);
-                }
+            my $dst_st = File::stat::lstat($destpath);
+            if (! $dst_st && $! != ENOENT) {
+                syserr(g_('cannot get target pathname %s metadata'), $destpath);
             }
-            my $dest_is_dir = -d _;
-            if ($dest_exists) {
-                if ($dest_is_dir && $src_is_dir) {
+
+            if (defined $dst_st) {
+                if (-d $dst_st && -d $src_st) {
                     # Refresh the destination directory attributes with the
                     # ones from the tarball.
-                    chmod $mode, $destpath
+                    chmod $src_st->mode, $destpath
                         or syserr(g_('cannot change directory %s mode'), $File::Find::name);
-                    utime $atime, $mtime, $destpath
+                    utime $src_st->atime, $src_st->mtime, $destpath
                         or syserr(g_('cannot change directory %s times'), $File::Find::name);
 
                     # We should do nothing, and just walk further tree.
                     return;
-                } elsif ($dest_is_dir) {
+                } elsif (-d $dst_st) {
                     rmdir $destpath
                         or syserr(g_('cannot remove destination directory %s'), $destpath);
                 } else {
@@ -241,7 +234,7 @@ sub extract {
                 }
             }
             # If we are moving a directory, we do not need to walk it.
-            if ($src_is_dir) {
+            if (-d $src_st) {
                 $File::Find::prune = 1;
             }
             rename $File::Find::name, $destpath

scripts/Dpkg/Source/Functions.pm
@@ -42,6 +42,7 @@ our @EXPORT_OK = qw(
 
 use Exporter qw(import);
 use Errno qw(ENOENT);
+use File::stat ();
 
 use Dpkg::ErrorHandling;
 use Dpkg::Gettext;
@@ -91,7 +92,9 @@ sub fixperms {
 # but not necessarily ownership of those files.
 sub chmod_if_needed {
     my ($newperms, $pathname) = @_;
-    my $oldperms = (stat $pathname)[2] & 0o7777;
+    my $st = File::stat::stat($pathname);
+    return 0 if ! defined $st;
+    my $oldperms = $st->mode & 0o7777;
 
     return 1 if $oldperms == $newperms;
     return chmod $newperms, $pathname;
@@ -114,10 +117,10 @@ sub fs_time {
         utime(undef, undef, $file) or
             syserr(g_('cannot change timestamp for %s'), $file);
     }
-    stat($file) or syserr(g_('cannot read timestamp from %s'), $file);
-    my $mtime = (stat(_))[9];
+    my $st = File::stat::stat($file)
+        or syserr(g_('cannot read timestamp from %s'), $file);
     unlink($file) if $is_temp;
-    return $mtime;
+    return $st->mtime;
 }
 
 sub is_binary {

scripts/Dpkg/Source/Package/V1.pm
@@ -34,6 +34,7 @@ use v5.36;
 
 use Errno qw(ENOENT);
 use Cwd;
+use File::stat ();
 use File::Basename;
 use File::Temp;
 use File::Spec;
@@ -285,11 +286,11 @@ sub do_extract {
         # make sure debian/rules is executable if it exists. Otherwise the
         # debian-rules build driver will take care of the warnings.
         my $rules = File::Spec->catfile($newdirectory, 'debian', 'rules');
-        my @s = lstat $rules;
-        if (not scalar @s) {
+        my $st = File::stat::lstat($rules);
+        if (! defined $st) {
             syserr(g_('cannot stat %s'), $rules) if $! != ENOENT;
-        } elsif (-f _) {
-            chmod $s[2] | 0o111, $rules
+        } elsif (-f $st) {
+            chmod $st->mode | 0o111, $rules
                 or syserr(g_('cannot make %s executable'), $rules);
         } else {
             warning(g_('%s is not a plain file'), $rules);

scripts/Dpkg/Source/Patch.pm
@@ -33,6 +33,7 @@ package Dpkg::Source::Patch 0.01;
 use v5.36;
 
 use POSIX qw(:errno_h :sys_wait_h);
+use File::stat ();
 use File::Find;
 use File::Basename;
 use File::Spec;
@@ -86,6 +87,8 @@ sub get_header {
 
 sub _gen_hires_diff_label($filename)
 {
+    # XXX: Make do without File::stat::stat as otherwise we need to use the
+    # undocumented File::stat::populate() function with Time::HiRes::stat().
     my $mtime = (Time::HiRes::stat($filename))[9];
     my $t = POSIX::strftime('%Y-%m-%d %H:%M:%S', gmtime $mtime);
     return sprintf "\t%s.%09d +0000", $t, ($mtime - int $mtime) * 1_000_000_000;
@@ -194,10 +197,11 @@ sub add_diff_directory {
         my $fn = (length > length($new)) ? substr($_, length($new) + 1) : '.';
         return if $diff_ignore->($fn);
         $files_in_new{$fn} = 1;
-        lstat("$new/$fn") or syserr(g_('cannot stat file %s'), "$new/$fn");
-        my $mode = S_IMODE((lstat(_))[2]);
-        my $size = (lstat(_))[7];
-        if (-l _) {
+        my $new_st = File::stat::lstat("$new/$fn")
+            or syserr(g_('cannot stat file %s'), "$new/$fn");
+        my $mode = S_IMODE($new_st->mode);
+        my $size = $new_st->size;
+        if (-l $new_st) {
             unless (-l "$old/$fn") {
                 $self->_fail_not_same_type("$old/$fn", "$new/$fn", $fn);
                 return;
@@ -213,7 +217,7 @@ sub add_diff_directory {
             unless ($n eq $n2) {
                 $self->_fail_not_same_type("$old/$fn", "$new/$fn", $fn);
             }
-        } elsif (-f _) {
+        } elsif (-f $new_st) {
             my $old_file = "$old/$fn";
             if (not lstat("$old/$fn")) {
                 if ($! != ENOENT) {
@@ -231,14 +235,14 @@ sub add_diff_directory {
             }
             push @diff_files, [$fn, $mode, $size, $old_file, "$new/$fn",
                                $label_old, "$basedir/$fn"];
-        } elsif (-p _) {
+        } elsif (-p $new_st) {
             unless (-p "$old/$fn") {
                 $self->_fail_not_same_type("$old/$fn", "$new/$fn", $fn);
             }
-        } elsif (-b _ || -c _ || -S _) {
+        } elsif (-b $new_st || -c $new_st || -S $new_st) {
             $self->_fail_with_msg("$new/$fn",
                 g_('device or socket is not allowed'));
-        } elsif (-d _) {
+        } elsif (-d $new_st) {
             if (not lstat("$old/$fn")) {
                 if ($! != ENOENT) {
                     syserr(g_('cannot stat file %s'), "$old/$fn");
@@ -533,13 +537,13 @@ sub analyze {
             $dirtocreate{$dirname} = 1;
         }
 
-        if (-e $fn) {
-            if (not -f _) {
+        my $st = File::stat::stat($fn);
+        if (defined $st) {
+            if (not -f $st) {
                 error(g_("diff '%s' patches something which is not a plain file"),
                       $diff);
             }
-            my $nlink = (stat _)[3];
-            if ($nlink > 1) {
+            if ($st->nlink > 1) {
                 warning(g_("diff '%s' patches hard link %s which can have " .
                            'unintended consequences'), $diff, $fn);
             }

commit 4274bf806f8c3df02fe9b66cf95290b139d4adfd

dpkg-name: Fix typos from File::stat switch
One of the functions had a typo in the module name, and the check was
comparing mismatched dev vs ino.

Fixes: commit 35c5fb5b42f2a48f14adc80d47ca392263fd7260
Changelog: silent

scripts/dpkg-name.pl
@@ -83,9 +83,9 @@ sub filesame
 {
     my ($a, $b) = @_;
     my $sta = File::stat::stat($a);
-    my $stb = file::stat::stat($b);
+    my $stb = File::stat::stat($b);
 
-    return $sta && $stb && $sta->dev == $stb->dev && $sta->ino == $stb->dev;
+    return $sta && $stb && $sta->dev == $stb->dev && $sta->ino == $stb->ino;
 }
 
 sub getfields