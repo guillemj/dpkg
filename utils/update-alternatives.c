guillemj/dpkg

commit d6d81695e2dd53305ab4c64f1642c356a3c2ee30

Use "symbolic link" instead of "symlink"/"link" in output messages
Improve the output message wording to avoid function names or very
cryptic words, to make it understandable for the user and translators.

utils/update-alternatives.c
@@ -151,7 +151,7 @@ usage(void)
 "\n"));
 
 	printf(_(
-"<link> is the symlink pointing to %s/<name>.\n"
+"<link> is the symbolic link pointing to %s/<name>.\n"
 "  (e.g. /usr/bin/pager)\n"
 "<name> is the master name for this link group.\n"
 "  (e.g. pager)\n"
@@ -628,7 +628,8 @@ fsys_xreadlink(const char *linkname)
 
 	buf = fsys_areadlink(linkname);
 	if (buf == NULL)
-		syserr(_("cannot read link '%s%s'"), instdir, linkname);
+		syserr(_("cannot read symbolic link '%s%s'"),
+		       instdir, linkname);
 
 	return buf;
 }
@@ -659,7 +660,7 @@ fsys_set_ref_time(const char *linkname, const char *target)
 
 	root_linkname = fsys_get_path(linkname);
 	if (lutimes(root_linkname, tv) < 0 && errno != ENOSYS)
-		syserr(_("cannot set symlink '%s' timestamp"), root_linkname);
+		syserr(_("cannot set symbolic link '%s' timestamp"), root_linkname);
 	free(root_linkname);
 #endif
 }
@@ -2098,7 +2099,7 @@ alternative_prepare_install(struct alternative *a, const char *choice)
 		if (alternative_path_can_remove(sl->link))
 			alternative_add_commit_op(a, OPCODE_RM, sl->link, NULL);
 		else
-			warning(_("not removing %s since it is not a symlink"),
+			warning(_("not removing %s since it is not a symbolic link"),
 			        sl->link);
 		alternative_add_commit_op(a, OPCODE_RM, fn, NULL);
 		free(fn);

dselect/methods/media/install.pl
@@ -291,7 +291,7 @@ foreach my $need (@media) {
     }
     unlink <tmp/*>;
 
-    print "creating symlinks...\n";
+    print "creating symbolic links...\n";
     foreach my $pkgname (@{$medium{$need}}) {
         my $basename;
 

scripts/Dpkg/Source/Package/V3/Bzr.pm
@@ -71,13 +71,13 @@ sub _check_workdir {
     # Symlinks from .bzr to outside could cause unpack failures, or point to
     # files they should not, so check for and do not allow.
     if (-l "$srcdir/.bzr") {
-        error(g_('%s is a symlink'), "$srcdir/.bzr");
+        error(g_('%s is a symbolic link'), "$srcdir/.bzr");
     }
     my $abs_srcdir = Cwd::abs_path($srcdir);
     find(sub {
         if (-l) {
             if (Cwd::abs_path(readlink) !~ /^\Q$abs_srcdir\E(?:\/|$)/) {
-                error(g_('%s is a symlink to outside %s'),
+                error(g_('%s is a symbolic link to outside %s'),
                       $File::Find::name, $srcdir);
             }
         }

scripts/Dpkg/Source/Package/V3/Quilt.pm
@@ -150,7 +150,7 @@ sub apply_patches {
         # Do not overwrite real files.
         unless (-f _) {
             symlink($basename, $dest)
-                or syserr(g_('cannot create symlink %s'), $dest);
+                or syserr(g_('cannot create symbolic link %s'), $dest);
         }
     }
 

scripts/Dpkg/Source/Patch.pm
@@ -210,11 +210,11 @@ sub add_diff_directory {
             }
             my $n = readlink("$new/$fn");
             unless (defined $n) {
-                syserr(g_('cannot read link %s'), "$new/$fn");
+                syserr(g_('cannot read symbolic link %s'), "$new/$fn");
             }
             my $n2 = readlink("$old/$fn");
             unless (defined $n2) {
-                syserr(g_('cannot read link %s'), "$old/$fn");
+                syserr(g_('cannot read symbolic link %s'), "$old/$fn");
             }
             unless ($n eq $n2) {
                 $self->_fail_not_same_type("$old/$fn", "$new/$fn", $fn);
@@ -273,7 +273,7 @@ sub add_diff_directory {
         } elsif (-d _) {
             warning(g_('ignoring deletion of directory %s'), $fn);
         } elsif (-l _) {
-            warning(g_('ignoring deletion of symlink %s'), $fn);
+            warning(g_('ignoring deletion of symbolic link %s'), $fn);
         } else {
             $self->_fail_not_same_type("$old/$fn", "$new/$fn", $fn);
         }
@@ -749,7 +749,7 @@ sub get_type {
             if -f _ ;
         return g_('directory')
             if -d _;
-        return sprintf(g_('symlink to %s'), readlink($file))
+        return sprintf(g_('symbolic link to %s'), readlink($file))
             if -l _;
         return g_('block device')
             if -b _;

src/deb/build.c
@@ -231,7 +231,8 @@ check_ctrl_perms(const char *ctrldir)
 			if (S_ISLNK(mscriptstab.st_mode))
 				continue;
 			if (!S_ISREG(mscriptstab.st_mode))
-				ohshit(_("maintainer script '%s' is not a plain file or symlink"),
+				ohshit(_("maintainer script '%s' is not "
+				         "a plain file or symbolic link"),
 				       *mscriptp);
 			if ((mscriptstab.st_mode & 07557) != 0555)
 				ohshit(_("maintainer script '%s' has bad permissions %03lo "

src/dpkg-maintscript-helper.sh
@@ -449,7 +449,7 @@ prepare_dir_to_symlink()
     case "$LINE" in
     "$PATHNAME"/*)
       error "directory '$PATHNAME' contains conffiles," \
-            "cannot switch to symlink"
+            "cannot switch to symbolic link"
       ;;
     esac
   done
@@ -460,7 +460,7 @@ prepare_dir_to_symlink()
   find "$DPKG_ROOT$PATHNAME" -print0 \
     | xargs -0 -n1 "$0" _internal_pkg_must_own_file "$PACKAGE" ||
       error "directory '$PATHNAME' contains files not owned by" \
-            "package $PACKAGE, cannot switch to symlink"
+            "package $PACKAGE, cannot switch to symbolic link"
   unset DPKG_MAINTSCRIPT_HELPER_INTERNAL_API
 
   # At this point, we know that the directory either contains no files,

src/main/archives.c
@@ -571,7 +571,7 @@ tarobject_set_perms(struct tar_entry *te, const char *path, struct file_stat *st
 	if (te->type == TAR_FILETYPE_SYMLINK) {
 		rc = lchown(path, st->uid, st->gid);
 		if (forcible_nonroot_error(rc))
-			ohshite(_("cannot set ownership of symlink '%s'"),
+			ohshite(_("cannot set ownership of symbolic link '%s'"),
 			        path);
 	} else {
 		rc = chown(path, st->uid, st->gid);
@@ -614,7 +614,7 @@ tarobject_matches(struct tarcontext *tc,
 			break;
 		linksize = file_readlink(fn_old, &linkname, stab->st_size);
 		if (linksize < 0)
-			ohshite(_("cannot read link '%s'"), fn_old);
+			ohshite(_("cannot read symbolic link '%s'"), fn_old);
 		else if (linksize > stab->st_size)
 			ohshit(_("symbolic link '%s' size has changed from %jd to %zd"),
 			       fn_old, (intmax_t)stab->st_size, linksize);
@@ -687,7 +687,7 @@ linktosameexistingdir(const struct tar_entry *ti, const char *fname,
 	statr = stat(fname, &oldstab);
 	if (statr) {
 		if (!(errno == ENOENT || errno == ELOOP || errno == ENOTDIR))
-			ohshite(_("cannot stat (dereference) existing symlink '%s'"),
+			ohshite(_("cannot stat (dereference) existing symbolic link '%s'"),
 			        fname);
 		return false;
 	}
@@ -712,8 +712,8 @@ linktosameexistingdir(const struct tar_entry *ti, const char *fname,
 	if (statr) {
 		if (!(errno == ENOENT || errno == ELOOP || errno == ENOTDIR))
 			ohshite(_("cannot stat (dereference) proposed "
-			          "new symlink target '%s'"
-			          "for symlink '%s'"),
+			          "new symbolic link target '%s'"
+			          "for symbolic link '%s'"),
 			        symlinkfn->buf, fname);
 		return false;
 	}
@@ -1163,7 +1163,7 @@ tarobject(struct tar_archive *tar, struct tar_entry *ti)
 			 * of a symlink is the same as linking to it.) */
 			linksize = file_readlink(fnamevb.buf, &symlinkfn, stab.st_size);
 			if (linksize < 0)
-				ohshite(_("cannot read link '%s'"),
+				ohshite(_("cannot read symbolic link '%s'"),
 				        ti->name);
 			else if (linksize > stab.st_size)
 				ohshit(_("symbolic link '%s' size has changed from %jd to %zd"),
@@ -1172,11 +1172,11 @@ tarobject(struct tar_archive *tar, struct tar_entry *ti)
 				warning(_("symbolic link '%s' size has changed from %jd to %zd"),
 				       fnamevb.buf, (intmax_t)stab.st_size, linksize);
 			if (symlink(symlinkfn.buf, fnametmpvb.buf))
-				ohshite(_("cannot make backup symlink for '%s'"),
+				ohshite(_("cannot make backup symbolic link for '%s'"),
 				        ti->name);
 			rc = lchown(fnametmpvb.buf, stab.st_uid, stab.st_gid);
 			if (forcible_nonroot_error(rc))
-				ohshite(_("cannot chown backup symlink for '%s'"),
+				ohshite(_("cannot chown backup symbolic link for '%s'"),
 				        ti->name);
 			tarobject_set_se_context(fnamevb.buf, fnametmpvb.buf,
 			                         stab.st_mode);

commit da88e32b58ab920a68e7b38e10a27f1ff6d9382d

u-a: Fix coding style
Fixes: commit d6d81695e2dd53305ab4c64f1642c356a3c2ee30
Changelog: silent

utils/update-alternatives.c
@@ -660,7 +660,8 @@ fsys_set_ref_time(const char *linkname, const char *target)
 
 	root_linkname = fsys_get_path(linkname);
 	if (lutimes(root_linkname, tv) < 0 && errno != ENOSYS)
-		syserr(_("cannot set symbolic link '%s' timestamp"), root_linkname);
+		syserr(_("cannot set symbolic link '%s' timestamp"),
+		       root_linkname);
 	free(root_linkname);
 #endif
 }