guillemj/dpkg

commit b155f466be77854384ae1363c88dd240e7620cb5

libdpkg: Add new str_vfmt() function

lib/dpkg/string.c
@@ -73,6 +73,27 @@ str_match_end(const char *str, const char *end)
  * Print formatted output to an allocated string.
  *
  * @param fmt The format string.
+ * @param args The format arguments.
+ *
+ * @return The new allocated formatted output string (never NULL).
+ */
+char *
+str_vfmt(const char *fmt, va_list args)
+{
+	va_list args_copy;
+	char *str;
+
+	va_copy(args_copy, args);
+	m_vasprintf(&str, fmt, args);
+	va_end(args);
+
+	return str;
+}
+
+/**
+ * Print formatted output to an allocated string.
+ *
+ * @param fmt The format string.
  * @param ... The format arguments.
  *
  * @return The new allocated formatted output string (never NULL).

lib/dpkg/libdpkg.map
@@ -106,6 +106,7 @@ LIBDPKG_PRIVATE {
 	str_match_end;
 	str_fnv_hash;
 	str_concat;
+	str_vfmt;
 	str_fmt;
 	str_escape_fmt;
 	str_strip_quotes;

lib/dpkg/string.h
@@ -22,6 +22,7 @@
 #define LIBDPKG_STRING_H
 
 #include <stddef.h>
+#include <stdarg.h>
 #include <stdbool.h>
 
 #include <dpkg/macros.h>
@@ -63,6 +64,9 @@ str_fnv_hash(const char *str);
 char *
 str_concat(char *dst, ...)
 	DPKG_ATTR_SENTINEL;
+char *
+str_vfmt(const char *fmt, va_list args)
+	DPKG_ATTR_VPRINTF(1);
 char *
 str_fmt(const char *fmt, ...)
 	DPKG_ATTR_PRINTF(1);

commit 82f4fc7c23e9705ed6ebf2b5a2a2dad677270d0b

libdpkg: Fix va_copy() usage
Use the correct args the v* all and the va_end() call.

Fixes: commit b155f466be77854384ae1363c88dd240e7620cb5
Changelog: silent

lib/dpkg/string.c
@@ -84,8 +84,8 @@ str_vfmt(const char *fmt, va_list args)
 	char *str;
 
 	va_copy(args_copy, args);
-	m_vasprintf(&str, fmt, args);
-	va_end(args);
+	m_vasprintf(&str, fmt, args_copy);
+	va_end(args_copy);
 
 	return str;
 }