guillemj/dpkg

commit b09785f509a71bbafcc7e695bcdf439ce5a953cc

dpkg: Fix memory leaks for varbufs during conffile upgrade handling
We need to free these structs before returning from the function.

Warned-by: coverity
Stable-Candidate: 1.22.x

src/main/unpack.c
@@ -707,6 +707,9 @@ pkg_remove_conffile_on_upgrade(struct pkginfo *pkg,
 		warning(_("%s: cannot rename obsolete conffile '%s' to '%s': %s"),
 		        pkg_name(pkg, pnaw_nonambig),
 		        cdr.buf, cdrext.buf, strerror(errno));
+
+	varbuf_destroy(&cdrext);
+	varbuf_destroy(&cdr);
 }
 
 /* TODO: Refactor to reduce nesting levels. */

commit 62de09bf17df9ba26e47ab1bec4d5ca83ef7f9bd

dpkg: Fix memory leaks for varbufs during conffile upgrade (round two)
These early returns were missed in the previous commit.

Fixes: commit b09785f509a71bbafcc7e695bcdf439ce5a953cc
Warned-by: coverity
Stable-Candidate: 1.22.x
Changelog: silent

src/main/unpack.c
@@ -667,6 +667,8 @@ pkg_remove_conffile_on_upgrade(struct pkginfo *pkg,
 		         "namenode '%s' owned by other %s, remove-on-upgrade ignored",
 		         namenode->name, pkg_name(otherpkg, pnaw_always));
 		fsys_node_pkgs_iter_free(iter);
+		varbuf_destroy(&cdrext);
+		varbuf_destroy(&cdr);
 		return;
 	}
 	fsys_node_pkgs_iter_free(iter);
@@ -683,8 +685,11 @@ pkg_remove_conffile_on_upgrade(struct pkginfo *pkg,
 	md5hash(pkg, currenthash, cdr.buf);
 
 	/* Has it been already removed (e.g. by local admin)? */
-	if (strcmp(currenthash, NONEXISTENTFLAG) == 0)
+	if (strcmp(currenthash, NONEXISTENTFLAG) == 0) {
+		varbuf_destroy(&cdrext);
+		varbuf_destroy(&cdr);
 		return;
+	}
 
 	/* For unmodified conffiles, we just remove them. */
 	if (strcmp(currenthash, namenode->oldhash) == 0) {
@@ -693,6 +698,8 @@ pkg_remove_conffile_on_upgrade(struct pkginfo *pkg,
 			warning(_("%s: cannot remove '%s': %s"),
 			        pkg_name(pkg, pnaw_nonambig), cdr.buf,
 			        strerror(errno));
+		varbuf_destroy(&cdrext);
+		varbuf_destroy(&cdr);
 		return;
 	}
 