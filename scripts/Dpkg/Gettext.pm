guillemj/dpkg

commit fd9a1ad6a1b94e2e739f74ef0e556d0eda99dcc7

Dpkg::Gettext: Add support for context functions

scripts/Dpkg/Gettext.pm
@@ -78,6 +78,11 @@ our $DEFAULT_TEXT_DOMAIN = 'dpkg-dev';
 Calls dgettext() on the $msgid and returns its translation for the current
 locale. If dgettext() is not available, simply returns $msgid.
 
+=item my $trans = C_($msgctxt, $msgid)
+
+Calls dgettext() on the $msgid and returns its translation for the specific
+$msgctxt supplied. If dgettext() is not available, simply returns $msgid.
+
 =item my $trans = P_($msgid, $msgid_plural, $n)
 
 Calls dngettext(), returning the correct translation for the plural form
@@ -87,6 +92,8 @@ or $msgid_plural otherwise.
 =back
 
 =cut
+
+use constant GETTEXT_CONTEXT_GLUE => "\004";
 
 BEGIN {
     eval 'use Locale::gettext';
@@ -105,6 +112,10 @@ BEGIN {
                     return $msgid_plural;
                 }
             }
+            sub C_ {
+                my ($msgctxt, $msgid) = @_;
+                return $msgid;
+            }
             sub P_ {
                 return ngettext(@_);
             }
@@ -113,6 +124,11 @@ BEGIN {
         eval q{
             sub g_ {
                 return dgettext($DEFAULT_TEXT_DOMAIN, shift);
+            }
+            sub C_ {
+                my ($msgctxt, $msgid) = @_;
+                return dgettext($DEFAULT_TEXT_DOMAIN,
+                                $msgctxt . GETTEXT_CONTEXT_GLUE . $msgid);
             }
             sub P_ {
                 return dngettext($DEFAULT_TEXT_DOMAIN, @_);
@@ -138,6 +154,8 @@ sub _g ## no critic (Subroutines::ProhibitUnusedPrivateSubroutines)
 
 Now the short aliases (g_ and P_) will call domain aware functions with
 $DEFAULT_TEXT_DOMAIN.
+
+New function: C_().
 
 =head2 Version 1.01
 

commit 59f6c2ff3f6770d0b99dbc5c3e5029fb5da28deb

Dpkg::Gettext: Fix C_() context string handling on missing translations
We need to compare the returned string against the message id, and if
they are the same, then we need to return the message id, instead of the
constructed message context plus the message id, as that is not intended
for user consumption.

Fixes: commit fd9a1ad6a1b94e2e739f74ef0e556d0eda99dcc7

scripts/Dpkg/Gettext.pm
@@ -165,8 +165,10 @@ BEGIN {
         };
         *C_ = sub {
             my ($msgctxt, $msgid) = @_;
-            return dgettext($DEFAULT_TEXT_DOMAIN,
-                            $msgctxt . GETTEXT_CONTEXT_GLUE . $msgid);
+            my $msgctxtid = $msgctxt . GETTEXT_CONTEXT_GLUE . $msgid;
+            my $msg = dgettext($DEFAULT_TEXT_DOMAIN, $msgctxtid);
+
+            return $msg eq $msgctxtid ? $msgid : $msg;
         };
         *P_ = sub {
             return dngettext($DEFAULT_TEXT_DOMAIN, @_);