guillemj/dpkg

commit ae03bfd28afea2fdc10be9657d78a1fe29dfa58a

libdpkg: Remove varbuf_end_str() function
Calling this function is not needed anymore, as varbuf handling is now
C string safe, because every varbuf function terminates strings with a
NUL character.

lib/dpkg/varbuf.c
@@ -103,8 +103,6 @@ varbuf_str(struct varbuf *v)
 	if (v->buf == NULL)
 		return "";
 
-	varbuf_end_str(v);
-
 	return v->buf;
 }
 
@@ -113,7 +111,6 @@ varbuf_set_buf(struct varbuf *v, const void *buf, size_t size)
 {
 	varbuf_reset(v);
 	varbuf_add_buf(v, buf, size);
-	varbuf_end_str(v);
 }
 
 void
@@ -176,13 +173,6 @@ varbuf_add_buf(struct varbuf *v, const void *s, size_t size)
 	varbuf_grow(v, size + 1);
 	memcpy(v->buf + v->used, s, size);
 	v->used += size;
-	v->buf[v->used] = '\0';
-}
-
-void
-varbuf_end_str(struct varbuf *v)
-{
-	varbuf_grow(v, 1);
 	v->buf[v->used] = '\0';
 }
 

lib/dpkg/t/t-varbuf.c
@@ -238,19 +238,15 @@ test_varbuf_add_str(void)
 	varbuf_init(&vb, 5);
 
 	varbuf_add_str(&vb, "1234567890");
-	varbuf_end_str(&vb);
 	test_str(vb.buf, ==, "1234567890");
 
 	varbuf_add_str(&vb, "abcd");
-	varbuf_end_str(&vb);
 	test_str(vb.buf, ==, "1234567890abcd");
 
 	varbuf_add_strn(&vb, "1234567890", 5);
-	varbuf_end_str(&vb);
 	test_str(vb.buf, ==, "1234567890abcd12345");
 
 	varbuf_add_strn(&vb, "abcd", 0);
-	varbuf_end_str(&vb);
 	test_str(vb.buf, ==, "1234567890abcd12345");
 
 	varbuf_destroy(&vb);

lib/dpkg/command.c
@@ -247,7 +247,6 @@ command_in_path(const char *cmd)
 		if (path_len)
 			varbuf_add_char(&filename, '/');
 		varbuf_add_str(&filename, cmd);
-		varbuf_end_str(&filename);
 
 		if (file_is_exec(filename.buf)) {
 			varbuf_destroy(&filename);

lib/dpkg/db-ctrl-access.c
@@ -73,7 +73,6 @@ pkg_infodb_foreach(struct pkginfo *pkg, struct pkgbin *pkgbin,
 		pkgname = pkgbin_name(pkg, pkgbin, pnaw_never);
 
 	varbuf_add_dir(&db_path, pkg_infodb_get_dir());
-	varbuf_end_str(&db_path);
 	varbuf_snapshot(&db_path, &db_path_state);
 
 	db_dir = opendir(db_path.buf);
@@ -108,7 +107,6 @@ pkg_infodb_foreach(struct pkginfo *pkg, struct pkgbin *pkgbin,
 
 		varbuf_rollback(&db_path_state);
 		varbuf_add_str(&db_path, db_de->d_name);
-		varbuf_end_str(&db_path);
 		filename = db_path.buf;
 
 		func(filename, filetype);

lib/dpkg/db-ctrl-format.c
@@ -142,7 +142,6 @@ pkg_infodb_get_file(const struct pkginfo *pkg, const struct pkgbin *pkgbin,
 		varbuf_add_archqual(&vb, pkgbin->arch);
 	varbuf_add_char(&vb, '.');
 	varbuf_add_str(&vb, filetype);
-	varbuf_end_str(&vb);
 
 	return vb.buf;
 }

lib/dpkg/db-ctrl-upgrade.c
@@ -81,7 +81,6 @@ pkg_infodb_link_multiarch_files(void)
 	struct varbuf_state newname_state;
 
 	varbuf_add_dir(&oldname, pkg_infodb_get_dir());
-	varbuf_end_str(&oldname);
 	varbuf_snapshot(&oldname, &oldname_state);
 
 	varbuf_set_varbuf(&newname, &oldname);
@@ -131,13 +130,11 @@ pkg_infodb_link_multiarch_files(void)
 
 		varbuf_rollback(&oldname_state);
 		varbuf_add_str(&oldname, db_de->d_name);
-		varbuf_end_str(&oldname);
 
 		varbuf_rollback(&newname_state);
 		varbuf_add_pkgbin_name(&newname, pkg, &pkg->installed, pnaw_always);
 		varbuf_add_char(&newname, '.');
 		varbuf_add_str(&newname, filetype);
-		varbuf_end_str(&newname);
 
 		if (link(oldname.buf, newname.buf) && errno != EEXIST)
 			ohshite(_("error creating hard link '%.255s'"),

lib/dpkg/dbmodify.c
@@ -102,7 +102,6 @@ static void cleanupdates(void) {
     for (i=0; i<cdn; i++) {
       varbuf_rollback(&updatefn_state);
       varbuf_add_str(&updatefn, cdlist[i]->d_name);
-      varbuf_end_str(&updatefn);
       parsedb(updatefn.buf, pdb_parse_update, NULL);
     }
 
@@ -112,7 +111,6 @@ static void cleanupdates(void) {
       for (i=0; i<cdn; i++) {
         varbuf_rollback(&updatefn_state);
         varbuf_add_str(&updatefn, cdlist[i]->d_name);
-        varbuf_end_str(&updatefn);
         if (unlink(updatefn.buf))
           ohshite(_("failed to remove incorporated update file %.255s"),
                   updatefn.buf);
@@ -193,7 +191,6 @@ modstatdb_init(void)
 
   varbuf_init(&updatefn, strlen(updatesdir) + 1 + IMPORTANTMAXLEN);
   varbuf_add_dir(&updatefn, updatesdir);
-  varbuf_end_str(&updatefn);
   varbuf_snapshot(&updatefn, &updatefn_state);
 
   db_initialized = true;

lib/dpkg/dump.c
@@ -484,7 +484,6 @@ write_stanza(FILE *file, const char *filename,
   struct varbuf vb = VARBUF_INIT;
 
   varbuf_stanza(&vb, pkg, pkgbin);
-  varbuf_end_str(&vb);
 
   if (fputs(varbuf_str(&vb), file) < 0)
     ohshite(_("failed to write details of '%.50s' to '%.250s'"),
@@ -524,7 +523,6 @@ writedb_stanzas(FILE *fp, const char *filename, enum writedb_flags flags)
 
     varbuf_stanza(&vb, pkg, pkgbin);
     varbuf_add_char(&vb, '\n');
-    varbuf_end_str(&vb);
     if (fputs(varbuf_str(&vb), fp) < 0)
       ohshite(_("failed to write %s database stanza about '%s' to '%s'"),
               which, pkgbin_name(pkg, pkgbin, pnaw_nonambig), filename);

commit 359b960162bcc6476726dcd82b9409e68f347c88

libdpkg: Do not skip varbuf allocations on additions with zero sizes
If we are adding a zero sized buffer or string, we should make sure we
allocate at least an empty NUL-terminated string, as that's what the
users of the API expect. There's not much benefit from short-circuiting
the allocation as this should be in general the unlikely case, where we
really want a proper string to avoid segfaults and crashes.

This was affecting at least the deb822 parser with an empty first field
on the first stanza (such as «Version: »), and missing versions on
dependencies (such as «pkgname (= )».

Reported-by: Elias Hasas (zer0d4y5) <eliash977@gmail.com> (empty first field)
Fixes: commit ae03bfd28afea2fdc10be9657d78a1fe29dfa58a
Closes: #1147121
Stable-Candidate: 1.22.x

lib/dpkg/varbuf.c
@@ -178,12 +178,11 @@ varbuf_add_char(struct varbuf *v, int c)
 void
 varbuf_dup_char(struct varbuf *v, int c, size_t n)
 {
-	if (n == 0)
-		return;
-
 	varbuf_grow(v, n + 1);
-	memset(v->buf + v->used, c, n);
-	v->used += n;
+	if (n > 0) {
+		memset(v->buf + v->used, c, n);
+		v->used += n;
+	}
 	v->buf[v->used] = '\0';
 }
 
@@ -208,11 +207,11 @@ varbuf_add_dir(struct varbuf *v, const char *dirname)
 void
 varbuf_add_buf(struct varbuf *v, const void *s, size_t size)
 {
-	if (size == 0)
-		return;
 	varbuf_grow(v, size + 1);
-	memcpy(v->buf + v->used, s, size);
-	v->used += size;
+	if (size > 0) {
+		memcpy(v->buf + v->used, s, size);
+		v->used += size;
+	}
 	v->buf[v->used] = '\0';
 }
 

lib/dpkg/t/t-varbuf-cpp.cc
@@ -509,8 +509,8 @@ test_varbuf_detach(void)
 	vb.init(0);
 	vb.add_buf(nullptr, 0);
 	test_pass(vb.used == 0);
-	test_pass(vb.size == 0);
-	test_pass(vb.buf == nullptr);
+	test_pass(vb.size > 0);
+	test_pass(vb.buf != nullptr);
 	str = vb.detach();
 	test_str(str, ==, "");
 	test_pass(vb.used == 0);

lib/dpkg/t/t-varbuf.c
@@ -581,8 +581,8 @@ test_varbuf_detach(void)
 	varbuf_init(&vb, 0);
 	varbuf_add_buf(&vb, NULL, 0);
 	test_pass(vb.used == 0);
-	test_pass(vb.size == 0);
-	test_pass(vb.buf == NULL);
+	test_pass(vb.size > 0);
+	test_pass(vb.buf != NULL);
 	str = varbuf_detach(&vb);
 	test_str(str, ==, "");
 	test_pass(vb.used == 0);