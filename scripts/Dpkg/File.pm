guillemj/dpkg

commit 13378506876e3fe43f7539ccf0704273e24a9bb4

Dpkg::File: Fix file_touch() to update existing file timestamps
As the name implies, the function is supposed to act as touch(1), which
means that it should create the file if it does not exist, otherwise
update its timestamps.

Once we do that, we can switch Dpkg::Source::Functions::fs_time() to
rely on the timestamp update from file_touch(), instead of open coding
it in the function.

scripts/Dpkg/File.pm
@@ -87,6 +87,8 @@ sub file_touch {
     open my $fh, '>>', $file
         or syserr(g_('cannot touch file %s'), $file);
     close $fh or syserr(g_('cannot write %s'), $file);
+    utime undef, undef, $file
+        or syserr(g_('cannot change timestamp for %s'), $file);
 }
 
 =head1 CHANGES

scripts/Dpkg/Source/Functions.pm
@@ -112,12 +112,9 @@ sub fs_time {
     my $file = shift;
     my $is_temp = 0;
     if (not -e $file) {
-        file_touch($file);
         $is_temp = 1;
-    } else {
-        utime(undef, undef, $file) or
-            syserr(g_('cannot change timestamp for %s'), $file);
     }
+    file_touch($file);
     my $st = File::stat::stat($file)
         or syserr(g_('cannot read timestamp from %s'), $file);
     unlink($file) if $is_temp;

commit 85d03d977783692f24227026baa5c83ac2a4e13b

Dpkg::File: Do not fail when touching a non-file in file_touch()
When we moved the timestamp updating from fs_time() to file_touch(),
the change didn't take into account that in fs_time() the file_touch()
function was only called if the file did not exist, preventing errors
on non-files.

Apply for now the same check withing this function, even though it is
duplicative in some code paths, to fix the regression.

Fixes: commit 13378506876e3fe43f7539ccf0704273e24a9bb4

scripts/Dpkg/File.pm
@@ -84,9 +84,11 @@ sub file_dump {
 sub file_touch {
     my $file = shift;
 
-    open my $fh, '>>', $file
-        or syserr(g_('cannot touch file %s'), $file);
-    close $fh or syserr(g_('cannot write %s'), $file);
+    if (! -e $file) {
+        open my $fh, '>', $file
+            or syserr(g_('cannot create file %s'), $file);
+        close $fh or syserr(g_('cannot close file %s'), $file);
+    }
     utime undef, undef, $file
         or syserr(g_('cannot change timestamp for %s'), $file);
 }