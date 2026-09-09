guillemj/dpkg

commit 0e0f113547cffe21a8c3e9353956de2bc8f9d705

scripts: Parse directory options with parse_option_dir()
We should check whether we got a value at all, and if so whether it is
an existing directory. For now we warn on missing directories, but we'll
probably be making this an error in the future.

Ref: #1143871

scripts/dpkg-shlibdeps.pl
@@ -98,11 +98,11 @@ foreach (@ARGV) {
     } elsif (m/^-L(.*)$/) {
         $shlibslocal = $1;
     } elsif (m/^-l(.*)$/) {
-        push @priv_lib_dirs, $1;
+        push @priv_lib_dirs, parse_option_dir('-l', $1);
     } elsif (m/^-S(.*)$/) {
-        push @pkg_dir_to_search, $1;
+        push @pkg_dir_to_search, parse_option_dir('-S', $1);
     } elsif (m/^-I(.*)$/) {
-        push @pkg_dir_to_ignore, $1;
+        push @pkg_dir_to_ignore, parse_option_dir('-I', $1);
     } elsif (m/^-O$/) {
         $stdout = 1;
     } elsif (m/^-O(.+)$/) {

scripts/dpkg-genchanges.pl
@@ -265,7 +265,7 @@ while (@ARGV) {
     } elsif (m/^-D([^\=:]+)[=:](.*)$/s) {
         $override{$1} = $2;
     } elsif (m/^-u(.*)$/) {
-        $uploadfilesdir = $1;
+        $uploadfilesdir = parse_option_dir('-u', $1);
     } elsif (m/^-U([^\=:]+)$/) {
         $remove{$1} = 1;
     } elsif (m/^-V(\w[-:0-9A-Za-z]*)[=:](.*)$/s) {

scripts/dpkg-gencontrol.pl
@@ -144,7 +144,7 @@ while (@ARGV) {
     } elsif (m/^-l/p) {
         $changelogfile = ${^POSTMATCH};
     } elsif (m/^-P/p) {
-        $packagebuilddir = ${^POSTMATCH};
+        $packagebuilddir = parse_option_dir('-P', ${^POSTMATCH});
     } elsif (m/^-f/p) {
         $fileslistfile = ${^POSTMATCH};
     } elsif (m/^-v(.+)$/) {

scripts/dpkg-gensymbols.pl
@@ -158,8 +158,7 @@ while (@ARGV) {
                 unless scalar(@to_add);
         }
     } elsif (m/^-P(.+)$/) {
-        $packagebuilddir = $1;
-        $packagebuilddir =~ s{/+$}{};
+        $packagebuilddir = parse_option_dir('-P', $1);
     } elsif (m/^-O$/) {
         $stdout = 1;
     } elsif (m/^-I(.+)$/) {

commit dcf93e8535d1e63ae9964e7f3bd9ca3665d6d79a

dpkg-shlibdeps: Do not validate existence of -l option argument
We are using this directory to search for libraries within the source
tree package build directories, so we cannot check whether it exists
(as it will not unless we prefix to it the build directories).

We should probably check whether the option gets an argument at all, but
we can postpone this until we revamp the entire option parsing to handle
unbundled arguments and required values.

Fixes: commit 0e0f113547cffe21a8c3e9353956de2bc8f9d705

scripts/dpkg-shlibdeps.pl
@@ -98,7 +98,7 @@ foreach (@ARGV) {
     } elsif (m/^-L(.*)$/) {
         $shlibslocal = $1;
     } elsif (m/^-l(.*)$/) {
-        push @priv_lib_dirs, parse_option_dir('-l', $1);
+        push @priv_lib_dirs, $1;
     } elsif (m/^-S(.*)$/) {
         push @pkg_dir_to_search, parse_option_dir('-S', $1);
     } elsif (m/^-I(.*)$/) {